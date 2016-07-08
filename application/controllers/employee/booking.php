14625a49668a8e92aec7850b6c41a7639a71fc9c
Anuj Aggarwal <anuj.aggarwal@gmail.com>
2016-07-08 14:45:55 +0530

    minor

--- application/controllers/employee/booking.php
+++ application/controllers/employee/booking.php
@@ -1180,7 +1180,7 @@ class Booking extends CI_Controller {
             //Setting mail to vendor flag to 0, once booking is rescheduled
             $this->booking_model->set_mail_to_vendor_flag_to_zero($booking_id);
 
-            log_message('info', 'Rescheduled- Booking id: ' . $booking_id . " Rescheduled By " . $this->session->userdata('employee_id') . " data " . print_r($data));
+            log_message('info', 'Rescheduled- Booking id: ' . $booking_id . " Rescheduled By " . $this->session->userdata('employee_id') . " data " . print_r($data, true));
 
             redirect(base_url() . 'employee/booking/view', 'refresh');
         }
