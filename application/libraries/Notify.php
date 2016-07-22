a1cb820c8ea26cf7ea1399d430e612a55fbc2e71
Anuj Aggarwal <anuj.aggarwal@gmail.com>
2016-07-23 00:20:31 +0530

    minor

--- application/libraries/Notify.php
+++ application/libraries/Notify.php
@@ -81,7 +81,7 @@ class Notify {
 
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -180,7 +180,7 @@ class Notify {
 
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        //curl_setopt($ch, CURLOPT_VERBOSE, 1);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
