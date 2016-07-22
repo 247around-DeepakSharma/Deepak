0238ec51e09d7b88406230c177615a4e9b604136
Anuj Aggarwal <anuj.aggarwal@gmail.com>
2016-07-23 00:36:03 +0530

    minor

--- application/libraries/Notify.php
+++ application/libraries/Notify.php
@@ -194,10 +194,7 @@ class Notify {
 
         curl_close($ch);
 
-        log_message('info', __FUNCTION__ . "=> " . print_r(array($http_result, $error, $http_code), TRUE));
-
-        //print_r(array($http_result, $error, $http_code));
-        //echo "Response = " . print_r($http_result);
+        //log_message('info', __FUNCTION__ . "=> " . print_r(array($http_result, $error, $http_code), TRUE));
     }
 
 }
