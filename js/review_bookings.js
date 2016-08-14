d78c8a1a906671abaf72033a32c28044b598d0dd
Anuj Aggarwal <anuj.aggarwal@gmail.com>
2016-08-14 16:26:45 +0530

    local changes gone to server

--- js/review_bookings.js
+++ js/review_bookings.js
@@ -1,10 +1,10 @@
 var getUrl = window.location;
 
 //Un-comment below line for localhost
-var baseUrl = getUrl .protocol + "//" + getUrl.host  + "/" + getUrl.pathname.split('/')[1];
+//var baseUrl = getUrl .protocol + "//" + getUrl.host  + "/" + getUrl.pathname.split('/')[1];
 
 //Comment below line for localhost, this is for main server
-//var baseUrl = getUrl.protocol + "//" + getUrl.host;
+var baseUrl = getUrl.protocol + "//" + getUrl.host;
 
 var review_completeUrl = baseUrl + '/employee/booking/complete_review_booking/';
 var admin_remarksUrl = baseUrl + '/employee/booking/admin_remarks/';
