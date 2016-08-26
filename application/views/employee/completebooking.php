375ce0ce1ab0174c5a29d1a09765606bddea4cfd
anuj <abhaya@247around.com>
2016-08-26 10:28:38 +0530

    minor

--- application/views/employee/completebooking.php
+++ application/views/employee/completebooking.php
@@ -154,7 +154,7 @@
                                  <?php
                                  foreach ($prices[$keys] as $index => $value) { ?>
                                    <tr style="background-color:   #FF4500; color: white;">
-                                     <td> <input type="text" class="form-control" id="serial_number_1" name="<?php echo "serial_number[". $price['unit_id'] . "new".$value['id']."]"?>"  value="" placeholder= "Enter Serial Number" /></td>
+                                     <td> <input type="text" class="form-control"  id="<?php echo "serial_number".$count; ?>" name="<?php echo "serial_number[". $price['unit_id'] . "new".$value['id']."]"?>"  value="" placeholder= "Enter Serial Number" /></td>
                                      <td> <?php echo $value['service_category']; ?> </td>
                                      <td> <?php echo $value['customer_net_payable']; ?> </td>
                                      <td>  <input  type="text" class="form-control cost"   name="<?php echo "customer_basic_charge[". $price['unit_id'] . "new".$value['id']."]"?>"  value = "0.00">
