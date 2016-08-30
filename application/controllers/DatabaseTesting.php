85d996ce3ccf498321f519d1d63498067d8d118e
anuj <abhaya@247around.com>
2016-08-30 17:19:29 +0530

    data base testing script

---
+++ application/controllers/DatabaseTesting.php
@@ -0,0 +1,66 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * This Controller checks database
+ *
+ * @author Abhay Anand
+ */
+class DatabaseTesting extends CI_Controller {
+
+    function __Construct() {
+	parent::__Construct();
+
+	$this->load->model('database_testing_model');
+
+	$this->load->library('notify');
+	$this->load->library('email');
+    }
+
+    function index() {
+	  $data = $this->database_testing_model->check_unit_details();
+	if ($data) {
+	  log_message('info', " Unit details have some inconsistent data: " . print_r($data, true));
+	  }
+
+	  $is_pricetags = $this->database_testing_model->check_price_tags();
+	  if ($is_pricetags) {
+	  log_message('info', " Unit details have inconsistent data( price tags): " . print_r($is_pricetags, true));
+	  }
+
+	  $is_tax_rate = $this->database_testing_model->check_tax_rate();
+	  if ($is_tax_rate) {
+	  log_message('info', " Unit details have some inconsistent data(tax rate): " . print_r($is_tax_rate, true));
+	  }
+	  $is_unit_status = $this->database_testing_model->check_booking_unit_details_status();
+	  if ($is_unit_status) {
+
+	  log_message('info', " Unit details have some inconsistent data(unit details): " . print_r($is_unit_status, true));
+	  }
+
+	  $is_booking_details = $this->database_testing_model->check_booking_details();
+	  if ($is_booking_details) {
+
+	  log_message('info', " Unit details have some inconsistent data(booking details): " . print_r($is_booking_details, true));
+	  }
+
+	$is_booking_id = $this->database_testing_model->check_booking_exist_in_unit_details();
+	if ($is_booking_id) {
+	    log_message('info', " Unit details have some inconsistent data(booking id): " . print_r($is_booking_id, true));
+	}
+	$is_service_center = $this->database_testing_model->check_booking_exist_in_service_center();
+	if ($is_service_center) {
+	    log_message('info', " Unit details have some inconsistent data( service_center id): " . print_r($is_service_center, true));
+	}
+    }
+
+}
---
+++ application/models/database_testing_model.php
@@ -0,0 +1,160 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Database_testing_model extends CI_Model {
+
+    function __construct() {
+	parent::__Construct();
+
+	$this->db = $this->load->database('default', TRUE, TRUE);
+    }
+
+    /**
+     * check booking id, partner_id, service id, appliance id is  zero
+     * @return boolean
+     */
+    function check_unit_details() {
+	$this->db->select('booking_id, partner_id, service_id, appliance_id');
+	$this->db->or_where_in('booking_id', '0');
+	$this->db->or_where_in('partner_id', '0');
+	$this->db->or_where_in('appliance_id', '0');
+	$this->db->where('create_date >=', '2016-07-01 00:00:00');
+	$query = $this->db->get('booking_unit_details');
+	if ($query->num_rows > 0) {
+	    return $query->result_array();
+	} else {
+	    return false;
+	}
+    }
+
+    /**
+     * check price tag is empty or null when booking status is Pending, Completed, Cancelled, Rescgeduled
+     * @return boolean
+     */
+    function check_price_tags() {
+	$this->db->select('booking_unit_details.booking_id, price_tags');
+	$this->db->from('booking_unit_details');
+	$this->db->or_where_in('price_tags', '');
+	$this->db->or_where_in('price_tags', NULL);
+	$this->db->join('booking_details', 'booking_details.booking_id = booking_unit_details.booking_id');
+	$this->db->where_in('booking_details.current_status', array('Pending', 'Completed', 'Cancelled', 'Rescheduled'));
+	$this->db->where('booking_details.create_date >=', '2016-07-01 00:00:00');
+	$query = $this->db->get();
+	if ($query->num_rows > 0) {
+	    return $query->result_array();
+	} else {
+	    return false;
+	}
+    }
+
+    /**
+     * Check price is not empty or null for Pending, Completed, Cancelled or Rescheduled booking
+     * @return boolean
+     */
+    function check_tax_rate() {
+	$this->db->select('booking_unit_details.booking_id, tax_rate');
+	$this->db->from('booking_unit_details');
+	$this->db->where('tax_rate <=', '0');
+	$this->db->join('booking_details', 'booking_details.booking_id = booking_unit_details.booking_id');
+	$this->db->where_in('booking_details.current_status', array('Pending', 'Completed', 'Cancelled', 'Rescheduled'));
+	$this->db->where('booking_details.create_date >=', '2016-07-01 00:00:00');
+	$query = $this->db->get();
+	if ($query->num_rows > 0) {
+	    return $query->result_array();
+	} else {
+	    return false;
+	}
+    }
+
+    /**
+     *  check booking status is empty or null when booking current status is ccompleted or cancelled.
+     * @return boolean
+     */
+    function check_booking_unit_details_status() {
+	$this->db->select('booking_details.booking_id,booking_unit_details.booking_status');
+	$this->db->from('booking_unit_details');
+	$this->db->or_where('booking_status', '');
+	$this->db->or_where('booking_status', NULL);
+	$this->db->join('booking_details', 'booking_details.booking_id = booking_unit_details.booking_id');
+	$this->db->where_in('booking_details.current_status', array('Completed', 'Cancelled'));
+	$this->db->where('booking_details.create_date >=', '2016-07-01 00:00:00');
+	$query = $this->db->get();
+	if ($query->num_rows > 0) {
+	    return $query->result_array();
+	} else {
+	    return false;
+	}
+    }
+
+    /**
+     * check booking id, partner_id, service id, appliance id is  zero
+     * @return boolean
+     */
+    function check_booking_details() {
+	$this->db->select('booking_id, partner_id, service_id, user_id,source, type, booking_pincode, city, booking_pincode');
+	$this->db->or_where_in('booking_id', '0');
+	$this->db->or_where_in('partner_id', '0');
+	$this->db->or_where_in('service_id', '0');
+	$this->db->or_where_in('user_id', '0');
+	$this->db->or_where_in('source', '');
+	$this->db->or_where_in('source', NULL);
+	$this->db->or_where_in('type', NULL);
+	$this->db->or_where_in('booking_pincode', NULL);
+	$this->db->or_where_in('city', NULL);
+	$this->db->or_where_in('booking_primary_contact_no', '');
+	$this->db->or_where_in('booking_primary_contact_no', '0');
+	$this->db->where('booking_details.create_date >=', '2016-07-01 00:00:00');
+	$query = $this->db->get('booking_details');
+	if ($query->num_rows > 0) {
+	    return $query->result_array();
+	} else {
+	    return false;
+	}
+    }
+    /**
+     * Get booking id who is exist in booking details and not exist in the booking unit details
+     * @return boolean
+     */
+    function check_booking_exist_in_unit_details() {
+	$sql = "SELECT  `booking_id`  FROM  `booking_unit_details` WHERE  `booking_id` NOT IN "
+	    . " ( SELECT booking_id FROM booking_details WHERE create_date >=  '2016-07-01 00:00:00')";
+	$query = $this->db->query($sql);
+	if ($query->num_rows > 0) {
+	    return $query->result_array();
+	} else {
+	    return false;
+	}
+    }
+
+    function check_booking_exist_in_service_center() {
+	$this->db->select('booking_details.booking_id, count(booking_unit_details.booking_id) as count');
+	$this->db->from('booking_details');
+	$this->db->where('booking_details.`assigned_vendor_id` IS NOT NULL ', NULL, FALSE);
+	$this->db->where('booking_details.create_date >=', '2016-07-01 00:00:00');
+	$this->db->join('booking_unit_details', 'booking_unit_details.booking_id =  booking_details.booking_id');
+	$query = $this->db->get();
+	$data = $query->result_array();
+	$data_result = array();
+	print_r($data);
+	foreach ($data as $value) {
+	    $this->db->select('count(booking_id) as service_count');
+	    $this->db->where('booking_id', $value['booking_id']);
+	    $query = $this->db->get('service_center_booking_action');
+	    $result = $query->result_array();
+	    if (count($result[0]['service_count']) != $value['count']) {
+		array_push($data_result, $data);
+	    }
+	}
+	if (!empty($data_result)) {
+	    return $data_result;
+	} else {
+	    return false;
+	}
+    }
+
+}
