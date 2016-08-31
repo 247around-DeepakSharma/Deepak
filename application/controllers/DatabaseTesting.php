5d98549afef49d8df2a98e518f3ce37b1dcf9fa0
anuj <abhaya@247around.com>
2016-08-31 16:24:31 +0530

    database testing script

--- application/controllers/DatabaseTesting.php
+++ application/controllers/DatabaseTesting.php
@@ -27,31 +27,32 @@ class DatabaseTesting extends CI_Controller {
     }
 
     function index() {
-	  $data = $this->database_testing_model->check_unit_details();
+	$data = $this->database_testing_model->check_unit_details();
 	if ($data) {
-	  log_message('info', " Unit details have some inconsistent data: " . print_r($data, true));
-	  }
+	    log_message('info', " Unit details have some inconsistent data: " . print_r($data, true));
+	}
 
-	  $is_pricetags = $this->database_testing_model->check_price_tags();
-	  if ($is_pricetags) {
-	  log_message('info', " Unit details have inconsistent data( price tags): " . print_r($is_pricetags, true));
-	  }
+	$is_pricetags = $this->database_testing_model->check_price_tags();
+	if ($is_pricetags) {
+	    print_r($is_pricetags);
+	    log_message('info', " Unit details have inconsistent data( price tags): " . print_r($is_pricetags, true));
+	}
 
-	  $is_tax_rate = $this->database_testing_model->check_tax_rate();
-	  if ($is_tax_rate) {
-	  log_message('info', " Unit details have some inconsistent data(tax rate): " . print_r($is_tax_rate, true));
-	  }
-	  $is_unit_status = $this->database_testing_model->check_booking_unit_details_status();
-	  if ($is_unit_status) {
+	$is_tax_rate = $this->database_testing_model->check_tax_rate();
+	if ($is_tax_rate) {
+	    log_message('info', " Unit details have some inconsistent data(tax rate): " . print_r($is_tax_rate, true));
+	}
+	$is_unit_status = $this->database_testing_model->check_booking_unit_details_status();
+	if ($is_unit_status) {
 
-	  log_message('info', " Unit details have some inconsistent data(unit details): " . print_r($is_unit_status, true));
-	  }
+	    log_message('info', " Unit details have some inconsistent data(unit details): " . print_r($is_unit_status, true));
+	}
 
-	  $is_booking_details = $this->database_testing_model->check_booking_details();
-	  if ($is_booking_details) {
+	$is_booking_details = $this->database_testing_model->check_booking_details();
+	if ($is_booking_details) {
 
-	  log_message('info', " Unit details have some inconsistent data(booking details): " . print_r($is_booking_details, true));
-	  }
+	    log_message('info', " Unit details have some inconsistent data(booking details): " . print_r($is_booking_details, true));
+	}
 
 	$is_booking_id = $this->database_testing_model->check_booking_exist_in_unit_details();
 	if ($is_booking_id) {
@@ -59,8 +60,19 @@ class DatabaseTesting extends CI_Controller {
 	}
 	$is_service_center = $this->database_testing_model->check_booking_exist_in_service_center();
 	if ($is_service_center) {
-	   
+
 	    log_message('info', " Unit details have some inconsistent data( service_center id): " . print_r($is_service_center, true));
+	}
+
+	$is_action_table = $this->database_testing_model->check_service_center_action();
+	if ($is_action_table) {
+
+	    log_message('info', " Unit details have some inconsistent data( service_center action table): " . print_r($is_action_table, true));
+	}
+
+	$is_pending = $this->database_testing_model->check_pending_booking_in_action_table();
+	if ($is_pending) {
+	    log_message('info', " Unit details have some inconsistent data( Pending Booking not show in service center panel): " . print_r($is_pending, true));
 	}
     }
 
