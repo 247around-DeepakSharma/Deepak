329518a30fb942f6186e990f0a7ed760b5e8bd51
Anuj Aggarwal <anuj.aggarwal@gmail.com>
2016-09-08 12:42:14 +0530

    migration changes

--- application/models/database_testing_model.php
+++ application/models/database_testing_model.php
@@ -19,12 +19,19 @@ class Database_testing_model extends CI_Model {
      * @return boolean
      */
     function check_unit_details() {
-	$this->db->select('booking_id, partner_id, service_id, appliance_id');
-	$this->db->or_where('booking_id', '0');
-	$this->db->or_where('partner_id', '0');
-	$this->db->or_where('appliance_id', '0');
-	$this->db->where('create_date >=', '2016-07-01 00:00:00');
-	$query = $this->db->get('booking_unit_details');
+	log_message('info', __METHOD__);
+
+	$sql = "SELECT `booking_id`, `partner_id`, `service_id`, `appliance_id` FROM (`booking_unit_details`) WHERE (`booking_id` = '0' OR `partner_id` = '0' OR `appliance_id` = '0') AND `create_date` >= '2016-07-01 00:00:00'";
+//	$this->db->select('booking_id, partner_id, service_id, appliance_id');
+//	$this->db->or_where('booking_id', '0');
+//	$this->db->or_where('partner_id', '0');
+//	$this->db->or_where('appliance_id', '0');
+//	$this->db->where('create_date >=', '2016-07-01 00:00:00');
+//	$query = $this->db->get('booking_unit_details');
+
+	$query = $this->db->query($sql);
+	log_message('info', $this->db->last_query());
+
 	if ($query->num_rows > 0) {
 	    return $query->result_array();
 	} else {
@@ -37,15 +44,22 @@ class Database_testing_model extends CI_Model {
      * @return boolean
      */
     function check_price_tags() {
-	$this->db->select('booking_unit_details.booking_id, price_tags');
+	$this->db->select('booking_unit_details.booking_id, price_tags, booking_unit_details.create_date');
 	$this->db->from('booking_unit_details');
-	$this->db->or_where('price_tags', '');
-	$this->db->or_where('price_tags', NULL);
+	$where = array('', NULL);
+	$this->db->where_in('price_tags', $where);
+//	$this->db->or_where('price_tags', NULL);
 	$this->db->join('booking_details', 'booking_details.booking_id = booking_unit_details.booking_id');
+	$this->db->not_like('booking_details.booking_id', 'Q-');
 	$this->db->where_in('booking_details.current_status', array('Pending', 'Completed', 'Cancelled', 'Rescheduled'));
 	$this->db->where('booking_details.create_date >=', '2016-07-01 00:00:00');
 	$query = $this->db->get();
+
+	log_message('info', $this->db->last_query());
+	echo $this->db->last_query() . PHP_EOL;
+
 	if ($query->num_rows > 0) {
+	    echo "check_price_tags failed..." . PHP_EOL;
 	    return $query->result_array();
 	} else {
 	    return false;
@@ -57,13 +71,17 @@ class Database_testing_model extends CI_Model {
      * @return boolean
      */
     function check_tax_rate() {
-	$this->db->select('booking_unit_details.booking_id, tax_rate');
+	$this->db->select('booking_unit_details.booking_id, tax_rate, booking_unit_details.create_date');
 	$this->db->from('booking_unit_details');
 	$this->db->where('tax_rate <=', '0');
 	$this->db->join('booking_details', 'booking_details.booking_id = booking_unit_details.booking_id');
-	$this->db->where_in('booking_details.current_status', array('Pending', 'Completed', 'Cancelled', 'Rescheduled'));
+	$this->db->where_in('booking_details.current_status', array('Pending', 'Completed', 'Rescheduled'));
 	$this->db->where('booking_details.create_date >=', '2016-07-01 00:00:00');
 	$query = $this->db->get();
+
+	log_message('info', $this->db->last_query());
+	echo $this->db->last_query() . PHP_EOL;
+
 	if ($query->num_rows > 0) {
 	    return $query->result_array();
 	} else {
@@ -76,14 +94,18 @@ class Database_testing_model extends CI_Model {
      * @return boolean
      */
     function check_booking_unit_details_status() {
-	$this->db->select('booking_details.booking_id,booking_unit_details.booking_status');
+	$this->db->select('booking_details.booking_id,booking_unit_details.booking_status, booking_unit_details.create_date');
 	$this->db->from('booking_unit_details');
-	$this->db->or_where('booking_status', '');
-	$this->db->or_where('booking_status', NULL);
+	$where = array('', NULL);
+	$this->db->where_in('booking_status', $where);
+//	$this->db->or_where('booking_status', '');
+//	$this->db->or_where('booking_status', NULL);
 	$this->db->join('booking_details', 'booking_details.booking_id = booking_unit_details.booking_id');
 	$this->db->where_in('booking_details.current_status', array('Completed', 'Cancelled'));
 	$this->db->where('booking_details.create_date >=', '2016-07-01 00:00:00');
 	$query = $this->db->get();
+	log_message('info', $this->db->last_query());
+	echo $this->db->last_query() . PHP_EOL;
 	if ($query->num_rows > 0) {
 	    return $query->result_array();
 	} else {
@@ -96,7 +118,8 @@ class Database_testing_model extends CI_Model {
      * @return boolean
      */
     function check_booking_details() {
-	$this->db->select('booking_id, partner_id, service_id, user_id,source, type, booking_pincode, city, booking_pincode');
+	$this->db->select('booking_id, partner_id, service_id, user_id,source, type, booking_pincode, city, booking_pincode, '
+	    . 'booking_details.create_date');
 	$this->db->or_where('booking_id', '0');
 	$this->db->or_where('partner_id', '0');
 	$this->db->or_where('service_id', '0');
@@ -113,6 +136,8 @@ class Database_testing_model extends CI_Model {
 	$this->db->or_where('booking_primary_contact_no', '0');
 	$this->db->where('booking_details.create_date >=', '2016-07-01 00:00:00');
 	$query = $this->db->get('booking_details');
+	log_message('info', $this->db->last_query());
+	echo $this->db->last_query() . PHP_EOL;
 	if ($query->num_rows > 0) {
 	    return $query->result_array();
 	} else {
@@ -127,7 +152,10 @@ class Database_testing_model extends CI_Model {
 	$sql = "SELECT  `booking_id`  FROM  `booking_unit_details` WHERE  `booking_id` NOT IN "
 	    . " ( SELECT booking_id FROM booking_details WHERE create_date >=  '2016-07-01 00:00:00')";
 	$query = $this->db->query($sql);
+	log_message('info', $this->db->last_query());
+	echo $this->db->last_query() . PHP_EOL;
 	if ($query->num_rows > 0) {
+	    echo __FUNCTION__ . " failed \n\n";
 	    return $query->result_array();
 	} else {
 	    return false;
@@ -140,6 +168,8 @@ class Database_testing_model extends CI_Model {
 	    . " AND  `booking_details`.`create_date` >=  '2016-07-01 00:00:00'";
 
 	$query = $this->db->query($sql);
+	log_message('info', $this->db->last_query());
+	echo $this->db->last_query() . PHP_EOL;
 	$data = $query->result_array();
 	$data_result = array();
 
@@ -154,12 +184,13 @@ class Database_testing_model extends CI_Model {
 	    $query2 = $this->db->get('service_center_booking_action');
 	    $result2 = $query2->result_array();
 
-	    if (count($result1[0]['unit_count']) != $result2[0]['service_count']) {
+	    if ($result1[0]['unit_count'] != $result2[0]['service_count']) {
 		array_push($data_result, array('booking_id' => $value['booking_id']));
 	    }
 	}
 
 	if (!empty($data_result)) {
+	    echo __FUNCTION__ . " failed \n\n";
 	    return $data_result;
 	} else {
 	    return false;
@@ -175,7 +206,7 @@ class Database_testing_model extends CI_Model {
      */
     function check_service_center_action() {
 
-	$this->db->select('booking_id, unit_details_id, service_center_id,current_status, internal_status');
+	$this->db->select('booking_id, unit_details_id, service_center_id,current_status, internal_status, create_date');
 	$this->db->or_where('booking_id', '0');
 	$this->db->or_where('unit_details_id', NULL);
 	$this->db->or_where('unit_details_id', '0');
@@ -184,8 +215,11 @@ class Database_testing_model extends CI_Model {
 	$this->db->or_where('internal_status', '0');
 	$this->db->where('create_date >=', '2016-07-01 00:00:00');
 	$query = $this->db->get('service_center_booking_action');
+	log_message('info', $this->db->last_query());
+	echo $this->db->last_query() . PHP_EOL;
 	echo $this->db->last_query();
 	if ($query->num_rows > 0) {
+	    echo __FUNCTION__ . " failed \n\n";
 	    return $query->result_array();
 	} else {
 	    return false;
@@ -197,10 +231,12 @@ class Database_testing_model extends CI_Model {
      * @return boolean
      */
     function check_pending_booking_in_action_table() {
-	$this->db->select('booking_id');
+	$this->db->select('booking_id, create_date');
 	$this->db->where_in('booking_details.current_status', array('Pending', 'Rescheduled'));
 	$this->db->where('booking_details.create_date >=', '2016-07-01 00:00:00');
 	$query = $this->db->get('booking_details');
+	log_message('info', $this->db->last_query());
+	echo $this->db->last_query() . PHP_EOL;
 	if ($query->num_rows > 0) {
 	    $data = $query->result_array();
 	    $data_array = array();
@@ -210,6 +246,7 @@ class Database_testing_model extends CI_Model {
 		$this->db->where_not_in('current_status', array('Pending', 'Inprocess'));
 		$query1 = $this->db->get('service_center_booking_action');
 		if ($query1->num_rows > 0) {
+	    echo __FUNCTION__ . " failed \n\n";
 		    array_push($data_array, array('booking_id' => $value['booking_id']));
 		}
 	    }
