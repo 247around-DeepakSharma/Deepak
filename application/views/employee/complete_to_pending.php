ecafdceda52df34d3b55927c4446e6e070f4b21e
247Abhay <abhaya@247around.com>
2016-09-16 15:33:48 +0530

    Edit view in complete to pending view

--- application/views/employee/complete_to_pending.php
+++ application/views/employee/complete_to_pending.php
@@ -36,15 +36,22 @@
                 </div>
 
 
-               <div class="input-group input-append date">
-                    <input id="booking_date" class="form-control" style="z-index: 10000;" name="booking_date" type="text" value = "<?php echo set_value('booking_date'); ?>" required>
-                      <span class="input-group-addon add-on"><span class="glyphicon glyphicon-calendar"></span></span>
+               <div class="form-group <?php if( form_error('booking_date') ) { echo 'has-error';} ?>">
+                  <label for="booking_date" class="col-md-2">New Booking Date</label>
+                  <div class="col-md-6">
+                    <div class="input-group input-append date">
+                                    <input id="booking_date" class="form-control" style="z-index: 10000;" name="booking_date" type="text" value = "<?php echo set_value('booking_date'); ?>" required>
+                                    <span class="input-group-addon add-on"><span class="glyphicon glyphicon-calendar"></span></span>
                     </div>
+                        <?php echo form_error('booking_date'); ?>
+                  </div>
+                </div>
+
 
                 <div class="form-group <?php if( form_error('booking_timeslot') ) { echo 'has-error';} ?>">
                   <label for="booking_timeslot" class="col-md-2">New Booking Timeslot</label>
                   <div class="col-md-6">
-                    <select type="text" name="booking_timeslot" value="<?php echo set_value('booking_timeslot'); ?>" >
+                    <select type="text" class="form-control" name="booking_timeslot" value="<?php echo set_value('booking_timeslot'); ?>" >
                     <option>Select</option>
                     <option>10AM-1PM</option>
                     <option>1PM-4PM</option>
