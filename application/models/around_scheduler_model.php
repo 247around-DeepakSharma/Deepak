37289d22739146a62e218025a0a63be7a289f58d
Anuj Aggarwal <anuj@247around.com>
2016-09-21 17:18:40 +0530

    Minor

--- application/models/around_scheduler_model.php
+++ application/models/around_scheduler_model.php
@@ -20,7 +20,11 @@ class Around_scheduler_model extends CI_Model {
      * Current status => FollowUp
      */
     function send_remainder_installation_sms(){
-    	$sql = " SELECT booking_details.*, `services`.services from booking_details, services where partner_source = 'Snapdeal-shipped-excel' AND internal_status = 'Missed_call_confirmed' AND estimated_delivery_date > CURDATE() AND estimated_delivery_date = (CURDATE() + INTERVAL 1 DAY) AND current_status= 'FollowUp' AND `booking_details`.service_id = `services`.id AND booking_pincode In (Select Pincode from vendor_pincode_mapping)  ";
+    	$sql = " SELECT booking_details.*, `services`.services from booking_details, services "
+                . "where partner_source = 'Snapdeal-shipped-excel' AND internal_status = 'Missed_call_confirmed' "
+                . "AND estimated_delivery_date > CURDATE() AND estimated_delivery_date = (CURDATE() + INTERVAL 1 DAY) "
+                . "AND current_status= 'FollowUp' AND `booking_details`.service_id = `services`.id "
+                . "AND booking_pincode In (Select Pincode from vendor_pincode_mapping);";
 
     	$query = $this->db->query($sql);
     	return  $query->result_array();
