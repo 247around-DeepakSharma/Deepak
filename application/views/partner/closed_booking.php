297134a8230911e9a00f622417a79e6d4a4e7b5d
Mohd Bred <Mohd Bred>
2016-09-22 17:02:35 +0530

    Removed count in Partner CRM

--- application/views/partner/closed_booking.php
+++ application/views/partner/closed_booking.php
@@ -3,7 +3,7 @@
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h2 class="panel-title"><i class="fa fa-money fa-fw"></i> <?php echo $status." Bookings" ?>  (<?php echo $count; ?>)</h2>
+                    <h2 class="panel-title"><i class="fa fa-money fa-fw"></i> <?php echo $status." Bookings" ?></h2>
                 </div>
 
                 <div class="panel-body">
