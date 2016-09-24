31b999983bd6220ce4731a10cd192d72270d9b0c
Anuj Aggarwal <anuj@247around.com>
2016-09-24 16:40:57 +0530

    Save appliance tag as usual

--- application/controllers/employee/do_background_upload_excel.php
+++ application/controllers/employee/do_background_upload_excel.php
@@ -222,8 +222,7 @@ class Do_background_upload_excel extends CI_Controller {
 		$appliance_details['category'] = $unit_details['appliance_category'] = '';
 		$appliance_details['capacity'] = $unit_details['appliance_capacity'] = '';
 		$appliance_details['model_number'] = $unit_details['model_number'] = $value['Model'];
-		$tag = explode('&', $value['Brand']);
-		$appliance_details['tag'] = $unit_details['appliance_tag'] = $tag . " " . $value['Product'];
+		$appliance_details['tag'] = $unit_details['appliance_tag'] = $value['Brand'] . " " . $value['Product'];
 		$booking['booking_remarks'] = '';
 		$booking['booking_alternate_contact_no'] = '';
 		$appliance_details['purchase_month'] = $unit_details['purchase_month'] = date('m');
