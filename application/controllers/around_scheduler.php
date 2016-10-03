9ec02f80c113d4431ef4be2dfc58d4bd0672993d
Anuj Aggarwal <anuj@247around.com>
2016-10-03 19:25:38 +0530

    Send SMS for installation where EDD: 1. Today 2. Tomorrow and later

--- application/controllers/around_scheduler.php
+++ application/controllers/around_scheduler.php
@@ -11,6 +11,7 @@ class Around_scheduler extends CI_Controller {
         parent::__Construct();
 
         $this->load->model('around_scheduler_model');
+        $this->load->model('booking_model');
 
         $this->load->library('s3');
         $this->load->library('email');
@@ -19,28 +20,73 @@ class Around_scheduler extends CI_Controller {
     }
 
     /**
-     * @desc: This is used to send SMS to customer who have given miss call to customer support number.
-     * Note: SMS will not be sent to those customers who has EDD as tomorrow and vendor is not available
-     * in their pincode.
+     * @desc: This is used to send SMS to customers for whom delivery is scheduled for Today
+     * to inform them about free installation when they give a missed call.
      */
-    function send_reminder_installation_sms() {
-        log_message ('info', __METHOD__);
+    function send_reminder_installation_sms_today() {
+        log_message ('info', __METHOD__ . '=> Entering...');
         
-	$data = $this->around_scheduler_model->get_reminder_installation_sms_data();
+        //Get all queries where SMS needs to be sent
+	$data1 = $this->around_scheduler_model->get_reminder_installation_sms_data_today();
+        
+        //Tag queries where Vendor is not available
+        $data2 = $this->booking_model->searchPincodeAvailable($data1);
+        
+        //Send SMS to only customers where Vendor is Available
 	$sms['tag'] = "sd_edd_missed_call_reminder";
-	foreach ($data as $value) {
-	    $sms['phone_no'] = $value['booking_primary_contact_no'];
-            
-            //Ordering of SMS data is important, check SMS template before changing it
-            $sms['smsData']['message'] = $this->notify->get_product_free_not($value['services']);
-	    $sms['smsData']['service'] = $value['services'];
-	    
-            $sms['booking_id'] = $value['booking_id'];
-	    $sms['type'] = "user";
-	    $sms['type_id'] = $value['user_id'];
-            
-	    $this->notify->send_sms($sms);
+        
+	foreach ($data2 as $value) {
+            if ($value->vendor_status !== "Vendor Not Available") {
+                $sms['phone_no'] = $value->booking_primary_contact_no;
+
+                //Ordering of SMS data is important, check SMS template before changing it
+                $sms['smsData']['message'] = $this->notify->get_product_free_not($value->services);
+                $sms['smsData']['service'] = $value->services;
+
+                $sms['booking_id'] = $value->booking_id;
+                $sms['type'] = "user";
+                $sms['type_id'] = $value->user_id;
+
+                $this->notify->send_sms($sms);                
+            }
 	}
+        
+        log_message ('info', __METHOD__ . '=> Exiting...');
+    }
+
+    /**
+     * @desc: This is used to send SMS to customers for whom delivery is scheduled for tomorrow 
+     * or later to inform them about free installation when they give a missed call.
+     */
+    function send_reminder_installation_sms_future() {
+        log_message ('info', __METHOD__ . '=> Entering...');
+        
+        //Get all queries where SMS needs to be sent
+	$data1 = $this->around_scheduler_model->get_reminder_installation_sms_data_future();
+        
+        //Tag queries where Vendor is not available
+        $data2 = $this->booking_model->searchPincodeAvailable($data1);
+        
+        //Send SMS to only customers where Vendor is Available
+	$sms['tag'] = "sd_edd_missed_call_reminder";
+        
+	foreach ($data2 as $value) {
+            if ($value->vendor_status !== "Vendor Not Available") {
+                $sms['phone_no'] = $value->booking_primary_contact_no;
+
+                //Ordering of SMS data is important, check SMS template before changing it
+                $sms['smsData']['message'] = $this->notify->get_product_free_not($value->services);
+                $sms['smsData']['service'] = $value->services;
+
+                $sms['booking_id'] = $value->booking_id;
+                $sms['type'] = "user";
+                $sms['type_id'] = $value->user_id;
+
+                $this->notify->send_sms($sms);                
+            }
+	}
+        
+        log_message ('info', __METHOD__ . '=> Exiting...');
     }
 
 
