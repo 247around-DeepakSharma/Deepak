32cb7b343da1e2a176fc7d5c92217bfd5f0c6d3c
abhaya@247around.com <abhaya@247around.com>
2016-10-12 15:59:31 +0530

    Edit in vendor Invoice Summary table

--- application/views/service_centers/invoice_summary.php
+++ application/views/service_centers/invoice_summary.php
@@ -14,8 +14,11 @@
          <th>Additional Service Charges</th>
          <th>Parts / Stands</th>
          <th>Total</th>
+         <th>TDS</th>
          <th>Around Royalty</th>
-         <th>Amt Paid by Partner / Paid by 247around</th>
+         <th>Paid Amount</th>
+         <th>Amt Paid by Partner</th>
+         <th>Paid by 247around</th>
          <th>Sent Date</th>
          
       </tr>
@@ -33,20 +36,51 @@
        if(!empty($invoice_array)){
          foreach($invoice_array as $key =>$invoice) {?>
 
-      <tr>
+      <tr <?php if($invoice['settle_amount'] == 1){ ?> style="background-color: #90EE90; " <?php } ?>>
          <td><?php echo $count;?></td>
+         
          <td><?php echo date("jS F, Y", strtotime($invoice['from_date'])). " to ". date("jS F, Y", strtotime($invoice['to_date'])); ?></td>
          <td><?php echo $invoice['type']; ?></td>
          <td><a href="https://s3.amazonaws.com/bookings-collateral/invoices-excel/<?php echo $invoice['invoice_file_excel']; ?>"><?php echo $invoice['invoice_file_excel']; ?></a></td>
          <td><a href="https://s3.amazonaws.com/bookings-collateral/invoices-pdf/<?php echo $invoice['invoice_file_pdf']; ?>"><?php echo $invoice['invoice_file_pdf']; ?></a></td>
          <td><?php echo $invoice['num_bookings'];  $sum_no_of_booking += $invoice['num_bookings']; ?></td>
+         <?php if($invoice['type'] == "Cash"){ ?>
          <td><?php echo $invoice['total_service_charge']; $sum_of_total_service_charges +=  $invoice['total_service_charge']; ?></td>
          <td><?php echo $invoice['total_additional_service_charge']; $sum_total_additional_service_charge += $invoice['total_additional_service_charge'];?></td>
          <td><?php echo $invoice['parts_cost']; $sum_total_parts_cost += $invoice['parts_cost']; ?></td>
          <td><?php echo $invoice['total_amount_collected']; $total_amount_collected += $invoice['total_amount_collected'];?></td>
+          <td><?php echo $invoice['tds_amount']; ?></td>
          <td><?php echo $invoice['around_royalty']; $around_royalty += $invoice['around_royalty']; ?></td>
-         <td><?php echo $invoice['amount_collected_paid']; $amount_collected_paid += $invoice['amount_collected_paid']; ?></td>
-         <td><?php echo date("jS F, Y", strtotime($invoice['create_date'])); ?></td>
+         <?php $amount_collected_paid += $invoice['amount_collected_paid']; ?>
+
+          <td><?php if(abs($invoice['amount_collected_paid']) == $invoice['amount_paid'] ){ echo abs($invoice['amount_paid']); } else { echo abs($invoice['amount_collected_paid'] + $invoice['amount_paid']);}   ?></td>
+
+         <?php $amount_collected_paid += $invoice['amount_collected_paid']; ?>
+         <td><?php if($invoice['amount_collected_paid'] >0){ echo abs($invoice['amount_collected_paid']) - $invoice['amount_paid'];} else { echo "0.00"; }?></td>
+         <td><?php if($invoice['amount_collected_paid'] <=0){ echo abs($invoice['amount_collected_paid']) + $invoice['amount_paid'] ; } else { echo "0.00"; } ?></td>
+
+         <?php } else if($invoice['type'] == "FOC"){ ?>
+
+         <td><?php echo (($invoice['total_service_charge'] + $invoice['service_tax'] )* basic_percentage); $sum_of_total_service_charges +=  (($invoice['total_service_charge'] + $invoice['service_tax']) * basic_percentage); ?></td>
+
+         <td><?php echo ($invoice['total_additional_service_charge'] * basic_percentage); $sum_total_additional_service_charge += ($invoice['total_additional_service_charge'] *basic_percentage);?></td>
+
+         <td><?php echo (($invoice['parts_cost'] + $invoice['vat'])*basic_percentage); $sum_total_parts_cost += (($invoice['parts_cost'] + $invoice['vat'])*basic_percentage ); ?></td>
+         
+
+         <td><?php echo ((($invoice['total_service_charge'] + $invoice['service_tax'] )* basic_percentage) + ($invoice['total_additional_service_charge'] * basic_percentage) + (($invoice['parts_cost'] + $invoice['vat'])*basic_percentage)); ?></td>
+
+         <td><?php echo $invoice['tds_amount']; ?></td>
+
+         <td>0.00</td>
+         <td><?php if(abs($invoice['amount_collected_paid']) == $invoice['amount_paid'] ){ echo $invoice['amount_paid']; } else { echo abs($invoice['amount_collected_paid'] + $invoice['amount_paid']);}   ?></td>
+         <?php $amount_collected_paid += $invoice['amount_collected_paid']; ?>
+
+         <td><?php if($invoice['amount_collected_paid'] >0){ echo abs($invoice['amount_collected_paid']) - $invoice['amount_paid'];} else { echo "0.00"; }?></td>
+         <td><?php if($invoice['amount_collected_paid'] <=0){ echo abs($invoice['amount_collected_paid'] + $invoice['amount_paid']) ; } else { echo "0.00"; } ?></td>
+
+          <?php } ?>
+           <td><?php echo date("jS F, Y", strtotime($invoice['create_date'])); ?></td>
          <?php  $count = $count+1;  ?>
 
       </tr>
@@ -63,6 +97,7 @@
          <td><?php echo $sum_total_additional_service_charge; ?></td>
          <td><?php echo $sum_total_parts_cost; ?></td>
          <td><?php echo $total_amount_collected; ?></td>
+         <td></td>
          <td><?php echo $around_royalty; ?></td>
          <td></td>
          <td></td>
