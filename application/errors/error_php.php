7a148ad52ce0a7e3c15062724299211fd1114d2d
abhaya@247around.com <abhaya@247around.com>
2016-11-22 16:33:15 +0530

    capture error in a text file

--- application/errors/error_php.php
+++ application/errors/error_php.php
@@ -8,3 +8,19 @@
 <p>Line Number: <?php echo $line; ?></p>
 
 </div>
+
+<?php 
+
+    $file = fopen(FCPATH."/application/logs/error_" . date('Y-m-d') . ".txt", "a+") or die("Unable to open file!");
+    $res = 0;
+    system(" chmod 777 ".FCPATH."/application/logs/error_" . date('Y-m-d') . ".txt", $res);
+    fwrite($file, "\n A PHP Error was encountered: ".date('Y-m-d H:i:s')."\n");
+    fwrite($file, "Severity: " . print_r($severity, TRUE)."\n");
+    fwrite($file, "Message: " . print_r($message, TRUE)."\n");
+    fwrite($file, "Filename: " . print_r($filepath, TRUE)."\n");
+    fwrite($file, "Line Number: " . print_r($line, TRUE)."\n");
+    
+    
+    fclose($file);
+
+?>
