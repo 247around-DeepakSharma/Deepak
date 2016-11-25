de3062e092c06e8ff7cd5a140f07dec670cb930e
mohdbelal <belal@247around.com>
2016-11-25 12:07:19 +0530

    Brackets Changes

--- application/models/inventory_model.php
+++ application/models/inventory_model.php
@@ -158,7 +158,7 @@ class Inventory_model extends CI_Model {
      * 
      */
     function get_brackets_by_order_id($order_id){
-        $this->db->select('brackets.id,brackets.order_id,brackets.invoice_id,brackets.order_received_from,brackets.order_given_to,brackets.order_date,brackets.shipment_date,'
+        $this->db->select('brackets.id,brackets.order_id,brackets.order_received_from,brackets.order_given_to,brackets.order_date,brackets.shipment_date,'
                 . 'brackets.received_date,brackets.19_24_requested,brackets.26_32_requested,brackets.36_42_requested,'
                 . 'brackets.total_requested,brackets.19_24_shipped,brackets.26_32_shipped,brackets.36_42_shipped,brackets.total_shipped,'
                 . 'brackets.19_24_received,brackets.26_32_received,brackets.36_42_received,brackets.total_received,brackets.is_shipped,brackets.is_received,'
@@ -182,7 +182,7 @@ class Inventory_model extends CI_Model {
      * 
      */
     function get_vendor_bracket_invoices($vendor_id,$date_range){
-        //Getting date range
+       //Getting date range
         $custom_date = explode("-", $date_range);
         $from_date = str_replace("/", "-", $custom_date[0]);
         $to_date = str_replace("/","-",$custom_date[1]);
@@ -212,6 +212,19 @@ class Inventory_model extends CI_Model {
         $this->db->select('order_id');
         $this->db->order_by('id','desc');
         $query = $this->db->get('brackets');
+        return $query->result_array();
+    }
+    
+    /**
+     * 
+     * @Desc: This function is used to get invoice id from vendor partner invoices table
+     * @parmas: order_id
+     * @return: Array
+     */
+    function get_brackets_invoice_by_order_id($order_id){
+        $this->db->select('invoice_id');
+        $this->db->where('order_id',$order_id);
+        $query = $this->db->get('vendor_partner_invoices');
         return $query->result_array();
     }
     
