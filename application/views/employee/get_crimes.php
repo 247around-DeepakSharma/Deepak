434c190e729dc897006ded14506f4adffd505eb0
mohdbelal <belal@247around.com>
2017-01-16 11:21:24 +0530

    Sc Crime Report Changes

--- application/views/employee/get_crimes.php
+++ application/views/employee/get_crimes.php
@@ -22,6 +22,9 @@
                <th  style="border-bottom-width: 2px;border: 1px solid #ddd;
                   vertical-align: bottom;padding: 8px;
                   line-height: 1.42857143;     text-align: center;background: #DDDDDD">SF Name</th>
+               <th  style="border-bottom-width: 2px;border: 1px solid #ddd;
+                  vertical-align: bottom;padding: 8px;
+                  line-height: 1.42857143;     text-align: center;background: #DDDDDD">Booking Updated</th>
 <!--               <th  style="border-bottom-width: 2px;border: 1px solid #ddd;
                   vertical-align: bottom;padding: 8px;
                   line-height: 1.42857143;     text-align: center;">Engineer Not Assigned </th>-->
@@ -40,11 +43,13 @@
             <?php 
             $count = 0;
             $not_update = 0;
+            $updated = 0;
             $total_booking = 0;
             $old_crimes = 0;
             foreach ($data as $key => $value) {if($value['not_update'] > 0) {
                 $count++;
                 $not_update += $value['not_update'];
+                $updated += $value['update'];
                 $total_booking += $value['total_booking'];
                 $old_crimes += $value['old_crimes'];
                 ?>
@@ -55,6 +60,9 @@
                <td style="    border: 1px solid #ddd;    padding: 8px;
                   line-height: 1.42857143;
                   vertical-align: top;    text-align: center;"><?php echo $value['service_center_name']; ?></td>
+               <td style="    border: 1px solid #ddd;    padding: 8px;
+                  line-height: 1.42857143;
+                  vertical-align: top;    text-align: center;"><?php echo $value['update']; ?></td>
 <!--               <td style="    border: 1px solid #ddd;    padding: 8px;
                   line-height: 1.42857143;
                   vertical-align: top;    text-align: center;"><?php// echo $value['un_assigned']; ?></td>-->
@@ -76,6 +84,9 @@
                 <td style="    border: 1px solid #ddd;    padding: 8px;
                   line-height: 1.42857143;
                   vertical-align: top;    text-align: center;background: #FAC575"><b>TOTAL</b></td>
+                <td style="    border: 1px solid #ddd;    padding: 8px;
+                  line-height: 1.42857143;
+                  vertical-align: top;    text-align: center;background: #FAC575"><b><?php echo $updated ?></b></td>
                 <td style="    border: 1px solid #ddd;    padding: 8px;
                   line-height: 1.42857143;
                   vertical-align: top;    text-align: center;background: #FAC575"><b><?php echo ($not_update.' / '.$total_booking) ?></b></td>
