588bb6fbc291e92b1f052bb409b483c695acf347
Abhay <abhaya@247around.com>
2017-02-23 17:49:13 +0530

    change model to library

--- application/controllers/employee/do_background_upload_excel.php
+++ application/controllers/employee/do_background_upload_excel.php
@@ -23,7 +23,7 @@ class Do_background_upload_excel extends CI_Controller {
 	$this->load->helper(array('form', 'url'));
 
 	$this->load->library('asynchronous_lib');
-    $this->load->library('miscelleneous');
+        $this->load->library('miscelleneous');
 	$this->load->library('notify');
 	$this->load->helper(array('form', 'url'));
 	$this->load->helper('download');
@@ -1347,7 +1347,7 @@ class Do_background_upload_excel extends CI_Controller {
     function check_upcountry($booking, $appliance, $is_price, $appliance_category, $file_type, $partner_data) {
         if ($is_price) {
             log_message('info', __FUNCTION__ . ' Check Upcountry Vendor Availability');
-            $data = $this->upcountry_model->check_upcountry_vendor_availability($booking['city'], $booking['booking_pincode'], $booking['service_id'], $partner_data, false);
+            $data = $this->miscelleneous->check_upcountry_vendor_availability($booking['city'], $booking['booking_pincode'], $booking['service_id'], $partner_data, false);
             $charges = 0;
             if ($is_price['is_upcountry'] == 0) {
                 $charges = "Rs. " . (($data['upcountry_distance'] * DEFAULT_UPCOUNTRY_RATE) +
