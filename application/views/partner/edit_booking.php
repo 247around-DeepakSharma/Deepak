2d4554c7220f42fd5e035ec0b2bee4618d1ad99b
abhaya@247around.com <abhaya@247around.com>
2017-04-03 10:29:55 +0530

    Call Model function

--- application/views/partner/edit_booking.php
+++ application/views/partner/edit_booking.php
@@ -95,7 +95,7 @@
                                 <div class="form-group col-md-12 <?php if( form_error('appliance_capacity') ) { echo 'has-error';} ?>">
                                     <label for="appliance_capacity">Capacity  <span id="error_capacity" style="color: red;"> <span style="color:grey;display:none" id="capacity_loading">Loading ...</span></label>
                                    
-                                    <select type="text" class="form-control appliance_capacity"   id="appliance_capacity_1" name="appliance_capacity" onchange="return getPrice()">
+                                    <select type="text" class="form-control appliance_capacity"   id="appliance_capacity_1" name="appliance_capacity" onchange="return get_models(), getPrice()">
                                         <option selected disabled value="option1">Select Appliance Capacity</option>
                                     </select>
                                     <?php echo form_error('appliance_capacity'); ?>
@@ -536,6 +536,7 @@
                     //$("#appliance_capacity_1 option[value !='option1']").remove();
                     //$('#appliance_capacity_1').append(data).change();
                     $('#appliance_capacity_1').html(data).change();
+                    get_models();
                     getPrice();
                 },
             complete: function(){
