bfd752b00b25094e9e316ed698d57b01c2db519a
Abhay <abhaya@247around.com>
2017-04-18 14:29:24 +0530

    Update Spare Parts Booking

--- application/views/employee/sparepart_on_tab.php
+++ application/views/employee/sparepart_on_tab.php
@@ -37,8 +37,8 @@
                                         <td class="text-center"><?php echo $value['parts_requested'];?></td>
                                         <td class="text-center"><?php echo $value['parts_shipped'];?></td>
                                         <td class="text-center"><?php echo $value['current_status'];?></td> 
-                                    
-                                        <td class="text-center"><a href="#" disabled class="btn btn-sm btn-primary" ><i class="fa fa-edit"></i></a></td>
+
+                                        <td class="text-center"><a href="<?php echo base_url(); ?>employee/inventory/update_spare_parts/<?php echo $value['id'];?>" class="btn btn-sm btn-primary" ><i class="fa fa-edit"></i></a></td>
                                    </tr>
                              
                                     <?php $sn_no++; }}?>
@@ -202,7 +202,7 @@
                                         <td class="text-center"><?php echo $value['parts_shipped'];?></td>  
                                         <td class="text-center"><?php echo $value['current_status'];?></td> 
 
-                                        <td class="text-center"><a href="#" disabled class="btn btn-sm btn-primary" ><i class="fa fa-edit"></i></a></td>
+                                        <td class="text-center"><a href="<?php echo base_url(); ?>employee/inventory/update_spare_parts/<?php echo $value['id'];?>" class="btn btn-sm btn-primary" ><i class="fa fa-edit"></i></a></td>
                                    </tr>
                              
                                         <?php $sn_no++; }}?>
