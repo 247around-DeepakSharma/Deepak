e4c512e1abb49df67475ed7e7836ee0e63f4742a
Anuj Aggarwal <anuj@247around.com>
2017-05-21 23:03:48 +0530

    Upcountry distance calculation script modified - Echos commented

--- application/controllers/utilities.php
+++ application/controllers/utilities.php
@@ -56,30 +56,30 @@ class Utilities extends CI_Controller {
     function get_distance_between_pincode_sf_hq_level() {
         echo "Entering" . PHP_EOL;
         $data = $this->upcountry_model->get_sub_service_center_details(array());
-        echo "GET SUB Office details" . PHP_EOL;
+//        echo "GET SUB Office details" . PHP_EOL;
         foreach ($data as $key1 => $value) {
-            echo " Foreach data" . PHP_EOL;
+//            echo " Foreach data" . PHP_EOL;
             $pincode_list = $this->vendor_model->getPincode_from_india_pincode($value['district']);
-            echo "Service ID " . PHP_EOL;
+//            echo "Service ID " . PHP_EOL;
             $service_id_list = $this->vendor_model->get_distinct_vendor_service_details($value['service_center_id'], $value['pincode']);
-            echo "GET PIncode List" . PHP_EOL;
+//            echo "GET PIncode List" . PHP_EOL;
             $pincode_details = array();
             foreach ($pincode_list as $key2 => $pincode) {
                 if ($pincode['pincode'] != $value['pincode']) {
                     $is_distance = $this->upcountry_model->calculate_distance_between_pincode($value['pincode'], $value['district'], $pincode['pincode'], $value['district']);
                     if ($is_distance) {
                         $distance1 = (round($is_distance['distance']['value'] / 1000, 2));
-                        echo "Distance " . $distance1;
-                        echo PHP_EOL;
+                        //echo "Distance " . $distance1;
+                        echo ".";
                         array_push($pincode_details, array("pincode" => $pincode['pincode'], "distance" => $distance1));
                     } else {
-                        echo "Distance not calculated  Pincode1" . $value['pincode'] . " Pincode 2 " . $pincode['pincode'];
+                        echo PHP_EOL . "Distance not calculated=> Pincode1: " . $value['pincode'] . ",Pincode 2: " . $pincode['pincode'] . PHP_EOL;
                         log_message("info", __METHOD__ . " Distance can not calcuate Pincode 1" . $value['pincode'] . " Pincode 2 " . $pincode['pincode']);
                     }
                     unset($pincode_list[$key2]);
                 }
             }
-            echo "Foreach Service Id" . PHP_EOL;
+//            echo "Foreach Service Id" . PHP_EOL;
             foreach ($service_id_list as $key3 => $service_id) {
                 if (!empty($pincode_details)) {
                     foreach ($pincode_details as $details) {
@@ -97,7 +97,7 @@ class Utilities extends CI_Controller {
 
                         $status = $this->upcountry_model->insert_sf_hq_level_distance_details($array);
                         if ($status) {
-                            echo "Inserted";
+                            echo "+";
                             log_message("info", __METHOD__ . " Inserted " . $value['pincode'] . " Pincode 2 " . $pincode['pincode']);
                         } else {
                             echo "Not Inserted " . $value['pincode'] . " Pincode 2 " . $pincode['pincode'] . " SF ID "
