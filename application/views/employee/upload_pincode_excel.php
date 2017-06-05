2764836a1a74ea95fd1dff2e032298df06569cec
sachinbit <sachinj@247around.com>
2017-06-05 15:39:02 +0530

    add upload pincode file history view

--- application/views/employee/upload_pincode_excel.php
+++ application/views/employee/upload_pincode_excel.php
@@ -35,6 +35,15 @@
                                </div>';
                             }
                             ?>
+                            <?php if($this->session->flashdata('success_msg')) {
+                                echo '<div class="alert alert-danger alert-dismissible" role="alert" style="margin-top:15px;">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                    </button>
+                                <strong>' . $this->session->flashdata('success_msg') . '</strong>
+                               </div>';
+                            }
+                            ?>
                         <form class="form-horizontal" action="<?php echo base_url()?>employee/vendor/process_pincode_excel_upload_form" method="POST" enctype="multipart/form-data">
                             <div class="col-md-6">
                                 <div class="form-group col-md-12 <?php if( form_error('excel') ) { echo 'has-error';} ?>">
@@ -51,13 +60,15 @@
                                             <input type= "submit"  class="btn btn-danger btn-md" value ="Upload" >
                                            <!--  <input type="hidden" value="https://s3.amazonaws.com/bookings-collateral/vendor-pincodes/<?php echo $pincode_mapping_file[0]['file_name']; ?>" id="fileUrl"></input> -->
 
-                                            <a href="<?php echo base_url()?>employee/vendor/download_pincode_latest_file" class="btn btn-primary btn-md">Download latest File</a> 
+                                            <a href="<?php if(!empty($pincode_mapping_file_list)){echo base_url()?>employee/vendor/download_pincode_latest_file/<?php echo $pincode_mapping_file_list[0]['file_name'];}else{echo "javascript:void()"; } ?>" class="btn btn-primary btn-md">Download latest File</a> 
                                             <a href="<?php echo base_url()?>employee/vendor/download_unique_pincode_excel" class="btn btn-primary btn-md">Get Unique Pincode</a> 
                                             
                                         </center>
                                     </div>
                                 </div>
+                                <div class="info_text"><p class="alert alert-danger"><i class="fa fa-info-circle" aria-hidden="true"></i>  Zipped CSV file Must Be name as  <strong>vendor_pincode_mapping.csv</strong></p></div>
                             </div>
+                            
                             <div class='col-md-6'>
                                 <div class="col-md-6">
                                     Total Pincode&nbsp;:&nbsp;&nbsp;<b><?php echo $total_pincode?></b>
@@ -67,11 +78,11 @@
                                         <tbody>
                                             <tr>
                                                 <td><strong>Uploaded By</strong></td>
-                                                <td><?php echo $latest_file[0]['full_name']?></td>
+                                                <td><?php if(!empty($pincode_mapping_file_list)){echo $pincode_mapping_file_list[0]['agent_name'];} ?></td>
                                             </tr>
                                             <tr>
                                                 <td><strong>Upload Date</strong></td>
-                                                <td><?php echo date('d-m-Y',  strtotime($latest_file[0]['upload_date']))?></td>
+                                                <td><?php if(!empty($pincode_mapping_file_list)){echo date('d-m-Y',  strtotime($pincode_mapping_file_list[0]['upload_date']));} ?></td>
                                             </tr>
                                         </tbody>
                                     </table>
@@ -105,11 +116,36 @@
                                 </div>
                             </div>
                         </form>
+                    
+                    <div class="col-xs-12 file_upload_history">
+                        <h3>File Upload History</h3>
+                        <table class="table table-bordered table-hover table-responsive">
+                            <thead>
+                                <tr>
+                                    <th>S.No.</th>
+                                    <th>Download</th>
+                                    <th>Uploaded By</th>
+                                    <th>Uploaded Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php  $sn = 1; foreach($pincode_mapping_file_list as $value) { ?>
+                                <tr>
+                                    <td><?php echo $sn; ?></td>
+                                    <td><a href="<?php echo base_url()?>employee/vendor/download_pincode_latest_file/<?php echo $value['file_name']; ?>"><div class="btn btn-success btn-sm">Download</div></a></td>
+                                    <td><?php echo $value['agent_name']; ?></td>
+                                    <td><?php echo date('d-m-Y',  strtotime($value['upload_date'])); ?></td>
+                                </tr>
+                                <?php  $sn++;}?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <?php $this->session->unset_userdata('file_error'); ?>
+<?php $this->session->unset_userdata('success_msg'); ?>
 
 
