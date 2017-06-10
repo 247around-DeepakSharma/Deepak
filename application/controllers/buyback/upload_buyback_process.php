132bf27e3fbbed58679e03cf80d554a3f25300f8
Abhay <abhaya@247around.com>
2017-06-10 12:56:51 +0530

    buyback controller

---
+++ application/controllers/buyback/upload_buyback_process.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class Upload_buyback_process extends CI_Controller {
+
+    /**
+     * load list modal and helpers
+     */
+    function __Construct() {
+        parent::__Construct();
+
+        $this->load->helper(array('form', 'url'));
+
+        $this->load->library('form_validation');
+
+        if (($this->session->userdata('loggedIn') == TRUE) && ($this->session->userdata('userType') == 'employee')) {
+            return TRUE;
+        } else {
+            echo PHP_EOL . 'Terminal Access Not Allowed' . PHP_EOL;
+            redirect(base_url() . "employee/login");
+        }
+    }
+    
+}
