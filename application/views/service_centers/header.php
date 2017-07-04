1078f91d0045c0e6456d43303d7143328ff5f73e
Abhay <abhaya@247around.com>
2017-07-04 14:42:20 +0530

    jaidev number

--- application/views/service_centers/header.php
+++ application/views/service_centers/header.php
@@ -164,6 +164,24 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Escalation Resolution Contact:</h4>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="card" style="width:40%;">
+                                        <div class="container">
+                                            <h5><b>Mr. Jaidev Sharma</b></h5> 
+                                            <p>Delhi Office</p>
+                                            <p>9582528116 <span> <strong>|</strong> English and Hindi</span></p>
+                                            <p>jaidevs@247around.com</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -204,6 +222,7 @@
                                 </div>
                             </div>
                         </div>
+                        
                         <div class="modal-header text-center" style="background-color:#E5E5FF;">
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
                             <h4 class="modal-title">247around Call Center - 9555000247</h4>
