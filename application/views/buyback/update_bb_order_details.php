cb08aa419b938ce75a777d9cabbc1f078d32bba4
sachinbit <sachinj@247around.com>
2017-07-19 11:17:37 +0530

    remove multiple image from report issue form

--- application/views/buyback/update_bb_order_details.php
+++ application/views/buyback/update_bb_order_details.php
@@ -77,8 +77,8 @@
                                 <div class="row">
                                     <div class="order_upload_file">
                                         <div class="form-group form-inline">
-                                            <label for="order_files" class="col-md-4 info_tooltip"><i class="fa fa-info info-labal text-info fa_custom" data-toggle="tooltip" title="You can upload multiple images"></i> &nbsp;Upload Order Id Image:</label>
-                                            <input type="file" class="form-control col-md-8" id="order_files" name="order_files[]" accept="image/*" multiple required></textarea>
+                                            <label for="order_files" class="col-md-4 info_tooltip">Upload Order Id Image:</label>
+                                            <input type="file" class="form-control col-md-8" id="order_files" name="order_files" accept="image/*" required></textarea>
                                         </div>
                                     </div>
                                 </div>
