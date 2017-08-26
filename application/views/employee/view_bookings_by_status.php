5f73f61d04929d997ee3a4d5a02b920f0e650cd5
sachinbit <sachinj@247around.com>
2017-08-26 18:15:31 +0530

    add alert msg on complete and cancelled booking page

--- application/views/employee/view_bookings_by_status.php
+++ application/views/employee/view_bookings_by_status.php
@@ -11,6 +11,35 @@
     <div class="row">
         <h1> <?php echo ucfirst($booking_status);?> Bookings</h1>
         <hr>
+        <?php
+        if ($this->session->userdata('success')) {
+            echo '<div class="alert alert-success alert-dismissible" role="alert" style="width: 60%;margin-left: 20%;margin-top: 0px;">
+
+                   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                       <span aria-hidden="true">&times;</span>
+                   </button>
+                   <strong>' . $this->session->userdata('success') . '</strong>
+               </div>';
+        }
+        if ($this->session->userdata('error')) {
+            echo '<div class="alert alert-danger alert-dismissible" role="alert" style="width: 60%;margin-left: 20%;margin-top: 0px;">
+
+                   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                       <span aria-hidden="true">&times;</span>
+                   </button>
+                   <strong>' . $this->session->userdata('error') . '</strong>
+               </div>';
+        }
+        if ($this->session->userdata('failed')) {
+            echo '<div class="alert alert-danger alert-dismissible" role="alert" style="width: 60%;margin-left: 20%;margin-top: 0px;">
+
+                   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                       <span aria-hidden="true">&times;</span>
+                   </button>
+                   <strong>' . $this->session->userdata('failed') . '</strong>
+               </div>';
+        }
+        ?> 
         <div class="table_filter">
             <div class="row">
                 <div class="col-md-3">
@@ -265,3 +294,6 @@
 
 
 </script>
+<?php $this->session->unset_userdata('success'); ?>
+<?php $this->session->unset_userdata('error'); ?>
+<?php $this->session->unset_userdata('failed'); ?>
