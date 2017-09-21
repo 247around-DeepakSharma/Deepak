6283a361aaccd4e5d6c68a077105a9643ab48eca
sachinbit <sachinj@247around.com>
2017-09-21 17:50:27 +0530

    add partner and employee in broadcast email

--- application/models/partner_model.php
+++ application/models/partner_model.php
@@ -778,10 +778,12 @@ class Partner_model extends CI_Model {
      * @param String $is_reporting_mail (O or 1)
      * @return Array
      */
-    function getpartner_details($select, $where, $is_reporting_mail="") {
+    function getpartner_details($select, $where = "", $is_reporting_mail="") {
 
 	$this->db->select($select);
-	$this->db->where($where);
+        if(!empty($where)){
+            $this->db->where($where);
+        }
         $this->db->from('partners');
 	if ($is_reporting_mail != "") {
 	    $this->db->where_in('is_reporting_mail', $is_reporting_mail);
