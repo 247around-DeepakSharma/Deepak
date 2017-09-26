0d2d8a999c60239fd929f82cecaaac88b623754a
chhavi <chhavid@247around.com>
2017-09-26 17:32:17 +0530

    Advance Search on bookings

---
+++ application/views/employee/advanced_search.php
@@ -0,0 +1,192 @@
+<script src="<?php echo base_url(); ?>js/base_url.js"></script>
+<script src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+<script src="<?php echo base_url(); ?>js/employee/employee.js"></script>
+<script src="<?php echo base_url(); ?>js/employee/app.js"></script>
+<script src="<?php echo base_url(); ?>js/employee/controller/controllers.js"></script>
+<script src="<?php echo base_url(); ?>js/employee/directives/directives.js"></script>
+<link rel="stylesheet" href="<?php echo base_url();?>css/jquery.loading.css">
+
+<script src="<?php echo base_url();?>js/jquery.loading.js"></script>
+<style>#datatable1_filter{display: none;}</style>
+<div class="right_col" role="main"  ng-app="booking_advanced_search">
+    <div class="clearfix"></div>
+    <div class="row" ng-controller="bookingAdvancedSearchController">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel" style="height: auto;">
+                <form method="post" action="<?php echo base_url();?>employee/booking/download_booking_snapshot" >
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="x_title">
+                        <h2>
+                          <i class="fa fa-filter"></i> Advanced Search on Bookings                           
+                        </h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li>  <input type="submit" class="btn btn-primary" value="Download Booking List" ></li>
+                        </ul>
+                        <div class="clearfix"></div>
+                        
+                       
+                    </div>
+                    <div class="x_content">
+                        <div class="x_content" >
+                           
+                            <table class="table table-striped table-bordered"  >
+                                <thead>
+                                    <tr>
+                                        <td style="width:13%;"><input type="text" placeholder="Booking Date" class="form-control" id="booking_date" name="booking_date"/></td>
+                                        <td style="width:13%;"><input type="text" placeholder="Close Date" class="form-control" id="close_date" name="close_date"/></td>
+                                        <td>
+                                            <select style="width:100%" name="partner" ui-select2 id="partner"  class="form-control data_change" data-placeholder="Select Partner">
+                                               <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in partner_list" value="{{y.partner_id}}">{{y.source}}</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="city" ui-select2 id="city"  class="form-control data_change" data-placeholder="Select City">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in city_list" value="{{y.city}}">{{y.city}}</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%"  name="sf" ui-select2 id="sf"  class="form-control data_change" data-placeholder="Select SF">
+                                                <option value=""  ng-show="false"></option>
+                                                <option ng-repeat="y in service_centers_list" value="{{y.id}}">{{y.name}}</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="current_status" ui-select2 id="current_status"  class="form-control data_change" data-placeholder="Current Status">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in current_status_list" value="{{y.current_status}}">{{y.current_status}}</option>   
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="internal_status" ui-select2 id="internal_status"  class="form-control data_change" data-placeholder="Internal Status">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in internal_status_list" value="{{y.status}}">{{y.status}}</option>   
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="product_or_service" ui-select2 id="product_or_service"  class="form-control data_change" data-placeholder="Product Or Service">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in product_or_service_list" value="{{y.option}}">{{y.option}}</option>
+                                            </select>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <select style="width:100%" name="upcountry" ui-select2 id="upcountry"  class="form-control data_change" data-placeholder="Is Upcountry">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in is_upcountry_list" value="{{y.value}}">{{y.option}}</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="rating" ui-select2 id="rating"  class="form-control data_change" data-placeholder="Rating">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in rating_list" value="{{y.option}}">{{y.option}}</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="service" ui-select2 id="service"  class="form-control data_change" data-placeholder="Service">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in service_list" value="{{y.id}}">{{y.services}}</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="brand" ui-select2 id="brand"  class="form-control data_change"data-placeholder="Brand">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in brand_list" value="{{y.brand_name}}">{{y.brand_name}}</option>   
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="categories" ui-select2 id="categories"  class="form-control data_change "data-placeholder="Categories">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in category_list" value="{{y.category}}">{{y.category}}</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="capacity" ui-select2 id="capacity"  class="form-control data_change"data-placeholder="Capacity">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in capacity_list" value="{{y.capacity}}">{{y.capacity}}</option>   
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="paid_by" ui-select2 id="paid_by"  class="form-control data_change"data-placeholder="Paid By">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in paid_by_list" value="{{y.option}}">{{y.option}}</option>   
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <select style="width:100%" name="request_type" ui-select2 id="request_type"  class="form-control data_change"data-placeholder="Request Type">
+                                                <option value="" ng-show="false"></option>
+                                                <option ng-repeat="y in request_type_list" value="{{y.request_type}}">{{y.request_type}}</option>
+                                            </select>
+                                        </td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
+                       </form>
+                        <form action="#" method="POST" id="reAssignForm" name="reAssignForm">
+                            <table id="datatable1" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Booking ID</th>
+                                        <th>Partner</th>
+                                        <th>City</th>
+                                        <th>Service Center</th>
+                                        <th>Service</th>
+                                        <th>Brand</th>
+                                        <th>Category</th>
+                                        <th>Capacity</th>
+                                        <th>Request Type</th>
+                                        <th>Product/Service</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </form>
+                    </div>
+                   
+                </div>
+            
+            </div>
+        </div>
+    </div>
+    <div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Error In Assign</h4>
+      </div>
+      <div class="modal-body">
+         <table class="table table-bordered table-hover table-responsive">
+                        <thead>
+                        <th>S.No.</th>
+                        <th>Order ID</th>   
+                        <th>Message</th>   
+                        </thead>
+                        <tbody  id="error_td">
+                           
+                        </tbody>
+                    </table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+</div>
+<script>
+    $('select').on('change', function(){
+         ad_table.ajax.reload( function ( json ) {} );
+    });
+    </script>
