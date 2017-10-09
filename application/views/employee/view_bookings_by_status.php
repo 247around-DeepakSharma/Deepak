d6fad4a0fc8bd9ef1a7c5e22c17462e1b9a4d70f
sachinbit <sachinj@247around.com>
2017-10-09 12:33:32 +0530

    add filter in pending queries and cancelled page

--- application/views/employee/view_bookings_by_status.php
+++ application/views/employee/view_bookings_by_status.php
@@ -5,6 +5,67 @@
     .col-md-3 {
         width: 18%;
     }
+    
+    .spinner {
+        margin: 0px auto;
+        width: 50px;
+        height: 50px;
+        text-align: center;
+        font-size: 10px;
+    }
+
+    .spinner > div {
+        height: 100%;
+        width: 6px;
+        display: inline-block;
+
+        -webkit-animation: sk-stretchdelay 1.2s infinite ease-in-out;
+        animation: sk-stretchdelay 1.2s infinite ease-in-out;
+    }
+
+    .spinner .rect2 {
+        -webkit-animation-delay: -1.1s;
+        animation-delay: -1.1s;
+    }
+
+    .spinner .rect3 {
+        -webkit-animation-delay: -1.0s;
+        animation-delay: -1.0s;
+    }
+
+    .spinner .rect4 {
+        -webkit-animation-delay: -0.9s;
+        animation-delay: -0.9s;
+    }
+
+    .spinner .rect5 {
+        -webkit-animation-delay: -0.8s;
+        animation-delay: -0.8s;
+    }
+
+    @-webkit-keyframes sk-stretchdelay {
+        0%, 40%, 100% { -webkit-transform: scaleY(0.4) }  
+        20% { -webkit-transform: scaleY(1.0) }
+    }
+
+    @keyframes sk-stretchdelay {
+        0%, 40%, 100% { 
+            transform: scaleY(0.4);
+            -webkit-transform: scaleY(0.4);
+        }  20% { 
+            transform: scaleY(1.0);
+            -webkit-transform: scaleY(1.0);
+        }
+    }
+    
+    #datatable1_processing{
+            position: absolute;
+            z-index: 999999;
+            width: 100%;
+            background: rgba(0,0,0,0.5);
+            height: 100%;
+            top: 10px;
+    }
 </style>
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />
 <div id="page-wrapper" >
@@ -217,7 +278,15 @@
         
         datatable1 = $('#datatable1').DataTable({
             "processing": true, 
-            "serverSide": true, 
+            "serverSide": true,
+            "language":{ 
+                "processing": "<div class='spinner'>\n\
+                                    <div class='rect1' style='background-color:#db3236'></div>\n\
+                                    <div class='rect2' style='background-color:#4885ed'></div>\n\
+                                    <div class='rect3' style='background-color:#f4c20d'></div>\n\
+                                    <div class='rect4' style='background-color:#3cba54'></div>\n\
+                                </div>"
+            },
             "order": [], 
             "pageLength": 50,
             "ordering": false,
@@ -234,6 +303,7 @@
                     d.appliance =  $('#appliance').val();
                  }
             },
+            "deferRender": true,
             "fnInitComplete": function (oSettings, response) {
                $('input[type="search"]').attr("name", "search_value");           
             }        
