837a9b01aa1a90891a0865517499ab3bb7772def
sachinbit <sachinj@247around.com>
2017-11-08 10:50:44 +0530

    modified dashboard presentation

--- application/views/dashboard/dashboard_title.php
+++ application/views/dashboard/dashboard_title.php
@@ -1,6 +1,22 @@
-<?php foreach ($query as $key => $value) { ?>
-    <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-        <span class="count_top" style="display:block;min-height: 60px;"><?php echo $value['description'] ?></span>
-        <div class="count"><?php echo $data[$key][0]['count'] ?></div>
+<?php foreach ($data as $key => $value) { ?>
+    <div class="col-md-3 col-sm-6 col-xs-12 tile_stats_count">
+        <div class="count_top text-center"><strong><?php echo ucwords(str_replace("_", " ", $value['main_description'])); ?></strong></div>
+        <hr>
+        <?php if(isset($value['data']['query2'])){ ?>
+            <div class="col-md-6 col-sm-6 col-xs-12 sub_description1 text-center">
+        <?php }else{ ?>
+            <div class="col-md-12 col-sm-6 col-xs-12 sub_description1 text-center">
+        <?php } ?>
+            <div class="query_description"><?php echo ucwords(str_replace("_", " ", $value['data']['query1']['description'])); ?></div>
+            <div class="count text-center"><?php echo $value['data']['query1']['query_data']; ?></div>
+        </div>
+        <?php if(isset($value['data']['query2'])) { ?>
+        <div class="col-md-6 sub_description2 text-center">
+            <div class="query_description"><?php echo ucwords(str_replace("_", " ", $value['data']['query2']['description'])); ?></div>
+            <div class="count text-center"><?php echo $value['data']['query2']['query_data']; ?></div>
+        </div>
+        <?php } ?>
+        
+            </div>
     </div>
 <?php } ?>
