d0693fce24cc059609b43ac96ea58789e9408502
Abhay <abhaya@247around.com>
2017-12-01 12:45:04 +0530

    Add upcountry symbol at Partner spare pending page

--- application/views/partner/spare_parts_booking.php
+++ application/views/partner/spare_parts_booking.php
@@ -42,6 +42,9 @@
                                 <?php $sn_no1 = 1; foreach($spare_parts as $key =>$row){?>
                                 <tr style="text-align: center;">
                                     <td>
+                                        <?php if($row['is_upcountry'] == 1 && $row['upcountry_paid_by_customer'] == 0) { ?>
+                                        <i style="color:red; font-size:20px;" onclick="open_upcountry_model('<?php echo $row['booking_id'];?>', '<?php echo $row['amount_due'];?>')"
+                                       class="fa fa-road" aria-hidden="true"></i><?php } ?>
                                         <?php echo $sn_no1; ?>
                                     </td>
                                      <td>
@@ -154,6 +157,20 @@ $('body').popover({
       }
       
   }
+  
+  function open_upcountry_model(booking_id, amount_due){
+      
+       $.ajax({
+      type: 'POST',
+      url: '<?php echo base_url(); ?>employee/partner/booking_upcountry_details/'+ booking_id+"/"+amount_due,
+      success: function (data) {
+         // console.log(data);
+       $("#modal-content1").html(data);   
+       $('#myModal1').modal('toggle');
+    
+      }
+    });
+    }
 </script>
 
  <?php $this->session->unset_userdata('success'); ?>
