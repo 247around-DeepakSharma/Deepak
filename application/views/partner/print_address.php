7eb11004ee3e35a19e525c3e650eebb722d8f04a
Abhay <abhaya@247around.com>
2017-12-15 12:24:53 +0530

    print gst no in shippment address

--- application/views/partner/print_address.php
+++ application/views/partner/print_address.php
@@ -13,7 +13,8 @@
         <p style="max-width: 280px; word-wrap:break-word;margin-bottom: 1px;margin-top:1px;">C/o - <?php echo $value['owner_name'];?></p>
         <p style="max-width: 280px; word-wrap:break-word;margin-bottom: 1px;margin-top:2px;">Address - <?php echo $value['address']." ".$value['sc_district']." ".$value['state']." ". $value['pincode'];?></p>
         <p style="max-width: 280px; word-wrap:break-word;margin-bottom: 1px;margin-top:1px;">Phone - <?php echo $value['primary_contact_phone_1']." ". $value['primary_contact_phone_2'];?></p>
-        <p style="max-width: 280px; word-wrap:break-word;margin-bottom: 1px;margin-top:1px;">Job No- <?php echo $value['booking_id'];?></p>
+        <p style="max-width: 280px; word-wrap:break-word;margin-bottom: 1px;margin-top:1px;">Job No - <?php echo $value['booking_id'];?></p>
+        <p style="max-width: 280px; word-wrap:break-word;margin-bottom: 1px;margin-top:1px;">GST No - <?php echo $value['gst_no'];?></p>
     </div>
     <div style="float:right;margin-left: 5px;">
        <span>From: <?php echo $value['partner']['company_name'];?></span><br/>
