3d03bc0c30420b0ab893870a146d1e3dba5b6902
Abhay <abhaya@247around.com>
2017-12-23 10:56:57 +0530

    Bug: get dealer in partner edit booking page

--- application/controllers/employee/partner.php
+++ application/controllers/employee/partner.php
@@ -1281,6 +1281,7 @@ class Partner extends CI_Controller {
                     "search" => array(),
                     "order_by" => "");
                 $select = "dealer_details.dealer_id, dealer_name, dealer_phone_number_1";
+                $condition['length'] = -1;
                 $dealer_data = $this->dealer_model->get_dealer_mapping_details($condition, $select);
 
                 if (!empty($dealer_data)) {
