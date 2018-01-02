8d6f709aff11aa8b9d70f058a005a8d0ff09a543
chhavi <chhavid@247around.com>
2018-01-02 12:22:47 +0530

    #CRM-50 Escalation View For admin and change view for RM as well

--- application/views/dashboard/admin_dashboard.php
+++ application/views/dashboard/admin_dashboard.php
@@ -1,5 +1,9 @@
+<script src="<?php echo base_url(); ?>js/base_url.js"></script>
+<script src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+<script src="<?php echo base_url(); ?>js/buyback_app/app.js"></script>
+<script src="<?php echo base_url(); ?>js/buyback_app/controller/controllers.js"></script>
 <!-- page content -->
-<div class="right_col" role="main">
+<div class="right_col" role="main" ng-app="admin_dashboard">
     <!-- top tiles -->
     <div class="row tile_count" id="title_count">
         <div class="col-md-12">
@@ -25,6 +29,40 @@
         </div>
     </div>
     <!-- End Missing Pincode Section -->
+    <!-- Escalation Start-->
+    <div class="table-responsive" id="escalation_data" ng-controller="admin_escalationController">
+    <input type="text" id="session_id_holder" style="display:none;" value="<?php if($this->session->userdata('user_group') == 'regionalmanager') {echo $this->session->userdata('id');} ?>">
+   <button type="button"class="btn btn-default" style="float:right" data-toggle="tooltip"data-placement="left"title="To calculate escalation percentage, logic use current months booking and current month escalation ">?</button>
+    <button type="button" class="btn btn-info" ng-click="mytoggle=!mytoggle" id="order_by_toggal" onclick="change_toggal_text()"style="float:right">Sort By Number Of Escalation</button>
+    <form class="form-inline"style="float:left;background: #46b8da;color: #fff;padding: 3px;border-radius: 4px;">
+        <div class="form-group">
+            <input type="text" class="form-control" name="daterange" id="daterange_id" ng-change="daterangeloadFullRMView()" ng-model="dates">
+        </div>
+    </form>
+    <p>
+    <table class="table table-striped table-bordered jambo_table bulk_action">
+    <thead>
+      <tr>
+        <th>S.no</th>
+        <th>Vendor</th>
+        <th>Total Booking</th>
+        <th>Total Escalation</th>
+        <th>Escalation %</th>
+      </tr>
+    </thead>
+    <tbody>
+     <tr ng-repeat="y in escalationAllRMData  |orderBy:!mytoggle?'-esclation_per':'-total_escalation'">
+        <td>{{$index+1}}</td>
+        <td><a type="button" id="vendor_{{y.vendor_id}}" class="btn btn-info" target="_blank" href="<?php echo base_url(); ?>employee/dashboard/escalation_full_view/{{y.rm_id}}/{{y.startDate}}/{{y.endDate}}">{{y.rm_name}}</a></td>
+        <td>{{y.total_booking}}</td>
+        <td>{{y.total_escalation}}</td>
+         <td>{{y.esclation_per}}%</td>
+      </tr>
+    </tbody>
+    </table>
+    <center><img id="loader_gif_escalation" src="<?php echo base_url(); ?>images/loadring.gif" ></center>
+                    </div>
+    <!-- Escalation End-->
     <!-- Partner Booking Status -->
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
@@ -840,3 +878,29 @@
     
     
 </script>
+<script>
+     function change_toggal_text(){
+        var currentValue = document.getElementById("order_by_toggal").innerHTML;
+        if(currentValue =="Sort By Number Of Escalation"){
+            document.getElementById("order_by_toggal").innerHTML="Sort BY perentage";
+        }
+        else{
+             document.getElementById("order_by_toggal").innerHTML="Sort By Number Of Escalation";
+        }
+    }
+$(function() {
+        var d = new Date();
+        n = d.getMonth()+1;
+        y = d.getFullYear();
+        date = d.getDate();
+    $('input[name="daterange"]').daterangepicker({
+        timePicker: true,
+        timePickerIncrement: 30,
+        locale: {
+            format: 'YYYY-MM-DD'
+        },
+        startDate: y+'-'+n+'-01'
+    });
+});
+    </script>
+
