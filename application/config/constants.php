fd7b2f45d862f2f3780d8bddc51225de51237bc4
Abhay <abhaya@247around.com>
2018-01-08 18:28:38 +0530

    . #CRM-86 lacklist Brand should not allow to insert booking from API

--- application/config/constants.php
+++ application/config/constants.php
@@ -318,6 +318,7 @@ define('ERR_INVALID_INSTALLATION_TIMESLOT_CODE', -1014);
 define('ERR_INVALID_PARTNER_NAME_CODE', -1015);
 define('ERR_INVALID_JSON_INPUT_CODE', -1016);
 define('ERR_INVALID_PRODUCT_TYPE_CODE', -1017);
+define('ERR_INVALID_BRAND_CODE', -1021);
 
 define('ERR_BOOKING_NOT_INSERTED_MSG', 'Booking Insertion Failed');
 define('ERR_GENERIC_ERROR_MSG', 'Unknown Error');
@@ -338,6 +339,7 @@ define('ERR_INVALID_INSTALLATION_TIMESLOT_MSG', 'Invalid Installation Timeslot')
 define('ERR_INVALID_PARTNER_NAME_MSG', 'Invalid Partner Name');
 define('ERR_INVALID_JSON_INPUT_MSG', 'Invalid JSON Input');
 define('ERR_INVALID_PRODUCT_TYPE_MSG', 'DENIED BY VENDOR');
+define('ERR_INVALID_BRAND_TYPE_MSG', 'Invalid Brand');
 
 //ICICI Bank name constant
 define('ICICI_BANK_NAME', 'ICICI Bank Ltd');
