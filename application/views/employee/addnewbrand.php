6d5bd867063962f46f74eef7760a3d625a736c71
chhavi <chhavid@247around.com>
2018-01-12 17:40:56 +0530

    #CRM-65 Add new Brands Hint

--- application/views/employee/addnewbrand.php
+++ application/views/employee/addnewbrand.php
@@ -17,7 +17,7 @@
         		<td><?php echo $count++;?>.</td>
         		<td width="200px;">
         			<select type="text" class="form-control"  id="<?php echo 'service_'.$i ?>" name="new_brand[]" 
-        				value="<?php echo set_value('new_brand'); ?>" onchange="assign(this.value)">
+        				value="<?php echo set_value('new_brand'); ?>" onchange="get_hint(<?php echo $i ?>)">
                                     <option selected disabled>Select</option>
         				<?php foreach($services as $key => $values) {?>
         			  	<option  value=<?=$values->id;?>>
@@ -50,22 +50,21 @@
     }
     var allBrands;
     var brand_value;
+    var hintArray = [];
     function getBrandHintArray(){
-        var arr = [];
         for(var i=0;i<window.allBrands.length;i++){
               var allBrandValue = window.allBrands[i].toLowerCase();
               var currentBrandValue = window.brand_value.toLowerCase();
               var index = allBrandValue.indexOf(currentBrandValue);
-            if(index == 0){
-                arr.push(window.allBrands[i]);         
+            if(index === 0){
+                window.hintArray.push(window.allBrands[i]);         
             }
         }
-        return arr;
     }
-    function createHintsList(hintArray,row_id){
+    function createHintsList(row_id){
         var hintString = '';
-        for(var i=0;i<hintArray.length;i++){
-            var hintString = hintString+hintArray[i];
+        for(var i=0;i< window.hintArray.length;i++){
+            var hintString = hintString+ window.hintArray[i];
             var hintString = hintString+"<br>";
         }
         if(hintString){
@@ -76,30 +75,26 @@
             document.getElementById("show_hInt_"+row_id).style.display = 'none';
         }
     }
-        function show_hint(row_id) {
-           serviceID = $("#service_"+row_id).val();
-            if(serviceID){
-                window.brand_value = $("#brands_"+row_id).val();
-                if(window.brand_value.length === 1){
-                    var data = {};
+    function get_hint(row_id){
+            serviceID = $("#service_"+row_id).val();
+            var data = {};
                     url =  '<?php echo base_url(); ?>employee/booking/get_all_brands/'+serviceID;
                     post_request = "GET";
                      sendAjaxRequest(data,url,post_request).done(function(response){
                           window.allBrands = JSON.parse(response);
-                                var hintArray = getBrandHintArray();
-                                createHintsList(hintArray,row_id);
                         });
-                }
-                else{
-                    var hintArray = getBrandHintArray();
-                    createHintsList(hintArray,row_id);
-                }
-            }
+        }
+        function show_hint(row_id) {
+              window.hintArray = [];
+              window.brand_value = $("#brands_"+row_id).val();
+              getBrandHintArray();
+              createHintsList(row_id);
         }
         function remove_hint(){
-        var elements = document.getElementsByClassName('show_hint');
-        for(var t=0; t<elements.length; t++) { 
-          elements[t].style.display='none';
+            var elements = document.getElementsByClassName('show_hint');
+            for(var t=0; t<elements.length; t++) { 
+              elements[t].style.display='none';
+            }
         }
-        }
+        
     </script>
