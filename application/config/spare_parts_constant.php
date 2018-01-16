350b47b9051b35834205bf945fbbf7cb46d0b7ae
Abhay <abhaya@247around.com>
2018-01-16 17:45:32 +0530

    separate Spare Parts Constant File

---
+++ application/config/spare_parts_constant.php
@@ -0,0 +1,34 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+define("SPARE_HSN_CODE", "85299090");
+define('SPARE_INVOICE_EMAIL_TAG','spare_invoice_sent');
+define('SEND_DEFECTIVE_SPARE_PARTS_NOTIFICATION',7 );
+define("SPARE_PART_RADIO_BUTTON_NOT_REQUIRED", "0"); 
+define("ESTIMATE_APPROVED_BY_CUSTOMER", "Spare Estimate Approved By Customer");
+define("REPAIR_OOW_TAG", "Repair - Out Of Warranty");
+define("REPAIR_IN_WARRANTY_TAG", "Repair - In Warranty");
+define('SPARE_OOW_EST_REQUESTED','Request Quote for Spare Part');
+define('SPARE_OOW_EST_GIVEN','Spare Estimate Cost Given');
+define('SPARE_OOW_EST_MARGIN','0.25');
+define('REPAIR_OOW_PARTS_PRICE_TAGS','Spare Parts');
+define('REPAIR_OOW_VENDOR_PERCENTAGE','10');
+define('REPAIR_OOW_AROUND_PERCENTAGE','0.15');
+define('SPARE_PARTS_REQUIRED', 'Spare Parts Required');
+define('SPARE_PARTS_REQUESTED', 'Spare Parts Requested');
+// Defective Parts pending
+define('DEFECTIVE_PARTS_PENDING','Defective Part Pending');
+// Defective Parts Shipped by SF
+define('DEFECTIVE_PARTS_SHIPPED','Defective Part Shipped By SF');
+// Defective Parts Received by SF
+define('DEFECTIVE_PARTS_RECEIVED','Defective Part Received By Partner');
+// Defective Parts Received by SF
+define('DEFECTIVE_PARTS_REJECTED','Defective Part Rejected By Partner');
+//Zopper booking- Part Arrange By Same Vendor
+define("PART_ARRANGE_BY_SAME_VENDOR", 2);
+//Zopper booking- Part Arrange By Different Vendor
+define("PART_ARRANGE_BY_DIFF_VENDOR", 1);
+//Spare Parts Received By SF
+define('SPARE_PARTS_DELIVERED', 'Spare Parts Delivered to SF');
+//Spare Parts Shipped By SF
+define('SPARE_PARTS_SHIPPED', 'Spare Parts Shipped by Partner');
+// Spare Reschedule 
+define('SPARE_RESCHEDULED', 'Spare_Rescheduled');
