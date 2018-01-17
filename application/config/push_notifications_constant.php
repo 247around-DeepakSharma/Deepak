cd48e5407db229b4394dba90d8b4f740f765b93a
chhavi <chhavid@247around.com>
2018-01-17 14:38:45 +0530

    #CRM-110 Send Push Notification After We Reject the complete Booking(Review Page)

--- application/config/push_notifications_constant.php
+++ application/config/push_notifications_constant.php
@@ -1,3 +1,4 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 define('CUSTOMER_UPDATE_BOOKING_PUSH_NOTIFICATION_EMPLOYEE_TAG','sf_updates_the_booking');
 define('BOOKING_ASSIGN_TO_VENDOR','booking_assign_to_sf');
+define('BOOKING_REJECTED_BY_247AROUND','booking_rejected_by_247Around');
