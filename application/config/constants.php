f8036a6a4ca3b5d31c16a8d29b053fba0b026b1e
Abhay <abhaya@247around.com>
2018-01-17 12:10:22 +0530

    Remove duplicate constant

--- application/config/constants.php
+++ application/config/constants.php
@@ -492,9 +492,6 @@ define("NO_OF_DAYS_NOT_SHOW_NOT_RECEIVED_BUTTON", -3);
 
 define("AUTO_ACK_ADMIN_REMARKS", "Auto Acknowledge");
 
-
-define("SPARE_PART_RADIO_BUTTON_NOT_REQUIRED", "0"); 
-define("ESTIMATE_APPROVED_BY_CUSTOMER", "Spare Estimate Approved By Customer");
 //missing_pincode_email
 define("MISSING_PINCODE_EMAIL_TO", "ayush.bafna@paytmmall.com, sales@247around.com, rohit1.verma@paytm.com, simranpreet.sawhney@paytm.com, ankur.sethi@paytm.com, shivam.awasthi@paytm.com, nits@247around.com, ashish.singh@paytm.com, simranpreet.sawhney@paytmmall.com, aditya1.agrawal@paytmmall.com");
 
