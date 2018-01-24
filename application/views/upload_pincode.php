a8893f2a674204946afd965833f7056386a2d91c
Anuj Aggarwal <anuj@247around.com>
2018-01-24 21:34:54 +0530

    Minor changes

--- application/views/upload_pincode.php
+++ application/views/upload_pincode.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>247Around</title>
+        <title>Pincode Distance Calculator</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- include jQuery -->
@@ -65,6 +65,8 @@
         <div class="container">
             <div class="row">
                 <h3>Upload Pincode File To Get Distance<img style='float: right;width:268px;' src="<?php echo base_url();?>images/powered-by-google.png"></h3>
+                <br/>
+                <br/>
                 <form id="myform" method="post">
 
                     <div class="form-group">
