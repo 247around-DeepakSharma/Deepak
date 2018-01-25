2de8a599069c7fda0444ffeca7070418470008f4
chhavi <chhavid@247around.com>
2018-01-25 14:38:17 +0530

    CRM-60 Auto Hide Notifications

--- application/controllers/employee/do_background_process.php
+++ application/controllers/employee/do_background_process.php
@@ -296,6 +296,7 @@ class Do_background_process extends CI_Controller {
      */
     function send_asyn_push_notification(){
         $title = $msg = $url = $notification_type = $subscriberArray = NULL;
+         $auto_hide =0;
          if($this->input->post('title')){
             $title = $this->input->post('title');
         }
@@ -311,7 +312,10 @@ class Do_background_process extends CI_Controller {
         if($this->input->post('subscriberArray')){
             $subscriberArray = $this->input->post('subscriberArray');
         }
-        $this->push_notification_lib->send_push_notification($title,$msg,$url,$notification_type,$subscriberArray);
+        if($this->input->post('auto_hide')){
+            $auto_hide = $this->input->post('auto_hide');
+        }
+        $this->push_notification_lib->send_push_notification($title,$msg,$url,$notification_type,$subscriberArray,$auto_hide);
     }
 
     /* end controller */
