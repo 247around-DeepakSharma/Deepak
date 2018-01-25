c4aa88c0c2ecdb9b0528b13a42a922bec4234bac
sachinbit <sachinj@247around.com>
2018-01-25 12:35:45 +0530

    #CRM-53only admin and developer can open 1 month old cancelled bookings/queries

---
+++ application/views/partner/partner_footer.php
@@ -0,0 +1,21 @@
+<!-- footer content -->
+<footer style="margin-left: 0px;">
+    <div class="pull-right">
+        <a href="#">247AROUND</a>
+    </div>
+    <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+
+<!-- JS -->
+<script src="<?php echo base_url() ?>js/bootstrap.min.js"></script>
+<!-- bootstrap-progressbar -->
+<script src="<?php echo base_url() ?>js/bootstrap-progressbar.min.js"></script>  
+<!-- Custom Theme Scripts -->
+<script src="<?php echo base_url() ?>js/dashboard_custom.js"></script>
+<!-- Datatable JS-->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs/jszip-2.5.0/dt-1.10.16/af-2.2.2/b-1.5.1/b-colvis-1.5.1/b-flash-1.5.1/b-html5-1.5.1/b-print-1.5.1/cr-1.4.1/fc-3.2.4/fh-3.1.3/kt-2.3.2/r-2.2.1/rr-1.2.3/sc-1.4.3/sl-1.2.4/datatables.min.js"></script>
+</body>
+</html>
