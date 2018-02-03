2d525711ed8a29ce55aba9c017861f85a70131d2
Abhay <abhaya@247around.com>
2018-02-03 11:45:56 +0530

    Change Email tag for Advance Receipt/ Sweetener/ CRM -Setup voucher #CRM-217

---
+++ application/config/email_tag_constant.php
@@ -0,0 +1,26 @@
+<?php
+
+//invoice email tag
+define('PARTNER_INVOICE_DETAILED_EMAIL_TAG','partner_invoice_detailed');
+
+define('PARTNER_RECEIPT_VOUCHER_EMAIL_TAG','partner_receipt_voucher');
+
+define('CASH_DETAILS_INVOICE_FOR_VENDORS_EMAIL_TAG','cash_details_invoices_for_vendors');
+
+define("BUYBACK_DETAILS_INVOICE_FOR_VENDORS_EMAIL_TAG", "buyback_details_invoices_for_vendors");
+
+define('FOC_DETAILS_INVOICE_FOR_VENDORS_EMAIL_TAG','foc_details_invoices_for_vendors');
+
+define('NEGATIVE_FOC_INVOICE_FOR_VENDORS_EMAIL_TAG','negative_foc_invoice_for_vendors');
+
+define('BRACKETS_INVOICE_EMAIL_TAG','send_brackets_invoice_mail');
+
+define('DRAFT_BRACKETS_INVOICE_EMAIL_TAG','send_draft_brackets_invoice_mail');
+
+define('CRM_SETUP_INVOICE_EMAIL_TAG','crm_setup_invoice');
+
+define('BRACKETS_CREDIT_NOTE_INVOICE_EMAIL_TAG','brackets_credit_note_invoice');
+
+define('ADVANCE_RECEIPT_EMAIL_TAG','advance_receipt');
+
+define('SWEETENER_INVOCIE_EMAIL_TAG','sweetener_invoice');
