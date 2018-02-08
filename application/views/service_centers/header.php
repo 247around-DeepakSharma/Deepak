1262b1135749d2c6af32459e157b029c5e7d29be
chhavi <chhavid@247around.com>
2018-02-08 13:13:59 +0530

    navigation center on service Center Header

--- application/views/service_centers/header.php
+++ application/views/service_centers/header.php
@@ -208,6 +208,12 @@
                                 </li>
                             </ul>
                         </li>
+                        <li><div class="dropdown" style="float:right;margin: 15px 14px 0px 0px;">
+                                    <a class=" dropdown-toggle fa fa-bell" id="notification_holder"  data-toggle="dropdown" onclick="get_notifications(<?php echo $this->session->userdata('service_center_id'); ?>,'vendor')"></a>
+                                    <ul class="dropdown-menu" role="menu" aria-labelledby="notification_holder" id="notification_container" style="padding-top: 0px;margin-top: 32px;border: 1px solid #2c9d9c;"> 
+                                    <center><img id="loader_gif_escalation" src="<?php echo base_url(); ?>images/loadring.gif" ></center>
+                                    </ul>
+  </div></li>
                         <li><a href="<?php echo base_url()?>employee/service_centers/logout"><i class="fa fa-fw fa-power-off"></i></a></li>
                     </ul>
                 </div>
@@ -262,3 +268,46 @@ function showConfirmDialougeBox(url){
 }
 
 </script>
+<style>
+.nav .open>a, .nav .open>a:focus, .nav .open>a:hover {
+    background-color: #2c9d9c;
+    color: #fff;
+}
+    .navigation_li{
+    font: normal 16px/16px Century Gothic;
+    list-style: none;
+    padding: 15px 0px 6px 42px;
+    }
+    .navigation_li a:hover {
+    background: none;
+    color:#fff;
+}
+.navigation_li>li>a:focus, .dropdown-menu>li>a:hover {
+    color: #262626;
+    text-decoration: none;
+    background-color: transparent;
+}
+.normal{
+    background: url(<?php echo base_url() ?>/images/logo_small.png) 7px 3px no-repeat;
+}    
+.important{
+    background: url(<?php echo base_url() ?>/images/i.png) 7px 3px no-repeat;
+}
+.no_new_notification{
+    color: #000;
+        text-align: center;
+    padding: 3px;
+}
+            </style>
+            <script>
+                function get_notifications(entity_id,entity_type){
+                    $.ajax({
+                    type: 'POST',
+                    url: '<?php echo base_url(); ?>push_notification/get_notifications',
+                    data: {entity_id: entity_id,entity_type: entity_type},
+                    success: function (response) {
+                        $("#notification_container").html(response);
+                    }
+                    });
+                }
+                </script>
