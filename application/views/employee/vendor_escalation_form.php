be4b9d611dd3df4ee1a077fbab08c6686c1aee22
sachinbit <sachinj@247around.com>
2018-02-10 15:45:37 +0530

    #CRM-252 fix mutliple click on escalate booking form

--- application/views/employee/vendor_escalation_form.php
+++ application/views/employee/vendor_escalation_form.php
@@ -25,35 +25,35 @@
                      <div class="row">
                         <div class="col-md-8">
 			    <form class="form-horizontal" action="<?php echo base_url() ?>employee/vendor/process_vendor_escalation_form" method="POST" >
-                           <div class="form-group">
-                              <label for="Booking Id" class="col-md-2">Booking Id</label>
-                              <div class="col-md-6">
-                                 <input type="text" class="form-control"  name="booking_id"  value = "<?php echo $booking_id;?>" placeholder = "Booking Id"  readonly>
-                              </div>
-                           </div>
-                           <div class="form-group ">
-                              <label for="Vendor Id" class="col-md-2">Vendor Name</label>
-                              <div class="col-md-6">
-                                 <input type="text" class="form-control"  name="vendor_name"  value = "<?php echo  $vendor_details[0]['name']; ?>"  disabled>
-                                 <input type="hidden" class="form-control"  name="vendor_id"  value = "<?php echo  $vendor_details[0]['id']; ?>" >
-                              </div>
-                           </div>
-                           <div class="form-group  <?php if( form_error('escalation_reason_id') ) { echo 'has-error';} ?>">
-                              <label for="Service" class="col-md-2">Reason</label>
-                              <div class="col-md-6">
-                                 <select class=" form-control" name ="escalation_reason_id" >
-                                    <option selected disabled>----------- Select Reason ------------</option>
-                                    <?php 
-                                       foreach ($escalation_reason as $reason) {     
-                                       ?>
-                                    <option value = "<?php echo $reason['id']?>">
-                                       <?php echo $reason['escalation_reason'];?>
-                                    </option>
-                                    <?php } ?>
-                                 </select>
-                                 <?php echo form_error('escalation_reason_id'); ?>
-                              </div>
-                           </div>
+                                <div class="form-group">
+                                   <label for="booking_id" class="col-md-2">Booking Id</label>
+                                   <div class="col-md-6">
+                                       <input type="text" class="form-control"  id="booking_id" name="booking_id"  value = "<?php echo $booking_id;?>" placeholder = "Booking Id"  readonly>
+                                   </div>
+                                </div>
+                                <div class="form-group ">
+                                   <label for="vendor_name" class="col-md-2">Vendor Name</label>
+                                   <div class="col-md-6">
+                                       <input type="text" class="form-control"  id="vendor_name" name="vendor_name"  value = "<?php echo  $vendor_details[0]['name']; ?>"  disabled>
+                                      <input type="hidden" class="form-control"  name="vendor_id"  value = "<?php echo  $vendor_details[0]['id']; ?>" >
+                                   </div>
+                                </div>
+                                <div class="form-group  <?php if( form_error('escalation_reason_id') ) { echo 'has-error';} ?>">
+                                   <label for="Service" class="col-md-2">Reason</label>
+                                   <div class="col-md-6">
+                                      <select class=" form-control" name ="escalation_reason_id" >
+                                         <option selected disabled>----------- Select Reason ------------</option>
+                                         <?php 
+                                            foreach ($escalation_reason as $reason) {     
+                                            ?>
+                                         <option value = "<?php echo $reason['id']?>">
+                                            <?php echo $reason['escalation_reason'];?>
+                                         </option>
+                                         <?php } ?>
+                                      </select>
+                                      <?php echo form_error('escalation_reason_id'); ?>
+                                   </div>
+                                </div>
                                 <div class="form-group ">
                                     <label for="remarks" class="col-md-2">Remarks</label>
                                     <div class="col-md-6">
@@ -61,12 +61,12 @@
                                     </div>
                                 </div>    
                            
-                           <div class="form-group ">
-                              
-                                 <input type= "submit"  class=" col-md-3 col-md-offset-4 btn btn-primary btn-md" value ="Save">
-                             
-                           </div>
-                          
+                                <div class="form-group ">
+
+                                      <input type= "submit"  class=" col-md-3 col-md-offset-4 btn btn-primary btn-md" value ="Save" onclick="this.disabled=true;this.value='Submitting...'; this.form.submit();">
+
+                                </div>
+                            </form>
                         </div>
                      </div>
                   </div>
