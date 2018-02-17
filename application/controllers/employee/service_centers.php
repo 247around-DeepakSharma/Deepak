e3fad2054f16aba9b9f57964a0d60347e215e4ef
Abhay <abhaya@247around.com>
2018-02-17 10:29:15 +0530

    SF Panel completed page  #CRM-286 #287
    Penalty being shown on ALL bookings on SF CRM
    
    Serial number not proper on completed bookings on SF CRM

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -564,6 +564,7 @@ class Service_centers extends CI_Controller {
                 $bookings[$key]['penalty'] = $res['penalty'];
             }
         }
+        $data['serial_number'] = $offset;
         $data['bookings'] = $bookings;
         $data['status'] = "Completed";
 
