2c55d3c0ffb6045addd693ded485041d03fb13c8
chhavi <chhavid@247around.com>
2018-02-19 15:40:16 +0530

    #CRM-306 Remove view History from service center listing

--- application/views/employee/viewvendor.php
+++ application/views/employee/viewvendor.php
@@ -114,7 +114,6 @@
           	<th class="jumbotron">Permanent</th>
                 <th class="jumbotron">Add Pin Code</th>
                 <th class="jumbotron">Resend Login Details</th>
-                <th class="jumbotron">View History</th>
                 <?php if(isset($push_notification)){ ?>
                 <th class="jumbotron">Notifications</th>
                 <?php }?>
@@ -177,7 +176,7 @@
             </td>
             <td><button type="button" class="btn btn-small btn-success" id="<?php echo $row['id']; ?>" data-toggle="modal" data-target="#pin_code" onclick="createPinCodeForm(this.id,<?php echo "'".$row['name']."'"  ?>)">Pin Code</button></td>
             <td><a class="btn btn-warning" href="<?php echo base_url();?>employee/vendor/resend_login_details/vendor/<?php echo $row['id']?>">Resend Login Details</a></td>
-            <td>  <button type="button" class="btn btn-info btn-lg fa fa-eye" data-toggle="modal" data-target="#history_view" onclick="get_history_view(<?php echo $row['id']?>)" style="padding: 11px 6px;margin: 0px 10px;"></button></td>
+<!--            <td>  <button type="button" class="btn btn-info btn-lg fa fa-eye" data-toggle="modal" data-target="#history_view" onclick="get_history_view(<?php echo $row['id']?>)" style="padding: 11px 6px;margin: 0px 10px;"></button></td>-->
           <?php if(isset($push_notification)){ ?>
           <td align="center">
               <?php 
