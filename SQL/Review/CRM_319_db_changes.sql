01f28ca9f43688ade4abf970709186c79d7b67c2
chhavi <chhavid@247around.com>
2018-02-27 18:14:35 +0530

    #CRM-318 Database

--- SQL/Review/CRM_319_db_changes.sql
+++ SQL/Review/CRM_319_db_changes.sql
@@ -5,12 +5,14 @@ CREATE TABLE `paytm_transaction_callback` (
   `order_id` varchar(50) NOT NULL,
   `txn_id` varchar(200) NOT NULL,
   `paid_amount` varchar(20) DEFAULT NULL,
-  `user_guid` varchar(200) NOT NULL,
-  `refund_amount` varchar(20) NOT NULL,
-  `is_cashback` int(2) NOT NULL DEFAULT '0',
+  `user_guid` varchar(100) NOT NULL,
+  `cashback_amount` varchar(20) NOT NULL,
+  `cashback_txn_id` varchar(200) DEFAULT NULL,
+  `cashback_from` varchar(100) NOT NULL,
   `cash_back_status` varchar(100) DEFAULT NULL,
   `cash_back_message` text,
-  `create_date` datetime NOT NULL
+  `create_date` datetime NOT NULL,
+  `cashback_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -21,7 +23,9 @@ CREATE TABLE `paytm_transaction_callback` (
 -- Indexes for table `paytm_transaction_callback`
 --
 ALTER TABLE `paytm_transaction_callback`
-  ADD PRIMARY KEY (`id`,`user_guid`,`refund_amount`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `order_id` (`order_id`),
+  ADD UNIQUE KEY `txn_id` (`txn_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -31,4 +35,4 @@ ALTER TABLE `paytm_transaction_callback`
 -- AUTO_INCREMENT for table `paytm_transaction_callback`
 --
 ALTER TABLE `paytm_transaction_callback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
