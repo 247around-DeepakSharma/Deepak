4ec4229b6e58b81c7cc6998fd7a80f0c238f74cb
chhavi <chhavid@247around.com>
2018-02-27 15:08:29 +0530

    #CRM-318 Add true in log print_r

--- application/libraries/paytm_payment_lib.php
+++ application/libraries/paytm_payment_lib.php
@@ -373,7 +373,7 @@ class paytm_payment_lib {
      * @output - structured response Array
      */
     function CHECKSTATUS_checkstatus_success_handler($responseArray){
-        log_message('info', __FUNCTION__ . " Function Start With  ".print_r($responseArray));
+        log_message('info', __FUNCTION__ . " Function Start With  ".print_r($responseArray,true));
         $transactionsArray = $responseArray['response']['txnList'];
         foreach ($transactionsArray as $transaction){
             $orderID = $transaction['merchantOrderId'];
@@ -390,7 +390,7 @@ class paytm_payment_lib {
             // Save data into transaction table
             $this->P_P->reusable_model-> insert_into_table("paytm_transaction_callback",$data);
         }
-        log_message('info', __FUNCTION__ . " Function End With  ".print_r($data));
+        log_message('info', __FUNCTION__ . " Function End With  ".print_r($data,true));
         return $data;
     }
     /*
