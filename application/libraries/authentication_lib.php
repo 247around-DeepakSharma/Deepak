b439e637c0a1b3954a299c7727174ee6da2354b7
chhavi <chhavid@247around.com>
2018-02-27 11:19:26 +0530

    #CRM-318 Authentication Error handling

--- application/libraries/authentication_lib.php
+++ application/libraries/authentication_lib.php
@@ -8,16 +8,23 @@ class authentication_lib {
    function checkAPIAuthentication() {
         log_message('info', __FUNCTION__ . "=> Entering ");
         $h = $this->getallheaders();
-        if (empty($h)) {
+        if (empty($h)) 
+        {
             log_message('info', __METHOD__ . ":: Empty Header: ");
             return array(false,$h,ERR_GENERIC_ERROR_CODE, ERR_GENERIC_ERROR_MSG);
-        } else {
+        } 
+        else {
+            if(array_key_exists("Mid", $h)){
                 if($h['Mid'] == MERCHANT_GUID){
                     return array(true,$h);  
                 }
                 else {
                      return array(false,$h,ERR_INVALID_MERCHANT_GUID, ERR_INVALID_MERCHANT_GUID_MSG);
                 }
+        }
+        else{
+            return array(false,$h,ERR_INVALID_MERCHANT_GUID, MID_NOT_AVAILABLE_MSG);
+        }
         }
     }
     function getallheaders() {
