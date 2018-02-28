834e4e517c389d31676d5a1e824b520dcc2f50ef
Abhay <abhaya@247around.com>
2018-02-28 17:17:29 +0530

    enable tiny url api

--- application/libraries/Miscelleneous.php
+++ application/libraries/Miscelleneous.php
@@ -2368,37 +2368,37 @@ function generate_image($base64, $image_name,$directory){
     }
     
     function getShortUrl($url) {
-        //return file_get_contents("http://tinyurl.com/api-create.php?url=".$url);
+        return file_get_contents("http://tinyurl.com/api-create.php?url=".$url);
 
-        $apiKey = 'AIzaSyAnx_YB-6oaNhyZPNJTpJqGKq2XHFbUOO8';
-
-        $postData = array('longUrl' => $url);
-        $jsonData = json_encode($postData);
-
-        //4
-        $curlObj = curl_init();
-        curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?key=' . $apiKey);
-        curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($curlObj, CURLOPT_HEADER, 0);
-        curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
-        curl_setopt($curlObj, CURLOPT_POST, 1);
-        curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
-
-        //5
-        $response = curl_exec($curlObj);
-
-        $json = json_decode($response);
-
-        curl_close($curlObj);
-
-        if (isset($json->error)) {
-           
-            log_message("info". __METHOD__. " Short url not generated ".$json->error->message);
-            return false;
-        } else {
-            return $json->id;
-        }
+//        $apiKey = 'AIzaSyAnx_YB-6oaNhyZPNJTpJqGKq2XHFbUOO8';
+//
+//        $postData = array('longUrl' => $url);
+//        $jsonData = json_encode($postData);
+//
+//        //4
+//        $curlObj = curl_init();
+//        curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?key=' . $apiKey);
+//        curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+//        curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+//        curl_setopt($curlObj, CURLOPT_HEADER, 0);
+//        curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+//        curl_setopt($curlObj, CURLOPT_POST, 1);
+//        curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
+//
+//        //5
+//        $response = curl_exec($curlObj);
+//
+//        $json = json_decode($response);
+//
+//        curl_close($curlObj);
+//
+//        if (isset($json->error)) {
+//           
+//            log_message("info". __METHOD__. " Short url not generated ".$json->error->message);
+//            return false;
+//        } else {
+//            return $json->id;
+//        }
     }
 
 }
