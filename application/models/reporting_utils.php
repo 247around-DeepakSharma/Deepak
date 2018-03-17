df7d87f3eb761aa6e28a6dcad19ff544deb5b48e
sachinbit <sachinj@247around.com>
2018-03-17 10:40:58 +0530

    #CRM-381  save file on server and use it for revert back file to partner

--- application/models/reporting_utils.php
+++ application/models/reporting_utils.php
@@ -1783,7 +1783,7 @@ function get_booking_by_service_center_query_data($where,$groupBY){
       
        $sql = "SELECT e.full_name as agent_name,p.file_name,p.create_date AS upload_date,p.result FROM file_uploads AS p  left JOIN employee AS e ON p.agent_id = e.id";
         if(!empty($post_data)){
-             $sql .=  " WHERE file_type= '".$post_data['file_type']."' ORDER BY p.create_date DESC";
+             $sql .=  " WHERE file_type = '".trim($post_data['file_type'])."' ORDER BY p.create_date DESC";
         }
         if(($post_data['start'] !== NULL)  && ($post_data['length'] !== NULL)){
             $sql .=" LIMIT ".$post_data['start'].",".$post_data['length'];
