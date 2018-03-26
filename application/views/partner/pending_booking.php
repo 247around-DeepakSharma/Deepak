1371f198dc4fa6aad178722e62b51e21275bf9db
sachinbit <sachinj@247around.com>
2018-03-26 16:13:07 +0530

    #CRM-468

--- application/views/partner/pending_booking.php
+++ application/views/partner/pending_booking.php
@@ -115,6 +115,13 @@
         </div>
     </div>
 </div>
+        <script>
+            $(document).on("click", ".open-AddBookDialog", function () {
+                var myBookId = $(this).data('id');
+                $(".modal-body #ec_booking_id").val( myBookId );
+
+            });
+        </script>
 <?php if(empty($is_ajax)) { ?> 
     </div>
     
