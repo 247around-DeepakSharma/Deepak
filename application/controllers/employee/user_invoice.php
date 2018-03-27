43114754bac7268ace7251ce79c7ed472cee6923
Abhay <abhaya@247around.com>
2018-03-27 17:11:17 +0530

    Generate Sf paytm payment voucher #CRM-483

--- application/controllers/employee/user_invoice.php
+++ application/controllers/employee/user_invoice.php
@@ -220,6 +220,122 @@ class User_invoice extends CI_Controller {
             
         return true;
     }
+    /**
+     * @desc This function is used to generate sf credit note.
+     * When customer paid through paytm and we will get tarnsaction callback
+     * @param String $booking_id
+     * @param String $amountPaid
+     * @param String $txnID
+     * @param String $agent_id
+     */
+    function sf_payment_creditnote($booking_id, $amountPaid, $txnID, $agent_id){
+        log_message("info",__METHOD__. " Enering .. for booking id ". $booking_id. " amount paid ".$amountPaid." Txn ID ". $txnID. " agent ID ".$agent_id);
+        $select = "service_centres.company_name, service_centres.address as sf_address, "
+                . "service_centres.pincode as sf_pincode, service_centres.district as sf_district, service_centres.state as sf_state, service_centres.gst_no, service_centres.owner_phone_1, "
+                . "users.name, users.home_address, users.phone_number,users.user_email, users.pincode, users.city, users.state, booking_details.amount_due, "
+                . "booking_details.amount_paid, booking_details.quantity, request_type, services, booking_details.quantity, booking_primary_contact_no,  "
+                . "sc_code, booking_details.user_id, booking_details.closed_date, booking_details.assigned_vendor_id, owner_email, primary_contact_email";
+        $request['where'] = array("booking_details.booking_id" => $booking_id);
+        $request['length'] = -1;
+        $data = $this->booking_model->get_bookings_by_status($request, $select);
+        if(!empty($data)){
+            $invoice = array();
+            
+            $invoice_id = $txnID;
+            $prod =  $data[0]->services . " (" . $data[0]->request_type . ") ";
+            $invoice[0]['description'] = $prod;
+            $invoice[0]['taxable_value'] = $amountPaid;
+            
+            $invoice[0]['product_or_services'] = "Service";
+            $invoice[0]['gst_number'] = "";
+            $invoice[0]['company_name'] = $data[0]->company_name;
+            $invoice[0]['company_address'] = $data[0]->sf_address;
+            $invoice[0]['district'] = $data[0]->sf_district;
+            $invoice[0]['pincode'] = $data[0]->sf_pincode;
+            $invoice[0]['state'] = $data[0]->sf_state;
+            $invoice[0]['rate'] = "";
+            $invoice[0]['qty'] = $data[0]->quantity;
+            //As Aditya, No need to add hsn code
+            $invoice[0]['hsn_code'] = "";
+
+            $sd = $ed = $invoice_date = date("Y-m-d");
+            $response = $this->invoices_model->_set_partner_excel_invoice_data($invoice, $sd, $ed, "Payment Voucher", $invoice_date);
+            $response['meta']['customer_name'] = $data[0]->name;
+            $response['meta']['customer_address'] = $data[0]->home_address . ", " . $data[0]->city . ", Pincode - " . $data[0]->pincode . ", " . $data[0]->state;
+            $response['meta']['customer_phone_number'] = $data[0]->booking_primary_contact_no;
+
+            $response['meta']['invoice_id'] = $invoice_id;
+            $response['meta']['booking_id'] = $booking_id;
+            $response['meta']['owner_phone_1'] = $data[0]->owner_phone_1;
+            $response['meta']['invoice_template'] = "paytm_payment_voucher.xlsx";
+            $response['meta']['gst_number'] = $data[0]->gst_no;
+            
+            $status = $this->invoice_lib->send_request_to_create_main_excel($response, "final");
+            
+            if ($status) {
+                log_message('info', __FUNCTION__ . ' Invoice File is created. invoice id' . $response['meta']['invoice_id']);
+                $convert = $this->invoice_lib->send_request_to_convert_excel_to_pdf($response['meta']['invoice_id'], "final", TRUE, FALSE);
+                $this->invoice_lib->upload_invoice_to_S3($response['meta']['invoice_id'], false, false);
+                log_message("info", __METHOD__. " SF Credit note uploaded to s3 for booking ID ".$booking_id." Invoice ID ". $response['meta']['invoice_id']);
+                $output_pdf_file_name = $convert['main_pdf_file_name'];
+                
+                $email_template = $this->booking_model->get_booking_email_template("paytm_payment_voucher");
+                $subject =  vsprintf($email_template[4], array($booking_id));
+                $message = $email_template[0];
+                $email_from = $email_template[2];
+
+                $to = $data[0]->owner_email;
+                $cc = $email_template[3].",".$data[0]->primary_contact_email;
+                $bcc = $email_template[5];
+
+
+                $pdf_attachement_url = S3_WEBSITE_URL. 'invoices-excel/' . $output_pdf_file_name;
+                $this->notify->sendEmail($email_from, $to, $cc, $bcc, $subject, $message, $pdf_attachement_url);
+                
+                $this->insert_sf_credit_note($booking_id, $response, $data[0]->assigned_vendor_id, $sd, $agent_id, $convert, $txnID);
+                 
+            } else {
+                log_message("info" . __METHOD__ . " Excel Not Created Booking ID" . $booking_id);
+            }
+        } else {
+            log_message("info" . __METHOD__ . " ooking ID Not found " . $booking_id);
+        }
     }
     
+    function insert_sf_credit_note($booking_id, $invoice, $sf_id, $invoice_date, $agent_id, $convert, $txnID) {
+        $invoice_details = array(
+            'invoice_id' => $invoice['meta']['invoice_id'],
+            'type' => 'FOC',
+            'type_code' => 'B',
+            'vendor_partner' => 'vendor',
+            'vendor_partner_id' => $sf_id,
+            'invoice_file_main' => $convert['main_pdf_file_name'],
+            'invoice_file_excel' => $invoice['meta']['invoice_id'] . '.xlsx',
+            'invoice_detailed_excel' => '',
+            'invoice_date' => $invoice_date,
+            'from_date' => date("Y-m-d", strtotime($invoice_date)),
+            'to_date' => date("Y-m-d", strtotime($invoice_date)),
+            'due_date' => date("Y-m-d", strtotime($invoice_date . "+1 month")),
+            'num_bookings' => 1,
+            "parts_count" => 0,
+            'total_service_charge' => $invoice['meta']['total_taxable_value'],
+            'total_additional_service_charge' => 0,
+            'parts_cost' => 0,
+            'total_amount_collected' => $invoice['meta']['sub_total_amount'],
+            'amount_collected_paid' => -$invoice['meta']['sub_total_amount'],
+            'agent_id' => $agent_id,
+            "cgst_tax_rate" => $invoice['meta']['cgst_tax_rate'],
+            "sgst_tax_rate" => $invoice['meta']['sgst_tax_rate'],
+            "igst_tax_rate" => $invoice['meta']['igst_tax_rate'],
+            "igst_tax_amount" => $invoice['meta']["igst_total_tax_amount"],
+            "sgst_tax_amount" => $invoice['meta']["sgst_total_tax_amount"],
+            "cgst_tax_amount" => $invoice['meta']["cgst_total_tax_amount"],
+            "invoice_file_pdf" => $convert['copy_file'],
+            "remarks" => $booking_id
+        );
+        //Insert Invoice
+        $this->invoices_model->insert_new_invoice($invoice_details);
+        $this->reusable_model->update_table("paytm_transaction_callback", array('vendor_invoice_id' => $invoice['meta']['invoice_id']),array('txn_id' => $txnID));
+    }
+
 }
