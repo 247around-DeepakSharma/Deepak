584a08d36f48df81082283c54106b037dd91325e
Abhay <abhaya@247around.com>
2018-03-31 17:28:27 +0530

    Improve Customer Invoice return message #CRM-501

--- application/controllers/employee/user_invoice.php
+++ application/controllers/employee/user_invoice.php
@@ -36,141 +36,153 @@ class User_invoice extends CI_Controller {
      * @param String $agent_id
      */
     function payment_invoice_for_customer($booking_id, $agent_id) {
-        log_message("info",__METHOD__. " Enering .. for booking id ". $booking_id. " agent ID ".$agent_id);
+        log_message("info", __METHOD__ . " Enering .. for booking id " . $booking_id . " agent ID " . $agent_id);
         $select = "service_centres.company_name, service_centres.address as sf_address, "
                 . "service_centres.pincode as sf_pincode, service_centres.district as sf_district, service_centres.state as sf_state, service_centres.gst_no, service_centres.owner_phone_1, "
                 . "users.name, users.home_address, users.phone_number,users.user_email, users.pincode, users.city, users.state, booking_details.amount_due, "
                 . "booking_details.amount_paid, booking_details.quantity, request_type, services, booking_details.quantity, booking_primary_contact_no,  "
                 . "sc_code, booking_details.user_id, booking_details.closed_date, booking_details.assigned_vendor_id, owner_email, primary_contact_email";
-
         $request['where'] = array("booking_details.booking_id" => $booking_id, 'amount_paid > ' . MAKE_CUTOMER_PAYMENT_INVOICE_GREATER_THAN => NULL);
         $request['length'] = -1;
         $data = $this->booking_model->get_bookings_by_status($request, $select);
+
         if (!empty($data)) {
-            $prod =  ucwords($data[0]->services) . " (" . $data[0]->request_type . ") ";
-           
-            $unit = $this->booking_model->get_unit_details(array("booking_id" => $booking_id, "booking_status != 'Cancelled' " => NULL));
+            $prod = '';
+
+            $unit = $this->booking_model->get_unit_details(array("booking_id" => $booking_id, "booking_status != 'Cancelled' " => NULL, 'user_invoice_id IS NULL' => NULL));
             if (!empty($unit)) {
                 $unique = array_unique(array_map(function ($k) {
                             return $k['price_tags'];
                         }, $unit));
 
-                $prod = $data[0]->services ." (" .implode(",", $unique).")";
+                $prod = $data[0]->services . " (" . implode(",", $unique) . ")";
             }
+            if (!empty($prod)) {
+                $invoice_id = $this->invoice_lib->create_invoice_id($data[0]->sc_code);
+                log_message("info", __METHOD__ . " Invoice ID created " . $invoice_id);
+                $invoice = array();
+                $invoice[0]['description'] = $prod;
+                $tax_charge = $this->booking_model->get_calculated_tax_charge($data[0]->amount_paid, DEFAULT_TAX_RATE);
+                $invoice[0]['taxable_value'] = ($data[0]->amount_paid - $tax_charge);
 
-            $invoice_id = $this->invoice_lib->create_invoice_id($data[0]->sc_code);
-            log_message("info", __METHOD__. " Invoice ID created ". $invoice_id);
-            $invoice = array();
-            $invoice[0]['description'] = $prod;
-            $tax_charge = $this->booking_model->get_calculated_tax_charge($data[0]->amount_paid, DEFAULT_TAX_RATE);
-            $invoice[0]['taxable_value'] = ($data[0]->amount_paid - $tax_charge);
+                $invoice[0]['product_or_services'] = "Service";
+                $invoice[0]['gst_number'] = $data[0]->gst_no;
+                $invoice[0]['company_name'] = $data[0]->company_name;
+                $invoice[0]['company_address'] = $data[0]->sf_address;
+                $invoice[0]['district'] = $data[0]->sf_district;
+                $invoice[0]['pincode'] = $data[0]->sf_pincode;
+                $invoice[0]['state'] = $data[0]->sf_state;
+                $invoice[0]['rate'] = "";
+                $invoice[0]['qty'] = $data[0]->quantity;
+                $invoice[0]['hsn_code'] = HSN_CODE;
 
-            $invoice[0]['product_or_services'] = "Service";
-            $invoice[0]['gst_number'] = $data[0]->gst_no;
-            $invoice[0]['company_name'] = $data[0]->company_name;
-            $invoice[0]['company_address'] = $data[0]->sf_address;
-            $invoice[0]['district'] = $data[0]->sf_district;
-            $invoice[0]['pincode'] = $data[0]->sf_pincode;
-            $invoice[0]['state'] = $data[0]->sf_state;
-            $invoice[0]['rate'] = "";
-            $invoice[0]['qty'] = $data[0]->quantity;
-            $invoice[0]['hsn_code'] = HSN_CODE;
+                $sd = $ed = $invoice_date = $data[0]->closed_date;
+                $response = $this->invoices_model->_set_partner_excel_invoice_data($invoice, $sd, $ed, "Tax Invoice", $invoice_date, true, $data[0]->state);
+                $response['meta']['customer_name'] = $data[0]->name;
+                $response['meta']['customer_address'] = $data[0]->home_address . ", " . $data[0]->city . ", Pincode - " . $data[0]->pincode . ", " . $data[0]->state;
+                $response['meta']['customer_phone_number'] = $data[0]->booking_primary_contact_no;
 
-            $sd = $ed = $invoice_date = $data[0]->closed_date;
-            $response = $this->invoices_model->_set_partner_excel_invoice_data($invoice, $sd, $ed, "Tax Invoice", $invoice_date, true, $data[0]->state);
-            $response['meta']['customer_name'] = $data[0]->name;
-            $response['meta']['customer_address'] = $data[0]->home_address . ", " . $data[0]->city . ", Pincode - " . $data[0]->pincode . ", " . $data[0]->state;
-            $response['meta']['customer_phone_number'] = $data[0]->booking_primary_contact_no;
+                $response['meta']['invoice_id'] = $invoice_id;
+                $response['meta']['booking_id'] = $booking_id;
+                $response['meta']['owner_phone_1'] = $data[0]->owner_phone_1;
 
-            $response['meta']['invoice_id'] = $invoice_id;
-            $response['meta']['booking_id'] = $booking_id;
-            $response['meta']['owner_phone_1'] = $data[0]->owner_phone_1;
+                $status = $this->invoice_lib->send_request_to_create_main_excel($response, "final", true);
+                if ($status) {
+                    log_message('info', __FUNCTION__ . ' Invoice File is created. invoice id' . $response['meta']['invoice_id']);
+                    $convert = $this->invoice_lib->send_request_to_convert_excel_to_pdf($response['meta']['invoice_id'], "final", true, true);
+                    $this->invoice_lib->upload_invoice_to_S3($response['meta']['invoice_id'], false, true);
 
-            $status = $this->invoice_lib->send_request_to_create_main_excel($response, "final", true);
-            if ($status) {
-                log_message('info', __FUNCTION__ . ' Invoice File is created. invoice id' . $response['meta']['invoice_id']);
-                $convert = $this->invoice_lib->send_request_to_convert_excel_to_pdf($response['meta']['invoice_id'], "final", true, true);
-                $this->invoice_lib->upload_invoice_to_S3($response['meta']['invoice_id'], false, true);
-                
-                $output_pdf_file_name = $convert['main_pdf_file_name'];
-
-                $copy_pdf_file_name = $convert['copy_file'];
-                //$triplicate_pdf_file_name = $convert['triplicate_file'];
-
-                $pathinfo1 = pathinfo($output_pdf_file_name);
-                if($pathinfo1['extension'] == 'xls' || $pathinfo1['extension'] == 'xlsx'){
-                    log_message("info", __METHOD__. " SF Invoice Pdf is not generated ".$output_pdf_file_name );
-                }  else {
-                    //Send invoice to SF
-                    $email_template = $this->booking_model->get_booking_email_template("customer_paid_invoice_to_vendor");
-                    $subject =  vsprintf($email_template[4], array($booking_id));
-                    $message = $email_template[0];
-                    $email_from = $email_template[2];
-
-                    $to = $data[0]->owner_email;
-                    $cc = $email_template[3].",".$data[0]->primary_contact_email;
-                    $bcc = $email_template[5];
-
-
-                    $pdf_attachement_url = S3_WEBSITE_URL . 'invoices-excel/' . $output_pdf_file_name;
-                    $this->notify->sendEmail($email_from, $to, $cc, $bcc, $subject, $message, $pdf_attachement_url);
-                }
-                
-                $pathinfo = pathinfo($copy_pdf_file_name);
-                if($pathinfo['extension'] == 'xls' || $pathinfo['extension'] == 'xlsx'){
-                    log_message("info", __METHOD__. " Invoice Pdf is not generated ".$copy_pdf_file_name );
-                    $sms['tag'] = "customer_paid_invoice_pdf_not_generated";
-                    
-                } else {
-                    $customer_attachement_url = S3_WEBSITE_URL . 'invoices-excel/' .$copy_pdf_file_name ;
-
-                    $tinyUrl = $this->miscelleneous->getShortUrl($customer_attachement_url);
-                    // If invoice links is not generating then we will not send invoice Link in sms
-                    if($tinyUrl){
-
-                        $sms['tag'] = "customer_paid_invoice";
-
+                    $output_pdf_file_name = $convert['main_pdf_file_name'];
+                    $copy_pdf_file_name = $convert['copy_file'];
+                    //$triplicate_pdf_file_name = $convert['triplicate_file'];
+                    $pathinfo1 = pathinfo($output_pdf_file_name);
+                    if ($pathinfo1['extension'] == 'xls' || $pathinfo1['extension'] == 'xlsx') {
+                        log_message("info", __METHOD__ . " SF Invoice Pdf is not generated " . $output_pdf_file_name);
                     } else {
-                        $sms['tag'] = "customer_paid_invoice_pdf_not_generated";
-                        
-                        log_message("info", __METHOD__. " Short url failed for booking id ". $booking_id);
-                    }
-                
-                    // Send Invoice to Customer
-                    if (filter_var($data[0]->user_email, FILTER_VALIDATE_EMAIL)) {
-                        $email_template = $this->booking_model->get_booking_email_template("customer_paid_invoice");
-                        $subject =  vsprintf($email_template[4], array($booking_id));
+                        //Send invoice to SF
+                        $email_template = $this->booking_model->get_booking_email_template("customer_paid_invoice_to_vendor");
+                        $subject = vsprintf($email_template[4], array($booking_id));
                         $message = $email_template[0];
                         $email_from = $email_template[2];
 
-                        $to = $data[0]->user_email;
-                        $cc = $email_template[3];
+                        $to = $data[0]->owner_email;
+                        $cc = $email_template[3] . "," . $data[0]->primary_contact_email;
                         $bcc = $email_template[5];
 
-                        $this->notify->sendEmail($email_from, $to, $cc, $bcc, $subject, $message, $customer_attachement_url);
+
+                        $pdf_attachement_url = S3_WEBSITE_URL . 'invoices-excel/' . $output_pdf_file_name;
+                        $this->notify->sendEmail($email_from, $to, $cc, $bcc, $subject, $message, $pdf_attachement_url);
                     }
+
+                    $pathinfo = pathinfo($copy_pdf_file_name);
+                    if ($pathinfo['extension'] == 'xls' || $pathinfo['extension'] == 'xlsx') {
+                        log_message("info", __METHOD__ . " Invoice Pdf is not generated " . $copy_pdf_file_name);
+                        $sms['tag'] = "customer_paid_invoice_pdf_not_generated";
+                    } else {
+                        $customer_attachement_url = S3_WEBSITE_URL . 'invoices-excel/' . $copy_pdf_file_name;
+
+                        $tinyUrl = $this->miscelleneous->getShortUrl($customer_attachement_url);
+                        // If invoice links is not generating then we will not send invoice Link in sms
+                        if ($tinyUrl) {
+
+                            $sms['tag'] = "customer_paid_invoice";
+                        } else {
+                            $sms['tag'] = "customer_paid_invoice_pdf_not_generated";
+
+                            log_message("info", __METHOD__ . " Short url failed for booking id " . $booking_id);
+                        }
+
+                        // Send Invoice to Customer
+                        if (filter_var($data[0]->user_email, FILTER_VALIDATE_EMAIL)) {
+                            $email_template = $this->booking_model->get_booking_email_template("customer_paid_invoice");
+                            $subject = vsprintf($email_template[4], array($booking_id));
+                            $message = $email_template[0];
+                            $email_from = $email_template[2];
+
+                            $to = $data[0]->user_email;
+                            $cc = $email_template[3];
+                            $bcc = $email_template[5];
+
+                            $this->notify->sendEmail($email_from, $to, $cc, $bcc, $subject, $message, $customer_attachement_url);
+                        }
+                    }
+                    $sms['smsData']['amount'] = $data[0]->amount_paid;
+                    $sms['smsData']['booking_id'] = $booking_id;
+                    if ($sms['tag'] == "customer_paid_invoice") {
+
+                        $sms['smsData']['url'] = $tinyUrl;
+                    }
+
+                    $sms['phone_no'] = $response['meta']['customer_phone_number'];
+                    $sms['booking_id'] = $booking_id;
+                    $sms['type'] = "user";
+                    $sms['type_id'] = $data[0]->user_id;
+                    $this->notify->send_sms_msg91($sms);
+
+                    $this->insert_payment_invoice($booking_id, $response, $data[0]->assigned_vendor_id, $data[0]->closed_date, $agent_id, $convert, $data[0]->user_id);
+                    echo json_encode(array(
+                        'status' => true,
+                        'message' => $invoice_id
+                    ));
+                } else {
+                    echo json_encode(array(
+                        'status' => false,
+                        'message' => 'Excel is not created'
+                    ));
+                    log_message("info" . __METHOD__ . " Excel Not Created Booking ID" . $booking_id);
                 }
-
-                $sms['smsData']['amount'] = $data[0]->amount_paid;
-                $sms['smsData']['booking_id'] = $booking_id;
-                if($sms['tag'] == "customer_paid_invoice"){
-                    
-                    $sms['smsData']['url'] = $tinyUrl;
-                } 
-                
-                $sms['phone_no'] = $response['meta']['customer_phone_number'];
-                $sms['booking_id'] = $booking_id;
-                $sms['type'] = "user";
-                $sms['type_id'] = $data[0]->user_id;
-                $this->notify->send_sms_msg91($sms);
-                
-                $this->insert_payment_invoice($booking_id, $response, $data[0]->assigned_vendor_id, 
-                      $data[0]->closed_date, $agent_id, $convert, $data[0]->user_id);
-
             } else {
-                log_message("info" . __METHOD__ . " Excel Not Created Booking ID" . $booking_id);
+                log_message("info" . __METHOD__ . " User Invoice is Not Null" . $booking_id);
+                echo json_encode(array(
+                    'status' => false,
+                    'message' => 'User Invoice Already Generated'
+                ));
             }
         } else {
+            echo json_encode(array(
+                'status' => false,
+                'message' => 'Booking Not Found'
+            ));
             log_message("info" . __METHOD__ . " Data Not Found Booking ID" . $booking_id);
         }
     }
