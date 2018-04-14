fcd3c1123b4c413b5c23e0a35e296d46c3dfbc1f
chhavi <chhavid@247around.com>
2018-04-14 12:10:30 +0530

    #CRM-328 Delete distrcit from sf_not_found table because city is already district

--- application/controllers/employee/partner.php
+++ application/controllers/employee/partner.php
@@ -2703,8 +2703,7 @@ class Partner extends CI_Controller {
             }
             echo $option;
         } else {
-            $booking = array('booking_id' => 'Not_Generated', 'booking_pincode' => $pincode,'service_id' => $service_id, 'partner_id' => $this->session->userdata('partner_id'),'city'=>'Not_Received',
-                'district'=>'Not_Received');
+            $booking = array('booking_id' => 'Not_Generated', 'booking_pincode' => $pincode,'service_id' => $service_id, 'partner_id' => $this->session->userdata('partner_id'),'city'=>'Not_Received');
             $this->miscelleneous->sf_not_exist_for_pincode($booking);
             echo 'ERROR';
         }
