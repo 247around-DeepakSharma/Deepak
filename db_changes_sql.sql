3081c25026cbe1b117dd7d8ea3f6c711ced99e1c
chhavi <chhavid@247around.com>
2018-05-02 17:10:58 +0530

    Merge

--- db_changes_sql.sql
+++ db_changes_sql.sql
@@ -4654,3 +4654,8 @@ UPDATE `sms_template` SET `template` = 'We have received reschedule request for 
 
 --Abhay 25April
 ALTER TABLE  `partner_leads` ADD  `spd_date` VARCHAR( 64 ) NULL DEFAULT NULL AFTER  `update_date` ;
+
+--Chhavi 2nd May
+ALTER TABLE `booking_details` ADD `rating_unreachable_count` INT(10) NOT NULL DEFAULT '0' AFTER `dependency_on`;
+INSERT INTO `sms_template` (`id`, `tag`, `template`, `comments`, `active`, `create_date`) VALUES (NULL, 'customer_not_reachable_for_rating', 'Hello %s! 247around team tried to reach you for your feedback. If you are HAPPY with our service, give miss call @ %s. If not, give miss call @ %s.', NULL, '1', CURRENT_TIMESTAMP);
+
