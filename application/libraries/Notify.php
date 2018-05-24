02f12d633a5c80586cee63d120354b2ea8d28449
Abhay Anand <abhaya@247around.com>
2018-05-24 12:47:56 +0530

    remove developer email

--- application/libraries/Notify.php
+++ application/libraries/Notify.php
@@ -642,7 +642,7 @@ class Notify {
                     $message = "Please check SMS tag and phone number. Booking id is : " .
                             $sms['booking_id'] . " Tag is '" . $sms['tag'] . "' & phone number is :" . $sms['phone_no'] . " Result:"
                             . " " . $status['content'];
-                    $to = DEVELOPER_EMAIL;
+                    $to = "anuj@247around.com, sachinj@247around.com";
 
                     $this->sendEmail(NOREPLY_EMAIL_ID, $to, "", "", $subject, $message, "",'sms_sending_failed');
                 }
@@ -654,7 +654,7 @@ class Notify {
                 $message = "Please check SMS tag and phone number. Booking id is : " .
                         $sms['booking_id'] . " Tag is '" . $sms['tag'] . "' & phone number is :" . $sms['phone_no'] . " Result:"
                         . " " . $status['content'];
-                $to = DEVELOPER_EMAIL;
+                $to = "anuj@247around.com, sachinj@247around.com";
 
                 $this->sendEmail(NOREPLY_EMAIL_ID, $to, "", "", $subject, $message, "",'sms_not_sent_template_not_found');
             }
