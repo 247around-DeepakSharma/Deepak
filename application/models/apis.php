f6d61721a719870a44d6fc8f9052511daa047086
Abhay Anand <abhaya@247around.com>
2018-05-28 14:48:59 +0530

    Database Error- Purchase date issues when customer insert from APP #CRM-794

--- application/models/apis.php
+++ application/models/apis.php
@@ -1828,7 +1828,7 @@ class Apis extends CI_Model {
             $sql2 = "INSERT INTO appliance_details "
                 . "(`service_id`, `brand`, `category`, `capacity`, "
                 . "`model_number`, `tag`, `purchase_date`, `rating`, `user_id`)"
-                . "VALUES (?,?,?,?,?, ?,?,?,?,?)";
+                . "VALUES (?,?,?,?,?,?,?,?,?)";
 
             $this->db->query($sql2, $appl[$i]);
 
