c3b473606fc4507c619574f5c58f2dddf22d4788
chhavi <chhavid@247around.com>
2018-06-25 14:28:43 +0530

    Send EMail From Service Center and partner Panel DB Changes

--- db_changes_sql.sql
+++ db_changes_sql.sql
@@ -7444,3 +7444,80 @@ INSERT INTO `email_template` (`id`, `tag`, `subject`, `template`, `from`, `to`, 
 
 -- 20 June
 ALTER TABLE `spare_parts_details` ADD `courier_pic_by_partner` VARCHAR(1024) NULL DEFAULT NULL AFTER `courier_price_by_partner`;
+
+--sachin 14 june
+ALTER TABLE `contact_person` CHANGE `officail_email` `official_email` VARCHAR(256) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
+
+--sachin 22 june
+
+INSERT INTO `email_template` (`id`, `tag`, `subject`, `template`, `from`, `to`, `cc`, `bcc`, `active`, `create_date`) 
+VALUES (NULL, 'spare_send_by_partner_to_wh', 'Spare shipped by %s to %s', 
+'Dear Partner,<br><br>
+
+<b>%s</b> shipped below spare to your warehouse.<br><br>
+%s
+<br>
+
+<b>Courier Details </b><br><br>
+%s<br>
+
+Regards,<br>
+247around
+', 
+'noreply@247around.com', '', 'sachins@247around.com', '', '1', '2016-06-17 00:00:00');
+
+
+INSERT INTO `email_template` (`id`, `tag`, `subject`, `template`, `from`, `to`, `cc`, `bcc`, `active`, `create_date`) VALUES 
+(NULL, 'spare_received_by_wh_from_partner', 'Spare received by %s send from %s', 
+'Dear Partner,<br><br> <b>%s</b> received below spare <br><br> %s <br> Regards,<br> 247around', 
+'noreply@247around.com', '', 'sachinj@247around.com', '', '1', '2016-06-17 00:00:00');
+
+INSERT INTO `email_template` (`id`, `tag`, `subject`, `template`, `from`, `to`, `cc`, `bcc`, `active`, `create_date`) 
+VALUES (NULL, 'defective_spare_send_by_wh_to_partner', 'Defective Spare shipped by %s to %s', 
+'Dear Partner,<br><br> <b>%s</b> shipped below defective spare to your warehouse.<br><br> %s <br> <b>Courier Details </b><br><br> %s<br> Regards,<br> 247around', 
+'noreply@247around.com', '', 'sachins@247around.com', '', '1', '2016-06-17 00:00:00');
+
+INSERT INTO `email_template` (`id`, `tag`, `subject`, `template`, `from`, `to`, `cc`, `bcc`, `active`, `create_date`) 
+ (NULL, 'defective_spare_received_by_partner_from_wh', 'Defective Spare received by %s', 
+'Dear Partner,<br><br> <b>%s</b> received below defective spare <br><br> %s <br> Regards,<br> 247around', 
+'noreply@247around.com', '', 'sachinj@247around.com', '', '1', '2016-06-17 00:00:00');
+
+INSERT INTO `entity_role` (`id`, `entity_type`, `department`, `role`, `is_filter_applicable`, `create_date`) VALUES
+(null, 'partner', 'warehouse', 'wh_incharge', 0, '2018-06-22 11:16:20'),
+(null, 'vendor', 'warehouse', 'wh_incharge', 0, '2018-06-22 11:16:20');
+
+-- Chhavi 25th June
+CREATE TABLE `booking_internal_conversation` (
+  `id` int(10) NOT NULL,
+  `booking_id` varchar(100) NOT NULL,
+  `subject` text NOT NULL,
+  `msg` text NOT NULL,
+  `sender_entity_type` varchar(100) NOT NULL,
+  `sender_entity_id` int(10) NOT NULL,
+  `agent_id` int(10) NOT NULL,
+  `email_to` varchar(256) NOT NULL,
+  `email_cc` varchar(256) NOT NULL,
+  `email_from` varchar(256) DEFAULT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_active` int(10) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking_internal_conversation`
+--
+ALTER TABLE `booking_internal_conversation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking_internal_conversation`
+--
+ALTER TABLE `booking_internal_conversation`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;COMMIT;
