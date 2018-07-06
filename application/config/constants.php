15c1066fb599248d78acb8d87ba7c256f2d3ba82
chhavi <chhavid@247around.com>
2018-07-06 18:22:39 +0530

    knowlarity Testing

--- application/config/constants.php
+++ application/config/constants.php
@@ -593,5 +593,10 @@ define('NEW_CHARGES_ADDED','New Charges Added');
 //Partner Login
 define('PARTNER_POC_ROLE_ID','1');
 
+define('KNOWLARITY_APPLICATION_KEY', 'L2WufpUCXD5kkFiCSDzHh1FafEVUsR4M5CNaiID3');
+define('KNOWLARITY_API_KEY', '925e6a88-be5e-408b-b211-58bd1a09a63c');
+define('KNOWLARITY_NUMBER', '+918971877477');
+define('KNOWLARITY_OUTGOING_CALL_URL', 'https://kpi.knowlarity.com/Basic/v1/account/call/makecall');
+
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
