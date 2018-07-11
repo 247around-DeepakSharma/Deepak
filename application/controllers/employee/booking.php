92ceb7f7f822a2bba8bdc8675caed350d8fd4023
namrata <namratak@247around.com>
2018-07-11 12:45:15 +0530

    Maintain diary for daily updates corresponding to a booking #CRM-1013

--- application/controllers/employee/booking.php
+++ application/controllers/employee/booking.php
@@ -2416,6 +2416,7 @@ class Booking extends CI_Controller {
        
         $data['comments'] = $this->booking_model->get_remarks($booking_id);
         $data['booking_id'] = $booking_id;
+        $data['user_id'] = $this->session->userdata('id');
         $this->load->view('employee/comment_section', $data);
     }
     function addComment(){
@@ -2427,6 +2428,7 @@ class Booking extends CI_Controller {
         $data['booking_id'] = $this->input->post('booking_id');
         $data['entity_id'] = _247AROUND;
         $data['entity_type'] = '247around';
+        $data['isActive'] = 1;
         $data['create_date'] = date("Y-m-d H:i:s");
          $status = $this->booking_model->add_comment($data);
            }
@@ -2434,6 +2436,10 @@ class Booking extends CI_Controller {
                $this->addComment();
            }           
     }
+    
+    function deleteComment(){
+        $comment_id = $this->input->post('comment_id');
+        $status=$this->booking_model->delete_comment($comment_id);
     }
 
     /**
