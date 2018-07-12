e502464007876ce58528e8b4b6128110154bf770
sachin <sachinj@247around.com>
2018-07-12 13:19:05 +0530

    #CRM-935  inventory dashboard

--- application/views/dashboard/closure_dashboard.php
+++ application/views/dashboard/closure_dashboard.php
@@ -8,6 +8,73 @@
     </div>
     <!-- /top tiles -->
     <hr>
+    //<?php
+//    if($this->session->userdata('id') == INVENTORY_HANDLER_ID){
+//    ?>
+        <!-- SF Brackets snapshot Section -->
+<!--    <div class="row" ng-app="inventory_dashboard">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <div class="col-md-6">
+                        <h2>Service Center Brackets Snapshot</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="pull-right">
+                            <a class="btn btn-sm btn-success" href="//<?php //echo base_url();?>employee/dashboard/brackets_snapshot_full_view" target="_blank">Show All</a>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <div class="table-responsive">
+                        <div class="table-responsive" id="escalation_data" ng-controller="bracketsSnapshot_Controller" ng-cloak="">
+                            <center><img id="brackets_loader" src="//<?php //echo base_url(); ?>images/loadring.gif"></center>
+                            <div ng-if="brackets_div">
+                                <table class="table table-striped table-bordered jambo_table bulk_action">
+                                    <thead>
+                                        <tr>
+                                            <th>S.no</th>
+                                            <th>Service Center Name</th>
+                                            <th colspan="2">Current Stock</th>
+                                            <th>Expected Days Left to Consume Brackets</th>
+                                            <th>Order Brackets</th>
+                                        </tr>
+                                        <tr>
+                                            <th></th>
+                                            <th></th>
+                                            <th>Less than 32"</th>
+                                            <th>32" and above</th>
+                                            <th></th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr ng-repeat="x in bracketsSnapshot | limitTo:quantity">
+                                           <td>{{$index+1}}</td>
+                                           <td>{{x.sf_name}}</td>
+                                           <td>{{x.l_32}}</td>
+                                           <td>{{x.g_32}}</td>
+                                           <td>{{x.brackets_exhausted_days}}</td>
+                                           <td><a class="btn btn-sm btn-success" href="//<?php //echo base_url();?>employee/inventory/get_bracket_add_form/{{x.sf_id}}/{{x.sf_name}}" target="_blank">Order brackets</a></td>
+                                        </tr>
+                                    </tbody>
+                                </table>                             
+                            </div>
+                            <div ng-if="brackets_div_err_msg">
+                                <p class="text-center text-danger">{{brackets_div_err_msg_text}}</p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>-->
+    <!-- SF Brackets Snapshot Section -->
+    //<?php
+//        }
+//    ?>
     <!-- Agent Graph -->
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
