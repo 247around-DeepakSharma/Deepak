0accaa9b4c8d674dec03416a6c1e48cb51dee1bb
Abhay Anand <abhaya@247around.com>
2018-07-13 11:44:00 +0530

    update initial booking date on create #CRM-1058

--- application/controllers/employee/booking.php
+++ application/controllers/employee/booking.php
@@ -394,7 +394,11 @@ class Booking extends CI_Controller {
                 $is_send_sms = 1;
                 $booking_id_with_flag['new_state'] = _247AROUND_PENDING;
                 $booking_id_with_flag['old_state'] = _247AROUND_NEW_BOOKING;
-                 $booking['initial_booking_date'] = $booking['booking_date'];
+
+                if ($booking['type'] == "Booking") {
+                    $booking['initial_booking_date'] = $booking['booking_date'];
+                }
+
                 log_message('info', "New Booking ID created" . print_r($booking['booking_id'], true));
                 break;
             default :
