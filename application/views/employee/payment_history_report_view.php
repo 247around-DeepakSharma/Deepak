a68c86ac9831a5a3cf6b5975e24915686228866e
sachin <sachinj@247around.com>
2018-08-09 11:36:45 +0530

    use datatable in accounting report instead of report library

--- application/views/employee/payment_history_report_view.php
+++ application/views/employee/payment_history_report_view.php
@@ -1,5 +1,4 @@
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />
-<script src="<?php echo base_url() ?>js/report.js"></script>
 <style type="text/css">
     div.pager {
         text-align: center;
@@ -99,20 +98,13 @@
             <section class="payment_preview">
 
             </section>
-            <hr>
-            <section class="download_report" style="display: none;">
-                <div class="row">
-                    <div class="text-center">
-                        <button class="btn btn-success" id="download_report">Download Report</button>
-                    </div>
-                </div>
-            </section>
         </div>
     </div>
 </div> 
 <script type="text/javascript" src="//cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
 <script type="text/javascript">
+    var time = moment().format('D-MMM-YYYY');
     $(function () {
         $('input[name="daterange"]').daterangepicker({
             locale: {
@@ -155,7 +147,6 @@
                             $('#loader').hide();
                             $('.permission-error').hide();
                             $('.payment_preview').hide();
-                            $('.download_report').hide();
                             $('.data-error').show();
                         } else {
                             $('#loader').hide();
@@ -163,8 +154,23 @@
                             $('.permission-error').hide();
                             $('.payment_preview').show();
                             $('.payment_preview').html(response);
-                            $('.download_report').show();
-                            table_pagination();
+                            //table_pagination();
+                            $('#payment_history_table').DataTable({
+                                dom: 'Bfrtip',
+                                pageLength: 50,
+                                ordering: false,
+                                buttons: [
+                                    {
+                                        extend: 'excel',
+                                        text: 'Export',
+                                        exportOptions: {
+                                            columns: [ 0, 1, 2,3,4, 5,6,7,8,9,10]
+                                        },
+                                        title: $('#partner_vendor option:selected').text().trim() +'_' + $('#type option:selected').text().trim()+'_report_'+time
+                                    }
+                                ]
+                            });
+                            
                         }
 
 
@@ -172,48 +178,6 @@
                 });
             }
         });
-    });
-
-    $('#download_report').click(function (e) {
-        var type = $('#type').val();
-        var partner_vendor = $('#partner_vendor').val();
-        var report_type = $('#report_type').val();
-        var time = moment().format('D-MMM-YYYY');
-        if (type === 'A' && partner_vendor === 'partner') {
-            filename = 'partner_sales_report_' + time;
-        } else if (type === 'A' && partner_vendor === 'vendor') {
-            filename = 'vendor_sales_report_' + time;
-        } else if (type === 'A' && partner_vendor === 'stand') {
-            filename = 'stand_sales_report_' + time;
-        } else if (type === 'B' && partner_vendor === 'partner') {
-            filename = 'partner_purchase_report_' + time;
-        } else if (type === 'B' && partner_vendor === 'vendor') {
-            filename = 'vendor_purchase_report_' + time;
-        } else if (type === 'B' && partner_vendor === 'stand') {
-            filename = 'stand_purchase_report_' + time;
-        } else if (type === 'tds' && partner_vendor === 'partner') {
-            filename = 'partner_tds_report_' + time;
-        } else if (type === 'tds' && partner_vendor === 'vendor' && report_type === 'draft') {
-            filename = 'vendor_tds_draft_report_' + time;
-        } else if (type === 'tds' && partner_vendor === 'vendor' && report_type === 'final') {
-            filename = 'vendor_tds_final_report_' + time;
-        } else if (type === 'tds' && partner_vendor === 'stand') {
-            filename = 'stand_tds_report_' + time;
-        }else if (type === 'buyback') {
-            filename = 'buyback_' + time;
-        }
-
-        e.preventDefault();
-
-        //getting data from table
-        var data_type = 'data:application/vnd.ms-excel';
-        var table_div = document.getElementById('payment_history_table');
-        var table_html = table_div.outerHTML.replace(/ /g, '%20');
-
-        var a = document.createElement('a');
-        a.href = data_type + ', ' + table_html;
-        a.download = filename + '.xls';
-        a.click();
     });
 
 </script>
