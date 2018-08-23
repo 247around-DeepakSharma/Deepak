e17d4511d1a40a1844a085aee25bd05dcf95d832
Abhay Anand <abhaya@247around.com>
2018-08-23 18:07:22 +0530

    Create invoices from MPDF #CRM-939

--- application/views/templates/247around_Tax_Invoice_Inter_State.php
+++ application/views/templates/247around_Tax_Invoice_Inter_State.php
@@ -1,35 +1,44 @@
 <html>
     <head>
-        
+        <title>247 Around Tax Invoice</title>
         <style type="text/css">
+            .table{
+                border:solid 1px;
+                border-collapse: collapse;
+                width: 960px;
+                height: 75%;
+                font-family: sans-serif;
+                font-size: 100%;
+                margin: auto;
+            }
 
             td{
-            border:solid 2px;
-            padding: 1%;
+                border:solid 1px;
+                padding: 1%;
             }
+
+
+
         </style>
     </head>
     <body>
-        <table  style="border:solid 2px; border-collapse: collapse; width: 960px;height: 75%;font-family: sans-serif;margin: auto;">
+        <table  style="border:solid 1px; border-collapse: collapse; width: 960px;height: 75%;font-family: sans-serif;margin: auto;">
             <tr>
-                <td style="text-align: center;border-bottom: hidden; " colspan="8" >
-                    <h1>Blackmelon Advance Technology Co. Pvt. Ltd.</h1>
+                <td style="text-align: center; border-bottom: hidden;" colspan="8" >
+                    <h1 style="margin: 0px;">Blackmelon Advance Technology Co. Pvt. Ltd.</h1>
                 </td>
             </tr>
-            <tr style="border-bottom:hidden;">
-                <td rowspan="2" width="20%" align="left" style="border-right: hidden;"><img style="padding: 5px;" src="<?php echo base_url();?>images/logo.jpg"></td>
-                <td colspan="6" width="75%" align="center" style="border:hidden; padding-left: 5%">A-1/7a, A BLOCK, KRISHNA NAGAR, DELHI 110051</td>
-                <td rowspan="3" colspan="1" style="text-align: right; width: 20%; border-left: hidden;"><b>(<?php echo $meta['recipient_type'];?>)</td>
+            <tr style="">
+                <td align="left" style="border-right: hidden;"><img style="padding: 5px;" src="<?php echo base_url();?>images/logo.jpg"></td>
+                <td colspan="5" align="center" style="border:hidden;">A-1/7a, A BLOCK, KRISHNA NAGAR, DELHI 110051<br>Email: seller@247around.com<br><br><b>GSTIN: 07AAFCB1281J1ZQ</b></td>
+                <td colspan="2" style="text-align: right;  border-left: hidden;"><b>(<?php echo $meta['recipient_type'];?>)</td>
             </tr>
-            <tr style="border: hidden;">
-                <td colspan="6" align="center" style="padding-right: 15%; border-right: hidden;">Email: seller@247around.com</td>
-            </tr>	
-            <tr style="text-align: center;" >
-                <td colspan="8"><b>GSTIN: 07AAFCB1281J1ZQ</td>
-            </tr>
-            <tr style="text-align: center; height: 50px; font-size: 40px;background-color: rgb(211,211,211);">
-                <td colspan="8"><b><?php echo $meta['invoice_type'];?></b>
-            </tr>
+<!--            <tr style="text-align: center; height: 50px; font-size: 40px;background-color: rgb(211,211,211);">
+                <td colspan="8"><b><?php// echo $meta['invoice_type']; ?></b>
+            </tr>-->
+             <tr style="text-align: center; height: 50px; font-size: 40px;background-color: rgb(211,211,211);">	
+                 <td colspan="8"  align="center"><b><?php echo $meta['invoice_type']; ?></b>
+             </tr>
             <tr>
                 <td colspan="3">Invoice No:<?php echo $meta['invoice_id'];?></td>
                 <td colspan="5">Reference No: 	<?php echo $meta['reference_invoice_id'];?></td>
@@ -44,7 +53,7 @@
                 <td colspan="5"></td>
             </tr>        
             <tr style="text-align: center;background-color: rgb(211,211,211);">
-                <td colspan="8"><b>Bill to Party</b>
+                <td colspan="8" align="center"><b>Bill to Party</b>
             </tr>
             <tr style="height: 20px">
                 <td colspan="8">Name : <?php echo $meta['company_name'];?>247around service center</td>
@@ -54,77 +63,77 @@
             </tr>
             <tr>
                 <td colspan="2">GSTIN: <?php echo $meta['gst_number'];?></td>
-                <td colspan="5" style="border-left: hidden;">Place of Supply: <?php echo $meta['state'];?></td>
-                <td>Code:<?php echo $meta['state_code'];?></td>
+                <td colspan="4" style="">Place of Supply: <?php echo $meta['state'];?></td>
+                <td colspan="2">Code:<?php echo $meta['state_code'];?></td>
             </tr>
-            <tr style="text-align: center;background-color: rgb(211,211,211);">
-                <td rowspan="2" width="30%"><b>Product Description</td>
-                <td rowspan="2" width="6%" style="padding: 1px"><b>HSN/SAC<br>Code</td>
-                <td rowspan="2" width="10%"><b>Qty</td>
-                <td rowspan="2" width="10%"><b>Rate</td>
-                <td rowspan="2"><b>Taxable value</td>
-                <td colspan="2" width="30%"><b>IGST</td>
-                <td rowspan="2"><b>Total</td>
+            <tr style="text-align: center; background-color: rgb(211,211,211);">
+                <td rowspan="2" style="font-size: 13px;"><b>Product Description</td>
+                <td rowspan="2" style="font-size: 13px;" style="padding: 1px"><b>HSN/SAC<br>Code</td>
+                <td rowspan="2" style="font-size: 13px;"><b>Qty</td>
+                <td rowspan="2" style="font-size: 13px;"><b>Rate</td>
+                <td rowspan="2" style="font-size: 13px;"><b>Taxable value</td>
+                <td colspan="2" style="font-size: 13px;" style="text-align: center;"><b>IGST</td>
+                <td rowspan="2" style="font-size: 13px;"><b>Total</td>
             </tr>
             <tr align="center" style="background-color: rgb(211,211,211)">
-                <td width="12%" style="padding: 1px"><b>Rate</td>
-                <td width="15%"><b>Amount</td>
+                <td style="padding: 1px">Rate</td>
+                <td>Amount</td>
             </tr>
             <?php 
                 foreach ($booking as $data) { ?>
             <tr style='text-align: center;'>
-                <td width='30%'><?php echo $data['description']; ?></td>
-                <td width='17%'><?php echo $data['hsn_code']; ?></td>
-                <td width='10%'><?php echo $data['qty']; ?></td>
-                <td width='8%'><?php echo $data['rate']; ?></td>
-                <td><?php echo $data['taxable_value']; ?></td>
-                <td width='12%'><?php echo $data['igst_rate']; ?></td>
-                <td width='15%'><?php echo $data['igst_tax_amount']; ?></td>
-                <td><?php echo $data['toal_amount']; ?></td>
+                <td width="30%"><?php echo $data['description']; ?></td>
+                <td width="8%"><?php echo $data['hsn_code']; ?></td>
+                <td width="8%"><?php echo $data['qty']; ?></td>
+                <td width="12%"><?php echo $data['rate']; ?></td>
+                <td width="12%"><?php echo $data['taxable_value']; ?></td>
+                <td width="8%"><?php echo $data['igst_rate']; ?></td>
+                <td width="12%"><?php echo $data['igst_tax_amount']; ?></td>
+                <td width="20%"><?php echo $data['total_amount']; ?></td>
             </tr>
                 	
               <?php  } ?>
            
             <tr align="center">
                 <td style="background-color: rgb(211,211,211);"><b>Total</td>
-                <td style="border-right: hidden;"></td>
-                <td><b><?php echo $meta['total_qty'];?></td>
-                <td style="border-left: hidden;"></td>
+                <td ></td>
+                <td><?php echo $meta['total_qty'];?></td>
+                <td></td>
                 <td><?php echo $meta['total_taxable_value'];?></td>
                 <td></td>
                 <td><?php echo $meta['igst_total_tax_amount'];?></td>
-                <td><b><?php echo $meta['sub_total_amount'];?></td>
+                <td><?php echo $meta['sub_total_amount'];?></td>
             </tr>
             
             <tr align="left" >
                 <td width="60%" colspan="4" align="center"><b>Total Invoice amount in words</b></td>
                 <td width="30%" colspan="3"><b>Total Amt before Tax</td>
-                <td width="10%" colspan="1"><b><?php echo $meta['total_taxable_value']; ?></td>
+                <td width="10%" colspan="1"><?php echo $meta['total_taxable_value']; ?></td>
             </tr>
             <tr align="left">
                 <td colspan="4" rowspan="2" style="padding: 2%" align="center"><?php echo $meta['price_inword'];?></td>
                 <td colspan="3"><b>Add: IGST</td>
-                <td colspan="1"><b><?php echo $meta['igst_total_tax_amount'];?></td>
-            <tr style="font-weight: bold;text-align: left;">
-                <td colspan="3">Total Amt after Tax</td>
+                <td colspan="1"><?php echo $meta['igst_total_tax_amount'];?></td>
+            <tr style="text-align: left;">
+                <td colspan="3" style="font-weight: bold;">Total Amt after Tax</td>
                 <td colspan="1"><?php echo $meta['sub_total_amount'];?></td>
             </tr>
             <tr>
-                <td align="center" style="background-color: rgb(211,211,211);"><b>Bank Details</td>
-                <td rowspan="6" colspan="3">stamp</td>
-                <td colspan="3"><b>GST on Reverse Charge</td>
-                <td colspan="1">0</td>
+                <td align="center" style="background-color: rgb(211,211,211); width:30px;"><b>Bank Details</td>
+                <td rowspan="6" colspan="3" align="center"><img  src="<?php echo base_url()."images/247aroundstamp.png"; ?>" style="width: 160px;"></td>
+                <td align="left" colspan="3"><b>GST on Reverse Charge</td>
+                <td align="left" colspan="1">0</td>
             </tr>
             <tr>
-                <td>Bank Name: ICICI Bank</td>
-                <td rowspan="2" colspan="4" style="padding: 2%; background-color: rgb(211,211,211);" align="center"><b>For Blackmelon Advance <br>Technology Co. Pvt. Ltd.</td>
+                <td width="30%">Bank Name: ICICI Bank</td>
+                <td rowspan="2" colspan="4" style=" background-color: rgb(211,211,211);" align="center"><b>For Blackmelon Advance <br>Technology Co. Pvt. Ltd.</td>
             </tr>
             <tr>
-                <td>Acc No: 102405500277</td>
+                <td width="30%">Acc No: 102405500277</td>
             </tr>
             <tr>
-                <td>IFSC: ICIC0001024</td>
-                <td rowspan="2" colspan="4">sign</td>
+                <td width="30%">IFSC: ICIC0001024</td>
+                <td rowspan="2" colspan="4" align="center"><img src="<?php echo base_url()."images/anujsign.png"; ?>" style="width: 170px;"></td>
             </tr>
             <tr>
                 <td style="border-bottom: hidden;">	</td>
