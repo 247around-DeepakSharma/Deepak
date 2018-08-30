b08dc2587812cf99cd438c11b240bbc32541c57b
chhavi <chhavid@247around.com>
2018-08-30 11:51:33 +0530

    #CRM-1354  Contant file

--- application/config/constants.php
+++ application/config/constants.php
@@ -626,5 +626,6 @@ define('ASP_PASSWORD', 'priya@b30');
 define('BOOKING_CAN_NOT_BE_CANCEL_AFTER_THIS_TIME_INTERVAL',10);
 //show this text when partner cancel booking before time interval
 define('TEXT_FOR_BOOKING_CAN_NOT_BE_CANCEL_AFTER_THIS_TIME_INTERVAL','This booking can not be cancelled');
+define('REVIEW_NOTIFICATION_TO_PARTNER_DAYS','2');
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
