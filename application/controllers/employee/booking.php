f38d1c10e30fe1559a667615f802b6a7be96f24b
chhavi <chhavid@247around.com>
2018-09-06 15:20:18 +0530

    Blank Booking ID CHeck

--- application/controllers/employee/booking.php
+++ application/controllers/employee/booking.php
@@ -1766,28 +1766,28 @@ class Booking extends CI_Controller {
     function checked_complete_review_booking() {
         $requested_bookings = $this->input->post('approved_booking');
         if($requested_bookings){
-        $where['is_in_process'] = 0;
-        $whereIN['booking_id'] = $requested_bookings; 
-        $tempArray = $this->reusable_model->get_search_result_data("booking_details","booking_id",$where,NULL,NULL,NULL,$whereIN,NULL,array());
-        foreach($tempArray as $values){
-            $approved_booking[] = $values['booking_id'];
-        }
-        $inProcessBookings = array_diff($requested_bookings,$approved_booking);
-        $this->session->set_flashdata('inProcessBookings', $inProcessBookings);
-        $this->booking_model->mark_booking_in_process($approved_booking);
-        $url = base_url() . "employee/do_background_process/complete_booking";
-        if (!empty($approved_booking)) {
-            $data['booking_id'] = $approved_booking;
-            $data['agent_id'] = $this->session->userdata('id');
-            $data['agent_name'] = $this->session->userdata('employee_id');
-            $data['partner_id'] = $this->input->post('partner_id');
-            $data['approved_by'] = $this->input->post('approved_by'); 
-            $this->asynchronous_lib->do_background_process($url, $data);
-            $this->push_notification_lib->send_booking_completion_notification_to_partner($approved_booking);
-        } else {
-            //Logging
-            log_message('info', __FUNCTION__ . ' Approved Booking Empty from Post');
-        }
+            $where['is_in_process'] = 0;
+            $whereIN['booking_id'] = $requested_bookings; 
+            $tempArray = $this->reusable_model->get_search_result_data("booking_details","booking_id",$where,NULL,NULL,NULL,$whereIN,NULL,array());
+            foreach($tempArray as $values){
+                $approved_booking[] = $values['booking_id'];
+            }
+            $inProcessBookings = array_diff($requested_bookings,$approved_booking);
+            $this->session->set_flashdata('inProcessBookings', $inProcessBookings);
+            $this->booking_model->mark_booking_in_process($approved_booking);
+            $url = base_url() . "employee/do_background_process/complete_booking";
+            if (!empty($approved_booking)) {
+                $data['booking_id'] = $approved_booking;
+                $data['agent_id'] = $this->session->userdata('id');
+                $data['agent_name'] = $this->session->userdata('employee_id');
+                $data['partner_id'] = $this->input->post('partner_id');
+                $data['approved_by'] = $this->input->post('approved_by'); 
+                $this->asynchronous_lib->do_background_process($url, $data);
+                $this->push_notification_lib->send_booking_completion_notification_to_partner($approved_booking);
+            } else {
+                //Logging
+                log_message('info', __FUNCTION__ . ' Approved Booking Empty from Post');
+            }
         }
         redirect(base_url() . 'employee/booking/review_bookings');
     }
