174c0f69afb012bc10558c7a35ea525f492e8db1
chhavi <chhavid@247around.com>
2018-09-10 11:49:57 +0530

    #CRM-1350 Pending Booking Page Sorting

--- application/views/employee/view_pending_bookings.php
+++ application/views/employee/view_pending_bookings.php
@@ -411,7 +411,7 @@
             },
             "columnDefs": [
                 {
-                    "targets": [0,1,2,3,4,6,7,8,9,10,11,12,13,14,15], //first column / numbering column
+                    "targets": [0,1,2,3,4,6,7,8,9,10,11,12,13,14,15,16], //first column / numbering column
                     "orderable": false //set not orderable
                 }
             ],  
