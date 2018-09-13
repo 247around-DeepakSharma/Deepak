06d15e4667b08f396a6751b0a8828431db5b68d8
chhavi <chhavid@247around.com>
2018-09-13 19:00:39 +0530

    #CRM-1468 Datatables  - SF needs to send Parts

--- application/views/partner/sf_needs_to_send_parts.php
+++ application/views/partner/sf_needs_to_send_parts.php
@@ -1,8 +1,3 @@
-<?php if ($this->uri->segment(3)) {
-    $sn_no = $this->uri->segment(3) + 1;
-} else {
-    $sn_no = 1;
-} ?>
 <?php if(empty($is_ajax)) { ?>
 <div class="right_col" role="main">
         <?php
@@ -20,10 +15,24 @@
 <div class="col-md-12 col-sm-12 col-xs-12">
     <div class="x_panel">
         <div class="x_title">
-            <h2>Defective Spares Pending on SF </h2>
+                   <h2>Defective Spares Pending on SF </h2>
             <div class="pull-right"><a style="background: #2a3f54;border-color: #2a3f54;" href="<?php echo base_url(); ?>partner/download_sf_needs_to_send_parts"  class="btn btn-sm btn-primary">Download</a></div>
-            <div class="clearfix"></div>
-        </div>
+                    <div class="right_holder" style="float:right;margin-right:10px;">
+                            <select class="form-control " id="state_search" style="border-radius:3px;" onchange="booking_search()">
+                    <option value="">States</option>
+      <?php
+      foreach($states as $state){
+          ?>
+      <option value="<?php echo $state['state'] ?>"><?php echo $state['state'] ?></option>
+      <?php
+      }
+      ?>
+  </select>            
+</div>
+                    <div class="clearfix"></div>
+                    
+                </div>
+        <input type="text" id="booking_id_search" onchange="booking_search()" style="float: right;margin-bottom: -32px;border: 1px solid #ccc;padding: 5px;z-index: 100;position: inherit;">
         <div class="x_content">
             <form target="_blank"  action="<?php echo base_url(); ?>partner/print_all" name="fileinfo1"  method="POST" enctype="multipart/form-data">
                 <table class="table table-bordered table-hover table-striped" id="sf_needs_to_send_table">
@@ -39,56 +48,7 @@
                             <th class="text-center">Age</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <?php foreach ($spare_parts as $key => $row) { ?>
-                            <tr style="text-align: center;">
-                                <td>
-                                    <?php echo $sn_no; ?>
-                                </td>
-                                <td>
-                                    <a  style="color:blue" href="<?php echo base_url(); ?>partner/booking_details/<?php echo $row['booking_id']; ?>"  title='View'><?php echo $row['booking_id']; ?></a>
-                                </td>
-                                <td>
-                                    <?php echo $row['name']; ?>
-                                </td>
-    <!--                                    <td>
-                                    <?php //echo $row['age_of_booking'];  ?>
-                                </td>-->
-                                <td>
-                                    <?php echo $row['defective_part_shipped']; ?>
-                                </td>
-                                   <td>
-                                        <?php echo $row['courier_name_by_partner']; ?>
-                                    </td>
-                                    <td>
-                                        <?php echo $row['awb_by_partner']; ?>
-                                    </td>
-                                     <td> 
-                                        <?php  if(!empty($row['partner_challan_file'])) { ?> 
-                                            <a href="https://s3.amazonaws.com/<?php echo BITBUCKET_DIRECTORY ?>/vendor-partner-docs/<?php echo $row['partner_challan_file']; ?>" target="_blank"><?php echo $row['partner_challan_number']?></a>
-                                        <?php }
-                                        else if(!empty($row['partner_challan_number'])) {
-                                            echo $row['partner_challan_number'];
-                                        }
-?>
-                                      </td>
-                                 <td>
-                                    <?php echo $row['aging']; ?>
-                                </td>
-
-
-                            </tr>
-                        <?php $sn_no++;
-                    } ?>
-                    </tbody>
                 </table>
-                 <div class="custom_pagination" style="margin-left: 16px;" > 
-                    <?php
-                        if (isset($links)) {
-                            echo $links;
-                        }
-                    ?>
-                </div>
         </div>
     </div>
 </div>
@@ -99,10 +59,38 @@
 <div class="clearfix"></div>
 <?php if($this->session->userdata('success')){$this->session->unset_userdata('success');} ?>
 <script type="text/javascript">
- var table = $('#sf_needs_to_send_table').DataTable(
-          {
-                    "pageLength": 50
-                });
+    
+    $(document).ready(function () {
+        $('#state_search').select2();
+        sf_needs_to_send_table = $('#sf_needs_to_send_table').DataTable({
+            "processing": true,
+            "language":{ 
+                "processing": "<center><img id='loader_gif_title' src='<?php echo base_url(); ?>images/loadring.gif'></center>",
+            },
+            "serverSide": true, 
+            "order": [], 
+            "pageLength": 50,
+            "ajax": {
+                "url": "<?php echo base_url(); ?>employee/partner/get_sf_needs_to_send_spare/",
+                "type": "POST",
+                "data": function(d){
+                    d.booking_id =  $('#booking_id_search').val();
+                    d.state =  $('#state_search').val();
+                 }
+            },
+            "columnDefs": [
+                {
+                    "targets": [0,2,6], //first column / numbering column
+                    "orderable": false //set not orderable
+                }
+            ],  
+            "deferRender": true 
+        });
+    });
+    function booking_search(){
+             sf_needs_to_send_table.ajax.reload();
+        }
+ 
 function confirm_received(){
     var c = confirm("Continue?");
     if(!c){
@@ -112,10 +100,11 @@ function confirm_received(){
 
 </script>
     <style>
-        .paging_simple_numbers{
-            display: none;
-        }
-                .dataTables_info{
-    display: none;
+#sf_needs_to_send_table_filter{
+      display: none;
+}
+#sf_needs_to_send_table_processing{
+    border:none !important;
+    background-color: transparent !important;
 }
         </style>
