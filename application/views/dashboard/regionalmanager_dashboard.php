c3e20ac62fbba1cd38b7c2ecfb68b70e02d70bd8
chhavi <chhavid@247around.com>
2018-09-15 15:16:48 +0530

    #CRM-1196 add multiple selection in request type for d0 reporting Everywhere

--- application/views/dashboard/regionalmanager_dashboard.php
+++ application/views/dashboard/regionalmanager_dashboard.php
@@ -60,10 +60,9 @@
                     <div class="item form-group">
                         <div class="col-md-12 col-sm-12 col-xs-12">
                             <label for="">Request Type</label>
-                            <select class="form-control filter_table" id="request_type" name="request_type">
+                            <select class="form-control filter_table" id="request_type" name="request_type" multiple="">
                                 <option value="">All</option>
                                 <option value="Installation" selected="selected">Installations</option>
-                                <option value="Repair">Repair</option>
                                 <option value="Repair_with_part">Repair With Spare</option>  
                                 <option value="Repair_without_part">Repair Without Spare</option>  
                             </select>
@@ -569,6 +568,18 @@ if($this->session->userdata("wrong_pincode_msg")){
     }
 </style>
 <script>
+    $('#request_type').select2();
+        function getMultipleSelectedValues(fieldName){
+    fieldObj = document.getElementById(fieldName);
+    var values = [];
+    var length = fieldObj.length;
+    for(var i=0;i<length;i++){
+       if (fieldObj[i].selected == true){
+           values.push(fieldObj[i].value);
+       }
+    }
+   return values.join(":");
+}
     var post_request = 'POST';
     var get_request = 'GET';
     var url = '';
