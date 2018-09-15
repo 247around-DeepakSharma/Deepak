5651776c8dad351ab3f4119e771438df00d911af
chhavi <chhavid@247around.com>
2018-09-15 15:35:41 +0530

    Revert "#CRM-1196 add multiple selection in request type for d0 reporting Everywhere"
    This reverts commit 1c362217759501a061648beafaf67d5fba8732df.

--- application/views/dashboard/accountmanager_dashboard.php
+++ application/views/dashboard/accountmanager_dashboard.php
@@ -46,9 +46,10 @@
                     <div class="item form-group">
                         <div class="col-md-12 col-sm-12 col-xs-12">
                             <label for="">Request Type</label>
-                            <select class="form-control filter_table" id="request_type_am" name="request_type_am" multiple="">
+                            <select class="form-control filter_table" id="request_type_am" name="request_type_am">
                                 <option value="">Request Type</option>
                                 <option value="Installation" selected="selected">Installations</option>
+                                <option value="Repair">Repair</option>
                                 <option value="Repair_with_part">Repair With Spare</option>  
                                 <option value="Repair_without_part">Repair Without Spare</option>  
                             </select>
@@ -157,9 +158,10 @@
                     <div class="item form-group">
                         <div class="col-md-12 col-sm-12 col-xs-12">
                             <label for="">Request Type</label>
-                            <select class="form-control filter_table" id="request_type" name="request_type" multiple="">
+                            <select class="form-control filter_table" id="request_type" name="request_type">
                                 <option value="">All</option>
                                 <option value="Installation" selected="selected">Installations</option>
+                                <option value="Repair">Repair</option>
                                 <option value="Repair_with_part">Repair With Spare</option>  
                                 <option value="Repair_without_part">Repair Without Spare</option>  
                             </select>
@@ -383,19 +385,6 @@
 <!-- /page content -->
 <!-- Chart Script -->
 <script>
-    $('#request_type_am').select2();
-    $('#request_type').select2();
-        function getMultipleSelectedValues(fieldName){
-    fieldObj = document.getElementById(fieldName);
-    var values = [];
-    var length = fieldObj.length;
-    for(var i=0;i<length;i++){
-       if (fieldObj[i].selected == true){
-           values.push(fieldObj[i].value);
-       }
-    }
-   return values.join(":");
-}
     var post_request = 'POST';
     var get_request = 'GET';
     var url = '';
