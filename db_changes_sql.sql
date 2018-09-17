a31f6b8d7319125326e2486d4e9aa6a36ee6a6bd
kalyanisahu786 <kalyanisahu786@gmail.com>
2018-09-17 11:10:05 +0530

    Add search by description, amount on bank transaction page #CRM-1292

--- db_changes_sql.sql
+++ db_changes_sql.sql
@@ -8110,8 +8110,9 @@ INSERT INTO `email_template` (`id`, `tag`, `subject`, `template`, `from`, `to`, 
 
 INSERT INTO `email_template` (`id`, `tag`, `subject`, `template`, `from`, `to`, `cc`, `bcc`, `active`, `create_date`) VALUES (NULL, 'postpaid_above_due_date_invoice_notification', '%s', 'Dear Partner,\r\n<br/>\r\n%s<br/>\r\n%s\r\n', 'bill@247around.com', '', 'anuj@247around.com', 'abhaya@247around.com', '1', '2018-09-11 00:00:00');
 
---kalyani 
+--Kalyani 
 INSERT INTO `header_navigation` (`id`, `entity_type`, `title`, `title_icon`, `link`, `level`, `parent_ids`, `groups`, `nav_type`, `is_active`, `create_date`) VALUES (null, '247Around', 'Upload Docket Number', NULL, 'employee/inventory/upload_docket_number', '2', '89', 'admin,closure,developer,regionalmanager,accountmanager', 'main_nav', '1', CURRENT_TIMESTAMP);
-
 INSERT INTO `sms_template` (`id`, `tag`, `template`, `comments`, `active`, `create_date`) VALUES (NULL, 'sms_to_vendor_poc', '%s', '', '1', CURRENT_TIMESTAMP);
 
+--Kalyani 17-sept
+INSERT INTO `header_navigation` (`id`, `entity_type`, `title`, `title_icon`, `link`, `level`, `parent_ids`, `groups`, `nav_type`, `is_active`, `create_date`) VALUES (NULL, '247Around', 'Search Bank Transactions', NULL, 'employee/accounting/bank_transactions', '2', '56', 'admin,developer', 'main_nav', '1', CURRENT_TIMESTAMP);
