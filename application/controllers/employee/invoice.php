72b9d7d08127cdc01388629ca82db17ceba815a0
kalyani tekpure <kalyanit@247around.com>
2018-10-24 17:41:54 +0530

    minor in partner setup invoice

--- application/controllers/employee/invoice.php
+++ application/controllers/employee/invoice.php
@@ -3019,12 +3019,12 @@ class Invoice extends CI_Controller {
             
              $this->invoices_model->insert_new_invoice($invoice_details);
              log_message('info', __METHOD__ . ": Invoice ID inserted");
-             $this->session->set_flashdata('file_error', $description.'Invoice Generated');
+             $this->session->set_flashdata('file_error', $description.' Invoice Generated');
              redirect(base_url() . "employee/invoice/invoice_partner_view");
 
         } else {
             log_message('info', __METHOD__ . ": Invoice ID inserted");
-            $this->session->set_flashdata('file_error', $description. 'Not Generated');
+            $this->session->set_flashdata('file_error', $description. ' Not Generated');
             log_message('info', __METHOD__ . ": Validation Failed");
             $this->invoice_partner_view();
         }
@@ -3321,7 +3321,7 @@ class Invoice extends CI_Controller {
                 $subject = vsprintf($email_template[4], array($partner_data['company_name'], $sd, $ed));
                 $message = $email_template[0];
                 $email_from = $email_template[2];
-                if($email_tag === CRM_SETUP_INVOICE_EMAIL_TAG){
+                if($email_tag == CRM_SETUP_INVOICE_EMAIL_TAG){
                     $to = $partner_data['invoice_email_to'].",".$email_template[1];
                     $cc = $partner_data['invoice_email_cc'].",".$email_template[3];
                 }
