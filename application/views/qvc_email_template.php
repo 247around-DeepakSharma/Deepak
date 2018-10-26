f80cd27bb859c80c832308eafa9f48e6d6789e07
Shraddhanand <abhaya@247around.com>
2018-10-26 10:42:18 +0530

    Create form to send email to Qwikcilver  #CRM-1563

---
+++ application/views/qvc_email_template.php
@@ -0,0 +1,66 @@
+<?php 
+/*
+$transction_date="12-10-2018";
+
+$transction_amount =500;
+$review='Hi,this is the testing text.';
+ * 
+ */
+?>
+
+<style>
+/*table, th, tr {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, tr {
+    padding: 5px;
+    text-align: left;
+    
+    
+}*/
+</style> 
+
+<table style="width:100%; border:1px solid #000;">
+    <tr>
+        <th colspan="3" style = "text-align:center; border-bottom: 1px solid #000; background:grey; "><span style="color: #fff;">SVC Load Request Form</span></th>
+                    </tr>
+                     <tr>
+                         <th style="text-align:center; border-bottom: 1px solid #000; border-right:1px solid #000;">Client Name</th>
+                        <th style="text-align:center; border-bottom: 1px solid #000;  border-right:1px solid #000;">Amazon_Ext_Buyback_247Around</th>
+                        <th style="text-align:center; border-bottom: 1px solid #000;">Amazon SPOC Name</th>
+                    </tr>
+                   <?php if(!empty($transction_date))?>
+                    <tr>
+                        <th style = "text-align:center; border-bottom: 1px solid #000; border-right:1px solid #000;">Date of filling up form</th>
+                        <th style = "text-align:center; border-bottom: 1px solid #000; border-right:1px solid #000;"><?php echo date('d-F-y', strtotime($transction_date));?></th>
+                         <th style = "text-align:center; border-bottom: 1px solid #000;">Order Delivery Date </th>
+                    </tr>
+                    <?php if(!empty($transction_amount))?>
+                    <tr>
+                        
+                        <th style = "text-align:center; border-bottom: 1px solid #000; border-right:1px solid #000;">Amount(Rs.)</th>
+                        <th style = "text-align:center; border-bottom: 1px solid #000; border-right:1px solid #000;"><?php echo $transction_amount;?> </th>
+                    </tr>
+                    <tr>
+                        <th style = "text-align:center; border-bottom: 1px solid #000; border-right:1px solid #000;">Discount%</th>
+                         <th style = "text-align:center; border-bottom: 1px solid #000; border-right:1px solid #000;">%</th>
+                    </tr>
+                    <?php if(!empty($transction_amount))?>
+                    <tr>
+                        <th style = "text-align:center; border-bottom: 1px solid #000; border-right:1px solid #000;">Final Payable amount</th>
+                        <th style = "text-align:center; border-bottom: 1px solid #000; border-right:1px solid #000;"><?php echo $transction_amount;?></th>
+                        <th style = "text-align:center; border-bottom: 1px solid #000;"></th>
+                    </tr
+                    
+                    <?php if(!empty($review))?> 
+                    <tr>
+                        <th style = "text-align:center; border-right:1px solid #000;">Special remarks for invoicing (if any)</th>
+                        <th style = "text-align:center; border-right:1px solid #000;"><?php echo $review;?></th>
+                    </tr>
+ </table>
+
+
+
+
+
