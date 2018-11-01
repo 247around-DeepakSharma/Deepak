92a637d277f736351a8e8492ed0121c1c3d9fc57
kalyani tekpure <kalyanit@247around.com>
2018-11-01 11:18:07 +0530

    CRM-1256 collect gstr2a data from taxpro api manage it and create its report

---
+++ application/views/employee/generate_taxpro_GSTR2a_Data.php
@@ -0,0 +1,109 @@
+<script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
+
+<div id="page-wrapper">
+    <div class="panel panel-info" style="margin-top:20px;">
+        <div class="panel-heading" style="padding-left: 38%;">Generate GSTR2a Report</div>
+            <div class="panel-body">
+            <div class="row">
+                <div id="success_msg_div" class="alert alert-success alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <strong id="success_msg"></strong>
+                </div>
+                <div id="error_msg_div" class="alert alert-danger alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    <strong id="error_msg"></strong>
+                </div>
+                <form name="myForm" class="form-horizontal" novalidate="novalidate" action="<?php echo base_url(); ?>employee/accounting/generate_taxpro_auth_token"  method="POST" enctype="multipart/form-data">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="col-md-3"></div>
+                            <div class="col-md-6">
+                                <div  class="form-group">
+                                    <label  for="vendor_partner" class="col-md-2">Enter OTP *</label>
+                                    <div class="col-md-10">
+                                        <input type="text" id="otp" name="otp" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3"></div>
+                        </div>
+                        <div class="form-group col-md-12">
+                            <div class="col-md-3"></div>
+                            <div class="col-md-6 form-group">
+                                <div class="col-md-2"></div>
+                                <div class="col-md-10">
+                                <center>
+                                    <div><a onclick="generate_otp()">Request OTP</a></div>
+                                    <div style="margin-top:5px">
+                                        <input type="button" onclick="create_autntoken();" name="submit_btn" class="btn btn-info" value="Submit"/>
+                                    </div>
+                                </center>
+                                </div>
+                            </div>
+                            <div class="col-md-3"></div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#success_msg_div").hide();
+        $("#error_msg_div").hide();
+    });
+    function generate_otp(){
+        $.ajax({
+                type: 'POST',
+                url: '<?php echo base_url(); ?>employee/accounting/generate_taxpro_otp',
+                data: {},
+                success: function (data) {
+                    console.log(data);
+                    //data = '{"status_cd":"1"}';
+                    data = JSON.parse(data);
+                    if(data.status_cd == '1'){
+                        $("#success_msg").text("OTP generated successfully.");
+                        $("#success_msg_div").show();
+                        $("#error_msg_div").hide();
+                    }
+                    else{
+                        $("#error_msg").text("Error in generating OTP. Please contact tech team.");
+                        $("#success_msg_div").hide();
+                        $("#error_msg_div").show();
+                    }
+            }
+        });
+    }
+    
+    function create_autntoken(){ 
+        if($("#otp").val()){ 
+            $.ajax({
+                type: 'POST',
+                url: '<?php echo base_url(); ?>employee/accounting/generate_taxpro_auth_token',
+                data: {otp:$("#otp").val()},
+                success: function (data) {
+                   // console.log(data);
+                    if(data == "success"){
+                        $("#success_msg").text("GSTR2a data updated successfully.");
+                        $("#success_msg_div").show();
+                        $("#error_msg_div").hide();
+                    }
+                    else{
+                        $("#error_msg").text("Error in updating GSTR2a data. Please contact tech team.");
+                        $("#success_msg_div").hide();
+                        $("#error_msg_div").show();
+                    }
+                }
+            });
+        }
+        else{
+            alert("Please Enter OTP");
+            return false;
+        }
+    }
+</script>
--- application/views/employee/show_taxpro_GSTR2a_Data.php
+++ application/views/employee/show_taxpro_GSTR2a_Data.php
@@ -1,9 +1,121 @@
-<table id></table>
+<style>
+    #GSTR2a_table_filter{
+        text-align: right;
+    }
+</style>
+<div id="page-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 ">
+                <h1 class="page-header">GSTR2A Report</h1>
+            </div>
+            <div class="col-md-12 ">
+                <table id="GSTR2a_table" class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                        <th>S No.</th>
+                        <th>Invoice No.</th>
+                        <th>Vendor Name</th>
+                        <th>GST No.</th>
+                        <th>IGST Amt</th>
+                        <th>CGST Amt</th>
+                        <th>SGST Amt</th>
+                        <th>Total Tax</th>
+                        <th>Taxable Amt</th>
+                        <th>Invoice Amt</th>
+                        <th>Releted Invoices</th>
+                        <th>Generate CN</th>
+                        <th>Reject</th>
+                    </thead>
+                    <tbody>
+
+                    </tbody>  
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 <script>
-    $.getJSON("<?php echo base_url(); ?>sample.json", function (data) {
-    $.each(data, function (index, value) {
-       console.log(value);
-        
+    var GSTR2a_datatable;
+    $(document).ready(function () {
+         
+        //datatables
+       GSTR2a_datatable = $('#GSTR2a_table').DataTable({
+            "processing": true, 
+            "serverSide": true,
+            "order": [],
+            "pageLength": 25,
+            "ajax": {
+                "url": "<?php echo base_url(); ?>employee/accounting/get_gst2ra_mapped_data",
+                "type": "POST",
+                "data": {}
+                
+            },
+            "columnDefs": [
+                {
+                    "targets": [0,1,2,3,4,5,6,7,8,9,10,11, 12], 
+                    "orderable": false 
+                }
+            ],
+            "drawCallback": function (oSettings, response) {
+               $(".invoice_select").select2();
+            } 
+        });
     });
-});
+    
+    function reject(id){
+        $.ajax({
+            type: 'POST',
+            url: '<?php echo base_url(); ?>employee/accounting/reject_taxpro_gstr2a',
+            data: {id:id},
+            success: function (data) {
+                console.log(data);
+                if(data==true){
+                    GSTR2a_datatable.ajax.reload();
+                }
+                else{
+                    alert("Error in rejecting data.");
+                }
+            }
+        });
+    }
+    
+    function generate_credit_note(id, btn){
+        var invoice_id = $("#selected_invoice_"+id).val();
+        var checksum = $(btn).attr("data-checksum");
+        var id = $(btn).attr("data-id");
+        if(invoice_id){
+            $.ajax({
+                type: 'POST',
+                url: '<?php echo base_url(); ?>employee/accounting/update_cn_by_taxpro_gstr2a',
+                data: {invoice_id:invoice_id, checksum:checksum, id:id},
+                success: function (data) {
+                    console.log(data);
+                    if(data==true){
+                        window.open("<?php echo base_url(); ?>employee/invoice/generate_gst_creditnote/"+invoice_id, '_blank');
+                        GSTR2a_datatable.ajax.reload();
+                    }
+                    else{
+                        alert("Error in generating credit note.");
+                    }
+                }
+            });
+        }
+        else{
+            alert("Please Select Invoice");
+        }
+    }
+    
+    function check_tax_amount(tax_amount, select){
+        var inv_tax = $(select).find(':selected').attr('data-tax');
+        console.log('inv_tax '+inv_tax);
+        console.log('tax_amount '+tax_amount);
+        var considarable_tax1 = tax_amount-10;
+        var considarable_tax2 = tax_amount+10;
+        if(inv_tax >= considarable_tax1 && inv_tax <= considarable_tax2){ 
+            $(select).closest("tr").find("button").attr("disabled", false);
+        }
+        else{ 
+            $(select).closest("tr").find("button").attr("disabled", true);
+        }
+    }
 </script>
