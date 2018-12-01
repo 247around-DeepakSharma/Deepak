f513f2bf17e154565a4268e3b36906725df0cbf6
kalyani tekpure <kalyanit@247around.com>
2018-12-01 10:47:07 +0530

    CRM-1791 Disable auto populate of Completed Bookings page and load data on search button

--- application/views/employee/closed_booking_filter_page.php
+++ application/views/employee/closed_booking_filter_page.php
@@ -1,9 +1,10 @@
 <div class="table_filter">
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-12" style="margin-bottom: 15px;">
+        <div class="col-md-4">
             <div class="item form-group">
                 <div class="col-md-12 col-sm-12 col-xs-12">
-                    <select class="form-control filter_table" id="partner_id">
+                    <select class="form-control" id="partner_id">
                         <option value="" selected="selected" disabled="">Select Partner</option>
                         <?php foreach($partners as $val){ ?>
                         <option value="<?php echo $val['id']?>"><?php echo $val['public_name']?></option>
@@ -12,10 +13,10 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="item form-group">
                 <div class="col-md-12 col-sm-12 col-xs-12">
-                    <select class="form-control filter_table" id="sf_id">
+                    <select class="form-control" id="sf_id">
                         <option value="" selected="selected" disabled="">Select Service Center</option>
                         <?php foreach($sf as $val){ ?>
                         <option value="<?php echo $val['id']?>"><?php echo $val['name']?></option>
@@ -24,10 +25,10 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="item form-group">
                 <div class="col-md-12 col-sm-12 col-xs-12">
-                    <select class="form-control filter_table" id="appliance">
+                    <select class="form-control" id="appliance">
                         <option value="" selected="selected" disabled="">Select Services</option>
                         <?php foreach($services as $val){ ?>
                         <option value="<?php echo $val->id?>"><?php echo $val->services?></option>
@@ -36,10 +37,12 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+        </div>
+        <div class="col-md-12" style="margin-bottom: 10px;">
+        <div class="col-md-4">
             <div class="item form-group">
                 <div class="col-md-12 col-sm-12 col-xs-12">
-                    <select class="form-control filter_table" id="ratings">
+                    <select class="form-control" id="ratings">
                         <option value="" selected="selected" disabled="">Select Rating</option>
                         <option value="a">Booking With Ratings</option>
                         <option value="b">Booking Without Ratings</option>
@@ -48,10 +51,10 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="item form-group">
                 <div class="col-md-12 col-sm-12 col-xs-12">
-                    <select class="form-control filter_table" id="city">
+                    <select class="form-control" id="city">
                         <option value="" selected="selected" disabled="">Select City</option>
                         <?php foreach($cities as $val){ ?>
                         <option value="<?php echo $val['city']?>"><?php echo $val['city']?></option>
@@ -60,12 +63,22 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="item form-group">
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <input type="text" class="form-control" id="closed_date" placeholder="Completion Date">
                 </div>
             </div>
+        </div>
+        </div>
+        <div class="col-md-12">
+        <div class="col-md-3">
+            <div class="item form-group">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <button type="button" class="btn btn-primary filter_table">Search</button>
+                </div>
+            </div>
+        </div>
         </div>
     </div>
 </div>
@@ -90,9 +103,15 @@
            placeholder: "Select Rating",
            allowClear: true
        });
-        $('.filter_table').on('change', function(){
+        $('.filter_table').click(function(){ 
+            if ( ! $.fn.DataTable.isDataTable( '#datatable1' ) ) {
+                $("#datatable1").show();
+                load_datatable();
+            }
+            else{
                datatable1.ajax.reload();
-           });
+            }
+        });
            
         $('#closed_date').daterangepicker({
                autoUpdateInput: false,
