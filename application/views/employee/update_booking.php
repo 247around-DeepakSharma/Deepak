8b8b7a06920e9d5598d58f611baaa0837379c29b
kalyani tekpure <kalyanit@247around.com>
2018-12-04 12:03:55 +0530

    CRM-1825 Resolve partner channel error occured on update booking on Admin CRM and partner CRM

--- application/views/employee/update_booking.php
+++ application/views/employee/update_booking.php
@@ -189,11 +189,7 @@
                                     <div class="col-md-6">
                                         <select class="form-control"  id="partner_source" name="partner_source" <?php if(!empty($booking_history[0]['partner_source'])){ echo "readonly";} ?>>
                                             <option value="">Please Select Seller Platform</option>
-                                            <?php foreach ($channel as $key => $values) { ?>
-                                            <option  <?php if($values['channel_name'] == $booking_history[0]['partner_source']){ echo "selected"; } else {if($is_repeat){echo 'disabled';}}?>>
-                                                <?php echo $values['channel_name']; ?> 
-                                            </option>
-                                            <?php }    ?>
+                                           
                                         </select>
                                     </div>
                                 </div>
