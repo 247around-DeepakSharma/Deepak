db1f83a41bad6d78b89e6335590769d26eda945a
Gorakh Mehta <gorakh@247around.com>
2018-12-07 14:02:25 +0530

    partner msl tag page email to sf if warehouse not found #CRM-1847
    partner msl tag page email to sf if warehouse not found #CRM-1847

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -2723,7 +2723,7 @@ class Inventory extends CI_Controller {
     function process_spare_invoice_tagging() {       
         log_message("info", __METHOD__ . json_encode($this->input->post(), true));
 //        $str = '{"is_wh_micro":"2","dated":"2018-11-20","invoice_id":"123456789","invoice_amount":"859","courier_name":"DTDC","awb_number":"123456","courier_shipment_date":"2018-11-20","wh_id":"1","part":[{"shippingStatus":"1","service_id":"46","part_name":"Back Cabinet  (TSA-2419)","part_number":"Back Cabinet  (TSA-2419)","booking_id":"","quantity":"1","part_total_price":"409.32","hsn_code":"8529","gst_rate":"18","inventory_id":"17"},{"shippingStatus":"1","service_id":"46","part_name":"Back Cover (Led Tsa 2276)","part_number":"Back Cover (Led Tsa 2276)","booking_id":"","quantity":"1","part_total_price":"318.64","hsn_code":"8529","gst_rate":"18","inventory_id":"179"}],"partner_id":"247073","partner_name":"T-Series","wh_name":" Delhi UNITED HOME CARE"}';
-//        $_POST = json_decode($str, true);
+//        $_POST = json_decode($str, true); 
         $partner_id = $this->input->post('partner_id');
         $invoice_id = $this->input->post('invoice_id');
         $invoice_dated = $this->input->post('dated');
@@ -2876,8 +2876,13 @@ class Inventory extends CI_Controller {
                                             'contact_person.entity_id' => $wh_id,
                                             'contact_person.entity_type' => _247AROUND_SF_STRING
                                         );
+                                        
                                         $email_details = $this->inventory_model->get_warehouse_details('contact_person.official_email', $wh_where, FALSE, TRUE);
-                                        if (!empty($email_details) && !empty($email_template)) {
+                                        if(empty($email_details)){
+                                            $email_details = $this->vendor_model->getVendorDetails('primary_contact_email as official_email', array('id'=>$wh_id));
+                                            
+                                        }
+                                        if (!empty($email_details) && !empty($email_template)) {    
                                             //generate part details table                                        
                                             $parts_details_table = $this->table->generate();
 
