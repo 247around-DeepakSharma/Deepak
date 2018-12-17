f4c85979e9693c5843d06a1e23d7b0358f2e78cf
Abhay Anand <abhaya@247around.com>
2018-12-17 12:48:28 +0530

    Add received button in the CP InProcess Tab. #CRM-1897 Search is not working in the Review Order Page #CRM-1899
    Add received button in the CP InProcess Tab.
    Search is not working in the Review Order Page

---
+++ application/config/buyback_constant.php
@@ -0,0 +1,37 @@
+<?php
+
+//Buyback constant
+define('_247AROUND_BB_PRICE_LIST','BB-Price-List');
+define('_247AROUND_BB_ORDER_LIST','BB-Order-List');
+define('_247AROUND_BB_PRICE_QUOTE','BB-Price-Quote');
+define('_247AROUND_BB_ORDER_ID_IMAGE_TAG','order_id_image');
+define('_247AROUND_BB_DAMAGED_ORDER_IMAGE_TAG','damaged_order_image');
+define('_247AROUND_BB_DELIVERED','Delivered');
+define('_247AROUND_BB_IN_PROCESS','InProcess');
+define('_247AROUND_BB_ORDER_COMPLETED_CURRENT_STATUS','Completed');
+define('_247AROUND_BB_REPORT_ISSUE_INTERNAL_STATUS','Claimed Raised By CP');
+define('_247AROUND_BB_ORDER_COMPLETED_INTERNAL_STATUS','Auto Approve Spec. Match');
+define('_247AROUND_BB_ORDER_NOT_RECEIVED_INTERNAL_STATUS','To Be Claimed Not Delivered');
+define('CLAIM_SUBMITTED','Claim Submitted');
+define('TO_BE_CLAIMED','To Be Claimed');
+define('CLAIM_SETTLED_BY_AMAZON','Claim Settled by Amazon');
+define('_247AROUND_BB_REPORT_ISSUE_IN_PROCESS','InProcess_Claimed_Raised_By_CP');
+define('_247AROUND_BB_NOT_DELIVERED_IN_PROCESS','InProcess_Not_delivered');
+define('_247AROUND_BB_247APPROVED_STATUS','Refunded');
+define('_247AROUND_BB_Damaged_STATUS','Damaged');
+define('_247AROUND_BB_NOT_DELIVERED','Not Delivered');
+define('_247AROUND_BB_TO_BE_CLAIMED','To Be Claimed');
+define('_247AROUND_BB_ORDER_MISMATCH','Product Mismatch');
+define('_247AROUND_BB_ORDER_REJECTED','Rejected');
+define('_247AROUND_BB_ORDER_TAT_BREACH','Tat Breach');
+define('_247AROUND_BB_TAG_CLAIMED_SUBMITTED_BROKEN', 'Broken');
+define('CLAIM_APPROVED','Claim Approved');
+define('CLAIM_REJECTED','Claim Rejected');
+define('CLAIM_SETTLED','Claim Settled');
+define('BUYBACK_TYPE','Buyback');
+define('CLAIM_DEBIT_NOTE_RAISED','Claim Debit Note Raised');
+//new shop address added
+define('NEW_SHOP_ADDRESS_ADDED','New Shop Address Added');
+define('SHOP_ADDRESS_DEACTIVATED','Shop Address De-Activated');
+define('SHOP_ADDRESS_ACTIVATED','Shop Address Activated');
+define('BB_CP_ADDRESS','CP_ADDRESS');
