3f7c2e2268119dd6613c3821a979d924bed96178
kalyani tekpure <kalyanit@247around.com>
2018-12-24 10:23:38 +0530

    CRM-1708 On success of add bank redirect page to add_bank

--- application/controllers/employee/service_centre_charges.php
+++ application/controllers/employee/service_centre_charges.php
@@ -2056,7 +2056,7 @@ class service_centre_charges extends CI_Controller {
         else{
             $this->session->set_userdata('success', 'Bank detail added successfully.');
         }
-        $this->add_bank();
+        redirect(base_url() . 'employee/service_centre_charges/add_bank');
     }
     
      /**
