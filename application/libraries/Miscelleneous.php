633bd2a9fd86ef8bd695a96d2db0ed4abddccf08
Chhavi <chhavid@247around.com>
2019-01-24 11:22:49 +0530

    missing pincode google API

--- application/libraries/Miscelleneous.php
+++ application/libraries/Miscelleneous.php
@@ -3826,4 +3826,26 @@ function generate_image($base64, $image_name,$directory){
         }
         echo json_encode($resultArray);
     }
+    function process_if_pincode_valid($pincode,$state,$city){
+         // Insert State City in India Pincode
+        $tempArray['district'] = $city;
+        $tempArray['taluk'] = $city;
+        $tempArray['region'] = $city;
+        $tempArray['state'] = $state;
+        $tempArray['pincode'] = $pincode;
+        $insertArray[] = $tempArray;  
+        $this->My_CI->vendor_model->insert_india_pincode_in_batch($insertArray);
+    }
+    function google_map_address_api($pincode){
+        $request = "https://maps.google.com/maps/api/geocode/json?address=".$pincode."&sensor=false&region=India&key=AIzaSyB4pxS4j-_NBuxwcSwSFJ2ZFU-7uep1hKc";
+        $ch = curl_init();
+        curl_setopt_array(
+        $ch, array(
+        CURLOPT_URL =>$request,
+        CURLOPT_RETURNTRANSFER => true
+        ));
+      echo  $output = curl_exec($ch);
+       // $output = '{ "results" : [ { "address_components" : [ { "long_name" : "110051", "short_name" : "110051", "types" : [ "postal_code" ] }, { "long_name" : "New Delhi", "short_name" : "New Delhi", "types" : [ "locality", "political" ] }, { "long_name" : "Delhi", "short_name" : "DL", "types" : [ "administrative_area_level_1", "political" ] }, { "long_name" : "India", "short_name" : "IN", "types" : [ "country", "political" ] } ], "formatted_address" : "New Delhi, Delhi 110051, India", "geometry" : { "bounds" : { "northeast" : { "lat" : 28.66559119999999, "lng" : 77.29854069999999 }, "southwest" : { "lat" : 28.6433122, "lng" : 77.2725126 } }, "location" : { "lat" : 28.6569035, "lng" : 77.28229229999999 }, "location_type" : "APPROXIMATE", "viewport" : { "northeast" : { "lat" : 28.66559119999999, "lng" : 77.29854069999999 }, "southwest" : { "lat" : 28.6433122, "lng" : 77.2725126 } } }, "place_id" : "ChIJ85SOHWD7DDkRI-0i7DDZy-M", "types" : [ "postal_code" ] } ], "status" : "OK" }';
+        return $output;
+    }
 }
