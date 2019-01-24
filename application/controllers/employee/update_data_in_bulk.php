2dabc01c9d3b37fc1e6914bedf207e2899ad4007
Chhavi <chhavid@247around.com>
2019-01-24 12:50:11 +0530

    #CRM-2072 Missing Pincode

--- application/controllers/employee/update_data_in_bulk.php
+++ application/controllers/employee/update_data_in_bulk.php
@@ -20,17 +20,14 @@ class update_data_in_bulk extends CI_Controller {
     
     function get_missing_pincodes_state_city(){
        $data =  $this->reusable_model->get_search_result_data("sf_not_exist_booking_details","DISTINCT pincode as pincode",array("(state = '' OR state IS NULL)"=>NULL),NULL,NULL,NULL,NULL,NULL,$groupBY=array('pincode'));
-     // $length = count($data);\
-        $length = 6;
+       $length = count($data);
        for($i = 0; $i<$length-1;$i++){
            if($data[$i]['pincode']) {
-               echo $data[$i]['pincode'];
-               echo "<br>";
                $pincodeJsonData =  $this->miscelleneous->google_map_address_api($data[$i]['pincode']);
                $pincodeArray = json_decode($pincodeJsonData,true);
+               $pincode = $data[$i]['pincode'];
                if($pincodeArray['status'] == 'OK'){
-                   // Insert State City in India Pincode
-                    $pincode = $data[$i]['pincode'];
+                   // Insert State City in India Pincode    
                     $state = $pincodeArray['results']['0']['address_components'][2]['long_name'];
                     $city = $pincodeArray['results']['0']['address_components'][1]['long_name'];
                     $this->miscelleneous->process_if_pincode_valid($pincode,$state,$city);
