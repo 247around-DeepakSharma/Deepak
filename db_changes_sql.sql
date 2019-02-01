30c887d4087550d0f259520e9b5f3766baca4aac
Chhavi <chhavid@247around.com>
2019-02-01 18:07:32 +0530

    #CRM-1674 - Send Email to CP with order list which will be auto acknowledge within 3 days

--- db_changes_sql.sql
+++ db_changes_sql.sql
@@ -9850,5 +9850,9 @@ ALTER TABLE `bb_unit_details` ADD `partner_discount` INT(10) NOT NULL DEFAULT '0
 ALTER TABLE `bb_unit_details` ADD `partner_reimbursement_invoice` TEXT NULL DEFAULT NULL AFTER `partner_discount`;
 ALTER TABLE `bb_order_details` ADD `auto_acknowledge_date` DATE NULL DEFAULT NULL AFTER `acknowledge_date`;
 
+
 --Kalyani 31-jan-2019
 ALTER TABLE `vendor_partner_variable_charges` ADD `status` TINYINT(1) NOT NULL DEFAULT '1' AFTER `validity_in_month`;
+
+INSERT INTO `email_template` (`id`, `tag`, `subject`, `template`, `from`, `to`, `cc`, `bcc`, `active`, `create_date`) VALUES (NULL, 'auto_acknowledge_alert_to_cp', 'Auto Acknowledge Orders', 'Dear Partner, <br>\r\nFollowing orders will be auto acknowledge soon <br><br>\r\n%s', 'noreply@247around.com', '', 'sunilk@247around.com', '', '1', '2019-02-01 16:41:14');
+
