69b45e37fb9b85d5eb1585779353927e12f4ac59
Rajshree Vyas <rajshreev@247around.com>
2019-02-06 18:22:23 +0530

    #CRM-1901.Upload Sample Number Picture at the add/edit Partner.some changes after testing.

--- application/views/employee/addpartner.php
+++ application/views/employee/addpartner.php
@@ -1608,7 +1608,7 @@
             
              <hr>
             <?php
-                if(!empty($result['sample_no_pic'])){
+            if(!empty($results['sample_no_pic'])){
                     ?>
             <div id="exist_documents">
                 <table class="table">
@@ -4234,13 +4234,23 @@
             });
         }
     }
-    function deletesamplenopic(id)
+    function deletesamplenopic(id,partner_id)
     {
         var url="<?php echo base_url()?>";
         var r=confirm("Do you want to delete this?");
+       
         if (r==true)
         {
-          window.location = url+"employee/partner/deletePartnerSampleNo/"+id;
+         // window.location = url+"employee/partner/deletePartnerSampleNo/"+id+"/"+partner_id;
+          $.ajax({
+                type: 'POST',
+                url: "<?php echo base_url(); ?>employee/partner/deletePartnerSampleNo",
+                data:{id:id,partner_id:partner_id},
+                success: function (data) {
+                     alert(data);
+                     location.reload();
+                }
+            });
         }
         else
         {
