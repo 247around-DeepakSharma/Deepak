68c07af13a7559378177771cfd4fecb6fe62cb4a
Gorakh Mehta <gorakh@247around.com>
2019-02-18 14:14:20 +0530

    Declaration Latter to be added in SF Panel for Courier  #CRM-2154
    Declaration Latter to be added in SF Panel for Courier  #CRM-2154
    
    application/views/service_centers/print_couriers_declaration_details.php

---
+++ application/views/service_centers/print_couriers_declaration_details.php
@@ -0,0 +1,60 @@
+
+<style>
+    h2{
+        text-decoration: underline; 
+        text-align:center;
+        padding:1px;
+        margin-top:10px; 
+    } 
+    p{
+        font-size:18px;
+    }
+    table, th, td{
+        border: 1px solid black;
+    }
+    th {
+        text-align: left;
+    }
+</style>
+
+<!DOCTYPE html>
+<html>
+    <body> 
+        <?php foreach ($coueriers_declaration as $value) { ?>
+            <br><br> 
+            <p>
+            <h2>TO WHOMSOEVER IT MAY CONCERN</h2>
+        </p>
+        <br><br><br> 
+        <p>
+            I/ We hereby confirm that the <?php echo $value['public_name']; ?> <?php echo $value['appliance_name']; ?> Spare Parts is being
+
+            sent to SF Name . ( Address :- <?php echo $value['address']; ?>, <?php echo $value['district']; ?> ,<?php echo $value['state']; ?>,
+
+            Pin Code  <?php echo $value['pincode']; ?>) is for the repair purpose and not for sale. It doesn’t
+
+            carry any commercial value.
+        </p>
+        <br><br><br><br>
+        <table>
+            <tr>
+                <th>S.No</th>
+                <th>Shipped Part</th>
+                <th style="width:20%">Spare Amount</th> 
+            </tr>
+            <tr>
+                <td>1</td>
+                <td><?php echo $value['parts_shipped']; ?></td>
+                <td><?php echo $value['challan_approx_value']; ?></td>
+            </tr>
+
+        </table>
+        <br><br><br><br>
+        <p>Thanking you.</p>
+        <br><br><br>
+        <p>Authorized Signatory.</p>
+        <br><br><br><br><br><br><br><br><br><br><br><br>
+<?php } ?>
+</body>
+</html>
+
