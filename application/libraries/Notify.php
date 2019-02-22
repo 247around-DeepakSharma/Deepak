936a8a68f91e9332c95c846fc32e1544d5921a00
Rajshree Vyas <rajshreev@247around.com>
2019-02-22 14:21:20 +0530

    # CRM-1780.Send SMS link to customer for Brand Collateral.use phone no of customer.

--- application/libraries/Notify.php
+++ application/libraries/Notify.php
@@ -614,7 +614,7 @@ class Notify {
                     if(!empty($data))
                     {
                             $finalString = 'Your Brand Collateral ->   '.nl2br ("\n");
-                            $index =0;
+                            $index =1;
                             foreach($data as $collatralData){
                                    if($collatralData['is_file']){
                                     $brand_coll_url = "https://s3.amazonaws.com/".BITBUCKET_DIRECTORY."/vendor-partner-docs/".$collatralData['file'];
@@ -633,7 +633,7 @@ class Notify {
                                 $index++;
                             }
                             $smsbody=$finalString;
-                            $status  = $this->My_CI->notify->sendTransactionalSmsMsg91($query1[0]['primary_contact_phone_1'],$smsbody,SMS_WITHOUT_TAG);
+                            $status  = $this->My_CI->notify->sendTransactionalSmsMsg91($query1[0]['booking_primary_contact_no'],$smsbody, SMS_WITHOUT_TAG);
             
                             //For saving SMS to the database on sucess
                             $this->My_CI->notify->add_sms_sent_details($query1[0]['user_id'], 'user' , $query1[0]['primary_contact_phone_1'],
