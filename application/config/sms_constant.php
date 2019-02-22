b9b8b9568059812804a4662f8cd7c69b1e0be36e
Gorakh Mehta <gorakh@247around.com>
2019-02-22 14:48:14 +0530

    Review changes on 62 branch #CRM-2221

--- application/config/sms_constant.php
+++ application/config/sms_constant.php
@@ -11,3 +11,9 @@ define('SPARE_REQUESTED_DEALER_SMS_TAG', 'sms_requested_dealer_sms_tag');
 define("SPARE_DELIVERED_CUSTOMER_SMS_TAG", "sms_delivered_customer_tag");
 //SMS send to delaer when new spare delivered to SF
 define('SPARE_DELIVERED_DEALER_SMS_TAG', 'sms_delivered_dealer_sms_tag');
+//SMS send to customer when requested spare parts is out of warranty on approval
+define('SPARE_ON_OUT_OF_WARRANTY_SMS_TAG', 'sms_oow_spare_parts_customer_tag');
+//SMS send to customer when requested spare parts is in warranty on approval
+define('SPARE_ON_IN_WARRANTY_SMS_TAG', 'sms_in_warranty_spare_parts_customer_tag');
+
+
