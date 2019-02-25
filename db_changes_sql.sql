c4caf54ef7c8e571ff516d4192a91a9dffab5456
kalyani tekpure <kalyanit@247around.com>
2019-02-25 12:56:35 +0530

    Review Changes 62

--- db_changes_sql.sql
+++ db_changes_sql.sql
@@ -9825,7 +9825,7 @@ ALTER TABLE `symptom_spare_request`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --Kalyani 30-jan-2019
-INSERT INTO `header_navigation` (`id`, `entity_type`, `title`, `title_icon`, `link`, `level`, `parent_ids`, `groups`, `nav_type`, `is_active`, `create_date`) VALUES (NULL, 'Partner', 'Appliance Model Mapping', 'partner/inventory/appliance_model_mapping', 'partner/inventory/appliance_model_mapping', '2', '148', 'Primary Contact,Area Sales Manager,Booking Manager,Owner', 'main_nav', '1', CURRENT_TIMESTAMP);
+INSERT INTO `header_navigation` (`id`, `entity_type`, `title`, `title_icon`, `link`, `level`, `parent_ids`, `groups`, `nav_type`, `is_active`, `create_date`) VALUES (NULL, 'Partner', 'Servicable BOM', 'partner/inventory/appliance_model_mapping', 'partner/inventory/appliance_model_mapping', '2', '148', 'Primary Contact,Area Sales Manager,Booking Manager,Owner', 'main_nav', '1', CURRENT_TIMESTAMP);
 --Kalyani 31-jan-2019
 ALTER TABLE `vendor_partner_variable_charges` ADD `status` TINYINT(1) NOT NULL DEFAULT '1' AFTER `validity_in_month`;
 
