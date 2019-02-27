08dbf3d35deaee778d7d1dd0c7443a463328831f
Gorakh Mehta <gorakh@247around.com>
2019-02-27 11:00:11 +0530

    View to show partners managed by account manager #CRM-1671
    View to show partners managed by account manager #CRM-1671

--- application/views/employee/partners_list_managed_by_account_manager.php
+++ application/views/employee/partners_list_managed_by_account_manager.php
@@ -48,7 +48,7 @@
                     pageSize: 'LEGAL',
                     title: 'Partners_manage_by_account_manager',
                     exportOptions: {
-                       columns: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16, 17],
+                       columns: [1,2],
                         modifier : {
                              // DataTables core
                              order : 'index',  // 'current', 'applied', 'index',  'original'
