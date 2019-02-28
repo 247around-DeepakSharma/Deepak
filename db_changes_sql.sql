5d286a26eb15f469cae6f2eac1ef72a9b4829b3c
Gorakh Mehta <gorakh@247around.com>
2019-02-28 12:08:50 +0530

    Approve Courier on Spare Parts Booking page #CRM-2296
    Approve Courier on Spare Parts Booking page #CRM-2296

--- db_changes_sql.sql
+++ db_changes_sql.sql
@@ -10068,3 +10068,12 @@ ALTER TABLE `partner_file_upload_header_mapping` ADD `category` VARCHAR(128) NUL
 --Abhay
 ALTER TABLE `partners` ADD `oot_spare_to_be_shipped` INT(11) NOT NULL DEFAULT '60' AFTER `is_defective_part_return_wh`;
 ALTER TABLE trigger_partners ADD `oot_spare_to_be_shipped` INT(11) NOT NULL DEFAULT '60' AFTER `is_defective_part_return_wh`;
+--- Gorakh 26-02-2019---
+INSERT INTO `sms_template` (`tag`, `template`, `comments`, `active`, `is_exception_for_length`, `create_date`) VALUES
+('sms_oow_spare_parts_customer_tag', 'Request of your payable  %s for  %s is placed. You will get an SMS on delivery. Thanks, 247around, 9555000247.', '', '1', 0, '2019-02-26 09:17:05'),
+('sms_in_warranty_spare_parts_customer_tag', 'Request of your free  %s for  %s is placed. You will get an SMS on delivery. Thanks, 247around, 9555000247.', '', '1', 0, '2019-02-26 09:17:16');
+
+INSERT INTO `email_template` (`tag`, `subject`, `template`, `from`, `to`, `cc`, `bcc`, `active`, `create_date`) VALUES
+('spare_parts_oow_email_to_customer', '%s Requested for booking id %s', 'Dear SF, <br/> <br/> Request of your %s to be paid by customer, is placed successfully, for booking id %s.\n<br/> <br/>Thanks,<br/>Team 247around', 'gorakhn@247around.com', 'gorakhn@247around.com', 'gorakhn@247around.com', '', '1', '2019-02-21 09:26:12'),
+('spare_parts_in_warranty_email_to_customer', '%s Requested for booking id %s', 'Dear SF, <br/> <br/> Request of your %s free for customer, is placed successfully,for booking id %s.<br/> <br/>Thanks,<br/>Team 247around', 'gorakhn@247around.com', 'gorakhn@247around.com', 'gorakhn@247around.com', '', '1', '2019-02-21 09:35:08');
+
