90dd13b7b1a31c007c3d40135ecc8dfa3d970770
Chhavi <chhavid@247around.com>
2019-02-28 18:13:08 +0530

    minor

--- application/views/dashboard/buyback_dashboard.php
+++ application/views/dashboard/buyback_dashboard.php
@@ -73,7 +73,8 @@
                            <td>{{$index+1}}</td>
                            <td>{{x.status}}</td>
                            <td>{{x.amount}}</td>
-                           <td><a target="_blank" href="<?php echo base_url()?>buyback/buyback_process/show_without_invoices_orders/{{x.status}}">{{x.count}}</a></td>
+<!--                           <td><a target="_blank" href="<?php echo base_url()?>buyback/buyback_process/show_without_invoices_orders/{{x.status}}">{{x.count}}</a></td>-->
+                            <td><a target="_blank">{{x.count}}</a></td>
                         </tr>
                     </tbody>
                 </table>
