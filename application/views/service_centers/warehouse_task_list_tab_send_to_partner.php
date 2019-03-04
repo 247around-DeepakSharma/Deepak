03d888b928c414784add1ef8e05a9d143eda42d8
Abhay Anand <abhaya@247around.com>
2019-03-04 12:31:31 +0530

    Invoice is not generting from new warehouse, part shipped by old warehouse #CRM-2349

--- application/views/service_centers/warehouse_task_list_tab_send_to_partner.php
+++ application/views/service_centers/warehouse_task_list_tab_send_to_partner.php
@@ -104,7 +104,7 @@
                                     </td>
                                     <td>
                                         
-                                        <input type="checkbox" class="check_single_row" data-entity_type ="<?php echo $row['entity_type']; ?>" data-service_center_id ="<?php echo $row['service_center_id']; ?>" data-part_name ="<?php echo $row['defective_part_shipped']; ?>" data-model="<?php echo $row['model_number_shipped']; ?>" data-shipped_inventory_id = "<?php echo $row['shipped_inventory_id']?>" data-booking_id ="<?php echo $row['booking_id']?>" data-partner_id = "<?php echo $row['partner_id']?>" data-spare_id = "<?php echo $row['id']?>" data-booking_partner_id = "<?php echo $row['booking_partner_id']?>">
+                                        <input type="checkbox" class="check_single_row" data-is_micro_wh ="<?php echo $row['is_micro_wh'];?>" data-defective_return_to_entity_type ="<?php echo $row['defective_return_to_entity_type']; ?>" data-defective_return_to_entity_id="<?php echo $row['defective_return_to_entity_id'];?>" data-entity_type ="<?php echo $row['entity_type']; ?>" data-service_center_id ="<?php echo $row['service_center_id']; ?>" data-part_name ="<?php echo $row['defective_part_shipped']; ?>" data-model="<?php echo $row['model_number_shipped']; ?>" data-shipped_inventory_id = "<?php echo $row['shipped_inventory_id']?>" data-booking_id ="<?php echo $row['booking_id']?>" data-partner_id = "<?php echo $row['partner_id']?>" data-spare_id = "<?php echo $row['id']?>" data-booking_partner_id = "<?php echo $row['booking_partner_id']?>">
                                     </td>
                             </tr>
                             <?php $sn_no++; } ?>
@@ -337,8 +337,11 @@
         $(".check_single_row:checked").each(function (key) {
             tmp_arr[key] = {};
             tmp_arr[key]['inventory_id'] = $(this).attr('data-shipped_inventory_id');
+            tmp_arr[key]['is_micro_wh'] = $(this).attr('data-is_micro_wh');
             tmp_arr[key]['booking_id'] = $(this).attr('data-booking_id');
             tmp_arr[key]['partner_id'] = $(this).attr('data-partner_id');
+            tmp_arr[key]['defective_return_to_entity_id'] = $(this).attr('data-defective_return_to_entity_id');
+            tmp_arr[key]['defective_return_to_entity_type'] = $(this).attr('data-defective_return_to_entity_type');
             tmp_arr[key]['spare_id'] = $(this).attr('data-spare_id');
             tmp_arr[key]['part_name'] = $(this).attr('data-part_name');
             tmp_arr[key]['service_center_id'] = $(this).attr('data-service_center_id');
