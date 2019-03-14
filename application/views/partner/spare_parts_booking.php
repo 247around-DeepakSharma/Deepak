e2f716a7122583ec6840ec5fe5b8a265b63c4541
Abhay Anand <abhaya@247around.com>
2019-03-14 12:06:17 +0530

    remove sorting from checkbox column

--- application/views/partner/spare_parts_booking.php
+++ application/views/partner/spare_parts_booking.php
@@ -171,7 +171,7 @@
             },
             "columnDefs": [
                 {
-                    "targets": [0,1,2,5,6,7,8,9,10,11,12,13], //first column / numbering column
+                    "targets": [0,1,2,5,6,7,8,9,10,11,12,13,14], //first column / numbering column
                     "orderable": false //set not orderable
                 }
             ],  
