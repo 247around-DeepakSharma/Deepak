12a5ef1b3a1a2b4d97e51ec8fbd82d841b0dcf49
Abhay Anand <abhaya@247around.com>
2019-04-02 20:07:53 +0530

    Exotel numbers migration #CRM-2604

--- application/config/constants.php
+++ application/config/constants.php
@@ -237,11 +237,11 @@ define('ERR_ORDER_ID_NOT_FOUND_CODE', -1007);
 //Define error code for order id not found for partner_sd_sb.php file
 define('ERR_ORDER_ID_NOT_FOUND_MSG', 'Order ID Does Not Exist');
 //Define snapdeal new missed call number
-define('PARTNERS_MISSED_CALLED_NUMBER','01130017601');
+define('PARTNERS_MISSED_CALLED_NUMBER','01143092150');
 //Define snapdeal new missed call number
 define('PARTNERS_MISSED_CALLED_NUMBER_2','01143092150');
 //Define snapdeal new missed call number
-define('SNAPDEAL_MISSED_CALLED_NUMBER','01139595247');
+define('SNAPDEAL_MISSED_CALLED_NUMBER','01143078384');
 //Define Android App new missed call number
 define('ANDROID_APP_MISSED_CALLED_NUMBER','01139585684');
 
