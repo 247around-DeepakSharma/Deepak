e7f354cc1ccd9f0fd7864d08b3c89ed0f9a9bc1f
Chhavi <chhavid@247around.com>
2019-04-09 12:49:55 +0530

    Minor

--- application/config/email_template_constant.php
+++ application/config/email_template_constant.php
@@ -1,4 +1,4 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 define('PARTNER_SUMMARY_EMAIL_TEMPLATE','81e09dfb-538b-4c8b-b089-e43822db88c2');
 define('COMPLETED_BOOKING_CUSTOMER_TEMPLATE','2b296031-dec8-4354-8408-846a0bbeff27');
-define('BUYBACK_DISPUTED_ORDERS_SUMMARY','buyback_disputed_orders_summary');
+
