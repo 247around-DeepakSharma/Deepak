9045dccf97f99d468a77b8466ef1e35d8ff492db
kalyani tekpure <kalyanit@247around.com>
2019-04-30 22:28:50 +0530

    print address dynamic

--- application/views/partner/print_address.php
+++ application/views/partner/print_address.php
@@ -4,7 +4,7 @@
 <div style="display:inline; height: 400px; float:left;border: 1px solid #ccc; margin-left: 10px;margin-top:10px;width: 320px;padding: 8px;">
     <div style="border: 1px solid #ccc;padding:8px; width: 300px;">
         <?php if($value['main_company_public_name']){ ?>
-        <img style='vertical-align:middle;width:50px;' src="<?php echo "https://s3.amazonaws.com/".BITBUCKET_DIRECTORY."/misc-images/".$meta['main_company_logo']; ?>">
+        <img style='vertical-align:middle;width:50px;' src="<?php echo "https://s3.amazonaws.com/".BITBUCKET_DIRECTORY."/misc-images/".$value['main_company_logo']; ?>">
         <?php } ?>
         <div style='vertical-align:middle; display:inline;font-weight:bold; font-size: 20px;margin-left:10px;'>
             <?php echo $value['main_company_logo']." Service Center";  ?>
