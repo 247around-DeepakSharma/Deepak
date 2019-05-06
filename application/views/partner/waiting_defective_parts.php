550dae7bb72e8da0b571b8a7fdebf37307ff57ba
abhishek2707 <abhisheka@247around.com>
2019-05-06 13:05:37 +0530

    Add Appliance Name in the Pending spare TAB in the Partner Panel #CRM-2857
    Add Appliance Name in the Pending spare TAB in the Partner Panel

--- application/views/partner/waiting_defective_parts.php
+++ application/views/partner/waiting_defective_parts.php
@@ -144,7 +144,7 @@
             },
             "columnDefs": [
                 {
-                    "targets": [0,2,5,6,7,8,10,11,12,13], //first column / numbering column
+                    "targets": [0,1,2,5,6,7,8,10,11,12,13], //first column / numbering column
                     "orderable": false //set not orderable
                 }
             ],  
