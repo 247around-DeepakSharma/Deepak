ee41998bc769e2ebc1d960337ca00d0d5f831402
abhishek2707 <abhisheka@247around.com>
2019-05-09 14:00:53 +0530

    view file remove view image in empty case #CRM-2914
    view file remove view image in empty case #CRM-2914

--- application/views/service_centers/get_update_form.php
+++ application/views/service_centers/get_update_form.php
@@ -145,7 +145,7 @@
                                             <input type="file" class="form-control spare_parts   " id="serial_number_pic"       name="serial_number_pic" >
                                             <input type="hidden" value="<?php if(isset($unit_serial_number_pic) && !empty($unit_serial_number_pic)){echo $unit_serial_number_pic;}  ?>"  name="serial_number_pic_exist" >
                                         </div>
-                                        <?php if(!isset($unit_serial_number_pic) ||  empty($unit_serial_number_pic)){ ?>
+                                        <?php if(isset($unit_serial_number_pic)  && !empty($unit_serial_number_pic)){ ?>
                                             <a target="_blank" class="<?php if(!isset($unit_serial_number_pic) ||  empty($unit_serial_number_pic)){echo 'hide';}  ?>" href="<?php if(isset($unit_serial_number_pic) && !empty($unit_serial_number_pic)){echo S3_WEBSITE_URL.$unit_serial_number_pic;}  ?>">View</a>
                                      <?php    } ?>
 
