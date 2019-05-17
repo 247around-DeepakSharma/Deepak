56c7c4f49701321864453f4d3b8f9c03814cdfbb
abhishek2707 <abhisheka@247around.com>
2019-05-17 18:05:26 +0530

    Parts Requested (Rejected) in admin panel in spare part bookings #CRM-3017
    Parts Requested (Rejected) in admin panel in spare part bookings

--- application/controllers/employee/spare_parts.php
+++ application/controllers/employee/spare_parts.php
@@ -863,15 +863,17 @@ class Spare_parts extends CI_Controller {
         $row[] = '<button type="button" data-booking_id="' . $spare_list->booking_id . '" data-url="' . base_url() . 'employee/inventory/update_action_on_spare_parts/' . $spare_list->id . '/' . $spare_list->booking_id . '/' . $required_parts . '" class="btn btn-sm ' . $cl . ' open-adminremarks" data-toggle="modal" data-target="#myModal2">' . $text . '</button>';
         
         if ($this->session->userdata('user_group') == 'admin'  || $this->session->userdata('user_group') == 'inventory_manager' || $this->session->userdata('user_group') == 'developer') {
-            if ($request_type == SPARE_PARTS_REQUESTED) {
-                if ($spare_list->part_requested_on_approval == '0' && $spare_list->status == SPARE_PART_ON_APPROVAL) {
+             if ($request_type == SPARE_PARTS_REQUESTED || $request_type == SPARE_PART_ON_APPROVAL ) {
+               // if ($spare_list->part_requested_on_approval == '0' && ($spare_list->status == SPARE_PART_ON_APPROVAL || $spare_list->status == SPARE_PA)) {
                     $appvl_text = 'Approve';
                     $cl = "btn-info";
                     $row[] = '<button type="button" data-keys="' . $spare_list->part_warranty_status . '" data-booking_id="' . $spare_list->booking_id . '" data-url="' . base_url() . 'employee/spare_parts/spare_part_on_approval/' . $spare_list->id . '/' . $spare_list->booking_id . '" class="btn  ' . $cl . ' open-adminremarks" data-toggle="modal" id="approval_' . $no . '" data-target="#myModal2">' . $appvl_text . '</button>';
-                } else {
-                    $row[] = '<button style="background-color: #1a8a2dd4;color:#fff;" disabled class="btn">Approved</button>';
+                }else{
+                    
+                    $appvl_text = 'Cancelled';
+                    $row[] = '<button class="btn btn-danger" type="button">' . $appvl_text . '</button>';  
                 }
-            }
+            //}
         }
         
         $c_tag = ($spare_list->part_warranty_status == SPARE_PART_IN_OUT_OF_WARRANTY_STATUS && $spare_list->status != SPARE_PARTS_REQUESTED) ? "QUOTE_REQUEST_REJECTED" : "CANCEL_PARTS";
@@ -898,10 +900,16 @@ class Spare_parts extends CI_Controller {
         if($this->input->post("status") == SPARE_PARTS_REQUESTED){
             $post['where_in']['status'] = array(SPARE_PARTS_REQUESTED, SPARE_PART_ON_APPROVAL);
             $post['request_type'] = SPARE_PARTS_REQUESTED;
-
-        }else{
+        }else if($this->input->post("status") == SPARE_PART_ON_APPROVAL){
+            $post['where_in']['status'] = array(SPARE_PART_ON_APPROVAL);
+            $post['request_type'] = SPARE_PART_ON_APPROVAL; 
+        }else if($this->input->post("status") == _247AROUND_CANCELLED){
             $post['where']['status'] = $this->input->post("status");  
-            $post['request_type'] = SPARE_OOW_EST_REQUESTED;
+            $post['request_type'] = _247AROUND_CANCELLED;  
+        }else{         
+            $post['where']['status'] = $this->input->post("status");  
+            $post['request_type'] = SPARE_OOW_EST_REQUESTED; 
+ 
         }
         
         
