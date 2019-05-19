f397381a79b7a5e1dc30658b6e75fbf74d4c713c
Gorakh Mehta <gorakh@247around.com>
2019-05-19 12:44:56 +0530

    Serviceable BOM details on SF panel #CRM-3023
    Serviceable BOM details on SF panel #CRM-3023

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -2240,15 +2240,11 @@ class Inventory extends CI_Controller {
         $row[] = $stock_list->type;
         $row[] = "<span style='word-break: break-all;'>" . $stock_list->part_name . "</span>";
         $row[] = "<span style='word-break: break-all;'>" . $stock_list->part_number . "</span>";
-        //$row[] = $stock_list->hsn_code;
-        //$row[] = "<i class ='fa fa-inr'></i> " . number_format((float)$stock_list->price+($stock_list->price*($stock_list->oow_around_margin)/100), 2, '.', '');
-        //$row[] = $stock_list->gst_rate . "%";
-        $total = number_format((float) ($stock_list->price + ($stock_list->price * ($stock_list->gst_rate / 100))), 2, '.', '');
+        $sf_price = number_format((float)$stock_list->price+($stock_list->price*($stock_list->oow_around_margin)/100), 2, '.', '');
+        $total = number_format((float) ($sf_price + ($sf_price * ($stock_list->gst_rate / 100))), 2, '.', '');
         $row[] = "<i class ='fa fa-inr'></i> " . $total;
         $row[] = $stock_list->oow_vendor_margin . " %";
-        $customertot = number_format((float)($stock_list->price + ($stock_list->price * ($stock_list->gst_rate /100))), 2, '.', '');
-        $customertot = number_format((float)$customertot+($customertot*($stock_list->oow_vendor_margin+$stock_list->oow_around_margin)/100), 2, '.', '');
-        $row[] = $customertot;
+        $row[] = number_format((float)$total+($total*($stock_list->oow_vendor_margin)/100), 2, '.', '');
         return $row;
     }
     
