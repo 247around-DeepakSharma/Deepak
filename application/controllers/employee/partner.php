b310532ef2152772179fd4721ebfce467121e19f
Chhavi <chhavid@247around.com>
2019-05-20 17:49:30 +0530

    #CRM-3035 - If Vendor not available allowed booking from Call Center Panel - Kenstar

--- application/controllers/employee/partner.php
+++ application/controllers/employee/partner.php
@@ -3218,7 +3218,22 @@ class Partner extends CI_Controller {
             'vendor_pincode_mapping.Pincode' => $pincode,
             'vendor_pincode_mapping.Appliance_ID' => $service_id);
         $city = $this->vendor_model->get_vendor_mapping_data($where, $select);
-        if (!empty($city)) {
+        if(empty($city)){
+            $saas_flag = $this->booking_utilities->check_feature_enable_or_not(PARTNER_ON_SAAS);
+            if($saas_flag){
+                $select = 'india_pincode.district as district,india_pincode.state as state';
+                $post_city = $this->input->post('city');
+                $where = array('india_pincode.pincode' => $pincode);
+                $city = $this->reusable_model->get_search_result_data('india_pincode',$select,$where,NULL,NULL,NULL,NULL,NULL,array());
+            }
+            else{
+               echo 'ERROR';
+            }
+            $booking = array('booking_id' => 'Not_Generated', 'booking_pincode' => $pincode,'service_id' => $service_id, 'partner_id' => $this->session->userdata('partner_id'),'city'=>'Not_Received',
+            'order_id'=>'Not_Received');
+            $this->miscelleneous->sf_not_exist_for_pincode($booking);
+        }
+        if(!empty($city)) {
             $stateWhere['service_id'] = $service_id;
             $stateWhere['state'] = $city[0]['state'];
             $stateWhere['partner_id'] = $this->session->userdata('partner_id');
@@ -3244,12 +3259,7 @@ class Partner extends CI_Controller {
             else{
                 echo "Not_Serve";
             }
-        } else {
-            $booking = array('booking_id' => 'Not_Generated', 'booking_pincode' => $pincode,'service_id' => $service_id, 'partner_id' => $this->session->userdata('partner_id'),'city'=>'Not_Received',
-                'order_id'=>'Not_Received');
-            $this->miscelleneous->sf_not_exist_for_pincode($booking);
-            echo 'ERROR';
-        }
+        } 
     }
 
     /**
