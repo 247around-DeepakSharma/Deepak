ad8e5b443507bbf0581c59fad8cf6b931baef89c
abhishek2707 <abhisheka@247around.com>
2019-05-22 11:47:58 +0530

    Add menu in Service Center Panel for bulk tranfer

--- application/views/service_centers/header.php
+++ application/views/service_centers/header.php
@@ -204,8 +204,7 @@
                                 if ($this->session->userdata('is_wh') == 1) { ?> 
                                 <li><a href="<?php echo base_url(); ?>service_center/spare_transfer">Spare Transfer</a></li>
                                 <li class="divider"></li>
-                                <li><a href="<?php echo base_url(); ?>service_center/bulkConversion">Bulk Spare Transfer</a></li> 
-                                <li class="divider"></li>
+                              
                                 <?php   }
                                     ?>
                             </ul>
@@ -287,7 +286,14 @@
                                 <?php if($this->session->userdata('is_micro_wh') == 1){ ?>
                                 <li class="divider"></li>
                                 <li><a href="<?php echo base_url();?>service_center/acknowledge_spares_send_by_partner"><strong>Acknowledge Spares Send By Partner </strong></a></li>
+                                <li class="divider"></li>
                                 <?php }?>  
+                                <?php  
+                                if ($this->session->userdata('is_wh') == 1) { ?> 
+                                <li><a href="<?php echo base_url(); ?>service_center/bulkConversion"><strong>Bulk Spare Transfer</strong></a></li> 
+                                <li class="divider"></li>
+                                <?php   }
+                                    ?>
                             </ul>
                         </li>
 
