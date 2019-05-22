e805a8491f090906aa4eae988196f3baf95c3c36
abhishek2707 <abhisheka@247around.com>
2019-05-22 17:33:14 +0530

    Shipment Parts type not showing on partner pane  #CRM-2981

--- application/views/partner/update_spare_parts_form.php
+++ application/views/partner/update_spare_parts_form.php
@@ -159,12 +159,14 @@
                                             <input type="hidden" id="<?php echo "shippedmodelnumber_".$key;?>" name="part[<?php echo $key;?>][shipped_model_number]">
                                         </div>
                                         <?php } else if(isset($appliance_model_details) && !empty($appliance_model_details)){ ?>
+                                            <div class="col-md-7">
                                             <select class="form-control spare_parts shipped_model_number_id" onchange="change_shipped_model('<?php echo $key;?>')" id="<?php echo "shippedmodelnumberid_".$key;?>" name="part[<?php echo $key; ?>][shipped_model_number_id]" required="">
-                                                <option value="" disabled="" selected="">Select Model Number</option>
+                                                <option value="" disabled="" selected="">Select Model Number  2</option>
                                                 <?php foreach ($appliance_model_details as $key1 => $value1) { ?> 
                                                 <option value="<?php echo $value1['id']; ?>" <?php if($value1['model_number'] == $value->model_number){ echo "selected"; } ?> ><?php echo $value1['model_number']; ?></option>
                                                 <?php } ?>
                                             </select>
+                                        </div>
                                             <input type="hidden" id="<?php echo "shippedmodelnumber_".$key;?>" name="part[<?php echo $key;?>][shipped_model_number]">
                                         
                                         <?php } else { ?> 
