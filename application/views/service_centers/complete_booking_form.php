e96bc6e1a240e4621db617023fde88c757624839
ankitr17 <ankitr@247around.com>
2019-05-25 10:53:11 +0530

    provision of sf purchase invoice functionality

--- application/views/service_centers/complete_booking_form.php
+++ application/views/service_centers/complete_booking_form.php
@@ -137,7 +137,10 @@
                     <input type="hidden" name="approval" value='0' />
                     <input type="hidden" name="count_unit" id ="count_unit" value="<?php echo count($bookng_unit_details);?>" />
                     <input type="hidden" name="mismatch_pincode" id="mismatch_pincode" value="<?php if(isset($mismatch_pincode)) { echo $mismatch_pincode; }?>" />
-                    <?php $k_count = 0; $count = 0; foreach ($bookng_unit_details as $key1 => $unit_details) { ?>
+                    <input type="hidden" name="is_sf_purchase_invoice_required" id="is_sf_purchase_invoice_required" value="<?= (!empty($is_sf_purchase_invoice_required) ? 1 : 0); ?>">
+                    <?php
+                    
+                    $k_count = 0; $count = 0; foreach ($bookng_unit_details as $key1 => $unit_details) { ?>
                     <div class="clonedInput panel panel-info " id="clonedInput1">
                         <div class="panel-body">
                             <div class="row">
@@ -155,8 +158,8 @@
                                             </div>
                                         </div>
                                         <input type="hidden" id="<?php echo "count_line_item_".$key1;?>" value="<?php echo count($unit_details['quantity']);?>"/>
-                                        <div class="form-group col-md-3" style="<?php if($this->session->userdata('is_engineer_app') == 1){?>width:22.6%;
-                                            <?php } else {?> width:22.6%;<?php }?>">
+                                        <div class="form-group col-md-3" style="<?php if($this->session->userdata('is_engineer_app') == 1){?>width:16.95%;
+                                            <?php } else {?> width:16.95%;<?php }?>">
                                             <div class="col-md-12 ">
                                                  <label> Brand</label>
                                                 <select type="text" disabled="" class="form-control appliance_brand"    name="appliance_brand[]" id="appliance_brand_1" >
@@ -164,7 +167,7 @@
                                                 </select>
                                             </div>
                                         </div>
-                                        <div class="form-group col-md-3" style="width:22.6%">
+                                        <div class="form-group col-md-3" style="width:16.95%">
                                             <div class="col-md-12 ">
                                                 <label> Category</label>
                                                 <select type="text" disabled="" class="form-control appliance_category"   id="appliance_category_1" name="appliance_category[]"  >
@@ -172,7 +175,7 @@
                                                 </select>
                                             </div>
                                         </div>
-                                        <div class="form-group col-md-3"style="width:22.6%" style=" padding-right: 0px;">
+                                        <div class="form-group col-md-3"style="width:16.95%" style=" padding-right: 0px;">
                                             <div class="col-md-12">
                                                 <label> Capacity</label>
                                                 <select type="text" disabled="" class="form-control appliance_capacity"   id="appliance_capacity_1" name="appliance_capacity[]" >
@@ -183,13 +186,31 @@
                                                 
                                             </div>
                                         </div>
-                                        <div class="form-group col-md-3"style="width:21.6%" style=" padding-right: 0px;">
+                                        <div class="form-group col-md-3"style="width:16.95%;margin-left:6px !important;" style=" padding-right: 0px;">
                                             <label> Purchase Date</label>
                                             <div class="input-group input-append date">
                                                 <input  autocomplete="off" onkeydown="return false" onchange="update_dop_for_unit('<?php echo $key1?>')"  id="<?php echo "dop_".$key1?>" class="form-control dop" placeholder="Purchase Date" name="dop[]" type="text" value="<?php if(isset($booking_history['spare_parts'])){  echo $booking_history['spare_parts'][0]['date_of_purchase']; } ?>">
                                                         <span class="input-group-addon add-on" onclick="dop_calendar('<?php echo "dop_".$key1?>')"><span class="glyphicon glyphicon-calendar"></span></span>
                                              </div>
                                         </div>
+                                        <div class="form-group col-md-3"style="width:16.95%;margin-left:15px !important;">
+                                            <label>SF Purchase Invoice</label>
+                                           
+                                            <input type="file" name="sf_purchase_invoice" id="sf_purchase_invoice" value="<?= (!empty($sf_purchase_invoice) ? $sf_purchase_invoice : ""); ?>">
+                                            
+                                            <?php $src = base_url() . 'images/no_image.png';
+                                            $image_src = $src;
+                                            if (!empty($sf_purchase_invoice)) {
+                                                //Path to be changed
+                                                $src = "https://s3.amazonaws.com/".BITBUCKET_DIRECTORY."/misc-images/".$sf_purchase_invoice;
+                                                //$image_src = base_url().'images/view_image.png';
+                                            }
+                                            ?>
+                                            <a id="a_order_support_file_0" href="<?php  echo $src?>" target="_blank"><small style="white-space:nowrap;"><?= (!empty($sf_purchase_invoice) ? $sf_purchase_invoice : ""); ?></small></a>
+                                            
+                                            
+                                        </div>
+                                        
                                         <div class="col-md-12" style="padding-left:0px;">
                                             <table class="table priceList table-striped table-bordered" name="priceList" >
                                                 <tr>
@@ -209,6 +230,7 @@
                                                         $paid_parts_cost = 0;
                                                         $serial_number = "";
                                                         $serial_number_pic = "";
+                                                      
                                                         foreach ($unit_details['quantity'] as $key => $price) {
                                                             if($price['booking_status'] != _247AROUND_CANCELLED){ 
                                                             ?>
@@ -578,6 +600,9 @@
 </div>
 </div>
 <script>
+    
+    var service_category_pod_required = <?php echo json_encode((!empty($is_sf_purchase_invoice_required)? array_column($is_sf_purchase_invoice_required, 'service_category') : [])); ?>
+    
     $(".model_number").select2();
     $("#technical_problem").select2();
     $('#technical_defect').select2();
@@ -586,7 +611,9 @@
     $("#booking_city").select2();
     
     
+    
     $(document).ready(function() {
+      
         //called when key is pressed in textbox
         $(".cost").keypress(function(e) {
             //if the letter is not digit then display error and don't type anything
@@ -691,12 +718,22 @@
         var prediv = -1;
         $(':radio:checked').each(function(i) {
             div_count = div_count + 1;
-       
+        
             //console.log($(this).val());
             var div_no = this.id.split('_');
             is_completed_checkbox[i] = div_no[0];
             if (div_no[0] === "completed") {
-                
+                if(service_category_pod_required.includes($.trim($('#price_tags'+i).text()))) {
+                    var is_sf_purchase_invoice_required = $('#is_sf_purchase_invoice_required').val();
+                    if(is_sf_purchase_invoice_required == '1') {
+                        var sf_purchase_invoice = $('#sf_purchase_invoice').val();
+                        if(sf_purchase_invoice == '') {
+                            alert("Please upload sf purchase invoice document.");
+                            flag = 1;
+                            return false;
+                        }
+                    }
+                }
                 //if POD is also 1, only then check for serial number.
                 if (div_no[1] === "1") {
                     var completedRadioButton = document.getElementById(this.id);
