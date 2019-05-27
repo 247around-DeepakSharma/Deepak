a730286964bd0d8b50c15cbb86e044bab94f701f
abhishek2707 <abhisheka@247around.com>
2019-05-27 12:19:31 +0530

    Add Employee name when someone deactivate vendor #CRM-3087
    hint: after resolving the conflicts, mark the corrected paths
    hint: with 'git add <paths>' or 'git rm <paths>'
    hint: and commit the result with 'git commit'

--- db_changes_sql2.sql
+++ db_changes_sql2.sql
@@ -3,7 +3,7 @@ ALTER TABLE `service_center_booking_action` ADD `added_by_sf` INT(1) NOT NULL DE
 ALTER TABLE `booking_unit_details` ADD `added_by_sf` INT(1) NOT NULL DEFAULT '0';
 
 
-<<<<<<< HEAD
+ 
 ----Abhishek -----
 
 UPDATE `email_template` SET `template` = 'SF has marked wrong call area, Please reasign correct SF for booking ID %s, <br/>city is %s,<br/> pincode is %s' WHERE `email_template`.`id` = 124;
@@ -14,6 +14,19 @@ INSERT INTO `booking_cancellation_reasons` (`id`, `reason`, `reason_of`, `show_o
 
 ALTER TABLE `spare_parts_details` ADD `defective_part_rejected_by_partner` TINYINT(4) NOT NULL DEFAULT '0' AFTER `part_requested_on_approval`;
 
+ 
+--Kajal 23/5/2019 starting --
+insert into `partner_permission`(partner_id,permission_type,is_on,create_date,update_date) 
+values(247001, 'partner_on_state_appliance',0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+
+insert into agent_filters(entity_type,entity_id,contact_person_id,agent_id,state) 
+SELECT '247around',id, 0,account_manager_id,state FROM `partners` where account_manager_id is not NULL;
+--Kajal 23/5/2019 ending --
+ 
+---ABhishek ----
+ALTER TABLE `spare_parts_details` ADD `defective_part_rejected_by_partner` TINYINT(4) NOT NULL DEFAULT '0' AFTER `part_requested_on_approval`;
+
+ 
 --Kalyani 25-05-2019---
 
 CREATE TABLE `entity_identity_proof` (
@@ -33,8 +46,18 @@ ALTER TABLE `entity_identity_proof`
 ALTER TABLE `entity_identity_proof`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-=======
+ 
 --Abhay 25/5/19
 INSERT INTO `partner_booking_status_mapping` (`id`, `partner_id`, `247around_current_status`, `247around_internal_status`, `partner_current_status`, `partner_internal_status`, `actor`, `next_action`, `create_date`) VALUES (NULL, '247001', 'Pending', 'Warehouse acknowledged to receive MSL', 'Booking In Progress', 'Warehouse acknowledged to receive MSL', 'Warehouse', 'Send Spare to SF', '0000-00-00 00:00:00');
 INSERT INTO `partner_booking_status_mapping` (`id`, `partner_id`, `247around_current_status`, `247around_internal_status`, `partner_current_status`, `partner_internal_status`, `actor`, `next_action`, `create_date`) VALUES (NULL, '247001', 'Pending', 'Partner shipped spare to Warehouse', 'Booking In Progress', 'Partner shipped spare to Warehouse', 'Warehouse', 'Acknowledge Spare', '0000-00-00 00:00:00');
->>>>>>> 3952d09c4... Link Booking status when MSL send by Partner/ Receive By Warehouse #CRM-3082
+ 
+--Kajal 25-05-2019---
+
+INSERT INTO `header_navigation` (`entity_type`, `title`, `title_icon`, `link`, `level`, `parent_ids`, `groups`, `nav_type`, `is_active`, `create_date`) 
+VALUES ('247Around', 'File Type List', NULL, 'employee/booking/show_file_type_list', '1', '190', 'admin,developer', 'main_nav', '1', CURRENT_TIMESTAMP);
+-- Abhishek ----
+UPDATE `email_template` SET `template` = 'Dear %s,<br><br> <b> %s </b> Service Franchise is Permanently <b> %s </b> now by %s.<br><br> Thanks<br> 247Around Team' WHERE `email_template`.`id` = 19;
+
+UPDATE `email_template` SET `template` = 'Dear %s,<br><br> <b> %s </b> Service Franchise is Temporarily <b> %s </b> now by %s. <br><br> Thanks<br> 247Around Team' WHERE `email_template`.`id` = 18;
+ 
+ 
