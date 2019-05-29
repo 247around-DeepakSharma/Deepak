12d22e5e560b161ec98135cb71608c101c5a816c
Gorakh Mehta <gorakh@247around.com>
2019-05-29 13:31:47 +0530

    Change text from the button as an icon in spare parts booking on all tabs #CRM-3107
    Change text from the button as an icon in spare parts booking on all
    tabs #CRM-3107

--- application/views/employee/sparepart_on_tab.php
+++ application/views/employee/sparepart_on_tab.php
@@ -634,6 +634,10 @@
             //Set column definition initialisation properties.
             columnDefs: [
                 {
+                    "targets": [13], //first column / numbering column
+                    "orderable": true //set not orderable
+                },
+                {
                     "targets": [0,1,2,3,4,9,10,11], //first column / numbering column
                     "orderable": false //set not orderable
                 }
@@ -712,6 +716,10 @@
             },
             //Set column definition initialisation properties.
             columnDefs: [
+                {
+                    "targets": [13], //first column / numbering column
+                    "orderable": true //set not orderable
+                },
                 {
                     "targets": [0,1,2,3,4,11,12,13,16], //first column / numbering column
                     "orderable": false //set not orderable
