6997284aeb1a39f0178e8b0eaee4d317520bbafd
kalyani tekpure <kalyanit@247around.com>
2019-05-30 12:49:02 +0530

    Remove conflict from service center page

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -701,7 +701,8 @@ class Service_centers extends CI_Controller {
                 redirect(base_url() . "service_center/pending_booking");
             }
         }
-    }       
+    }
+    }
  
  
     /**
