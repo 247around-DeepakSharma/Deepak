5f89ede9a2a856afebc6aa9c48dfb71708b4e2fe
Kajal_Chauhan <kajal@247around.com>
2019-05-31 13:14:12 +0530

    CRM-3117 : Make Capcity mandatory on Booking Form
    Trim extra spaces in js/custom.js

--- js/custom_js.js
+++ js/custom_js.js
@@ -128,7 +128,6 @@ function getCapacityForCategory(category, div_id, add_booking) {
                 getModelForServiceCategoryCapacity(div_id);
                 getPricesForCategoryCapacity(div_id);
             }
-            $("#appliance_capacity_" + div_no[2]).attr("required",true);
 
         } else {
             $("#priceList_" + div_no[2]).html("");
@@ -137,8 +136,14 @@ function getCapacityForCategory(category, div_id, add_booking) {
                 getModelForServiceCategoryCapacity(div_id,);
                 getPricesForCategoryCapacity(div_id,add_booking);
             }
-            $("#appliance_capacity_" + div_no[2]).removeAttr("required");
 
+        }
+        
+        if($.trim(data) !== "<option></option>") {
+            $("#appliance_capacity_" + div_no[2]).attr("required",true);
+        }
+        else {
+            $("#appliance_capacity_" + div_no[2]).removeAttr("required");
         }
 
     });
@@ -170,6 +175,7 @@ function getPricesForCategoryCapacity(div_id,add_booking) {
         } else {
 
             postData['capacity'] = "";
+            $("#appliance_capacity_" + div_no[2]).removeAttr("required");
         }
         if(postData['category']){
             //  $("#priceList_" + div_no[2]).html("Loading......");
