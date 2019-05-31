f0930966657c1df8f8a9dbfdefe7cad912404e97
ankitr17 <ankitr@247around.com>
2019-05-31 11:05:31 +0530

    CRM-3006 pagination issue resolved for cancellation reason filter

--- application/controllers/employee/booking.php
+++ application/controllers/employee/booking.php
@@ -5274,6 +5274,9 @@ class Booking extends CI_Controller {
         
         $this->checkUserSession();
         $whereIN = $where = $join = array();
+        if($booking_id == 0) {
+            $booking_id  = NULL;
+        }
         if($this->session->userdata('user_group') == 'regionalmanager'){
             $sf_list = $this->vendor_model->get_employee_relation($this->session->userdata('id'));
             $serviceCenters = $sf_list[0]['service_centres_id'];
@@ -5292,7 +5295,8 @@ class Booking extends CI_Controller {
            $whereIN['sc.cancellation_reason'] = [urldecode($cancellation_reason)];
         }
         $data['cancellation_reason'] = $this->reusable_model->get_search_result_data("booking_cancellation_reasons", "*", array(), NULL, NULL, NULL, NULL, NULL, array());
-
+        $data['cancellation_reason_selected'] = $cancellation_reason;
+        
         $total_rows = $this->service_centers_model->get_admin_review_bookings($booking_id,$status,$whereIN,$is_partner,NULL,-1,$where,0,NULL,NULL,0,$join);
         if(!empty($total_rows)){
             $data['per_page'] = 100;
