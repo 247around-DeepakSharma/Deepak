e4b3ec5bc172ea8cc2157e3364c80a0422fb4bbb
abhishek2707 <abhisheka@247around.com>
2019-06-03 15:50:06 +0530

    Minor consition chnage in success message

--- application/controllers/employee/spare_parts.php
+++ application/controllers/employee/spare_parts.php
@@ -2910,7 +2910,7 @@ class Spare_parts extends CI_Controller {
 
           }   /// for loop ends
           
-          if($tcount>0 && !empty($bookings_flash_data)){
+          if($tcount>0 && empty($bookings_flash_data)){
             $this->session->set_flashdata('success',$tcount. ' - Spares Transfered Successfully'.'  Click for details  ');
              if($this->session->userdata('userType') == 'service_center'){
                redirect('service_center/spare_transfer_from_wh_to_wh');
