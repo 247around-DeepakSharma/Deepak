2fdf72de9ae0c50e7d6aac83d90fa2459d499c3c
abhishek2707 <abhisheka@247around.com>
2019-06-10 10:59:10 +0530

    CRM-3180 - Add shipped part number and requested part number

--- application/views/employee/viewdetails.php
+++ application/views/employee/viewdetails.php
@@ -602,7 +602,7 @@
                                         <td><?php echo $sp['model_number']; ?></td>
                                         <td style=" word-break: break-all;"><?php echo $sp['parts_requested']; ?></td>
                                         <td style=" word-break: break-all;"><?php if(isset($sp['final_spare_parts'])){ echo $sp['final_spare_parts']; } ?></td>
-                                         <td style=" word-break: break-all;"><?php if(isset($sp['part_number'])){ echo $sp['part_number']; } ?></td>
+                                        <td style=" word-break: break-all;"><?php if(isset($sp['part_number'])){ echo $sp['part_number']; } ?></td>
                                         <td style=" word-break: break-all;"><?php echo $sp['parts_requested_type']; ?></td>  
                                         <td><?php echo $sp['quantity']; ?></td> 
                                         <td><?php echo $sp['shipped_quantity']; ?></td> 
