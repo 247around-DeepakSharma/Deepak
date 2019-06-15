473450ed4825e54c7b8f5ecc255c8829c556fe66
Kajal_Chauhan <kajal@247around.com>
2019-06-15 21:07:16 +0530

    CRM-3205 : Auto Reassign alternate spare pending on Partner while uploading alternate file
    i. Created new func to show inventory_model_mapping data without any other table join
    ii. Used this func to insert Inventory Model Mapping of  part no. and alternate part no.

--- application/controllers/file_upload.php
+++ application/controllers/file_upload.php
@@ -1002,8 +1002,8 @@ class File_upload extends CI_Controller {
                         
                       }
                     
-                    $insert_inventory = $this->insert_Inventory_Model_Data($partner_id, trim($val['inventory_id']), trim($val['alt_inventory_id']));
-                    $insert_alt_inventory = $this->insert_Inventory_Model_Data($partner_id, trim($val['alt_inventory_id']), trim($val['inventory_id']));
+                    $insert_inventory = $this->insert_Inventory_Model_Data(trim($val['inventory_id']), trim($val['alt_inventory_id']));
+                    $insert_alt_inventory = $this->insert_Inventory_Model_Data(trim($val['alt_inventory_id']), trim($val['inventory_id']));
                     
                     if ($insert_inventory && $insert_alt_inventory) {
                         log_message("info", __METHOD__ . " inventory model mapping created succcessfully");
@@ -1549,17 +1549,16 @@ class File_upload extends CI_Controller {
      * @param $inventory_id, $alt_inventory_id
      * @return $insert_id
      */
-    function insert_Inventory_Model_Data($partner_id, $inventory_id, $alt_inventory_id) {
+    function insert_Inventory_Model_Data($inventory_id, $alt_inventory_id) {
         $data_model_mapping = array();
         $insert_id = 0;
-        $model_where = array('appliance_model_details.entity_id' => $partner_id, 'appliance_model_details.entity_type' => _247AROUND_PARTNER_STRING, 'inventory_model_mapping.inventory_id' => trim($inventory_id),'appliance_model_details.active' => 1);
-        $inventory_details = $this->inventory_model->get_inventory_mapped_model_numbers('appliance_model_details.id,appliance_model_details.model_number',$model_where);
-        
+        $where = array('inventory_model_mapping.inventory_id' => trim($inventory_id));
+        $inventory_details = $this->inventory_model->get_inventory_model_data("*", $where);
         if(!empty($inventory_details)) {
             foreach($inventory_details as $inventory) {
                 $tmp = array();
                 $tmp['inventory_id'] = trim($alt_inventory_id);
-                $tmp['model_number_id'] = $inventory['id'];
+                $tmp['model_number_id'] = $inventory['model_number_id'];
                 array_push($data_model_mapping, $tmp);
             }
         }
