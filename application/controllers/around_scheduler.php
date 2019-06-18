d76d42fa8fa44313d71b0218597037ab8fe20393
Kajal_Chauhan <kajal@247around.com>
2019-06-19 03:45:05 +0530

    CRM-3221 : Review Branch 65
    1 . Show full length model number dropdown
    2. New Tab(Service Category changed by SF) not showing on Branch 65
    3. Add FIle type in the Dropdown
    4. Modify UI to add file type
    5. Delete file after download csv -> File Not Stored in Tmp Folder, No need to delete
    6. Use Icons for address & email in Contatct Person Page (SF Panel)
    7. Need to change Partner AM Mapping
    8. Add New data for AM Mapping
    9. Optimization in Upload Alternate Spare Parts -> insert inventory model mapping

--- application/controllers/around_scheduler.php
+++ application/controllers/around_scheduler.php
@@ -930,7 +930,7 @@ class Around_scheduler extends CI_Controller {
         //$partner_details = $this->partner_model->getpartner_details("partners.id, public_name, "
         //        . "is_active,invoice_email_to, invoice_email_cc, owner_phone_1 ,account_manager_id", array('is_prepaid' => 1, 'is_active' => 1));
         $partner_details = $this->partner_model->getpartner_data("partners.id, public_name, "
-                . "partners.is_active,invoice_email_to, invoice_email_cc, owner_phone_1 , group_concat(distinct agent_filters.agent_id) as account_manager_id", array('is_prepaid' => 1, 'partners.is_active' => 1, 'agent_filters.entity_type' => "247around"),"",NULL,1,1, "partners.id");
+                . "partners.is_active,invoice_email_to, invoice_email_cc, owner_phone_1 , group_concat(distinct agent_filters.agent_id) as account_manager_id", array('is_prepaid' => 1, 'partners.is_active' => 1),"",NULL,1,1, "partners.id");
         
         log_message("info", __METHOD__ . " All Active Prepaid Partner " . print_r($partner_details, true));
 
@@ -1918,7 +1918,7 @@ FIND_IN_SET(state_code.state_code,employee_relation.state_code) WHERE india_pinc
         //$partnerArray = $this->reusable_model->get_search_result_data("partners", "partners.*,employee.official_email as am_email", array("booking_review_for IS NOT NULL" => NULL), 
         //        array("employee"=>"employee.id = partners.account_manager_id"), NULL, NULL, NULL, NULL, array());
         $partnerArray = $this->partner_model->getpartner_data("partners.*,group_concat(distinct agent_filters.agent_id) as account_manager_id", 
-                array("booking_review_for IS NOT NULL" => NULL, 'agent_filters.entity_type' => "247around"),"",0,0,1,"partners.id");
+                array("booking_review_for IS NOT NULL" => NULL),"",0,0,1,"partners.id");
         foreach ($partnerArray as $partner) {
             $tempData = array();
             $tempData = $this->miscelleneous->get_review_bookings_for_partner($partner['id'], NULL, 1,REVIEW_NOTIFICATION_TO_PARTNER_DAYS);
