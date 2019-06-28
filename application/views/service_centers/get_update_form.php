8df681271e045689565696ec230c24286357b98d
abhishek2707 <abhisheka@247around.com>
2019-06-28 23:56:45 +0530

    CRM-3283 - Reapir OOW spare request goes to in warranty error

--- application/views/service_centers/get_update_form.php
+++ application/views/service_centers/get_update_form.php
@@ -180,7 +180,7 @@
                                         <div class="form-group ">
                                             <label for="part_warranty" class="col-md-4">Part Warranty Status  <?php echo $bookinghistory[0]['request_type'];  ?></label>                                             
                                             <div class="col-md-6">
-        <input type="text" id="sparewarranty"  value="<?php if(strpos($bookinghistory[0]['request_type'],'Out of Warranty') == true || strpos($bookinghistory[0]['request_type'],'Gas Recharge - Out')==true ){echo '2';}else{echo '1';}  ?>" name="part[0][part_warranty_status]">  
+        <input type="text" id="sparewarranty"  value="<?php if(strpos($bookinghistory[0]['request_type'],'Out Of Warranty') == true || strpos($bookinghistory[0]['request_type'],'Gas Recharge - Out')==true ){echo '2';}else{echo '1';}  ?>" name="part[0][part_warranty_status]">  
                                             </div>
                                         </div>
                                     </div>
@@ -278,7 +278,7 @@
                                             <div class="form-group ">
                                                 <label for="part_warranty" class="col-md-4">Part Warranty Status </label>                               
                                                 <div class="col-md-6">
-<input type="text" id="sparewarranty"  value="<?php if(strpos($bookinghistory[0]['request_type'],'Out of Warranty') == true || strpos($bookinghistory[0]['request_type'],'Gas Recharge - Out')==true ){echo '2';}else{echo '1';}  ?>" name="part[0][part_warranty_status]"> 
+<input type="text" id="sparewarranty"  value="<?php if(strpos($bookinghistory[0]['request_type'],'Out Of Warranty') == true || strpos($bookinghistory[0]['request_type'],'Gas Recharge - Out')==true ){echo '2';}else{echo '1';}  ?>" name="part[0][part_warranty_status]"> 
                                                 </div>
                                             </div>
                                         </div>
