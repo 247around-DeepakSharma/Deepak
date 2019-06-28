8fa348949227f516231cb1dfbba3214234a260e3
Kajal_Chauhan <kajal@247around.com>
2019-06-28 17:39:36 +0530

    Add Columns in Spare Received Part Excel of Spare Part Bookings

--- application/views/employee/sparepart_on_tab.php
+++ application/views/employee/sparepart_on_tab.php
@@ -935,7 +935,7 @@
                     extend: 'excelHtml5',
                     text: 'Export',
                     exportOptions: {
-                        columns: [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14 ]
+                        columns: [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18 ]
                     },
                     title: 'sf_received_part'
                 }
