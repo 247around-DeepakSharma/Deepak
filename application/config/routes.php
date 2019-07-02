8f58eab36756566888ef26a5a2c43c2e9879caf6
abhishek2707 <abhisheka@247around.com>
2019-07-02 12:42:01 +0530

    CRM-3299 - MSL UI and add in admin CRM

--- application/config/routes.php
+++ application/config/routes.php
@@ -221,9 +221,11 @@ $route['partner/search_docket_number'] = 'employee/partner/search_docket_number'
 $route['partner/review_bookings/(:any)/(:any)'] = 'employee/partner/partner_review_bookings/$1/$2';
 $route['partner/contacts'] = 'partner/manage_partner_contacts';
 $route['partner/brand_collateral']='employee/partner/brandCollateral';
+ 
 $route['partner/inventory/model_mapping'] = 'employee/partner/show_appliance_model_mapping';
-$route['partner/msl_excel_upload']='employee/partner/msl_excel_upload';
-
+ 
+$route['inventory/msl_excel_upload']='employee/inventory/msl_excel_upload';
+ 
 //$route['service_center/inventory/appliance_model_list'] = 'employee/inventory/appliance_model_list';
 
 
