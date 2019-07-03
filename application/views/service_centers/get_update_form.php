c82a99c18267365b636aabc27abc53683553a16d
Kajal_Chauhan <kajal@247around.com>
2019-07-03 13:00:46 +0530

    CRM-3303: Kenstar Issues
    1. Remove Edit Request Type Option on SF CRM ->  Update Booking .

--- application/views/service_centers/get_update_form.php
+++ application/views/service_centers/get_update_form.php
@@ -9,7 +9,9 @@
             <div class="col-md-12">
                 <h2 class="page-header">
                     Update Booking      
+                    <?php if(isset($saas_module) && (!$saas_module)) { ?>
                        <a target="_blank" href="<?php echo base_url(); ?>service_center/get_sf_edit_booking_form/<?php echo urlencode(base64_encode($bookinghistory[0]['booking_id']))?>" style="float: right;height: 29px;width: 36px;background: #795b95;border: #795b95;" class="btn btn-sm btn-primary"  title="Edit Request Type"><i class="fa fa-edit" aria-hidden="true"></i></a>
+                    <?php } ?>
                 </h2>
                 <?php if(validation_errors()) { ?>
                 <div class=" alert alert-danger">
