3b8e17a6de85409ed73ead0fa8bdb4e06042229c
abhishek2707 <abhisheka@247around.com>
2019-07-04 14:18:35 +0530

    Condition minor

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -2909,7 +2909,10 @@ class Service_centers extends CI_Controller {
         $_POST['partner_challan_number'][$value] = $spare_part[0]['partner_challan_number'];
         $_POST['challan_approx_value'][$value] = $spare_part[0]['challan_approx_value'];
         $_POST['parts_requested'][$value] = $spare_part[0]['parts_requested'];
-        $_POST['courier_boxes_weight_flag']=$count_spare;
+        if (!isset($_POST['courier_boxes_weight_flag']) || empty($_POST['courier_boxes_weight_flag'])) {
+        	   $_POST['courier_boxes_weight_flag'] = $count_spare;
+             } 
+       
 
         $this->process_update_defective_parts($value);
       }
