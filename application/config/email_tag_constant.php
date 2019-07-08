0f4d6f2c1e6c88e74dfe26f35a1225d975202f33
Gorakh Mehta <gorakh@247around.com>
2019-07-08 15:51:45 +0530

    Out of TAT - undelivered courier email template changes #CRM-3281
    Out of TAT - undelivered courier email template changes #CRM-3281

--- application/config/email_tag_constant.php
+++ application/config/email_tag_constant.php
@@ -132,4 +132,5 @@ define('UPDATE_PARTNER_AM_NOTIFICATION', 'update_partner_am_notification');
 define('CHANGE_PASSWORD','change_password');
 define('UPDATE_APPLIANCE_BY_SF','send_mail_for_insert_update_applaince_by_sf');
 define('TRANSFER_SPARE_FROM_WH_TO_WH','spare_not_transfer_from_wh_to_wh');
-define('SPARE_PART_SHIPMENT_PENDING', 'spare_part_shipment_pending');
+define('TRACKON_SPARE_PART_SHIPMENT_PENDING', 'trackon_spare_part_shipment_pending');
+define('GATI_SPARE_PART_SHIPMENT_PENDING', 'gati_spare_part_shipment_pending');
