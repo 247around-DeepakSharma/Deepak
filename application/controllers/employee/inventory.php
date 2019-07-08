3c0a03e861284a041b1565daf25d4a09da6e6741
Abhay Anand <abhaya@247around.com>
2019-07-08 12:52:55 +0530

    Defective Return ANNEXURE Invoice is not generating #CRM-3349

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -4579,8 +4579,8 @@ class Inventory extends CI_Controller {
                     $output_file = "";
                     $template = "partner_inventory_invoice_annexure-v1.xlsx";
                     $output_file = $response['meta']['invoice_id'] . "-detailed.xlsx";
-                    $this->invoice_lib->generate_invoice_excel($template, $response['meta'], $invoiceValue, TMP_FOLDER . $output_file);
-
+                    
+                    $this->invoice_lib->generate_invoice_excel($template, $response['meta'], $invoiceValue['data'], TMP_FOLDER . $output_file);
                     $this->invoice_lib->upload_invoice_to_S3($response['meta']['invoice_id'], true, false);
 
                     $invoice_details = array(
