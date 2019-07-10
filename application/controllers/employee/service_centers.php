f43545c2d25cf6e6b6cce55ce639d2d6a0780883
Gorakh Mehta <gorakh@247around.com>
2019-07-10 13:01:14 +0530

    Message:  Illegal string offset 'booking_id' fixed on print address when SF send defective spare parts #CRM-3361
    Message:  Illegal string offset 'booking_id' fixed on print address when
    SF send defective spare parts #CRM-3361

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -2830,7 +2830,7 @@ class Service_centers extends CI_Controller {
         $this->load->view('service_centers/header');
         $this->load->view('service_centers/defective_parts', $data);
     }
-
+    
     /**
      * @desc: This method is used to display list of booking which received by Partner
      * @param Integer $offset
@@ -3217,8 +3217,7 @@ class Service_centers extends CI_Controller {
         log_message('info', __FUNCTION__ . ' Used by :' . $this->session->userdata('service_center_name'));
         $booking_address = $this->input->post('download_address');
         $booking_history['details'] = array();
-        $i = 0;
-
+        
         $main_company_public_name = "";
         $main_company_logo = "";
         $partner_on_saas = $this->booking_utilities->check_feature_enable_or_not(PARTNER_ON_SAAS);
@@ -3230,7 +3229,7 @@ class Service_centers extends CI_Controller {
 
 
         if (!empty($booking_address)) {
-
+            $i = 0;
             foreach ($booking_address as $spare_id) {
 
                 $v_select = "spare_parts_details.entity_type,spare_parts_details.booking_id,spare_parts_details.partner_id,spare_parts_details.service_center_id,service_centres.name, service_centres.id, company_name, "
@@ -3239,7 +3238,7 @@ class Service_centers extends CI_Controller {
                         . "service_centres.primary_contact_phone_1,booking_details.partner_id as booking_partner_id, defective_return_to_entity_type,"
                         . "defective_return_to_entity_id";
                 $sp_details = $this->partner_model->get_spare_parts_by_any($v_select, array('spare_parts_details.id' => $spare_id), true, true);
-
+                
                 $select = "contact_person.name as  primary_contact_name,contact_person.official_contact_number as primary_contact_phone_1,contact_person.alternate_contact_number as primary_contact_phone_2,"
                         . "concat(warehouse_address_line1,',',warehouse_address_line2) as address,warehouse_details.warehouse_city as district,"
                         . "warehouse_details.warehouse_pincode as pincode,"
@@ -3249,8 +3248,7 @@ class Service_centers extends CI_Controller {
                 $where = array('contact_person.entity_id' => $sp_details[0]['defective_return_to_entity_id'],
                     'contact_person.entity_type' => $sp_details[0]['defective_return_to_entity_type']);
                 $wh_address_details = $this->inventory_model->get_warehouse_details($select, $where, false, true);
-
-                $booking_details = '';
+                $booking_details = array();
                 switch ($sp_details[0]['defective_return_to_entity_type']) {
                     case _247AROUND_PARTNER_STRING:
                         $booking_details = $this->partner_model->getpartner($sp_details[0]['defective_return_to_entity_id'])[0];
@@ -3278,7 +3276,7 @@ class Service_centers extends CI_Controller {
             //Logging
             log_message('info', __FUNCTION__ . ' No Download Address from POST');
         }
-
+                           
         $this->load->view('service_centers/print_partner_address', $booking_history);
     }
 
