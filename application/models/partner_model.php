c6d2fef8e01016616491f1b0d3a7cd62769bf8ab
abhishek2707 <abhisheka@247around.com>
2019-07-11 18:34:57 +0530

    CRM-3321
    Add sticker Available in MWH is spare available in MWH

--- application/models/partner_model.php
+++ application/models/partner_model.php
@@ -1480,7 +1480,7 @@ function get_data_for_partner_callback($booking_id) {
         if(!empty($post['is_original_inventory'])){
             $this->db->join('inventory_master_list as original_im','original_im.inventory_id = spare_parts_details.original_inventory_id', "left");
         }
-        
+        $this->db->order_by('spare_parts_details.entity_type', 'asc');
         if($group_by){
             
             $this->db->group_by($group_by);
