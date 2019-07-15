73ed0e376f8ba7f19ee92cd4afff47ab7987e9ff
Gorakh Mehta <gorakh@247around.com>
2019-07-15 17:34:35 +0530

    Wrong spare parts showing on regional manager panel#CRM-3402

--- application/controllers/employee/spare_parts.php
+++ application/controllers/employee/spare_parts.php
@@ -1178,6 +1178,7 @@ class Spare_parts extends CI_Controller {
             $post['vendor_partner'] = $this->input->post('vendor_partner');
         }else{
            $sf = $this->vendor_model->get_employee_relation($this->session->userdata("id")); 
+           $post['where']['status'] = $this->input->post("status"); 
         }
         $vendor_id = array();
         if(!empty($sf)){
