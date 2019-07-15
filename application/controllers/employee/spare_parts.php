45bc9c3c0e9ae88c8eebdbdb1c3f0b2fe0d0e4c3
abhishek2707 <abhisheka@247around.com>
2019-07-15 17:25:41 +0530

    Update Spare [pArt detail Database error   #CRM-3401

--- application/controllers/employee/spare_parts.php
+++ application/controllers/employee/spare_parts.php
@@ -1395,6 +1395,7 @@ class Spare_parts extends CI_Controller {
                     $data['spare_id'] = $spare_parts_id;
                     array_push($delivered_sp, $data);
                     $this->auto_delivered_for_micro_wh($delivered_sp, $partner_id);
+                    unset($data['spare_id']);
                 }
 
                 if (!empty($spare_parts_id)) {
@@ -2112,6 +2113,8 @@ class Spare_parts extends CI_Controller {
                         $data['spare_id'] = $spare_id;
                         array_push($delivered_sp, $data);
                         $this->auto_delivered_for_micro_wh($delivered_sp, $partner_id);
+                        unset($data['spare_id']);
+
                     }
                 }
                
