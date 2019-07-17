a81bed567e4d91d9426dc4426cae194e923ef49d
Gorakh Mehta <gorakh@247around.com>
2019-07-17 15:36:25 +0530

    Error Number: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'desc LIMIT 50' at line 10 #CRM-3423
    Error Number: 1064 You have an error in your SQL syntax; check the
    manual that corresponds to your MySQL server version for the right
    syntax to use near 'desc LIMIT 50' at line 10 #CRM-3423

--- application/views/partner/spare_parts_booking.php
+++ application/views/partner/spare_parts_booking.php
@@ -173,7 +173,7 @@
             },
             "columnDefs": [
                 {
-                    "targets": [0,1,2,5,6,7,8,9,10,11,12,13,14,15], //first column / numbering column
+                    "targets": [0,1,2,5,6,7,8,9,10,11,12,13,14,15,16], //first column / numbering column
                     "orderable": false //set not orderable
                 }
             ],  
