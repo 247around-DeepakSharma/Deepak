6a4d198b4ae15a2e5026943dd965df7958555a8d
pritys <pritys@247around.com>
2019-07-17 17:44:19 +0530

    Changes in Warranty Data Query and UI

--- application/models/warranty_model.php
+++ application/models/warranty_model.php
@@ -21,8 +21,8 @@ class Warranty_model extends CI_Model {
         endif;
 
         $strSelect = 'warranty_plans.*,
-                        group_concat(state_code.state) as states,
-                        group_concat(inventory_parts_type.part_type) as part_types';
+                        group_concat(distinct(state_code.state)) as states,
+                        group_concat(distinct(inventory_parts_type.part_type)) as part_types';
 
         $arrWhere = [
             'warranty_plans.partner_id' => $data['partner'],
