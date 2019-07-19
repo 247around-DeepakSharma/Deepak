83f3a12eff6b2d4ae8ab0feb538b1b19dd750387
abhishek2707 <abhisheka@247around.com>
2019-07-19 11:50:52 +0530

    CRM-3441 - Show Inventory Menu to all

--- application/views/partner/header_navigation.php
+++ application/views/partner/header_navigation.php
@@ -296,10 +296,7 @@
                                         <i class="<?php echo $main_nav['navData']["id_".$p_id]['title_icon']?>"></i> <span class="side_menu_list_title"><?php echo $main_nav['navData']["id_".$p_id]['title']?>
                                         </span><span class="fa fa-chevron-down"></span></a>
                                         <ul class="nav child_menu" style="display:none !important;">
-                                <?php
-                                    //if(($this->session->userdata('is_wh')==1) || ($this->session->userdata('is_micro_wh')==1)){ 
-                                ?>
-                                    
+                      
                                                 <?php
                                                 $t=0;
                                                 foreach($main_nav['navFlow']["id_".$p_id] as $childID){                                                    
@@ -308,21 +305,15 @@
                                                         $childLink =  base_url().$main_nav['navData']["id_".$childID]['link'];
                                                     }
                                                     ?>
-                                                    <?php if(($main_nav['navData']["id_".$childID]['title'] == 'Model List') && (($this->session->userdata('is_wh') != '1') || ($this->session->userdata('is_micro_wh')!= '1'))){ ?>
-                                                        <li><a href="<?php echo $childLink; ?>"><?php echo $main_nav['navData']["id_".$childID]['title']; ?></a></li>
-                                                    <?php } else if(($this->session->userdata('is_wh')==1) || ($this->session->userdata('is_micro_wh')==1)){ ?>
-                                                        <li><a href="<?php echo $childLink; ?>"><?php echo $main_nav['navData']["id_".$childID]['title']; ?></a></li>
-                                                    <?php
-                                                          } 
+                                                <li><a href="<?php echo $childLink; ?>"><?php echo $main_nav['navData']["id_".$childID]['title']; ?></a></li>
                                                 }
                                                 ?>
-                                           
-                                <?php //}  ?> 
+
                                      </ul>
                                     </li>
                                     <?php } else {
                                     
-                                                    ?>     
+                                    ?>     
                                     <li>
                                             <a data-toggle="tooltip" data-placement="right" title="" data-original-title="<?php echo $main_nav['navData']["id_".$p_id]['title']?>">
                                             <i class="<?php echo $main_nav['navData']["id_".$p_id]['title_icon']?>"></i> <span class="side_menu_list_title"><?php echo $main_nav['navData']["id_".$p_id]['title']?>
