ed8d34857d0adc1771f8ada1bc3a91981b668ce9
abhishek2707 <abhisheka@247around.com>
2019-07-19 10:30:13 +0530

    php error    #CRM-3437
    php error

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -824,8 +824,15 @@ class Service_centers extends CI_Controller {
                             'unit_details_id' => $k);
 
                         $this->service_centers_model->insert_update_applaince_by_sf($array1);
-                        $this->send_mail_for_insert_applaince_by_sf($model_details[0]['category'], $model_details[0]['capacity'], $v['appliance_brand'], $v['price_tags'], $booking_id);
+ 
                     } else {
+                        $price_tag='';
+                        if(!empty($v['price_tags'])){
+                            $price_tag= $v['price_tags'];
+                        }else{
+                             $price_tag='-';
+                        }
+                        $this->send_mail_for_insert_applaince_by_sf($model_details[0]['category'], $model_details[0]['capacity'], $v['appliance_brand'], $price_tag, $booking_id);
                         return FALSE;
                     }
                 }
