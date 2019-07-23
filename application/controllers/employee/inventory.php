e0bb2e90bb0e4211cdbc35c5e38a0c95631f0447
abhishek2707 <abhisheka@247around.com>
2019-07-23 14:06:41 +0530

    All gst data to partner panel

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -6682,15 +6682,17 @@ class Inventory extends CI_Controller {
     }
     
     function get_247around_wh_gst_number(){
+
         $state_vendor = $this->vendor_model->viewvendor($this->session->userdata('service_center_id'));
         $html = "<option value='' selected disabled>Selet GST Number</option>";
         $where = array(
             "entity_type" => _247AROUND_PARTNER_STRING,
             "entity_id" => _247AROUND,
-            "state_code.state"=>ucwords(strtolower($state_vendor[0]['state']))
- 
             );
-        $gst_numbers = $this->inventory_model->get_entity_gst_data("id, gst_number, state", $where);
+        if ($this->session->userdata('userType')=='service_center') {
+         $where['state_code.state']  =ucwords(strtolower($state_vendor[0]['state']));
+        }
+        $gst_numbers = $this->inventory_model->get_entity_gst_data("entity_gst_details.id as id, gst_number, state_code.state as state", $where);
         foreach($gst_numbers as $key => $value){
             $html .= "<option value='".$value['id']."'>".$value['state']." - ".$value['gst_number']."</option>";
         }
