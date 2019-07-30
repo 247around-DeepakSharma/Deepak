e457fd6af129ba7dd840daf35d3fa26f5dfcd786
ankitr17 <ankitr@247around.com>
2019-07-30 18:26:14 +0530

    CRM-3489 (66) Modification on service center report

--- application/views/employee/show_service_center_report.php
+++ application/views/employee/show_service_center_report.php
@@ -1,13 +1,21 @@
 <style type="text/css">
     #noty_topCenter_layout_container{margin-top:40px !important;}
 </style>
-<div class="container-fluid">
-    <div class="col-md-6">
-        <h2>Service Center Report</h2></div>
-    <div class="col-md-6">
-        <center>
-            <span class="btn btn-primary" style="margin-top:2%;" id="sendmail">Send Mail</span>
-        </center>
+<div class="container-fluid" style="margin-top: 4%;">
+    <div class="col-md-7">
+        <h2>Service Center Report</h2>
+    </div>
+
+    <div class="col-md-4">
+        <div class="col-md-12" style="margin-top: 4%;">
+            <div class="input-group input-append date">
+                <input id="date" class="form-control date"  name="date" type="text" value = "" autocomplete='off' onkeydown="return false" >
+                <span class="input-group-addon add-on"><span class="glyphicon glyphicon-calendar"></span></span>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-1" >
+        <span class="btn btn-primary" style="float:right;margin-top: 20%;" id="sendmail">Send Mail</span>
     </div>
     <div class="clear"></div>
     <?php echo $html; ?>
@@ -77,5 +85,43 @@
         });
 
     });
+    
+    $('#date').on('apply.daterangepicker', function (ev, picker) {
+        $(this).val(picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate.format('YYYY-MM-DD'));
+    });
+    
+    $(function() {
+      $('input[name="date"]').daterangepicker({
+        opens: 'left',
+        autoUpdateInput: false,
+        locale: {
+            format: 'YYYY-MM-DD',
+            cancelLabel: 'Clear',
+            maxDate: 'now'
+        }
+      }, function(start, end, label) {
+            var startDateObj = new Date(start);
+            var endDateObj = new Date(end);
+            var timeDiff = Math.abs(endDateObj.getTime() - startDateObj.getTime());
+            var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+            
+            var date = startDateObj.getFullYear()+'-'+(("0" + (startDateObj.getMonth() + 1)).slice(-2))+'-'+startDateObj.getDate()+' - '+endDateObj.getFullYear()+'-'+(("0" + (endDateObj.getMonth() + 1)).slice(-2))+'-'+endDateObj.getDate();
+            if(diffDays>31) {
+                alert("Maximum range allowed is 1 month.");
+                return false;
+            } else {
+                $.ajax({
+                    method:'POST',
+                    url: '<?php echo base_url() ?>employee/vendor/show_service_center_report',
+                    data: {date: date}
+                }).done(function(data) {
+                    $('body').html(data);
+                    $('#date').val(date);
+                });
+            }  
+      });
+    });    
+    
+
 </script>
 
