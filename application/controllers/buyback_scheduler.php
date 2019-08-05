923f63f9d7a0837a28a802e5ecdb2f06742bd7de
ankitr17 <ankitr@247around.com>
2019-08-05 10:33:58 +0530

    send buyback not delivered orders mail

--- application/controllers/buyback_scheduler.php
+++ application/controllers/buyback_scheduler.php
@@ -36,4 +36,77 @@ class buyback_scheduler extends CI_Controller {
                     $this->notify->sendEmail($email_from, $to, $cc, $bcc, $subject, $message, '', BUYBACK_DISPUTED_ORDERS_SUMMARY);
                 }
     }
+    
+    // This Function is used to send Buyback not delivered orders mail. 
+    // It Contains Following Orders
+    // 1) Not delivered orders.
+    // 2) Previous day from current day.
+    function send_not_delivered_orders_list() {
+        
+        $sql = "SELECT 
+                    bb_order_details.partner_order_id,
+                    bb_order_details.partner_tracking_id,
+                    services.services,
+                    bb_unit_details.category,
+                    bb_order_details.city,
+                    bb_order_details.order_date,
+                    bb_order_details.delivery_date,
+                    bb_cp_order_action.current_status,
+                    (bb_unit_details.partner_basic_charge + bb_unit_details.partner_tax_charge) as exchange_price
+                FROM
+                    bb_order_details
+                    LEFT JOIN bb_unit_details ON (bb_order_details.partner_order_id = bb_unit_details.partner_order_id)
+                    LEFT JOIN services ON (bb_unit_details.service_id = services.id)
+                    LEFT JOIN bb_cp_order_action ON (bb_order_details.partner_order_id = bb_cp_order_action.partner_order_id)
+                WHERE
+                    bb_cp_order_action.current_status = 'Not Delivered'	
+                    AND date(bb_cp_order_action.acknowledge_date) = (CURDATE() - INTERVAL 1 DAY)";
+        
+        $data = $this->db->query($sql)->result_array();
+        
+        // generate data in table format.
+        $table = '<table>';
+        $table .= '<thead><tr>
+                    <th>Order ID</th>
+                    <th>Tracking ID</th>
+                    <th>Appliance</th>
+                    <th>Category/Size</th>
+                    <th>City</th>
+                    <th>Order Date</th>
+                    <th>Delivery Date</th>
+                    <th>Current Status</th>
+                    <th>Exchange Price</th>
+            </tr></thead>';
+        
+        if(empty($data)) {
+            $table .= '<tr><td colspan="9">No data found.</td></tr>';
+        } else {
+            foreach($data as $d) {
+                $table .= '<tr>';
+                $table .= '<td>'.$d['partner_order_id'].'</td>';
+                $table .= '<td>'.$d['partner_tracking_id'].'</td>';
+                $table .= '<td>'.$d['services'].'</td>';
+                $table .= '<td>'.$d['category'].'</td>';
+                $table .= '<td>'.$d['city'].'</td>';
+                $table .= '<td>'.$d['order_date'].'</td>';
+                $table .= '<td>'.$d['delivery_date'].'</td>';
+                $table .= '<td>'.$d['current_status'].'</td>';
+                $table .= '<td>'.$d['exchange_price'].'</td>';
+                $table .= '</tr>';
+            }
+        }
+        
+        $table .= '</table>';
+        
+        // prepare mail
+        $from = 'sunilk@247around.com';
+        $to = 'kmardee@amazon.com,ybhargav@amazon.com';
+        $cc = 'sunilk@247around.com';
+        $subject = 'Delivered Not Received orders reporting - '.date('d-M-Y', strtotime('-1 day', strtotime(date('Y-m-d'))));
+        
+        $body = '<p>Hi Team,<br />
+                Please find below the Delivered Not Received orders reported on '.date('d-M-Y', strtotime('-1 day', strtotime(date('Y-m-d')))).' for your immediate action:</p><br /><br />'.$table;
+        
+        $this->notify->sendEmail($from, $to, $cc, NULL, $subject, $body, NULL, NULL);
+    }
 }
