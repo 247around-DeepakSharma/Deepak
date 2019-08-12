ecd6c09a10e86d5efe9fe0851833b0e8e26a4883
ankitr17 <ankitr@247around.com>
2019-08-12 14:38:55 +0530

    sf_purchase_date set to NULL if empty in service_center_booking_action

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -621,7 +621,7 @@ class Service_centers extends CI_Controller {
                                         $data['service_center_remarks'] = date("F j") . ":- " . $closing_remarks;
                                     }
                                 }
-                                $data['sf_purchase_date'] = $purchase_date[$unit_id];
+                                $data['sf_purchase_date'] = (!empty($purchase_date[$unit_id]) ? $purchase_date[$unit_id] : NULL);
                                 $data['sf_purchase_invoice'] = NULL;
                                 if (!empty($purchase_invoice[$unit_id]) || !empty($purchase_invoice_file_name)) {
                                     if(empty($purchase_invoice_file_name)) {
