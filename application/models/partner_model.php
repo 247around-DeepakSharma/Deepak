c204b9b80040329adbd936ce9d3919150f4b3e71
ankitr17 <ankitr@247around.com>
2019-08-29 15:25:38 +0530

    CRM-3563 minor issue resolved

--- application/models/partner_model.php
+++ application/models/partner_model.php
@@ -866,7 +866,7 @@ function get_data_for_partner_callback($booking_id) {
                     . "booking_details.amount_due,booking_details.state, booking_details.service_center_closed_date, booking_details.request_type, booking_details.current_status, booking_details.partner_current_status, booking_details.partner_internal_status,"
                 . " service_centres.name as vendor_name, service_centres.address, service_centres.district as sf_city,service_centres.state as sf_state, service_centres.gst_no, "
                 . " service_centres.pincode, service_centres.district,service_centres.id as sf_id,service_centres.is_gst_doc,service_centres.signature_file, service_centres.primary_contact_phone_1,"
-                . " DATEDIFF(CURRENT_TIMESTAMP,  STR_TO_DATE(date_of_request, '%Y-%m-%d')) AS age_of_request, sc.name as warehouse_name, spare_consumption_status.is_consumed = 1 ";
+                . " DATEDIFF(CURRENT_TIMESTAMP,  STR_TO_DATE(date_of_request, '%Y-%m-%d')) AS age_of_request, sc.name as warehouse_name, spare_consumption_status.is_consumed ";
             if($end){
                 $limit = "LIMIT $start, $end";
             }
