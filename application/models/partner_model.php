48260527388af7f992ae67ea5b06e3c064cd7725
ankitr17 <ankitr@247around.com>
2019-09-03 18:35:36 +0530

    CRM-3653 consumption reason is now displayed on view booking pages of Admin, SF, Partner CRM

--- application/models/partner_model.php
+++ application/models/partner_model.php
@@ -1515,6 +1515,7 @@ function get_data_for_partner_callback($booking_id) {
         $this->db->where($where,false);
         //$this->db->where('status',)
         $this->db->from('spare_parts_details');
+        $this->db->join('spare_consumption_status','spare_parts_details.consumed_part_status_id = spare_consumption_status.id', 'left');
         //$this->db->join('symptom_spare_request', 'symptom_spare_request.id = spare_parts_details.spare_request_symptom', 'left');
         if($is_join){
             $this->db->join('booking_details','spare_parts_details.booking_id = booking_details.booking_id');
