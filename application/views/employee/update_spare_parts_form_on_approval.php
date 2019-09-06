bea6653cf341c8f79d5ff6caa29bd6991bb3a831
abhi2707 <abhisheka@247around.com>
2019-09-06 15:22:26 +0530

    - Esit model session handle for update model #CRM-3712

--- application/views/employee/update_spare_parts_form_on_approval.php
+++ application/views/employee/update_spare_parts_form_on_approval.php
@@ -199,7 +199,12 @@
                         <input type="hidden" name="booking_id" value="<?php echo $spare_parts_details['booking_id']; ?>">
                         <input type="hidden" name="previous_inventory_id" value="<?php echo $spare_parts_details['requested_inventory_id']; ?>"> 
                         <input type="hidden" name="current_inventory_id" id="current_inventory_id" value="">
+                        <?php 
+                          if($this->session->userdata('user_group') == "inventory_manager" || $this->session->userdata('user_group') == "admin" || $this->session->userdata('user_group') == "developer"   ){  ?>
                         <input type="submit"  value="Update" id="submitform" style="background-color: #2C9D9C; border-color: #2C9D9C; " onclick="return submitForm();"   class="btn btn-danger btn-large">
+
+                       <?php } ?>
+
                     </div>
                 </form>
             </div>
