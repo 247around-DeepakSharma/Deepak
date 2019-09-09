62033de329be626865f9a8557d9936051266cba1
abhi2707 <abhisheka@247around.com>
2019-09-09 16:44:09 +0530

    - Fatal Error #CRM-3722

--- application/libraries/Miscelleneous.php
+++ application/libraries/Miscelleneous.php
@@ -4414,7 +4414,7 @@ function generate_image($base64, $image_name,$directory){
                     
                     $spare_pending_on_to='';
 
-                    if ($data['entity_type']==_247AROUND_SF_STRING) {
+                    if ($data['entity_type']==_247AROUND_SF_STRING && !empty($data['entity_id'])) {
                     $wh_details_to = $this->vendor_model->getVendorContact($data['entity_id']);
                     if(!empty($wh_details_to)){
                     $spare_pending_on_to = $wh_details_to[0]['district'] . ' Warehouse';   
@@ -4425,9 +4425,8 @@ function generate_image($base64, $image_name,$directory){
                         $spare_pending_on_to=$data['entity_id'];
                     }
 
-
                     $spare_pending_on='';
-                    if ($booking['entity_type']==_247AROUND_SF_STRING) {
+                    if ($booking['entity_type']==_247AROUND_SF_STRING && !empty($partner_id)) {
                     $wh_details = $this->vendor_model->getVendorContact($partner_id);
                     if(!empty($wh_details)){
                     $spare_pending_on = $wh_details[0]['district'] . ' Warehouse';   
