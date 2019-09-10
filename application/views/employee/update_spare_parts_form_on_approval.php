5dd4675471f21121ba13483914e213c3464e703c
Gorakh Mehta <gorakh@247around.com>
2019-09-10 17:53:47 +0530

    Some changes in select two #CRM-3727

--- application/views/employee/update_spare_parts_form_on_approval.php
+++ application/views/employee/update_spare_parts_form_on_approval.php
@@ -225,7 +225,8 @@ $(document).ready(function(){
                 data: { service_id:<?php echo $spare_parts_details['service_id']; ?>},
                 success:function(data){                       
                     $('.parts_type').html(data);
-                    $('.parts_type option[value="<?php echo $spare_parts_details['parts_requested_type']; ?>"]').attr('selected','selected');
+                    //$('.parts_type option[value="<?php echo $spare_parts_details['parts_requested_type']; ?>"]').attr('selected','selected');
+                    $("#parts_type").val("<?php echo $spare_parts_details['parts_requested_type']; ?>").change();
                     
                 }
             });
@@ -236,7 +237,7 @@ $(document).ready(function(){
 <?php if(!empty($inventory_details)){ ?>
 <script>
 $('#model_number_id').select2();
-//$('#parts_name').select2();
+$('#parts_name').select2();
 $(document).ready(function(){    
     
         $('#model_number_id').on('change', function() {        
@@ -314,7 +315,7 @@ $(document).ready(function(){
                     url:'<?php echo base_url(); ?>employee/inventory/get_parts_name',
                     data: {model_number_id:model_number_id,entity_id: '<?php echo $spare_parts_details['booking_partner_id']; ?>' , entity_type: '<?php echo _247AROUND_PARTNER_STRING; ?>' , service_id: '<?php echo $spare_parts_details['service_id']; ?>', part_type:part_type},
                     success:function(data){
-                        $('#parts_name').html(data);  
+                        $('#parts_name').html(data).change();  
                          var inventory_id =$("#parts_name").find('option:selected').attr("data-inventory"); 
                         $("#current_inventory_id").val(inventory_id);
                         $('#spinner').removeClass('fa fa-spinner').hide();
