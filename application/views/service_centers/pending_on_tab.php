e56a1831100dbcfa4fbc95d8b591cf4c47f889c1
ankitr17 <ankitr@247around.com>
2019-09-12 19:11:16 +0530

    upcountry modal issue resolved

--- application/views/service_centers/pending_on_tab.php
+++ application/views/service_centers/pending_on_tab.php
@@ -991,8 +991,7 @@
         } 
     });
     
-    // below line commented, As discussed with kalyani and abhay sir.
-    //setInterval(function(){ getBookingEngineers(); }, 30000);
+    setInterval(function(){ getBookingEngineers(); }, 30000);
     
     $(document).on("click", ".open-adminremarks", function () {
         
