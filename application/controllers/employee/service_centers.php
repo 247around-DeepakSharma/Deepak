7e5ec6d16654afc684f9ec649120f3aef39f8b1c
Priyank <priyankk@247around.com>
2019-09-19 16:25:29 +0530

    CRM-3748 | Show MSL security AMount IN SF Panel - resolved conflicts

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -6706,21 +6706,52 @@ class Service_centers extends CI_Controller {
         $this->load->view('service_centers/header');
         $this->load->view('service_centers/search_docket_number');
     }
-
-    function sf_dashboard() {
-        $this->checkUserSession();
-        $rating_data = $this->service_centers_model->get_vendor_rating_data($this->session->userdata('service_center_id'));
-        if (!empty($rating_data[0]['rating'])) {
-            $data['rating'] = $rating_data[0]['rating'];
-            $data['count'] = $rating_data[0]['count'];
-        } else {
-            $data['rating'] = 0;
-            $data['count'] = $rating_data[0]['count'];
-        }
-        $join['services'] = "services.id = vendor_pincode_mapping.Appliance_ID";
-        $data['services'] = $this->reusable_model->get_search_result_data("vendor_pincode_mapping", "DISTINCT vendor_pincode_mapping.Appliance_ID as id,services.services", array("Vendor_ID" => $this->session->userdata('service_center_id')), $join, NULL, array("services.services" => "ASC"), NULL, NULL, array());
-        $this->load->view('service_centers/header');
-        $this->load->view('service_centers/dashboard', $data);
+    function sf_dashboard(){
+            $this->checkUserSession();
+            $rating_data = $this->service_centers_model->get_vendor_rating_data($this->session->userdata('service_center_id'));
+            if(!empty($rating_data[0]['rating'])){
+                $data['rating'] = $rating_data[0]['rating'];
+                $data['count'] =  $rating_data[0]['count'];
+            }
+            else{
+                $data['rating'] = 0;
+                $data['count'] =  $rating_data[0]['count'];
+            }
+            $join['services'] = "services.id = vendor_pincode_mapping.Appliance_ID";
+            $data['services'] = $this->reusable_model->get_search_result_data("vendor_pincode_mapping","DISTINCT vendor_pincode_mapping.Appliance_ID as id,services.services",
+                 array("Vendor_ID"=>$this->session->userdata('service_center_id')),$join,NULL,array("services.services"=>"ASC"),NULL,NULL,array());
+            $mslSecurityData = $this->reusable_model->get_search_result_data(
+                'vendor_partner_invoices',
+                "vendor_partner, vendor_partner_id, sub_category,(total_amount_collected-amount_paid) as 'amount'",
+                array(
+                    "vendor_partner"=> "vendor",
+                    "vendor_partner_id"=> $this->session->userdata('service_center_id')
+                ),
+                NULL,NULL,NULL,
+                array(
+                    "sub_category"=>array(
+                        MSL_SECURITY_AMOUNT,
+                        MSL_NEW_PART_RETURN,
+                        MSL_DEFECTIVE_RETURN
+                    )
+                ),NULL,array()
+            );
+            $mslSecurityAmount = 0.0;
+            $mslAmount = 0.0;
+            foreach($mslSecurityData as $row){
+                if(!empty($row['sub_category']) && $row['sub_category']==MSL_SECURITY_AMOUNT){
+                    $mslSecurityAmount += floatval($row['amount']);
+                }else if(!empty($row['sub_category']) && ($row['sub_category']==MSL_DEFECTIVE_RETURN || $row['sub_category']==MSL_NEW_PART_RETURN)){
+                    $mslAmount += floatval($row['amount']);
+                }
+            }
+            $mslAmount = $mslSecurityAmount-$mslAmount;
+            $data['msl'] = array(
+                'security'=>sprintf("%01.2f", $mslSecurityAmount),
+                'amount'=>sprintf("%01.2f", $mslAmount)
+            );
+            $this->load->view('service_centers/header');
+            $this->load->view('service_centers/dashboard',$data);
     }
 
     function check_warehouse_shipped_awb_exist() {
