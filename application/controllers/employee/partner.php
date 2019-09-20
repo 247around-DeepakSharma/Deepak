83d40f8b9cfbed7b471563345dc906f79ef3aec9
abhi2707 <=abhisheka@247around.com>
2019-09-20 16:01:57 +0530

    Datatable error

--- application/controllers/employee/partner.php
+++ application/controllers/employee/partner.php
@@ -6638,7 +6638,7 @@ class Partner extends CI_Controller {
         $select = "defective_part_shipped,spare_parts_details.defactive_part_received_date_by_courier_api, "
                 . " spare_parts_details.booking_id, users.name, courier_name_by_sf, awb_by_sf,defective_part_shipped_date,"
                 . "remarks_defective_part_by_sf,spare_parts_details.sf_challan_number"
-                . ",spare_parts_details.sf_challan_file, spare_parts_details.shipped_quantity   as qty,spare_parts_details.partner_challan_number, spare_parts_details.id, spare_parts_details.status, i.part_number";
+                . ",spare_parts_details.sf_challan_file, spare_parts_details.shipped_quantity,spare_parts_details.partner_challan_number, spare_parts_details.id, spare_parts_details.status, i.part_number";
         //$group_by = "spare_qty_mgmt.spare_id";
          $group_by = "spare_parts_details.id";
         $bookingData = $this->service_centers_model->get_spare_parts_booking($where, $select, $group_by, $order_by, $postData['start'], $postData['length'],0,NULL,TRUE);
