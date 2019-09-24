c0bcfee78b94367fd07e0a35ad49110792c9f2de
pritys <pritys@247around.com>
2019-09-24 11:45:01 +0530

    CRM-3876 : Unable to select checkbox in show charges list

--- application/views/employee/generate_service_charges.php
+++ application/views/employee/generate_service_charges.php
@@ -1,6 +1,6 @@
 <script>$(".main_search").css("display","none");</script>
 
-<script src="https://rawgit.com/wasikuss/select2-multi-checkboxes/master/select2.multi-checkboxes.js"></script>
+<!--<script src="https://rawgit.com/wasikuss/select2-multi-checkboxes/master/select2.multi-checkboxes.js"></script>-->
 <div id="page-wrapper" >
 <div class="panel-heading">
     <h4>Generate <?php echo $public_name;?> Price </h4>
@@ -430,24 +430,26 @@
     
     
     jQuery(function($) {
-      
-      $('#capacity').select2MultiCheckboxes({
-        templateSelection: function(selected, total) {
-          return "Selected " + selected.length + " of " + total;
-        }
-      });
-      
-       $('#appliance_brand').select2MultiCheckboxes({
-        templateSelection: function(selected, total) {
-          return "Selected " + selected.length + " of " + total;
-        }
-      });
-      
-      $('#category').select2MultiCheckboxes({
-        templateSelection: function(selected, total) {
-          return "Selected " + selected.length + " of " + total;
-        }
-      });
+      $('#capacity').select2();
+      $('#appliance_brand').select2();
+      $('#category').select2();
+//      $('#capacity').select2MultiCheckboxes({
+//        templateSelection: function(selected, total) {
+//          return "Selected " + selected.length + " of " + total;
+//        }
+//      });
+//      
+//       $('#appliance_brand').select2MultiCheckboxes({
+//        templateSelection: function(selected, total) {
+//          return "Selected " + selected.length + " of " + total;
+//        }
+//      });
+//      
+//      $('#category').select2MultiCheckboxes({
+//        templateSelection: function(selected, total) {
+//          return "Selected " + selected.length + " of " + total;
+//        }
+//      });
       
     });
     
