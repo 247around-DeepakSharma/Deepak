d719d87964306081bcda63f9c95ee193053bd14a
abhi2707 <=abhisheka@247around.com>
2019-09-25 18:40:14 +0530

    Shipped  Qty

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -3033,7 +3033,8 @@ class Service_centers extends CI_Controller {
                 . " sf_challan_file as challan_file, "
                 . " remarks_defective_part_by_partner, "
                 . " remarks_by_partner, spare_parts_details.partner_id,spare_parts_details.service_center_id,spare_parts_details.defective_return_to_entity_id,spare_parts_details.entity_type,"
-                . " spare_parts_details.id,spare_parts_details.challan_approx_value ,i.part_number, spare_consumption_status.consumed_status,  spare_consumption_status.is_consumed";
+                . " spare_parts_details.id,spare_parts_details.shipped_quantity,spare_parts_details.challan_approx_value ,i.part_number, spare_consumption_status.consumed_status,  spare_consumption_status.is_consumed";
+
         $group_by = "spare_parts_details.id";
         $order_by = "status = '" . DEFECTIVE_PARTS_REJECTED . "', spare_parts_details.booking_id ASC";
 
@@ -3201,6 +3202,7 @@ function do_multiple_spare_shipping(){
         } else {
             
             $defective_courier_receipt = $this->input->post("sp_parts");
+            $spare_details = $this->partner_model->get_spare_parts_by_any("*",array('id'=>$sp_id));
             
             if (!empty($defective_courier_receipt)) {
                 if (!empty($sp_id)) {
@@ -3220,6 +3222,7 @@ function do_multiple_spare_shipping(){
                     }
 
                     $booking_id = $this->input->post('booking_id');
+                    $quantity = $spare_details[0]['shipped_quantity'];
                     $partner_id = $this->input->post('booking_partner_id');
                     $data['awb_by_sf'] = $awb;         
                     $kilo_gram = $this->input->post('defective_parts_shipped_kg') ? : '0';
