64877fd834cb4c5722bd78d1c7c1ed92d7353152
ankitr17 <ankitr@247around.com>
2019-09-27 16:05:37 +0530

    CRM-3927 (67) Booking closed by android app only

--- db_changes_sql2.sql
+++ db_changes_sql2.sql
@@ -1505,3 +1505,6 @@ INSERT INTO `penalty_details` (`id`, `partner_id`, `escalation_id`, `criteria`, 
 ALTER TABLE wrong_part_shipped_details ADD COLUMN active tinyint(1) NOT NULL DEFAULT 1;
 -- Prity Sharma 25-09-2019
 ALTER TABLE booking_unit_details CHANGE COLUMN sf_purchase_date sf_purchase_date date NULL DEFAULT NULL;
+
+-- Ankit 27-09-2019
+ALTER TABLE partners ADD COLUMN is_booking_close_by_app_only tinyint(1) NOT NULL DEFAULT 0 AFTER auth_token;
