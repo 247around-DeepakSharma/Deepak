ca2479dcab471aced140f75f30e65a0a76b2bb71
abhi2707 <=abhisheka@247around.com>
2019-10-07 16:08:58 +0530

    minor

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -2072,7 +2072,7 @@ class Service_centers extends CI_Controller {
         if($data['is_micro_wh']==1){
 
                 $data['spare_id'] = $this->input->post('spare_id');
-                $data['shipped_inventory_id'] = $spare_data['requested_inventory_id'];
+                $data['shipped_inventory_id'] = $data['requested_inventory_id'];
                 $data['shipped_quantity'] = $data['quantity'];
                 array_push($delivered_sp, $data);
                 unset($data['spare_id']);
