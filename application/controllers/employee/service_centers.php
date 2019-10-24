2ce2bf1ce1e43f0886eb2e390ee2878b362f5fcc
Gorakh Mehta <gorakh@247around.com>
2019-10-24 15:48:22 +0530

    Minor changes

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -6477,9 +6477,10 @@ function do_multiple_spare_shipping(){
     function warehouse_ack_partner_list() {
         $sf_id = $this->session->userdata('service_center_id');
         $where = array("spare_parts_details.defective_return_to_entity_id" => $sf_id,
-            "spare_parts_details.defective_return_to_entity_type" => _247AROUND_SF_STRING,
-            "defective_part_required" => 1,
-            "status IN ('" . _247AROUND_COMPLETED . "', '" . DEFECTIVE_PARTS_REJECTED . "') " => NULL);
+           "spare_parts_details.defective_return_to_entity_type" => _247AROUND_SF_STRING,
+            "spare_parts_details.defective_part_required" => 1,
+            "spare_parts_details.shipped_inventory_id != NULL" => NULL, 
+            "status IN ('"._247AROUND_COMPLETED."') " => NULL);
 
         $partner_id = $this->partner_model->get_spare_parts_by_any(' Distinct booking_details.partner_id', $where, true);
         if (!empty($partner_id)) {
@@ -6514,8 +6515,9 @@ function do_multiple_spare_shipping(){
         $sf_id = $this->session->userdata('service_center_id');
         $where = array("spare_parts_details.defective_return_to_entity_id" => $sf_id,
            "spare_parts_details.defective_return_to_entity_type" => _247AROUND_SF_STRING,
-            "defective_part_required" => 1, 
-            "status IN ('".DEFECTIVE_PARTS_RECEIVED."') " => NULL);
+            "spare_parts_details.defective_part_required" => 1, 
+            "spare_parts_details.shipped_inventory_id" => NULL, 
+            "spare_parts_details.status" => DEFECTIVE_PARTS_RECEIVED);
 
         $partner_id = $this->partner_model->get_spare_parts_by_any(' Distinct booking_details.partner_id', $where, true);
         if(!empty($partner_id)){
