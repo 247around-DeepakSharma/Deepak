45a3a0cf3010113f1b59c3da729a31b4b8ab6181
ankitr17 <ankitr@247around.com>
2019-10-24 19:07:20 +0530

    CRM-4311 (68) part lost case handling

--- application/libraries/Miscelleneous.php
+++ application/libraries/Miscelleneous.php
@@ -4834,7 +4834,9 @@ function generate_image($base64, $image_name,$directory){
                 // Do not remove empty initialization
                 $up = array();
                 $up = array('consumed_part_status_id' => $status_id);
-                if ($defective_part_required == 0) {
+                if($spare_part_detail['spare_lost'] == 1){
+                    continue;
+                } else if ($defective_part_required == 0) {
                     $status = _247AROUND_COMPLETED;
                     $up['status'] = $status;
                     
@@ -4850,7 +4852,7 @@ function generate_image($base64, $image_name,$directory){
                      }
                 }
                 
-                if(empty($spare_part_detail['defective_part_shipped']) && empty($spare_part_detail['defective_part_shipped_date'])) {
+                if((empty($spare_part_detail['defective_part_shipped']) && empty($spare_part_detail['defective_part_shipped_date'])) || $defective_part_required == 0) {
                     $this->My_CI->reusable_model->update_table('spare_parts_details', $up, ['id' => $spare_id]);
                 }
                 
