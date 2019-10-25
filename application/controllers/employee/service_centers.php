8ba2424ab5342386e1114a2215fcb207f3ce2b2a
Gorakh Mehta <gorakh@247around.com>
2019-10-25 11:19:36 +0530

    Add save defective parts sent date to partner in the spare table #CRM-4340

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -6737,10 +6737,10 @@ class Service_centers extends CI_Controller {
             $data['filtered_partner'] = $this->input->post('partner_id');
             $sf_id = $this->session->userdata('service_center_id');
             $where = "spare_parts_details.defective_return_to_entity_id = '" . $sf_id . "' AND spare_parts_details.defective_return_to_entity_type = '" . _247AROUND_SF_STRING . "'"
-                    . " AND booking_details.current_status ='"._247AROUND_COMPLETED."' AND defective_part_required = '1' AND status IN ('" . DEFECTIVE_PARTS_RECEIVED . "') ";
+                    . "AND spare_parts_details.wh_to_partner_defective_shipped_date IS NULL AND defective_part_required = '1' AND status IN ('" . DEFECTIVE_PARTS_RECEIVED . "') ";
             $where .= " AND spare_parts_details.entity_type = '" . _247AROUND_PARTNER_STRING . "' AND booking_details.partner_id = " . $partner_id;
             $data['spare_parts'] = $this->partner_model->get_spare_parts_booking_list($where, $offset, '', true, 0, null, false, " ORDER BY status = spare_parts_details.booking_id ");
-            
+
         } else {
             $data['spare_parts'] = array();
         }
