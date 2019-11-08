c2f476c3e88c4d3c5e1bf76e1b6c0b973f5832d0
Gorakh Mehta <gorakh@247around.com>
2019-11-08 19:09:41 +0530

    Shipped part code and rearrange column in the consolidated part report  #CRM-4462

--- application/models/inventory_model.php
+++ application/models/inventory_model.php
@@ -1656,6 +1656,7 @@ class Inventory_model extends CI_Model {
         $this->db->join('employee', "employee.id = agent_filters.agent_id", "left"); // new query for AM
         //$this->db->join('employee','partners.account_manager_id = employee.id'); // old query for AM
         $this->db->join('inventory_master_list as i', " i.inventory_id = spare_parts_details.requested_inventory_id", "left");
+        $this->db->join('inventory_master_list as iml', " iml.inventory_id = spare_parts_details.shipped_inventory_id", "left");
         $this->db->join('service_centres sc', 'spare_parts_details.partner_id = sc.id', 'left');
         $this->db->join('services', 'services.id = booking_details.service_id', 'left');
         $this->db->join('courier_company_invoice_details as cci', 'cci.id = spare_parts_details.awb_by_sf', 'left');
