6d4844fff9d3e88c96879a3063a6ed9af343592a
pritys <pritys@247around.com>
2019-11-11 15:32:27 +0530

    CRM-4464 : PHP Error

--- application/views/employee/completed_cancelled_review.php
+++ application/views/employee/completed_cancelled_review.php
@@ -270,10 +270,12 @@
              }
              for($i=0;$i<=$total_pages;$i++){
                  $offset = $per_page*$i;
+                 if(!empty($review_status)){
                  ?>
-    <a id="link_<?php echo $i;?>" style="background: #d7eaea;padding: 5px;" onclick="load_view('employee/booking/review_bookings_by_status/<?php echo  $review_status?>/<?php echo $offset;?>/<?php echo $is_partner; ?>/0/<?php echo $cancellation_reason_selected; ?>/<?php echo $partner_selected;?>/<?php echo $state_selected; ?>','<?php echo $tab ?>','link_<?php echo $i;?>')"><?php echo $i+1; ?></a>
+                 <a id="link_<?php echo $i;?>" style="background: #d7eaea;padding: 5px;" onclick="load_view('employee/booking/review_bookings_by_status/<?php echo  $review_status?>/<?php echo $offset;?>/<?php echo $is_partner; ?>/0/<?php echo $cancellation_reason_selected; ?>/<?php echo $partner_selected;?>/<?php echo $state_selected; ?>','<?php echo $tab ?>','link_<?php echo $i;?>')"><?php echo $i+1; ?></a>
                  <?php
-             }
+                }                
+            }
              ?>
                  </div>
 
