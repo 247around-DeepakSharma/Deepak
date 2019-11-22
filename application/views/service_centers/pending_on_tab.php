e2f9623b587ecb8d09e8740696ea052babf60993
abhi2707 <=abhisheka@247around.com>
2019-11-22 17:54:16 +0530

    minor UI

--- application/views/service_centers/pending_on_tab.php
+++ application/views/service_centers/pending_on_tab.php
@@ -129,9 +129,22 @@
                                             </div>
                                             <?php  echo $row->count_escalation." times"; ?>
                                         </td>
-                                        <?php if($is_engineer_app && $row->nrn_approved==0){ ?>
-                                        <td style="vertical-align: middle;"><select id="engineer_<?php echo $sn_no; ?>" class="engineer_select" service-id="<?php echo $row->service_id; ?>" engineer-id="<?php echo $row->assigned_engineer_id; ?>" booking-id="<?php echo $row->booking_id; ?>"></select>
+                                        <?php if($is_engineer_app){ ?>
+                                        <td style="vertical-align: middle;">
+                                            <?php if ($row->nrn_approved==0) { ?>
+                                                    
+                                            <select id="engineer_<?php echo $sn_no; ?>" class="engineer_select" service-id="<?php echo $row->service_id; ?>" engineer-id="<?php echo $row->assigned_engineer_id; ?>" booking-id="<?php echo $row->booking_id; ?>"></select>
+
                                             <a href='<?php echo base_url(); ?>service_center/add_engineer/<?php echo urlencode(base64_encode($row->booking_id)); ?>' class='btn btn-info btn-sm' target='_blank'><i class='fa fa-user' aria-hidden='true'></i></a>
+
+                                            <?php }else{ ?>
+
+                                                <select class="disabled">
+                                                    <option >Select Engineer</option>
+                                                </select>
+
+                                            <?php } ?>
+                                            
                                         </td>
                                         <?php }
                                         if(isset($saas_module) && (!$saas_module)) { ?>
