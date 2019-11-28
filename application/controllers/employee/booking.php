10638c0b38580dfd95fa81cc76df29eca6757bf8
pritys <pritys@247around.com>
2019-11-28 15:45:06 +0530

    CRM-4686 : [69] Extra columns required in file download on Pending Bookings page

--- application/controllers/employee/booking.php
+++ application/controllers/employee/booking.php
@@ -5360,6 +5360,7 @@ class Booking extends CI_Controller {
         $post['order'] = NULL;
         $post['draw'] = NULL;
         if($booking_status == 'Pending'){
+            $post['where']  = array('service_center_closed_date IS NULL' => NULL, 'internal_status NOT IN ("Spare Parts Shipped by Partner", "InProcess_Cancelled", "InProcess_Completed")' => NULL);
             $select = "booking_details.booking_id,DATEDIFF(CURDATE(),STR_TO_DATE(booking_details.booking_date,'%d-%m-%Y')) as Ageing,users.name as  Customer_Name,
             services.services,penalty_on_booking.active as penalty_active,users.phone_number,booking_details.order_id,booking_details.request_type,booking_details.internal_status,
             booking_details.booking_address,booking_details.booking_pincode,booking_details.booking_timeslot,
