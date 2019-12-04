ed416d7d3481345c450d6d013f42117a18b3d5b5
Kajal_Chauhan <kajal@247around.com>
2019-12-04 19:55:41 +0530

    CRM-4761 : Ask Partner GST number when return MSL to partner
    When New Part is being returned to Partner, Check Partner GST number & return New Part to location from where it has been sent .

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -7298,7 +7298,7 @@ function get_bom_list_by_inventory_id($inventory_id) {
 //    }
     
     function get_partner_gst_number(){
-        $html = "<option value='' selected disabled>Selet GST Number</option>";
+        $html = "<option value='' selected disabled>Select GST Number</option>";
         $where = array(
             "entity_type" => _247AROUND_PARTNER_STRING,
             "entity_id" => $this->input->post("partner_id")
@@ -7313,7 +7313,7 @@ function get_bom_list_by_inventory_id($inventory_id) {
     function get_247around_wh_gst_number(){
 
         $state_vendor = $this->vendor_model->viewvendor($this->session->userdata('service_center_id'));
-        $html = "<option value='' selected disabled>Selet GST Number</option>";
+        $html = "<option value='' selected disabled>Select GST Number</option>";
         $where = array(
             "entity_type" => _247AROUND_PARTNER_STRING,
             "entity_id" => _247AROUND,
