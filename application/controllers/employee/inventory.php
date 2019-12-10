a1a54a80e5e6ff2e264b962e6011722b224d268c
Kajal_Chauhan <kajal@247around.com>
2019-12-10 19:53:22 +0530

    CRM-4833 : Change the date format in invoice
    Change date format in invoice to 01/10/2019 instead of 1st Oct,2019 .

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -1534,7 +1534,7 @@ class Inventory extends CI_Controller {
         $data['capacity'] = $unit_details[0]['appliance_capacity'];
         $data['remarks'] = $this->input->post("estimate_remarks");
         $data['order_id'] = $booking_details[0]->order_id;
-        $data['date'] = date("jS M, Y");
+        $data['date'] = date("d/m/Y");
         $data['company_name'] = $partner_data[0]['company_name'];
         $data['company_address'] = $partner_data[0]['address'] . ", " . $partner_data[0]['district'] . ", Pincode " . $partner_data[0]['state'];
 
