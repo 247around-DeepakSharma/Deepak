6483ea297f00459afcca87e066ba2ff822fbdb50
kalyani tekpure <kalyanit@247around.com>
2019-12-17 18:52:42 +0530

    CRM-4920 Change failure message of serial no.

--- application/config/serial_number_failed_msg_constant.php
+++ application/config/serial_number_failed_msg_constant.php
@@ -46,7 +46,7 @@ define('JEEVES_SERIAL_NO_VALIDATION_FAILED_MSG', 'Serial No length should be 15 
 //Wybor 
 define('MONTH_POSIBLE_VALUES_2016', 'A,B,C,D,E,F,G,H,I,J,K,L');
 define('BRAND_POSIBLE_VALUES', 'W,O,B,E,BL');
-define('FAILURE_MSG', 'Please Enter Correct Serial Number');
+define('FAILURE_MSG', 'The serial number looks incorrect, please verify.');
 
 //Repeat Booking Failure Massege
 define('REPEAT_BOOKING_FAILURE_MSG', 'For Repeat Booking Serial Number Should be Similar to Parent Booking. Booking Will not be Complete Until Repeat Booking Will not have correcct Serial Number');
