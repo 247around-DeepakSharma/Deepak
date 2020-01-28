8071f9b7927459bb2ff8112551d9d73fbf7a35a7
pritys <pritys@247around.com>
2020-01-28 14:47:25 +0530

    CRM-5353 : Delete unlink file

--- application/controllers/employee/vendor.php
+++ application/controllers/employee/vendor.php
@@ -4374,6 +4374,9 @@ class vendor extends CI_Controller {
                 header('Content-Type: application/octet-stream');
                 header("Content-Disposition: attachment; filename=\"$output_file_name\""); 
                 readfile($output_file_excel);
+                // Delete file from temp folder
+                // @modified by : Ankit Rajvanshi
+                unlink($output_file_excel);
                 exit;
             } 
 
