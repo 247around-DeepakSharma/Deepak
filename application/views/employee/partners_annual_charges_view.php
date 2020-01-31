5ca1dd47e319ef2c8b9705e106fb84b347923d1f
Ghanshyam <ghanshyamg@247around.com>
2020-01-31 11:13:24 +0530

    CRM-5298 Check UI on Spare Part view Detail Page

--- application/views/employee/partners_annual_charges_view.php
+++ application/views/employee/partners_annual_charges_view.php
@@ -37,9 +37,10 @@
         <div class="panel-body">
             <div class="row">
                 <div class="col-md-12">
-                    <table class="table  table-striped table-bordered">
+                   <table class="table table-bordered  table-hover table-striped data" id="datatable"  >
                         <thead>
                             <tr>
+                                <th class="text-center">Sn.</th>
                                 <th class="text-center">Partner Name</th>
                                 <th class="text-center">Invoice ID</th>
                                 <th class="text-center">From Date</th>
@@ -49,18 +50,25 @@
                         </thead>
                         <tbody>
                             <?php  
+                                $total_amount=0;
+                                $rowcount=0;
                                 foreach ($annual_charges_data as $row)  
                                 {  //print_r($row);
                                    ?>
                             <tr>
+                                <td class="text-center"><?php echo ++$rowcount; ?></td>
                                 <td class="text-center"><a target="_blank" href="<?php echo base_url();?>employee/invoice/invoice_summary/partner/<?php echo $row->vendor_partner_id;?>"><?php echo $row->public_name;?></td>
                                 <td class="text-center"><a target="_blank" href="<?php echo S3_WEBSITE_URL;?>invoices-excel/<?php echo $row->invoice_file_main;?>"><?php echo $row->invoice_id;?></a></td>
                                 <td class="text-center"><?php echo date('d-M-Y', strtotime($row->from_date));?></td>
                                 <td class="text-center"><?php echo date('d-M-Y', strtotime($row->to_date));?></td>
                                 <td class="text-center"><?php echo "<i class='fa fa-inr'></i> ".$row->amount_collected_paid;?></td>
                             </tr>
-                            <?php }  
+                            <?php
+                            $total_amount=$total_amount+$row->amount_collected_paid;
+                            $total_amount=number_format((float)$total_amount, 2, '.', '');
+                            }  
                                 ?>  
+                            <tr><td></td><td></td><td></td><td></td><td class="text-center">Total Amount</td><td class="text-center"><i class='fa fa-inr'></i> <?php echo $total_amount; ?></td></tr>
                         </tbody>
                     </table>
                 </div>
@@ -69,3 +77,27 @@
             </div>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        var time = moment().format('D-MMM-YYYY');
+    $('#datatable').DataTable(
+       {     
+        dom: 'Bfrtip',
+        "buttons": [
+                {
+                    extend: 'excel',
+                    text: 'Export',
+                    title: 'annual_charges_'+time                    
+                },
+            ],
+        
+        "paging":   false,
+        "ordering": false,
+        "info":     false,
+        "searching": false
+      //"aLengthMenu": [[15, 18, 25, -1], [5, 10, 25, "All"]],
+        //"iDisplayLength": 5
+       } 
+        );
+} );
+    </script>
