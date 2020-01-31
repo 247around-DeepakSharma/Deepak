d8a781fde0fa462963465da8c0d2beada29d391a
abhi2707 <=abhisheka@247around.com>
2020-01-31 18:09:58 +0530

    Booking details update in response

--- application/libraries/booking_creation_lib.php
+++ application/libraries/booking_creation_lib.php
@@ -149,8 +149,13 @@ class booking_creation_lib {
             }
             
             $booking['is_repeat'] = $is_repeat;
+            /*  Update response for booking details */
             $booking['repeat_booking_flag'] = FALSE;
-            if (in_array(REPEAT_BOOKING_TAG, $booking['prices']))
+            $where_booking = array(
+                'booking_id' => $
+            );
+            $b_details = $this->My_CI->booking_model->get_bookings_count_by_any("booking_details.request_type",$where_booking);
+            if ($b_details[0]['request_type'] =REPEAT_BOOKING_TAG)
             {
              $booking['repeat_booking_flag'] = TRUE;
             }
