732f286acbc4a5999de08eec6fc62b4fb0ca2d41
Ghanshyam <ghanshyamg@247around.com>
2020-02-03 18:20:33 +0530

    CRM-5298 Check UI on Spare Part view Detail Page

--- application/views/partner/booking_details.php
+++ application/views/partner/booking_details.php
@@ -393,7 +393,7 @@
                                                                 <?php }else{ ?>
                                                                     <td> - </td>
                                                                 <?php } ?>
-                                                                <td><?php echo date("d-M-Y", strtotime($sp['acknowledge_date'])); ?></td>
+                                                                
                                                                 <td><?php echo $sp['remarks_by_sc']; ?></td>
                                                                 <td><?php echo $sp['status']; ?></td>
                                                                 <td><?php echo $sp['part_cancel_reason'];?></td>
