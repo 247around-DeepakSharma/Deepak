6dcc02575ccc5908e6ca6a8a21f3db7dcf5f1ea8
Gorakh Mehta <gorakh@247around.com>
2020-02-08 11:23:01 +0530

    Update docket number when acknowledge defective from warehouse panel send by SF #CRM-4945

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -6481,8 +6481,8 @@ class Service_centers extends CI_Controller {
         }
         if ($response) {
             
-            if(!empty($spare_part_detail[0]['awb_by_sf'])){
-                $this->inventory_model->update_courier_company_invoice_details(array('awb_number' => $spare_part_detail[0]['awb_by_sf'], 'delivered_date IS NULL' => NULL), array('delivered_date' => date('Y-m-d H:i:s'), 'actual_weight' => $received_weight, "billable_weight" => $received_weight));
+            if(!empty($spare_part_detail['awb_by_sf'])){
+                $this->inventory_model->update_courier_company_invoice_details(array('awb_number' => $spare_part_detail['awb_by_sf'], 'delivered_date IS NULL' => NULL), array('delivered_date' => date('Y-m-d H:i:s'), 'actual_weight' => $received_weight, "billable_weight" => $received_weight));
             }
 
             log_message('info', __FUNCTION__ . " Received Defective Spare Parts " . $booking_id
@@ -6539,7 +6539,7 @@ class Service_centers extends CI_Controller {
 
             if (empty($is_cron)) {
                 $userSession = array('success' => ' Received Defective Spare Parts');
-                echo 'Defective Spare Parts Received.';
+                //echo 'Defective Spare Parts Received.';
                 exit;
 //                $this->session->set_userdata($userSession);
 //                redirect(base_url() . "service_center/defective_spare_parts");
