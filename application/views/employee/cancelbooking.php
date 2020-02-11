5c6136f707028a7de36f193a4125bc9cc334bebc
ankitr17 <ankitr@247around.com>
2020-02-11 14:55:10 +0530

    CRM-5775 do not allow cancel booking if spares are still in request

--- application/views/employee/cancelbooking.php
+++ application/views/employee/cancelbooking.php
@@ -21,20 +21,26 @@ function check_text(){
 </script>
                    <?php $isdisable = false; if(isset($user_and_booking_details['spare_parts'])){ 
                        foreach($user_and_booking_details['spare_parts'] as $sp){
-                           switch ($sp['status']){
-                               case SPARE_PARTS_REQUESTED: 
-                                    $status = CANCEL_PAGE_SPARE_NOT_SHIPPED;
-                                    $isdisable= true;
-                                   break;
-                               default:
-                                    if(!empty($sp['shipped_date'])) {
-                                        $status = CANCEL_PAGE_SPARE_SHIPPED;
-                                        $isdisable= true;
+                           /**
+                            * check for non-cancelled spare parts.
+                            * modified by : Ankit Rajvanshi
+                            */
+                           if ($sp['status'] != _247AROUND_CANCELLED) {
+                                switch ($sp['status']){
+                                    case SPARE_PARTS_REQUESTED: 
+                                         $status = CANCEL_PAGE_SPARE_NOT_SHIPPED;
+                                         $isdisable= true;
+                                        break;
+                                    default:
+                                         if(!empty($sp['shipped_date'])) {
+                                             $status = CANCEL_PAGE_SPARE_SHIPPED;
+                                             $isdisable= true;
+                                         }
                                     }
-                           }
-                          
-                       }
-                   } ?>
+                                }
+                            }
+                        }
+                    ?>
 <div id="page-wrapper"> 
    <div class="container-fluid">
         <div class="row">
