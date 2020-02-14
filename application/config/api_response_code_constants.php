b97d72ffd527d58a051c295d3bf4e33ed34d1c37
abhi2707 <=abhisheka@247around.com>
2020-02-14 11:18:00 +0530

    CRM-5529	 Engineer Configuration page in CRM
    CRM-5529
    Engineer Configuration page in CRM

--- application/config/api_response_code_constants.php
+++ application/config/api_response_code_constants.php
@@ -1,4 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 ///  Making new constants for responses
+define("APP_VERSION_RESPONSE_CODE", "9999");
 define("API_NOTIFICATIONS_SUCCESS", "0000");
 define("API_NOTIFICATIONS_SUCCESS_MSG", "Notifications Found Successfully!");
---
+++ application/config/engineer_config.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+///  Making new constants for responses
+define("APP_VERSION", "2.19");
+define("FORCE_UPGRADE", "force_upgrade");
