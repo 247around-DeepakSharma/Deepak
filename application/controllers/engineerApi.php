1f3437ae3c330a0ffa43eca412ddb942ec4d6dff
abhi2707 <=abhisheka@247around.com>
2020-02-19 15:40:52 +0530

    / Remove hardcode test booking
    / Remove hardcode test booking

--- application/controllers/engineerApi.php
+++ application/controllers/engineerApi.php
@@ -3562,7 +3562,7 @@ class engineerApi extends CI_Controller {
             $select = 'spare_parts_details.id, spare_parts_details.shipped_inventory_id, spare_parts_details.parts_requested, spare_parts_details.parts_requested_type, spare_parts_details.status, spare_parts_details.quantity,spare_parts_details.parts_shipped,spare_parts_details.model_number_shipped,spare_parts_details.shipped_parts_type,spare_parts_details.shipped_date, inventory_master_list.part_number';
             /*  Response and condition according to Second Part Request and also acc to first part request */
             if(isset($requestData["pre_consume_req"]) && $requestData["pre_consume_req"]){
-            $response['spare_parts_details'] = $this->partner_model->get_spare_parts_by_any($select, ['booking_id' => 'LP-6228241911252', 'spare_parts_details.status != "' . _247AROUND_CANCELLED . '"' => NULL,'consumed_part_status_id is null' => NULL], FALSE, FALSE, FALSE, ['is_inventory' => true]);
+            $response['spare_parts_details'] = $this->partner_model->get_spare_parts_by_any($select, ['booking_id' => $booking_id, 'spare_parts_details.status != "' . _247AROUND_CANCELLED . '"' => NULL,'consumed_part_status_id is null' => NULL], FALSE, FALSE, FALSE, ['is_inventory' => true]);  // Remove hardcode test booking
             }else{
             $response['spare_parts_details'] = $this->partner_model->get_spare_parts_by_any($select, ['booking_id' => $booking_id, 'spare_parts_details.status != "' . _247AROUND_CANCELLED . '"' => NULL], FALSE, FALSE, FALSE, ['is_inventory' => true]);   
             }
