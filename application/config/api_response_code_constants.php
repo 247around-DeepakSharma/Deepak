f624eb8afb2ed6a049fa69e79e537eff7d38a9f6
abhi2707 <=abhisheka@247around.com>
2020-02-20 12:09:40 +0530

    Now if Engg Request for Second part . He/She has to fill the Previous conume reason
    Now if Engg Request for Second part . He/She has to fill the Previous
    conume reason

--- application/config/api_response_code_constants.php
+++ application/config/api_response_code_constants.php
@@ -2,3 +2,6 @@
 ///  Making new constants for responses
 define("API_NOTIFICATIONS_SUCCESS", "0000");
 define("API_NOTIFICATIONS_SUCCESS_MSG", "Notifications Found Successfully!");
+define("API_PREV_CONSUME_NOT_UPDATED", "0079");
+define("API_PREV_CONSUME_NOT_UPDATED_MSG", "Some Spares were not updated. Try Again! After some time.");
+define("API_PREV_CONSUME_NOT_UPDATED_KEY_MISS_MSG", "Please fill All Details");
