4778e441cf9f87c7f8dd38d279e61d4e37675e4e
Gorakh Mehta <gorakh@247around.com>
2020-02-20 12:52:45 +0530

    In case of oow booking when we request 2 spares and marked one spare for defective required then when we go to generate spare purchase invoice the value is miss match with the challan value to defective part required#CRM-5490

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -8525,12 +8525,17 @@ function get_bom_list_by_inventory_id($inventory_id) {
      */
     function get_spare_invoice_details(){
         $spare_id_array = $this->input->post("spare_id_array");
-        if(!empty($spare_id_array)){
-          $spare_ids = implode(',',$spare_id_array);
-          $select = 'spare_parts_details.booking_id,oow_spare_invoice_details.id,oow_spare_invoice_details.invoice_id,oow_spare_invoice_details.spare_id,oow_spare_invoice_details.invoice_date,oow_spare_invoice_details.hsn_code,oow_spare_invoice_details.invoice_amount,oow_spare_invoice_details.gst_rate,oow_spare_invoice_details.invoice_pdf';
-          $where = array("spare_parts_details.id IN(".$spare_ids.")" => NULL); 
-          
-          $invoice_details = $this->inventory_model->get_spare_invoice_details($select, $where); 
+
+        if (!empty($spare_id_array)) {
+            $spare_ids = implode(',', $spare_id_array);
+            $select = 'spare_parts_details.booking_id,oow_spare_invoice_details.id,oow_spare_invoice_details.invoice_id,oow_spare_invoice_details.spare_id,oow_spare_invoice_details.invoice_date,oow_spare_invoice_details.hsn_code,oow_spare_invoice_details.invoice_amount as total_invoice_amount,oow_spare_invoice_details.gst_rate,oow_spare_invoice_details.invoice_pdf';
+            $where = array("spare_parts_details.id IN(" . $spare_ids . ")" => NULL);
+            $oow_invoice_data = $this->inventory_model->get_spare_invoice_details($select, $where);
+            $invoice_details =array();
+            foreach ($oow_invoice_data as $key => $val ){
+                $tax_amount = $this->booking_model->get_calculated_tax_charge($val['total_invoice_amount'], $val['gst_rate']);
+                $invoice_details[$key]['invoice_amount'] = $val['total_invoice_amount']- $tax_amount;
+            }
         }
         
         echo json_encode($invoice_details);
