61c04fb6ea21e1f23f3ab6940bcdb888b2d4bfab
abhi2707 <=abhisheka@247around.com>
2020-02-20 12:09:40 +0530

    Now if Engg Request for Second part . He/She has to fill the Previous conume reason
    Now if Engg Request for Second part . He/She has to fill the Previous
    conume reason

--- application/config/api_response_code_constants.php
+++ application/config/api_response_code_constants.php
@@ -1,5 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 ///  Making new constants for responses
-define("APP_VERSION_RESPONSE_CODE", "9999");
 define("API_NOTIFICATIONS_SUCCESS", "0000");
 define("API_NOTIFICATIONS_SUCCESS_MSG", "Notifications Found Successfully!");
+define("API_PREV_CONSUME_NOT_UPDATED", "0079");
+define("API_PREV_CONSUME_NOT_UPDATED_MSG", "Some Spares were not updated. Try Again! After some time.");
+define("API_PREV_CONSUME_NOT_UPDATED_KEY_MISS_MSG", "Please fill All Details");
