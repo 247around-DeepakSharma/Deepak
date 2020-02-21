1c5c07ca567890b625adc61e7c403484ee560f9b
abhi2707 <=abhisheka@247around.com>
2020-02-21 14:45:25 +0530

    Undefined index solve
    Undefined index solve

--- application/controllers/engineerApi.php
+++ application/controllers/engineerApi.php
@@ -2231,7 +2231,7 @@ class engineerApi extends CI_Controller {
         if (!empty($requestData["booking_id"])) {
             $documets = $this->service_centers_model->get_collateral_for_service_center_bookingsAPI($requestData["booking_id"]); /// Makeing seperate function for API
             $i = 0;
-            foreach ($documets as $key => $value) {
+            foreach ($documets[0] as $key => $value) { /// Undefined index solve
                 if ($value['document_type'] == "pdf") {
                     $pdf['document_type'] = $value['document_type'];
                     $pdf['document_description'] = $value['document_description'];
