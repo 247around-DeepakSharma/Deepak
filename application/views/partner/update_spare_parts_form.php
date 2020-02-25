5b58d8735ddf131fd0901c76e6533a13e1bcdd7c
Gorakh Mehta <gorakh@247around.com>
2020-02-25 18:47:40 +0530

    PHP  occured while shipping the OOW spare from the Partner panel#CRM-5628

--- application/views/partner/update_spare_parts_form.php
+++ application/views/partner/update_spare_parts_form.php
@@ -322,6 +322,7 @@
                     <input type="hidden" name="part[<?php echo $key;?>][spare_id]"  id="<?php echo "spare_id_".$key; ?>" value="<?php echo $value->id;?>">
 <!--                    <input type="hidden" name="part[<?php echo $key;?>][inventory_id]" id="<?php echo "inventoryid_". $key;?>">-->
                     <input type="hidden" id="<?php echo "estimatecostgivendate_".$key ?>" name= "part[<?php echo $key;?>][estimate_cost_given_date_h]" value="<?php echo $value->estimate_cost_given_date; ?>">
+                    <input type="hidden" name= "part[<?php echo $key;?>][spare_part_warranty_status]" value="<?php echo $value->part_warranty_status ;?>">
                     <?php } ?>
                     
                     <div id="template" class="hide">
