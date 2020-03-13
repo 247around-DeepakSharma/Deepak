970dd60a5c2c3ff71917c39011d9200b1ae4102c
Gorakh Mehta <gorakh@247around.com>
2020-03-13 10:42:55 +0530

    While accepting the Defective at the CWH panel the Gram column is not taking *ZERO* twice it is accepting only single Zero #CRM-5570

--- application/views/service_centers/change_multiple_part_consumption.php
+++ application/views/service_centers/change_multiple_part_consumption.php
@@ -79,6 +79,7 @@
     $("#defective_parts_shipped_weight_in_kg").on({
         "click": function () {
             var weight_kg = $(this).val();
+            var weight_gram = $("#defective_parts_shipped_weight_in_gram").val();
              var isValid = false;
             if(weight_kg !=''){
                 var regex = /^[0-9\s]*$/;
@@ -93,13 +94,16 @@
                 return false;
             }
 
-            if (weight_kg == '0' || weight_kg == '00' || weight_kg == '000') {
+            if ((weight_kg == '0' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' )) ||
+                 (weight_kg == '00' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' )) ||
+                 (weight_kg == '000' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' ))) {
                 $(this).val('');
                 return false;
             }
         },
         "keypress": function () {
             var weight_kg = $(this).val();
+            var weight_gram = $("#defective_parts_shipped_weight_in_gram").val();
             var isValid = false;
             if(weight_kg !=''){
                 var regex = /^[0-9\s]*$/;
@@ -115,13 +119,16 @@
                 return false;
             }
 
-            if (weight_kg == '0' || weight_kg == '00' || weight_kg == '000') {
+            if ((weight_kg == '0' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' )) ||
+                 (weight_kg == '00' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' )) ||
+                 (weight_kg == '000' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' ))) {
                 $(this).val('');
                 return false;
             }
         },
         "mouseleave": function () {
             var weight_kg = $(this).val();
+            var weight_gram = $("#defective_parts_shipped_weight_in_gram").val();
             var isValid = false;
             if(weight_kg !=''){
                 var regex = /^[0-9\s]*$/;
@@ -136,7 +143,9 @@
                 $(this).val('');
                 return false;
             }
-            if (weight_kg == '0' || weight_kg == '00' || weight_kg == '000') {
+            if ((weight_kg == '0' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' )) ||
+                 (weight_kg == '00' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' )) ||
+                 (weight_kg == '000' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' ))) {
                 $(this).val('');
                 return false;
             }
@@ -164,6 +173,7 @@
     $("#defective_parts_shipped_weight_in_gram").on({
         "click": function () {
             var weight_kg = $(this).val();
+            var weight_gram = $("#defective_parts_shipped_weight_in_kg").val();
             if(weight_kg !=''){
                 var regex = /^[0-9\s]*$/;
                 isValid = regex.test(weight_kg);
@@ -177,7 +187,9 @@
                 return false;
             }
 
-            if (weight_kg == '0' || weight_kg == '00' || weight_kg == '000') {
+            if ((weight_kg == '0' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' )) ||
+                 (weight_kg == '00' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' )) ||
+                 (weight_kg == '000' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' ))) {
                 $(this).val('');
                 return false;
             }
@@ -185,6 +197,7 @@
         },
         "keypress": function () {
             var weight_kg = $(this).val();
+            var weight_gram = $("#defective_parts_shipped_weight_in_kg").val();
             if(weight_kg !=''){
                 var regex = /^[0-9\s]*$/;
                 isValid = regex.test(weight_kg);
@@ -198,7 +211,9 @@
                 return false;
             }
 
-            if (weight_kg == '0' || weight_kg == '00' || weight_kg == '000') {
+              if ((weight_kg == '0' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' )) ||
+                 (weight_kg == '00' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' )) ||
+                 (weight_kg == '000' && (weight_gram =='0' || weight_gram =='00' || weight_gram =='' ))) {
                 $(this).val('');
                 return false;
             }
