82e6fa7f15e6a6ec494d813de490edfabdcf1ea0
Ghanshyam <ghanshyamg@247around.com>
2020-03-16 09:17:24 +0530

    CRM-5792 Need to refresh the accessories list page after active/inactive the page.

--- application/views/employee/show_accessories_list.php
+++ application/views/employee/show_accessories_list.php
@@ -19,8 +19,9 @@
                                     <th class="text-center">Basic Charge</th>
                                     <th class="text-center">HSN Code</th>
                                     <th class="text-center">Tax Rate</th>
+                                    <th class="text-center">Status</th>
                                     <th class="text-center">Edit</th>
-                                    <th class="text-center">Delete / Restore</th>
+                                    <th class="text-center">Active / Inactive</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -40,18 +41,22 @@
                                         <td class="text-center"><?php echo $row['basic_charge']; ?></td>
                                         <td class="text-center"><?php echo $row['hsn_code']; ?></td>
                                         <td class="text-center"><?php echo $row['tax_rate']; ?></td>
-                                        <td class="text-center"><a type="button" class="btn btn-info" href="<?php echo base_url() . "employee/accessories/edit_accessories/" . $row['id']; ?>"><i class="fa fa-edit" aria-hidden="true"></i></a></td>
-
-                                        <td class="text-center">
-                                            <?php
+                                        <?php
                                             $deactivateshow = 'none';
                                             $activateshow = 'none';
                                             if ($row['status'] == 1) {
                                                 $deactivateshow = 'initial';
+                                                $statusShow = 'Active';
                                             } else {
                                                 $activateshow = 'initial';
+                                                $statusShow = 'Inactive';
                                             }
                                             ?>
+                                        <td class="text-center"><span id='status_s<?php echo $row['id']; ?>'><?php echo $statusShow; ?></span></td>
+                                        <td class="text-center"><a type="button" class="btn btn-info" href="<?php echo base_url() . "employee/accessories/edit_accessories/" . $row['id']; ?>"><i class="fa fa-edit" aria-hidden="true"></i></a></td>
+
+                                        <td class="text-center">
+                                            
                                             <a type="button" class="btn btn-sm btn-danger open-adminremarks" onclick="update_accessories_status(<?php echo $row['id']; ?>, 0)" id='btn_<?php echo $row['id']; ?>' title='delete' style='display:<?php echo $deactivateshow; ?>'><i class="fa fa-trash"></i></a>
 
                                             <a type="button" class="btn btn-sm btn-success open-adminremarks" onclick="update_accessories_status(<?php echo $row['id']; ?>, 1)" id='btn_s_<?php echo $row['id']; ?>' title='Restore' style='display:<?php echo $activateshow; ?>'><i class="fa fa-undo"></i></a>
@@ -81,7 +86,7 @@
                     title: 'accessories_list_<?php echo date('Ymd-His'); ?>',
                     footer: true,
                     exportOptions: {
-                        columns: [0, 1, 2, 3, 4, 5, 6],
+                        columns: [0, 1, 2, 3, 4, 5, 6,7],
                         modifier: {
                             // DataTables core
                             order: 'index', // 'current', 'applied', 'index',  'original'
@@ -137,11 +142,45 @@
                 if (status == 1)
                 {
                     $("#btn_" + idtodelete).show();
+                    $("#status_s" + idtodelete).html('Active');
                 } else
                 {
-                    $("#btn_s_" + idtodelete).show();
+                    $("#btn_s_" +idtodelete).show();
+                    $("#status_s" + idtodelete).html('Inactive');
                 }
-
+                $('#annual_charges_report').dataTable().fnDestroy();
+            $('#annual_charges_report').DataTable({
+            "processing": true,
+            "serverSide": false,
+            "dom": 'lBfrtip',
+            "buttons": [
+            {
+                extend: 'excel',
+                text: '<span class="fa fa-file-excel-o"></span>  Export',
+                title: 'accessories_list_<?php echo date('Ymd-His'); ?>',
+                footer: true,
+                exportOptions: {
+                    columns: [0, 1, 2, 3, 4, 5, 6,7],
+                    modifier: {
+                        // DataTables core
+                        order: 'index', // 'current', 'applied', 'index',  'original'
+                        page: 'all', // 'all',     'current'
+                        search: 'none'     // 'none',    'applied', 'removed'
+                    }
+                }
+            }
+            ],
+            "order": [],
+            "ordering": true,
+            "deferRender": true,
+            //"searching": false,
+            //"paging":false
+            "pageLength": 50,
+            "language": {
+            "emptyTable": "No Data Found",
+            "searchPlaceholder": "Search by any column."
+            },
+            });
             }
         });
     }
