dde67144cf42aca905b036599f69b3d0d4e42e94
ankitr17 <ankitr@247around.com>
2020-03-16 15:25:17 +0530

    CRM-5769 spare status updation if part not required

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -1223,6 +1223,17 @@ class Inventory extends CI_Controller {
                     $where = array('id' => $id);
                     $track_status = $new_state = "Spare Parts Not Required To Warehouse";
                     $old_state = SPARE_PARTS_REQUESTED;
+                    
+                    /**
+                     * If defective part not required after booking completion then change spare status accordingly.
+                     * @modifiedBy Ankit Rajvanshi
+                     */
+                    if(!empty($booking_details['service_center_closed_date'])) {
+                        $spare_part_detail = $this->reusable_model->get_search_result_data('spare_parts_details', '*', $where, NULL, NULL, NULL, NULL, NULL)[0];                    
+                        $is_spare_consumed = $this->reusable_model->get_search_result_data('spare_consumption_status', '*', ['id' => $spare_part_detail['consumed_part_status_id']], NULL, NULL, NULL, NULL, NULL)[0]['is_consumed'];
+                        $data['status'] = _247AROUND_COMPLETED;                 
+                    }
+                    
                     break;
 
                 CASE 'NOT_REQUIRED_PARTS_FOR_COMPLETED_BOOKING':
@@ -1233,8 +1244,17 @@ class Inventory extends CI_Controller {
                     $sc_data['current_status'] = "InProcess";
                     $sc_data['internal_status'] = "Completed";
                     $sc_data['update_date'] = date("Y-m-d H:i:s");
-
                     $this->vendor_model->update_service_center_action($booking_id, $sc_data);
+
+                    /**
+                     * If defective part not required after booking completion then change spare status accordingly.
+                     * @modifiedBy Ankit Rajvanshi
+                     */
+                    if(!empty($booking_details['service_center_closed_date'])) {
+                        $spare_part_detail = $this->reusable_model->get_search_result_data('spare_parts_details', '*', $where, NULL, NULL, NULL, NULL, NULL)[0];                    
+                        $is_spare_consumed = $this->reusable_model->get_search_result_data('spare_consumption_status', '*', ['id' => $spare_part_detail['consumed_part_status_id']], NULL, NULL, NULL, NULL, NULL)[0]['is_consumed'];
+                        $data['status'] = _247AROUND_COMPLETED;                 
+                    }
                     break;
 
                 CASE 'REQUIRED_PARTS':
