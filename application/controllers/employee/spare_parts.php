c728d231147dfe29e19f9378f9fd4064b664801a
Gorakh Mehta <gorakh@247around.com>
2020-03-18 17:02:58 +0530

    OOT showing data all #CRM-5823

--- application/controllers/employee/spare_parts.php
+++ application/controllers/employee/spare_parts.php
@@ -4307,8 +4307,11 @@ $select = 'spare_parts_details.entity_type,spare_parts_details.quantity,spare_pa
         unset($post['where']['status']);
 
         $post['where'] = array("DATEDIFF(CURRENT_TIMESTAMP,  STR_TO_DATE(spare_parts_details.shipped_date, '%Y-%m-%d')) >= 45" => NULL);
-        $post['where']['defective_part_shipped_date IS NULL'] = NULL;
-        $post['where']['defective_part_required'] = 1;
+        $post['where']['spare_parts_details.defective_part_shipped_date IS NULL'] = NULL;
+        $post['where']['spare_parts_details.defective_part_required'] = 1;
+        $post['where']['spare_parts_details.consumed_part_status_id !='] = 2;
+        
+        $post['where']['status in ("' . DEFECTIVE_PARTS_PENDING . '","' . OK_PART_TO_BE_SHIPPED . '","' . DAMAGE_PART_TO_BE_SHIPPED . '")'] = NULL;
         $post['is_inventory'] = TRUE;
 
         $list = $this->inventory_model->get_out_tat_spare_parts_list($post);
