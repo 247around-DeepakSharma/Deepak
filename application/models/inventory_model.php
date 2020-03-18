ea38ff1c695f55af8d0c260b94b0c881fc1be7c5
Gorakh Mehta <gorakh@247around.com>
2020-03-18 14:26:56 +0530

    DataBase Error #CRM-5806

--- application/models/inventory_model.php
+++ application/models/inventory_model.php
@@ -1581,6 +1581,7 @@ class Inventory_model extends CI_Model {
         $this->db->from('inventory_ledger');
         $this->db->join('courier_details','inventory_ledger.courier_id = courier_details.id');
         $this->db->join('spare_parts_details','inventory_ledger.booking_id = spare_parts_details.booking_id');
+        $this->db->join('inventory_master_list as im', 'spare_parts_details.shipped_inventory_id = im.inventory_id');
         $query = $this->db->get();
         return $query->result_array();
     }
