1eb9ec13816cb6ca079f0c0310b3aeae3052699e
abhi2707 <=abhisheka@247around.com>
2020-03-20 19:10:42 +0530

    DA-2 API for get States -- New Data from generic library
    DA-2
    
    API for get States -- New Data from generic library

---
+++ application/config/around_lib_constant.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+define("STATES_FOUND_MSG", "States Found Successfully !");
+define("STATES_FOUND_MSG_CODE", "0000");
+define('STATES_FOUND_MSG_ERR',"Error in fetching states list");
+define("STATES_FOUND_MSG_ERR_CODE", "1001");
