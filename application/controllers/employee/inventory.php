d3960608831caf1a8d5cf1f2029774f23894ae0b
Gorakh Mehta <gorakh@247around.com>
2020-03-30 14:48:45 +0530

    Add two colums billed to Partner courier charges and also add sender city #CRM-5565

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -9307,8 +9307,8 @@ function get_bom_list_by_inventory_id($inventory_id) {
 
         if (!empty($partner_id)) {
             $select = "courier_company_invoice_details.awb_number as 'Docket Number', courier_company_invoice_details.company_name as 'Docket Company Name', "
-                    . "partners.public_name as 'Partner Name',billed_docket.invoice_id as 'Invoice No.', courier_company_invoice_details.receiver_city as 'City',"
-                    . "courier_company_invoice_details.box_count as 'No. Of Boxes', courier_company_invoice_details.billable_weight as 'Weight', courier_company_invoice_details.courier_charge as 'Courier Charge', courier_company_invoice_details.courier_invoice_file as 'Courier Receipt Link'";
+                    . "partners.public_name as 'Partner Name',billed_docket.invoice_id as 'Invoice No.', courier_company_invoice_details.sender_city as 'Sender City', courier_company_invoice_details.receiver_city as 'Receiver City',"
+                    . "courier_company_invoice_details.box_count as 'No. Of Boxes', courier_company_invoice_details.billable_weight as 'Weight', courier_company_invoice_details.courier_charge as 'Courier Charge', billed_docket.basic_charge as 'Billed To Partner Courier Charges', courier_company_invoice_details.courier_invoice_file as 'Courier Receipt Link'";
             $where = array("billed_docket.entity_id" => $partner_id);
             $courier_invoice_details = $this->inventory_model->get_courier_invoice_data($select, $where);
             $this->load->dbutil();
