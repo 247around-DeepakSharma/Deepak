31398f266ea91ce4510a02651c65d31ad9345f74
abhi2707 <=abhisheka@247around.com>
2020-03-31 12:12:24 +0530

    AN-108	Add Provision to select Parent Booking id in case of multiple Parent Booking id
    AN-108
    Add Provision to select Parent Booking id in case of multiple Parent
    Booking id

--- application/controllers/engineerApi.php
+++ application/controllers/engineerApi.php
@@ -475,6 +475,10 @@ class engineerApi extends CI_Controller {
             case 'getEngineerIncentives':  
                 $this->get_engineer_incentives();  //// Getting engineer Incentives
                 break;
+/*   this API used to get parents of booking */
+            case 'checkBookingParents':  
+                $this->getBookingParents();  //// Getting parents
+                break;
 
             default:
                 break;
@@ -4452,6 +4456,42 @@ function submitPreviousPartsConsumptionData(){
         }    
 
    }
+
+
+
+   /**
+     * Get All parents of booking .
+     * @param 
+     * @author Abhishek Awasthi
+     */
+
+function getBookingParents(){
+
+	    log_message("info", __METHOD__ . " Entering..in getting parents");
+        $requestData = json_decode($this->jsonRequestData['qsh'], true);
+        $validation = $this->validateKeys(array("booking_id","primary_contact","service_id","service_id","partner_id"), $requestData);
+        if ($validation['status']) { 
+  	/* Getting booking initial date */			
+  		    $booking_details = $this->booking_creation_lib->get_edit_booking_form_helper_data($requestData['booking_id'], NULL, NULL);
+            $initial_booking_date  = $booking_details['booking_history'][0]['initial_booking_date'];
+            //getting possible parents //
+  				$response['parents'] = $this->booking_model->get_posible_parent_booking_id($requestData['primary_contact'],$requestData['service_id'],$requestData['partner_id'],30,$initial_booking_date);
+                  
+                $this->jsonResponseString['response'] =  $response; // All Data in response//
+                $this->sendJsonResponse(array('0000', 'success')); // send success response //
+               
+        } else {
+            log_message("info", __METHOD__ . $validation['message']);
+            $this->jsonResponseString['response'] = array(); /// Response one up according to umesh//
+            $this->sendJsonResponse(array("0099",'No parents  Found'));
+        } 
+
+}
+
+
+
+
+
 
 
 }
