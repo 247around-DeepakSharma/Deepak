a2ecfee98850aaa4749556c4a280993360601ca9
abhi2707 <=abhisheka@247around.com>
2020-04-01 13:40:09 +0530

    CRM-5944  Capture Whatsapp Inbound Message With Webhooks
    Capture Whatsapp Inbound Message With Webhooks

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -4515,40 +4515,37 @@ class Api extends CI_Controller {
      * 
      * @retun:void()
      */
-    public function karix_whatsapp(){
+    public function karix_whatsapp() {
         // log_message('info', _FUNCTION_ . "=> Entering ");
         $response = file_get_contents('php://input');
-         $json_decode = json_decode($json);
-         $type =  $json_decode->type;
-         $source =  $json_decode->data->source;
-         $total_cost = $json_decode->data->total_cost; 
-         $direction = $json_decode->data->direction; 
-         $destination =  $json_decode->data->destination;
-         $content_type = $json_decode->data->content_type;
-         $content =  $json_decode->data->content->text;
-         $status =  $json_decode->data->status;
-         $message_type = $json_decode->data->channel_details->whatsapp->type;
-         $whatsapp_profile = $json_decode->data->channel_details->whatsapp->source_profile->name;
-         $whatsapp = array(
-            'source'=>$source,
-            'destination'=>$destination,
-            'channel'=>'whatsapp',
-            'direction'=>$direction,
-            'content'=>$content,
-            'content_type'=>$content_type,
-            'source_profile'=>$whatsapp_profile,
-            'status'=>$status,
-            'type'=>trim($type),
-            'total_cost'=>$total_cost,
-            'message_type'=>$message_type,
-            'json_response'=>$json
-
-          );
-         $this->apis->logWhatsapp($whatsapp);
-
-
+        $json_decode = json_decode($response);
+        $type = $json_decode->type;
+        $source = $json_decode->data->source;
+        $total_cost = $json_decode->data->total_cost;
+        $direction = $json_decode->data->direction;
+        $destination = $json_decode->data->destination;
+        $content_type = $json_decode->data->content_type;
+        $content = $json_decode->data->content->text;
+        $status = $json_decode->data->status;
+        $message_type = $json_decode->data->channel_details->whatsapp->type;
+        $whatsapp_profile = $json_decode->data->channel_details->whatsapp->source_profile->name;
+        $whatsapp = array(
+            'source' => $source,
+            'destination' => $destination,
+            'channel' => 'whatsapp',
+            'direction' => $direction,
+            'content' => $content,
+            'content_type' => $content_type,
+            'source_profile' => $whatsapp_profile,
+            'status' => $status,
+            'type' => trim($type),
+            'total_cost' => $total_cost,
+            'message_type' => $message_type,
+            'json_response' => $json
+        );
+        $this->apis->logWhatsapp($whatsapp);
     }
-    
+
     /**
      * @desc: This function is used to insert rating if only one booking is 
      * exist for the given missed call number and rating column is null and current status is completed
