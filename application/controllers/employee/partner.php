9b477c1a292e00120f0480795fc0bfc8b4e27499
Abhay Anand <abhaya@247around.com>
2020-04-02 11:16:48 +0530

    Shipped inventory id issue while sending Parts to sf from Partner Panel #CRM-5954

--- application/controllers/employee/partner.php
+++ application/controllers/employee/partner.php
@@ -2275,9 +2275,9 @@ class Partner extends CI_Controller {
                     $data['quantity'] = $value['quantity'];
                     $data['shipped_quantity']=$value['shipped_quantity'];  
                     }
-                    $data['remarks_by_partner'] = $value['remarks_by_partner'];
-                    if (!empty($value['requested_inventory_id'])) {
-                        $data['shipped_inventory_id'] = $value['requested_inventory_id'];
+                    $remarks_by_partner = $data['remarks_by_partner'] = $value['remarks_by_partner'];
+                    if (!empty($value['inventory_id'])) {
+                        $data['shipped_inventory_id'] = $value['inventory_id'];
                     }
                     
                     if (!empty($value['spare_id'])) {
