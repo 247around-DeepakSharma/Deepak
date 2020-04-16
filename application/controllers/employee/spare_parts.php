e5ad9d77e90a7ef7e6d265a0c02d2275bbfb2aa6
Gorakh Mehta <gorakh@247around.com>
2020-04-16 21:42:03 +0530

    Changes #CRM-6030 and #CRM-6036

--- application/controllers/employee/spare_parts.php
+++ application/controllers/employee/spare_parts.php
@@ -4453,10 +4453,10 @@ $select = 'spare_parts_details.entity_type,spare_parts_details.quantity,spare_pa
 
         $post['where'] = array("DATEDIFF(CURRENT_TIMESTAMP,  STR_TO_DATE(spare_parts_details.shipped_date, '%Y-%m-%d')) >= 45" => NULL);
         $post['where']['spare_parts_details.shipped_date IS NOT NULL'] = NULL;
-        $post['where']['spare_parts_details.defective_part_shipped_date IS NULL'] = NULL;
         $post['where']['spare_parts_details.defective_part_required'] = 1;
         $post['where']['spare_parts_details.consumed_part_status_id !='] = 2;
         
+        $post['where']['spare_parts_details.defective_part_shipped_date IS NULL OR (spare_parts_details.defective_part_shipped_date IS NOT NULL) AND (spare_parts_details.status in ("' . DEFECTIVE_PARTS_REJECTED_BY_WAREHOUSE . '","' . OK_PARTS_REJECTED_BY_WAREHOUSE . '"))'] = NULL;
         //$post['where']['status in ("' . DEFECTIVE_PARTS_PENDING . '","' . OK_PART_TO_BE_SHIPPED . '","' . DAMAGE_PART_TO_BE_SHIPPED . '")'] = NULL;
         $post['is_inventory'] = TRUE;
 
