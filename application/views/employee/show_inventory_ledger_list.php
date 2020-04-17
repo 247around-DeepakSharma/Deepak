6fadf600b7adb125dc71e07562252442c456fa8a
abhi2707 <=abhisheka@247around.com>
2020-04-17 16:52:24 +0530

    CRM-6065 Show All option in Datatable in All three panels
    CRM-6065
    
    Show All option in Datatable in All three panels

--- application/views/employee/show_inventory_ledger_list.php
+++ application/views/employee/show_inventory_ledger_list.php
@@ -125,6 +125,7 @@
                 "processing": true,
                 "serverSide": false,
                 "pageLength": 50,
+                "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
                 dom: 'lBfrtip',
                 "buttons": [
                 {
