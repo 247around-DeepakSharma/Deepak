941a1e5f1a61409e0176e0c6dda04481dd857403
RamanMishra247 <ramanm@247around.com>
2020-04-17 16:57:28 +0530

    Revert "CRM-5992:Add export button on category and capacity list on admin crm"
    This reverts commit 3a93f77bc6df508fca344ab9d9521aa1bb9aca64.

--- application/views/category/index.php
+++ application/views/category/index.php
@@ -17,7 +17,6 @@
                         <th>Name</th>
                         <th class="no-sort">Active</th> 
                         <th class="no-sort">Action</th>
-                        <th class="pqr" style="display: none;">Active</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -40,11 +39,6 @@
                             </td>
                             <td>
                                 <a class="btn btn-primary btn-xs" href='javascript:void(0)' title="Update Category" id="update_category" category_id="<?= $rec->id ?>"><i class="glyphicon glyphicon-pencil"></i></a>
-                            </td>
-                            <td class="pqr" style="display: none;">
-                                <?php 
-                                    echo $rec->active;
-                                ?>
                             </td>
                         </tr>
                     <?php } ?>
@@ -107,24 +101,12 @@
 
 <script>
     $('#category_list').dataTable({
-       "order": [],
-         "columnDefs": [ {
-           "targets"  : 'no-sort',
-           "orderable": false,
-        }],
-        "dom": 'lBfrtip',
-        "buttons": [
-                {
-                    extend: 'excel',
-                    text: 'Export',
-                    title: 'Category-list',
-                    exportOptions: {
-                    columns: [0, 1, 2, 5]
-                }
-                }
-                ]
-                        
-     });
+        "order": [],
+        "columnDefs": [ {
+          "targets"  : 'no-sort',
+          "orderable": false,
+        }]
+    });
 
     $(document).on("click", "#add_category", function () {
         // Display the Bootstrap modal
