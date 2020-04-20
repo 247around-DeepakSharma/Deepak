d99fca8842659421cf44c0aafcfb75c41cf23f59
abhi2707 <=abhisheka@247around.com>
2020-04-20 19:54:43 +0530

    Header Click

--- application/views/service_centers/header.php
+++ application/views/service_centers/header.php
@@ -677,6 +677,17 @@
     function marquee_close(){
          $("#marquee_div").hide();
     }
+
+var btn = document.getElementById("myBtncovid");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+    
 </script>
 <style>
     .nav .open>a, .nav .open>a:focus, .nav .open>a:hover {
