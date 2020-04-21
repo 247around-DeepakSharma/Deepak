ff274e9e6573dfa3a4ea3cf4e7578d4f5fa851d1
sarvendra <savendrag518@gmail.com>
2020-04-22 00:39:14 +0530

    Add comments into CRM-5967, CRM-3450,CRM-4472

--- application/controllers/employee/vendor.php
+++ application/controllers/employee/vendor.php
@@ -1059,7 +1059,7 @@ class vendor extends CI_Controller {
             $agent_name = $this->session->userdata('emp_name');
             $this->vendor_model->edit_vendor($vendor, $id);
             
-            //$this->vendor_model->update_service_centers_login(array('service_center_id' => $id), array('active' => $is_active));
+            $this->vendor_model->update_service_centers_login(array('service_center_id' => $id), array('active' => $is_active));
 
             //Getting Vendor Details
             $sf_details = $this->vendor_model->getVendorContact($id);
@@ -1068,7 +1068,7 @@ class vendor extends CI_Controller {
             //Sending Mail to corresponding RM and admin group 
             $employee_relation = $this->vendor_model->get_rm_sf_relation_by_sf_id($id);  
             
-            //Sending Mail to Talevar Singh
+            //Get Talevar Singh user id from constant file as defind 
             if(TALEVAR_USER_ID){
                 $talevar_user_email = $this->user_model->getusername(TALEVAR_USER_ID);
             }
@@ -1082,10 +1082,12 @@ class vendor extends CI_Controller {
                     $email['rm_name'] = $employee_relation[0]['full_name'];
                     if($sf_details[0]['is_micro_wh'] == 1){
                         $to .= ",".$template[1];
+                        // Add Account's team email id
                         if(ACCOUNT_EMAIL_ID){
                             $to .= ",".ACCOUNT_EMAIL_ID;
                             $email['talevar_user_name'] = 'Account Team';
                         }
+                        // Add user Talevar Singh's email id
                         if($talevar_user_email){
                             $to .= ",".$talevar_user_email;
                             $email['talevar_user_name']= 'Talevar Singh';
