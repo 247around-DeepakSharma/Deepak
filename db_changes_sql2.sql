fb4f89a433a22c5253d7b09e481a3c501858efb9
unknown <ghanshyamg@247around.com>
2020-04-21 09:01:40 +0530

    CRM-5833 Reverse NRN - Config Update

--- db_changes_sql2.sql
+++ db_changes_sql2.sql
@@ -2256,7 +2256,7 @@ INSERT INTO `email_template` (`tag`, `subject`, `template`, `booking_id`, `from`
 ALTER TABLE service_centres ADD COLUMN last_foc_mail_send_date timestamp;
 
 ---Ghanshyam 2020-04-13
-INSERT INTO `partner_booking_status_mapping` ( `partner_id`, `247around_current_status`, `247around_internal_status`, `partner_current_status`, `partner_internal_status`, `actor`, `next_action`, `create_date`) VALUES ('247001', 'Pending', 'NRN Reverse', 'NRN Reverse', 'NRN Reverse', 'vendor', 'Visit to Customer', CURRENT_TIMESTAMP);
+INSERT INTO `partner_booking_status_mapping` ( `partner_id`, `247around_current_status`, `247around_internal_status`, `partner_current_status`, `partner_internal_status`, `actor`, `next_action`, `create_date`) VALUES ('247001', 'Pending', 'NRN Reverse by Partner', 'NRN Reverse by Partner', 'NRN Reverse by Partner', 'vendor', 'Visit to Customer', CURRENT_TIMESTAMP);
 
  
 ---Abhishek -- 15-04-2020
