65498c49a2f3a4855b05f8f0ed529fa4b1776cbc
abhi2707 <=abhisheka@247around.com>
2020-04-23 14:54:15 +0530

    AN-220	 Handle App request for covid19
    AN-220
    Handle App request for covid19

--- application/controllers/Covid19.php
+++ application/controllers/Covid19.php
@@ -70,9 +70,43 @@ class Covid19 extends CI_Controller {
             echo "cURL Error #:" . $err;
         } else {
 //file_put_contents('/247around_tmp/data.txt', serialize(json_decode($response, true)));
-//       print_r(json_decode($response, true));
-            echo $response;
+            // echo "<pre>";
+        $json_array =   json_decode($response, true);
+        $states_corrdinates =array();
+        foreach($json_array['features'] as $state){
+
+        $state_name = $state['properties']['name'];
+        $state_name = str_replace(' ', '', $state_name);
+        $state_name = strtoupper($state_name);
+        $coordinates = $state['geometry']['coordinates'];
+        $states_corrdinates[$state_name] = $coordinates;
+
+        
         }
+
+        //echo "<pre>";
+        $json_coordinates = json_encode($states_corrdinates);
+
+       //  echo $json_coordinates;
+
+        echo file_put_contents('states.json', $json_coordinates);
+  
+       // }
+      }
+    }
+
+
+    /**
+     * @desc This function is used to get Positive Areas of booking State
+     * @Abhishek Awasthi
+     */
+    function getCoronaArea($state=''){
+      $state_name = str_replace(' ', '', $state);
+      $state_name = strtoupper($state_name);
+      $states_json =   file_get_contents('states.json');
+      $states_array = json_decode($states_json,true);
+      $state_coordinates = $states_array[$state_name][0];
+      echo json_encode($state_coordinates);
     }
 
 }
