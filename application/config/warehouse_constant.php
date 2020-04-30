cff1cd8ab04b36afa7580c09fb3a50c23723c0fe
Abhay Anand <abhaya@247around.com>
2020-04-30 16:10:08 +0530

    Msl Boxes changes - make configurable with backend #CRM-6166

--- application/config/warehouse_constant.php
+++ application/config/warehouse_constant.php
@@ -3,3 +3,5 @@
 define("FIXED_MONTHLY_WAREHOUSE_CHARGES_TAG", "warehouse-fixed");
 define("PACKAGING_RATE_TAG", "packaging-variable");
 define("PART_TYPE_RETURN_MAPPING_FILE_VALUE" , "yes");
+define('MSL_PACKAGING_RATE_LARGE_TAG', 'msl-packaging-variable-large-box');
+define('MSL_PACKAGING_RATE_SMALL_TAG', 'msl-packaging-variable-small-box');
