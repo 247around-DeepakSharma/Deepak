78a6c0e9b158154e68c4ee6283e8a5346cd771f8
abhi2707 <=abhisheka@247around.com>
2020-04-30 18:14:07 +0530

    CRM-6168 Search Not Working in show_non_return_part
    CRM-6168
    
    Search Not Working in show_non_return_part

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -9878,8 +9878,8 @@ class Inventory extends CI_Controller {
         $post['select'] = "spare_parts_details.booking_id,spare_parts_details.id as sid,spare_parts_details.partner_id,spare_parts_details.shipped_quantity,spare_parts_details.parts_shipped,spare_parts_details.model_number_shipped, users.name, booking_primary_contact_no, service_centres.name as sc_name,"
                 . "partners.public_name as source, parts_requested, booking_details.request_type, spare_parts_details.id, spare_parts_details.part_warranty_status,"
                 . "spare_parts_details.defective_part_required, spare_parts_details.shipped_parts_type,spare_parts_details.is_micro_wh,status, inventory_master_list.part_number ";
-        $post['column_search'] = array('spare_parts_details.booking_id', 'partners.public_name', 'service_centres.name',
-            'parts_requested');
+        $post['column_search'] = array('spare_parts_details.booking_id', 'partners.public_name', 'service_centres.name','parts_requested');
+        $post['search']['value'] = $post['search_value'];
         $post['is_inventory'] = TRUE;
         $post['where']['booking_details.current_status'] = _247AROUND_COMPLETED;
 
@@ -9890,7 +9890,6 @@ class Inventory extends CI_Controller {
         $post['where']['spare_parts_details.defective_part_shipped_date IS NULL'] = NULL;
         $post['where']['spare_parts_details.shipped_inventory_id IS NOT NULL'] = NULL;
         $post['where_in']['spare_parts_details.is_micro_wh'] = array(1,2);
-
         $list = $this->inventory_model->get_spare_parts_query($post);
 
 
