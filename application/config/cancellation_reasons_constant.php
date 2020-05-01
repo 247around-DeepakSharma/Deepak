2e17e87013e94f8266d0e32967638ebe5a6d4fed
pritys <pritys@247around.com>
2020-05-01 17:20:38 +0530

    CRM-3103 : [73] Use separate Constant file for cancellation reason

---
+++ application/config/cancellation_reasons_constant.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Cancellation reason Ids
+define('CUSTOMER_NOT_REACHABLE_CANCELLATION_ID', 7);
+define('INSTALLATION_NOT_REQUIRED_CANCELLATION_ID', 34);
+// Need to be changed after row insertion
+define('UPCOUNTRY_CHARGES_NOT_APPROVED_CANCELLATION_ID', 1030);
+define('OTHERS_CANCELLATION_ID', 1000);
+define('PRODUCT_NOT_DELIVERED_TO_CUSTOMER_ID',21);
+define('SF_NOT_FOUND_BOOKING_CANCELLED_REASON', 30);
+define('_247AROUND_WRONG_PINCODE_CANCEL_REASON_ID', 1014);
+define('_247AROUND_WRONG_PINCODE_CANCEL_REASON', 'The Customer has given wrong pincode');
+
+//Sf Select Wrong Call Area
+define('CANCELLATION_REASON_WRONG_AREA_ID', 19);
+define('CANCELLATION_REASON_WRONG_AREA', 'Wrong call - Not in our area.');
+
--- application/config/constants.php
+++ application/config/constants.php
@@ -22,7 +22,7 @@ include('api_response_code_constants.php'); /// Include api constants response
 include('engineer_config.php'); /// Include engg constants
 include('around_lib_constant.php'); /// Include around generic constant file
 include('karix_constant.php');
-
+include('cancellation_reasons_constant.php');
 
 
 
@@ -615,9 +615,7 @@ define('_247AROUND_GST_DEBIT_NOTE_STRING','GST Debit Note');
 define('WAREHOUSE','Warehouse');  
 //constant to define New Query state
 define('_247AROUND_QUERY','Query');
-//Sf Select Wrong Call Area
-define('CANCELLATION_REASON_WRONG_AREA', 'Wrong call - Not in our area.');
-define('CANCELLATION_REASON_WRONG_AREA_ID', 19);
+
 //send broadcast sms to all vendors
 define('BROADCAST_SMS_TO_VENDOR', 'broadcast_sms_to_vendor');
 
@@ -655,7 +653,6 @@ define('_247AROUND_TAX_INVOICE_SIGN_CELL', 'K26');
 define('_247AROUND_COMPLETED_APPROVED','Completed_Approved');
 //constant to define Admin Approved Completed bookings
 define('_247AROUND_COMPLETED_REJECTED','Completed_Rejected');
-define('_247AROUND_WRONG_PINCODE_CANCEL_REASON', 'The Customer has given wrong pincode');
 //constant to define Admin Rejected cancelled bookings
 define('_247AROUND_CANCELED_REJECTED','Cancelled_Rejected');
 //constant to define Admin Approved canceled bookings
@@ -759,14 +756,5 @@ define('DASHBOARD_INVOICE_PROCESS_3', 'dashboard_process_3');
 define('DASHBOARD_INVOICE_PROCESS_4', 'dashboard_process_4');
 define('DASHBOARD_INVOICE_PROCESS_5', 'dashboard_process_5');
 
-// Cancellation reason Ids
-define('CUSTOMER_NOT_REACHABLE_CANCELLATION_ID', 7);
-define('INSTALLATION_NOT_REQUIRED_CANCELLATION_ID', 34);
-// Need to be changed after row insertion
-define('UPCOUNTRY_CHARGES_NOT_APPROVED_CANCELLATION_ID', 1030);
-define('OTHERS_CANCELLATION_ID', 1000);
-define('PRODUCT_NOT_DELIVERED_TO_CUSTOMER_ID',21);
-define('SF_NOT_FOUND_BOOKING_CANCELLED_REASON', 30);
-define('_247AROUND_WRONG_PINCODE_CANCEL_REASON_ID', 1014);
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
