a9822c25f9dbfa819c0ca63b1ca2c2f4af247422
abhi2707 <=abhisheka@247around.com>
2020-05-05 21:22:42 +0530

    AN-230	In search server error is coming on live app.
    AN-230
    In search server error is coming on live app.

--- application/controllers/engineerApi.php
+++ application/controllers/engineerApi.php
@@ -4528,7 +4528,7 @@ class engineerApi extends CI_Controller {
         if (!empty($state)) {    
         $state_name = str_replace(' ', '', $state);
         $state_name = strtoupper($state_name);
-        $states_json =   file_get_contents('states.json');
+        $states_json =   file_get_contents(TMP_FOLDER.'states.json');
         $states_array = json_decode($states_json,true);
         $state_coordinates = $states_array[$state_name][0];
         $latlong =array();
@@ -4536,7 +4536,7 @@ class engineerApi extends CI_Controller {
         foreach($state_coordinates as $key => $coordinate){
             $latlong['long'] = $coordinate[0];
             $latlong['lat'] = $coordinate[1];
-            $supcordinate[] = $latlong;
+           // $supcordinate[] = $latlong;
         }
 
         return $supcordinate; // All Data in response//
