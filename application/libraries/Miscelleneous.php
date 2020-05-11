333234c0b6afef6c9e1db9f003cf7ceacfcd086c
247around-raman <ramanm@247around.com>
2020-05-11 11:15:51 +0530

    CRM-4728: Partner Serviceability Report with Local/Upcountry and Municipal Limit

--- application/libraries/Miscelleneous.php
+++ application/libraries/Miscelleneous.php
@@ -3193,6 +3193,9 @@ function generate_image($base64, $image_name,$directory){
             $select[] = "vendor_pincode_mapping.City";
             $groupBY[] = 'vendor_pincode_mapping.City';
         }
+        //add 2 columns upcountry and Municipal Limit in the Partner Serviceability Report
+        $select[] = "CASE when service_centres.is_upcountry = '1' then 'Upcountry' Else 'Local' End as Flag";
+        $select[] = "service_centres.min_upcountry_distance as municipal_limit";
         $join['service_centres'] =  'service_centres.id = vendor_pincode_mapping.Vendor_ID AND service_centres.on_off = 1 AND service_centres.active = 1';
         $data = $this->My_CI->reusable_model->get_search_result_data('vendor_pincode_mapping',implode(',',$select),NULL,$join,NULL,$orderBY,$whereIN,NULL,$groupBY);
         foreach($data as $dataValues){
