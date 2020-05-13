3ddabc188335297e05004e132f2a8b54a992bb1c
247around-sarvendra <sarvendrag@247around.com>
2020-05-13 21:18:45 +0530

    CRM-6107 SF authorized certificate require Requirement by Rajendra Oza #CRM-6107

--- application/views/employee/sf_certificate_view.php
+++ application/views/employee/sf_certificate_view.php
@@ -1,49 +1,10 @@
 <html>
     <head>
-        <style>
-            #parent{
-                background-image: url('<?php echo base_url('images/247_letter_head.jpg'); ?>');
-                width: 595px;
-                margin: 0 auto;
-                background-size: contain;
-                background-position: top;
-                background-repeat: no-repeat;
-                height: 892px;
-            }
-            #top{
-                height: 200px;
-            }
-            #parent tr{
-                vertical-align: top;
-            }
-            #content-top{
-                height: fit-content;
-                vertical-align: bottom;
-            }
-            #content-body{
-                vertical-align: top;
-                height: fit-content;
-            }
-            #content-body td{
-                padding: 25px;
-                line-height: 24.5px;
-            }
-            #content tr{
-                height: fit-content;
-            }
-        </style>
-
     </head>
     <body>
-        <table id="parent" style=" background-image: url('<?php echo base_url('images/247_letter_head.jpg'); ?>');
-                width: 595px;
-                margin: 0 auto;
-                background-size: contain;
-                background-position: top;
-                background-repeat: no-repeat;
-                height: 892px;">
+        <table id="parent">
             <tbody>
-                <tr id="top" style="height: 200px;"><td></td></tr>
+                <tr id="top" ><td height="200"></td></tr>
                 <tr style="vertical-align: top;">
                     <td>
                         <table id="content">
@@ -57,13 +18,13 @@
                     <td style="padding: 25px;line-height: 24.5px;">
                         <p>This is certify that <?php echo $sf_deatils['name'] ?> located at <?php echo $sf_deatils['address'] . ' ' . $sf_deatils['district'] . ' ' . $sf_deatils['state'] . ' ' . $sf_deatils['pincode']; ?> is associated with us for after sale services for the product of <?php echo $sf_deatils['appliances'] . ' in ' . $sf_deatils['district']; ?>.</p>
                         <br/>
-                       
+
                     </td>
                 </tr>
                 <tr style="height: fit-content;">
                     <td>
             <center>Validity: <?php echo $financial_year; ?> </center>
-                        <br/><br/></td>
+            <br/><br/></td>
     </tr>
     <tr style="vertical-align: top;">
         <td style="text-align:right">
