6caee6d4ab02aaab39c6aa680db5207a54295bc0
abhi2707 <abhisheka@247around.com>
2020-05-15 17:33:02 +0530

    Covid Zone Mapping in Dashboard and Partner and SF and pending Booking

--- application/controllers/employee/booking.php
+++ application/controllers/employee/booking.php
@@ -3963,9 +3963,7 @@ class Booking extends CI_Controller {
         }
         if (strpos($districtZoneType, 'Green') !== false) {
         $districtZoneType = '<br><span class="label label-success">COVID ZONE</span>';
-        }
-
-        $inContainmentZone = $response['inContainmentZone'];    
+        }   
         }else{
 
         $districtZoneType = '<span class=""></span>';   
