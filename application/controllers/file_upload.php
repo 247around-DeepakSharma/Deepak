7b5fdaa1680ff50c081c1c7f11d088aa5e9e0109
unknown <ghanshyamg@247around.com>
2020-05-22 08:06:12 +0530

    Reject Model who has spl character Redirect issue

--- application/controllers/file_upload.php
+++ application/controllers/file_upload.php
@@ -131,7 +131,9 @@ class File_upload extends CI_Controller {
                             redirect(base_url() . $redirect_to);
                         } else {
                             $this->session->set_flashdata('details', $response['message']);
+                            if(!empty($redirect_to)){
                             redirect(base_url() . $redirect_to);
+                            }
                         }
                     }
                 } else {
