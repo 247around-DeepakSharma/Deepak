052e17090ea57b0bf0b690a4945214f756e2729c
ankitr17 <ankitr@247around.com>
2020-05-23 13:31:47 +0530

    CRM-6028 spare status updation correction on defective part required & not required

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -1245,14 +1245,14 @@ class Inventory extends CI_Controller {
                      * If defective part not required after booking completion then change spare status accordingly.
                      * @modifiedBy Ankit Rajvanshi
                      */
-                    if(!empty($booking_details['service_center_closed_date'])) {
+//                    if(!empty($booking_details['service_center_closed_date'])) {
                         $spare_part_detail = $this->reusable_model->get_search_result_data('spare_parts_details', '*', $where, NULL, NULL, NULL, NULL, NULL)[0];                    
                         $is_spare_consumed = $this->reusable_model->get_search_result_data('spare_consumption_status', '*', ['id' => $spare_part_detail['consumed_part_status_id']], NULL, NULL, NULL, NULL, NULL)[0]['is_consumed'];
                         $data['status'] = _247AROUND_COMPLETED;
                         if($booking_details['current_status'] == _247AROUND_COMPLETED && $line_items < 2) {
                             $b['internal_status'] = $data['status'];
                         }
-                    }
+//                    }
                     
                     break;
 
@@ -1287,7 +1287,7 @@ class Inventory extends CI_Controller {
                      * If defective part required after booking completion then change spare status accordingly.
                      * @modifiedBy Ankit Rajvanshi
                      */
-                    if(!empty($booking_details['service_center_closed_date'])) {
+//                    if(!empty($booking_details['service_center_closed_date'])) {
                         $spare_part_detail = $this->reusable_model->get_search_result_data('spare_parts_details', '*', $where, NULL, NULL, NULL, NULL, NULL)[0];                    
                         $is_spare_consumed = $this->reusable_model->get_search_result_data('spare_consumption_status', '*', ['id' => $spare_part_detail['consumed_part_status_id']], NULL, NULL, NULL, NULL, NULL)[0]['is_consumed'];
                         if(!empty($is_spare_consumed) && $is_spare_consumed == 1) {
@@ -1299,7 +1299,7 @@ class Inventory extends CI_Controller {
                         if($booking_details['current_status'] == _247AROUND_COMPLETED && $line_items < 2) {
                             $b['internal_status'] = $data['status'];
                         }
-                    }
+//                   }
                     $track_status = $new_state = "Spare Parts Required To Warehouse";
                     $old_state = SPARE_PARTS_REQUESTED;
                     break;
