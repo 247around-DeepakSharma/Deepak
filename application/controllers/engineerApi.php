b7d01196fd4a26415614faded4b4b799dc020542
abhi2707 <abhisheka@247around.com>
2020-05-27 17:55:39 +0530

    CRM-6319
    History is showing wrong after cancelled booking by engineer

--- application/controllers/engineerApi.php
+++ application/controllers/engineerApi.php
@@ -1576,11 +1576,15 @@ class engineerApi extends CI_Controller {
                     $actor = $booking['actor'] = $partner_status[2];
                     $next_action = $booking['next_action'] = $partner_status[3];
                 }
+                $where_cancel = array(
+                    'id'=>$requestData["cancellationReason"]
+                ); 
+                $reason = $this->booking_model->cancelreason($where_cancel);
 
                 $this->booking_model->update_booking($requestData["bookingID"], $booking);
 //  Appending Status of Engg //
-                $this->notify->insert_state_change($requestData["bookingID"], $requestData["cancellationReason"], _247AROUND_PENDING,
-                        BOOKING_CANCELLED_BY_ENGINEER_STATUS." - Booking Cancelled By Engineer From App",
+                $this->notify->insert_state_change($requestData["bookingID"], $reason[0]["reason"], _247AROUND_PENDING,
+                        BOOKING_CANCELLED_BY_ENGINEER_STATUS . " - Booking Cancelled By Engineer From App",
                         $requestData['sc_agent_id'], "", ACTOR_BOOKING_CANCELLED, NEXT_ACTION_CANCELLED_BOOKING, NULL, $requestData['service_center_id']);
 
                 $this->sendJsonResponse(array('0000', 'Booking Cancelled Successfully'));
