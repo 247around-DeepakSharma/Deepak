e978a136bc85522315f8082bddb408cbb7cfb5f8
247around-sarvendra <sarvendrag@247around.com>
2020-05-28 14:16:43 +0530

    CRM-5967 UI Changes #CRM-5967

--- application/views/partner/add_nrn_details.php
+++ application/views/partner/add_nrn_details.php
@@ -43,7 +43,7 @@
                     <form name="myForm" class="form-horizontal" id ="nrn_details_form" action="<?php echo base_url() ?>partner/add_nrn_details"  method="POST" enctype="multipart/form-data">
                         <legend>
                             <div class="row">
-                                <fieldset>247 CRM Based Initial Data
+                                <fieldset><center><b>247 CRM Based Initial Data</b></center>
                                     <div class="col-md-12">
                                         <br/>
                                         <div class="col-md-6">
@@ -257,8 +257,9 @@
                         </legend>
                         <legend>
                             <div class="row">
-                                <fieldset>As per Approval Mail (To be updated by Trivender)
+                                <fieldset><center><b>As per Approval Mail (To be updated by Trivender)</b></center>
                                     <div class="col-md-12">
+                                        <br/>
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label class="col-md-4">Approval Status * </label>
@@ -376,7 +377,7 @@
                         </legend>
                         <legend>
                             <div class="row">
-                                <fieldset>To be updated by Jatin <br/>
+                                <fieldset><center><b>To be updated by Jatin </b></center>
                                     <div class="col-md-12">
                                         <br/>
                                         <div class="col-md-6">
@@ -507,7 +508,7 @@
                         </legend>
                         <legend>
                             <div class="row">
-                                <fieldset>To be updated by alligned AKAI ASM
+                                <fieldset><center><b>To be updated by alligned AKAI ASM</b></center>
                                     <div class="col-md-12">
                                         <br/>
                                         <div class="col-md-6">
@@ -574,7 +575,7 @@
                         </legend>
                         <legend>
                             <div class="row">
-                                <fieldset>Final PDI Status
+                                <fieldset><center><b>Final PDI Status</b></center>
                                     <div class="col-md-12">
                                         <br/>
                                         <div class="col-md-6">
