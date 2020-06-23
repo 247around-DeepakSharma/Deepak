06c33ceecbdc0ad2ac7b745ba120c95c25c3abf4
ankitr17 <ankitr@247around.com>
2020-06-23 14:12:09 +0530

    CRM-6435 test controller added

--- application/controllers/employee/booking.php
+++ application/controllers/employee/booking.php
@@ -6725,12 +6725,4 @@ class Booking extends CI_Controller {
         echo $options;
     }
 
-    /**
-     * @desc This is used to get Repair- OOW Booking
-     */
-    function get_oow_booking_test(){
-        $this->miscelleneous->load_nav_header();
-        $this->load->view('employee/get_oow_booking_test');
-    }
-
 }
