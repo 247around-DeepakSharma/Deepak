261f2f50150b13a236e3d0b9ac599a3f2b141a8c
Gorakh Mehta <gorakh@247around.com>
2020-06-24 18:20:16 +0530

    Booking compilation date greater than sevend days #CRM-6396

--- application/controllers/employee/spare_parts.php
+++ application/controllers/employee/spare_parts.php
@@ -2620,12 +2620,12 @@ class Spare_parts extends CI_Controller {
         $part_warranty_status = $this->input->post('part_warranty_status');
         
          $where["spare_parts_details.booking_id"] = $booking_id;
-         $where["status IN  ('" . DEFECTIVE_PARTS_PENDING . "', '" . OK_PART_TO_BE_SHIPPED . "', '" . DEFECTIVE_PARTS_REJECTED_BY_WAREHOUSE . "', '" . OK_PARTS_REJECTED_BY_WAREHOUSE . "')"] = NULL;
+         $where["spare_parts_details.status IN  ('" . DEFECTIVE_PARTS_PENDING . "', '" . OK_PART_TO_BE_SHIPPED . "', '" . DEFECTIVE_PARTS_REJECTED_BY_WAREHOUSE . "', '" . OK_PARTS_REJECTED_BY_WAREHOUSE . "')"] = NULL;
          $where["spare_parts_details.sell_invoice_id IS NULL"] = NULL;
          $where["spare_parts_details.is_micro_wh != 1"] = NULL;
          $where["spare_parts_details.parts_shipped IS NOT NULL"] = NULL;
          $where["spare_parts_details.part_warranty_status"] = $part_warranty_status;
-         //$where["spare_parts_details.defective_part_shipped IS NULL"] = NULL;
+         $where["DATEDIFF(CURRENT_TIMESTAMP,  STR_TO_DATE(booking_details.closed_date, '%Y-%m-%d')) > 7"] = NULL;
         
         $data['data'] = $this->inventory_model->get_spare_parts_details($select, $where, true, true);
         $data['remarks'] = $internal_status;
