5d602ba133c6eb6357df30f4ea0a8fa7314a5c0a
ankitr17 <ankitr@247around.com>
2020-07-30 14:41:59 +0530

    CRMS-980 message changed

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -7071,7 +7071,7 @@ class Service_centers extends CI_Controller {
             $message = '';
             if (empty($is_cron)) {
                 $userSession = array('success' => ' Received Defective Spare Parts');
-                $message = 'Defective Spare Parts Received.';
+                $message = 'Parts have been received successfully.';
             //    exit;
 //                $this->session->set_userdata($userSession);
 //                redirect(base_url() . "service_center/defective_spare_parts");
