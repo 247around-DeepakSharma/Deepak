eb11dff09cb65a4507d50f0bc8831465132da641
ankitr17 <ankitr@247around.com>
2020-08-10 13:57:38 +0530

    CRMS-983 invoice pic is mandatory in IW and file will show on second part request

--- application/views/service_centers/get_update_form.php
+++ application/views/service_centers/get_update_form.php
@@ -202,8 +202,22 @@
                                     <div class="form-group">
                                         <label for="invoice_pic" class="col-md-4">Invoice Picture</label>
                                         <div class="col-md-6">
-                                            <input type="file" class="form-control spare_parts" id="invoice_pic" name="invoice_image">
-                                        </div>
+                                            <?php 
+                                                $invoice_file = '';
+                                                if(!empty($bookinghistory['spare_parts'])) {
+                                                    foreach($bookinghistory['spare_parts'] as $spares) {
+                                                        if(!empty($spares['invoice_pic']) && $spares['status'] != _247AROUND_CANCELLED) {
+                                                            $invoice_file = $spares['invoice_pic'];
+                                                            break;
+                                                        }
+                                                    }
+                                                } 
+                                            ?>
+                                            <input type="file" class="form-control spare_parts" id="invoice_pic" name="invoice_image" <?php if(!empty($invoice_file)) { echo 'style="cursor: not-allowed; pointer-events: none;background-color:#eee;"'; } ?>>
+                                            <?php if(!empty($invoice_file)) { ?>
+                                                <a href="https://s3.amazonaws.com/<?php echo BITBUCKET_DIRECTORY; ?>/misc-images/<?php echo $invoice_file; ?> " target="_blank">Click Here to view Invoice Picture</a>
+                                            <?php } ?>
+                                    </div>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
@@ -222,7 +236,7 @@
                                         <div class="form-group ">
                                             <label for="part_warranty" class="col-md-4">Part Warranty Status  <?php echo $bookinghistory[0]['request_type'];  ?></label>                                             
                                             <div class="col-md-6">
-        <input type="text" id="part_warranty_status_0"  value="<?php if(strpos($bookinghistory[0]['request_type'],'Out Of Warranty') == true || strpos($bookinghistory[0]['request_type'],'Gas Recharge - Out')==true ){echo '2';}else{echo '1';}  ?>" name="part[0][part_warranty_status]">  
+        <input type="text" id="part_warranty_status_0"  value="<?php if(strpos($bookinghistory[0]['request_type'],'Out Of Warranty') == true || strpos($bookinghistory[0]['request_type'],'Gas Recharge - Out')==true ){echo '2';}else{echo '1';}  ?>" name="part[0][part_warranty_status]" class="<?php if(strpos($bookinghistory[0]['request_type'],'Out Of Warranty') == false || strpos($bookinghistory[0]['request_type'],'Gas Recharge - Out')==false ){echo 'part_in_warranty_status';} ?>">  
                                             </div>
                                         </div>
                                     </div>
@@ -1046,8 +1060,8 @@ function alpha(e) {
                     }
                 }
             });
-    
-        var invoice_pic = $("#invoice_pic").val();    
+        <?php if(empty($invoice_file)) { ?>
+            var invoice_pic = $("#invoice_pic").val();    
             $('.part_in_warranty_status').each(function() {
                 var id = $(this).attr('id');
                 if(id !== "part_in_warranty_status"){
@@ -1058,12 +1072,12 @@ function alpha(e) {
                     }else if($(this).val() == 1 &&(invoice_pic =='' || invoice_pic == null)){
                         alert('Please Upload Invoice Picture.');    
                         checkbox_value = 0;
+                        return false;
                     }
                 }
             });
-            
-            <?php } ?>
-            
+           
+        <?php } } ?>
             /*$('.spare_request_symptom').each(function() {
                 var id = $(this).attr('id');
                 if(id !== "spare_request_symptom"){
