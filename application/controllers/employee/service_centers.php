9872c849950501aa4dfdab5fc0357c3d36333be7
ankitr17 <ankitr@247around.com>
2020-08-21 15:49:53 +0530

    CRMS-1171 Show shipped part number on Acknowledge From SF tab of Warehouse

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -6236,7 +6236,8 @@ class Service_centers extends CI_Controller {
 
         $select = "defective_part_shipped,spare_parts_details.defective_part_rejected_by_partner, spare_parts_details.shipped_quantity,spare_parts_details.id, spare_consumption_status.consumed_status, spare_consumption_status.is_consumed, spare_consumption_status.reason_text, "
                 . " spare_parts_details.booking_id, users.name as 'user_name', courier_name_by_sf, awb_by_sf,defective_part_shipped_date,"
-                . "remarks_defective_part_by_sf,booking_details.partner_id,service_centres.name as 'sf_name',service_centres.district as 'sf_city',i.part_number, spare_parts_details.defactive_part_received_date_by_courier_api, spare_parts_details.status";
+                . "remarks_defective_part_by_sf,booking_details.partner_id,service_centres.name as 'sf_name',service_centres.district as 'sf_city',s.part_number, spare_parts_details.defactive_part_received_date_by_courier_api, spare_parts_details.status, spare_parts_details.defective_part_rejected_by_wh";
+
         $group_by = "spare_parts_details.id";
         $limit = $post['length'];
         $offset = $post['start'];
