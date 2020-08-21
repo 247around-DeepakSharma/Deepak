1aaa9424aeb4db50e1dc4516e1c853237b267ab9
ayush kumar goel <ayushg@247around.com>
2020-08-21 17:45:58 +0530

    CRMS-1113 :- Contract Status Weekly Mail for Activated Partners

--- application/models/partner_model.php
+++ application/models/partner_model.php
@@ -85,6 +85,16 @@ class Partner_model extends CI_Model {
     }
 
     /*
+     * @desc: This is used to get active partner name those contarct has been expired or having no contract with us 
+     */
+    function get_expired_contarct_partner_list($select) {
+        $this->db->select($select);
+        $query = $this->db->get();
+        $this->db->last_query();
+        return $query->result_array();
+    }
+    
+    /*
      * @desc: This is used to get active partner details and also get partner details by partner id
      */
     function getpartner($partner_id = "", $is_active = true) {
