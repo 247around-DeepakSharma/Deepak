41df584cd832d8ee88277ab3b67b0af2aa563c8f
aakriti <aakritis@247around.com>
2020-08-22 22:00:44 +0530

    CRMS-947 : State-wise, Open TAT count is different from SF-wise Open TAT Count on All Panels

--- application/config/constants.php
+++ application/config/constants.php
@@ -732,6 +732,7 @@ define("_247AROUND_DEALER_STRING" , "dealer");
 define("RTO_CASE_CANCELLATION_REASON_ID" , "1028");
 
 define("LEG_DESCRIPTION", "<span style='color:red;'>*Leg_1 denotes the number of days between booking creation date and spare request date.<br/>*Leg_2 denotes the number of days between spare acknowledge/cancellation date and the date at which service center closes the booking<br/>*Total  denotes the number of days between between booking creation date and the date at which service center closes the booking</span>");
+define("BOOKING_COUNT_MISMATCH_MESSAGE", "<span style='color:red;'>*No. of Bookings may be less from main Dashboard, because SF is not assigned in some Bookings.</span>");
 define("PART_NOT_CONSUMED_CANCELLATION_REASON_ID" , "1029");
 
 define("UPDATE_INVENTORY_MASTER_LIST_MSG", "Mapping of this appliance with part type can only update from Part Master. Please update data from part master.");
