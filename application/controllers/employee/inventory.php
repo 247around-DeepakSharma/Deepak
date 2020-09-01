bc1594075010cc032a97b73e65d5fd2713b268aa
unknown <ghanshyamg@247around.com>
2020-09-01 09:10:49 +0530

    CRMS-1230 Internal status not matching

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -1074,6 +1074,9 @@ class Inventory extends CI_Controller {
             $this->checkUserSession();
         }
 
+        $booking_new_internal_status = '';
+        
+
         if (!empty($id)) {
 
             // fetch record from booking details of $booking_id.
@@ -1086,7 +1089,7 @@ class Inventory extends CI_Controller {
             $b = array();
             $line_items = '';
             
-            $select = 'spare_parts_details.id,spare_parts_details.entity_type,booking_details.partner_id as booking_partner_id, spare_parts_details.status';
+            $select = 'spare_parts_details.id,spare_parts_details.entity_type,booking_details.partner_id as booking_partner_id, spare_parts_details.status, spare_parts_details.inventory_invoice_on_booking, spare_parts_details.wh_ack_received_part';
 
             $spare_parts_details = $this->partner_model->get_spare_parts_by_any($select, array('spare_parts_details.booking_id' => $booking_id, 'status IN ("' . SPARE_PARTS_SHIPPED . '", "'
                 . SPARE_PARTS_REQUESTED . '", "' . SPARE_PART_ON_APPROVAL . '", "' . SPARE_OOW_EST_REQUESTED . '", "' . SPARE_PARTS_SHIPPED_BY_WAREHOUSE . '", "' . SPARE_DELIVERED_TO_SF . '", "'.DEFECTIVE_PARTS_PENDING.'", "'.OK_PART_TO_BE_SHIPPED.'", "'.OK_PARTS_SHIPPED.'", "'.DEFECTIVE_PARTS_SHIPPED.'", "'.DEFECTIVE_PARTS_RECEIVED_BY_WAREHOUSE.'","'.DEFECTIVE_PARTS_REJECTED.'", "'.DEFECTIVE_PARTS_RECEIVED.'", "'.DEFECTIVE_PARTS_REJECTED_BY_WAREHOUSE.'") ' => NULL), TRUE, false, false);
@@ -1103,7 +1106,49 @@ class Inventory extends CI_Controller {
                     $where = array('id' => $id);
                     $track_status = $data['status'] = _247AROUND_CANCELLED;
                     $data['spare_cancelled_date'] = date("Y-m-d h:i:s");
+                    $status_string = array();
+                    if ($line_items >= 2) {
+                        foreach ($spare_parts_details as $key => $value) {
+                            if ($value['id'] !== $id) {
+                                $status_string[$key] = $value['status'];
+                            }
+                        }
+                        if (in_array(SPARE_PART_ON_APPROVAL, $status_string)) {
+                            $booking_new_internal_status = SPARE_PART_ON_APPROVAL;
+                        } else if (in_array(SPARE_OOW_EST_REQUESTED, $status_string)) {
+                            $booking_new_internal_status = SPARE_OOW_EST_REQUESTED;
+                        } else if (in_array(SPARE_PARTS_REQUESTED, $status_string)) {
+                            foreach ($spare_parts_details as $key => $value) {
+                                if ($value['id'] !== $id) {
+                                    if ($value['status'] == SPARE_PARTS_REQUESTED && $value['inventory_invoice_on_booking'] == 0) {
+                                        $booking_new_internal_status = SPARE_PARTS_REQUESTED;
+                                        //If spare is requested and not shipped by partner
+                                        break;
+                                    }
 
+                                    if ($value['status'] == SPARE_PARTS_REQUESTED && $value['inventory_invoice_on_booking'] == 1 && $value['wh_ack_received_part'] == 0) {
+                                        $booking_new_internal_status = SPARE_SHIPPED_TO_WAREHOUSE;
+                                        //If spare part is requested and shipped by partner and not acknowledgeby warehouse
+                                        break;
+                                    }
+                                    if ($value['status'] == SPARE_PARTS_REQUESTED && $value['inventory_invoice_on_booking'] == 1 && $value['wh_ack_received_part'] == 1) {
+                                        $booking_new_internal_status = SPARE_SHIPPED_TO_WAREHOUSE;
+                                        //If spare part is requested and shipped by partner and  acknowledgeby warehouse
+                                        break;
+                                    }
+                                }
+                            }
+                        } else if (in_array(SPARE_PARTS_SHIPPED, $status_string)) {
+                            $booking_new_internal_status = SPARE_PARTS_SHIPPED;
+                        } else if (in_array(SPARE_PARTS_SHIPPED_BY_WAREHOUSE, $status_string)) {
+                            $booking_new_internal_status = SPARE_PARTS_SHIPPED_BY_WAREHOUSE;
+                        } else {
+                            if ($booking_details['current_status'] != _247AROUND_COMPLETED) {
+                                $booking_new_internal_status = SPARE_PARTS_CANCELLED;
+                            }
+                            //If defective part / ok part shipped received and booking is not completed rhen do not update status
+                        }
+                    }
                     //////   Handle agents for cancellation /// Abhishek
                     $approval_agent_id = _247AROUND_DEFAULT_AGENT;
                     $approval_entity_type = _247AROUND_SF_STRING; 
@@ -1363,10 +1408,13 @@ class Inventory extends CI_Controller {
                 }
 
             $partner_id = $this->reusable_model->get_search_query('booking_details', 'booking_details.partner_id', array('booking_details.booking_id' => trim($booking_id)), NULL, NULL, NULL, NULL, NULL)->result_array();
+            if(empty($b['internal_status']) && !empty($booking_new_internal_status)){
+                $b['internal_status'] = $booking_new_internal_status;
+            }
             if (!empty($partner_id) && !empty($b['internal_status'])) {
                 $partner_status = $this->booking_utilities->get_partner_status_mapping_data($booking_details['current_status'], $b['internal_status'], $partner_id[0]['partner_id'], $booking_id);
                 if (!empty($partner_status)) {
-                    if ($line_items < 2) {
+                    if ($line_items < 2 || !empty($booking_new_internal_status)) {
                         $b['partner_current_status'] = $partner_status[0];
                         $b['partner_internal_status'] = $partner_status[1];
                         $b['actor'] = $partner_status[2];
