18e8a5b3c7a50fefc18ccb6c4aff4dd5b427ea97
aakriti <aakritis@247around.com>
2020-09-06 18:21:12 +0530

    CRMS-1245 : Partner Panel - Repeat Booking - Booking Source of parent booking not coming and columns are also freezed

--- application/controllers/employee/partner.php
+++ application/controllers/employee/partner.php
@@ -8114,7 +8114,7 @@ class Partner extends CI_Controller {
         
         $channel = $this->input->post('channel');
         $fetch_data = $this->partner_model->get_channels($select, $where);
-        $html = '<option value="" selected disabled>Please select seller channel</option>';
+        $html = '<option value="">Please select seller channel</option>';
         foreach ($fetch_data as $key => $value) {
            $html .= '<option ';
            if($channel ==$value['channel_name'] ){
@@ -8122,7 +8122,7 @@ class Partner extends CI_Controller {
            }
            else{
                if($is_repeat){
-                    $html .= " disabled ";
+                    $html .= "  ";
                }
            }
            $html .=' >'.$value['channel_name'].'</option>'; 
