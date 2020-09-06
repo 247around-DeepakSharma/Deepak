90d511e9e0a3b9c0cb67defccb863891621202c2
aakriti <aakritis@247around.com>
2020-09-06 21:45:24 +0530

    CRMS-840 : RM name reflect in pending booking bucket

--- application/models/booking_model.php
+++ application/models/booking_model.php
@@ -2465,8 +2465,7 @@ class Booking_model extends CI_Model {
      *  @param : $select string
      *  @return: Array()
      */
-    function get_bookings_by_status($post, $select = "",$sfIDArray = array(),$is_download=0,$is_spare=NULL,$partner_details=0) {
-        $this->_get_bookings_by_status($post, $select);
+        function get_bookings_by_status($post, $select = "",$sfIDArray = array(),$is_download=0,$is_spare=NULL,$partner_details=0,$join_array=array(),$join_type_array=array()) {        $this->_get_bookings_by_status($post, $select);
         if ($post['length'] != -1) {
             $this->db->limit($post['length'], $post['start']);
         }
@@ -2491,6 +2490,16 @@ class Booking_model extends CI_Model {
         if($is_spare){
             $this->db->join('spare_parts_details', 'booking_details.booking_id  = spare_parts_details.booking_id', 'left');
             $this->db->group_by('booking_details.booking_id'); 
+        }
+        if(!empty($join_array)){
+            foreach ($join_array as $tableName => $joinCondition){
+                if(array_key_exists($tableName, $join_type_array)){
+                    $this->db->join($tableName,$joinCondition,$join_type_array[$tableName]);
+                }
+                else{
+                    $this->db->join($tableName,$joinCondition);
+                }
+            }
         }
         $query = $this->db->get();
         if($is_download){
