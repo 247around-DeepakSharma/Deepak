811754a6b3441e0e283d4fa133f88e2632149690
Gorakh Mehta <gorakh@247around.com>
2020-09-07 13:19:07 +0530

    Unable to not shipped part from partner panel #CRMS-1120

--- application/views/partner/update_spare_parts_form.php
+++ application/views/partner/update_spare_parts_form.php
@@ -776,20 +776,19 @@
  
             }else{
                 
-            if(!$('#courier_not_shipping').is(":checked")){
-                let kg = $("#defective_parts_shipped_weight_in_kg").val();
-                let gm = $("#defective_parts_shipped_weight_in_gram").val();
-                let total = parseInt(kg)+parseInt(gm);
-                if(!total){
-                swal("Error !", "Sum of weight in KG and GM must be greater than 0");
+                if(!$('#courier_not_shipping').is(":checked")){
+                    let kg = $("#defective_parts_shipped_weight_in_kg").val();
+                    let gm = $("#defective_parts_shipped_weight_in_gram").val();
+                    let total = parseInt(kg)+parseInt(gm);
+                    if(!total){
+                    swal("Error !", "Sum of weight in KG and GM must be greater than 0");
+                    }else{
+                    $("#invoice_id_0,#hsn_code_0,#shippedpart_type_0,#invoiceamount_0,#remarks_0,#gst_rate_0,#incominginvoice_0,#shippedparttype_0,#shippedpartsname_0,#shippedmodelnumberid_0").prop('disabled', false);
+                    form.submit();   
+                    }
                 }else{
-		$("#invoice_id_0,#hsn_code_0,#shippedpart_type_0,#invoiceamount_0,#remarks_0,#gst_rate_0,#incominginvoice_0,#shippedparttype_0,#shippedpartsname_0,#shippedmodelnumberid_0").prop('disabled', false);
-                form.submit();   
-                }
-            }else{
-                $("#invoice_id_0,#hsn_code_0,#shippedpart_type_0,#invoiceamount_0,#remarks_0,#gst_rate_0,#incominginvoice_0,#shippedparttype_0,#shippedpartsname_0,#shippedmodelnumberid_0").prop('disabled', false);
-               form.submit(); 
-            } 
+                   form.submit(); 
+                } 
                 }
 
                 }
@@ -1252,15 +1251,15 @@
         }
         
         $("#courier_not_shipping").on('click',function(){
-            $("#invoice_id_0,#hsn_code_0,#shippedpart_type_0,#invoiceamount_0,#remarks_0,#gst_rate_0,#incominginvoice_0,#shippedparttype_0,#shippedpartsname_0,#shippedmodelnumberid_0").prop('disabled', true);
+            $("#invoice_id_0,#hsn_code_0,#shippedpart_type_0,#invoiceamount_0,#remarks_0,#gst_rate_0,#incominginvoice_0,#shippedparttype_0,#shippedpartsname_0,#shippedmodelnumberid_0,#approx_value_0").prop('disabled', true);
         });
         
         $("#courier_shipping").on('click',function(){
-            $("#invoice_id_0,#hsn_code_0,#shippedpart_type_0,#invoiceamount_0,#remarks_0,#gst_rate_0,#incominginvoice_0,#shippedparttype_0,#shippedpartsname_0,#shippedmodelnumberid_0").prop('disabled', false);
+            $("#invoice_id_0,#hsn_code_0,#shippedpart_type_0,#invoiceamount_0,#remarks_0,#gst_rate_0,#incominginvoice_0,#shippedparttype_0,#shippedpartsname_0,#shippedmodelnumberid_0,#approx_value_0").prop('disabled', false);
         });
         
         $("#to_be_shipping").on('click',function(){
-            $("#invoice_id_0,#hsn_code_0,#shippedpart_type_0,#invoiceamount_0,#remarks_0,#gst_rate_0,#incominginvoice_0,#shippedparttype_0,#shippedpartsname_0,#shippedmodelnumberid_0").prop('disabled', true);
+            $("#invoice_id_0,#hsn_code_0,#shippedpart_type_0,#invoiceamount_0,#remarks_0,#gst_rate_0,#incominginvoice_0,#shippedparttype_0,#shippedpartsname_0,#shippedmodelnumberid_0,#approx_value_0").prop('disabled', true);
         });
         
         $("#courier_name").on('change',function(){
