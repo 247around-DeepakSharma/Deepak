8271f2a9d58b4cce2e5fc35e074a0edaddc2694d
pritys <pritys@247around.com>
2020-09-07 15:16:37 +0530

    CRMS-1122 : #Fixed Advance search datatable export not working

--- js/employee/employee.js
+++ js/employee/employee.js
@@ -95,8 +95,7 @@ var ad_table;
                     extend: 'excel',
                     text: 'Export',
                     exportOptions: {
-                        columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
-
+                        columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
                     },
                     title: 'advance_search_report'
                 }
