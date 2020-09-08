9b2973385f12133ae14881ad8071190969a69017
Gorakh Mehta <gorakh@247around.com>
2020-09-08 15:43:03 +0530

    Php Error while sending OOW spare from Partner to SF #CRMS-1321

--- application/controllers/employee/partner.php
+++ application/controllers/employee/partner.php
@@ -2158,7 +2158,7 @@ class Partner extends CI_Controller {
         log_message('info', __FUNCTION__ . " Pratner ID: " . $this->session->userdata('partner_id') . " Booking id: " . $booking_id);
         $this->checkUserSession();
         $part_warranty_status = $this->input->post('part_warranty_status');
-
+        
         if (!empty($this->input->post('courier_status'))) {
             $this->form_validation->set_rules('courier_name', 'Courier Name', 'trim|required');
             $this->form_validation->set_rules('awb', 'AWB', 'trim|required');
@@ -2243,8 +2243,7 @@ class Partner extends CI_Controller {
                 );
 
                 $this->service_centers_model->insert_into_awb_details($awb_data);
-            }
-            else {
+            } else {
                 $awb_data = array(
                     'company_name' => trim($this->input->post('courier_name')),
                     'partner_id' => $partner_id,
@@ -2258,13 +2257,12 @@ class Partner extends CI_Controller {
                     'is_exist' => 1
                 );
 
-                $this->service_centers_model->update_awb_details($awb_data,trim($this->input->post('awb')));
+                $this->service_centers_model->update_awb_details($awb_data, trim($this->input->post('awb')));
             }
-            
         }
-        
-        
-        
+
+
+
         $shipped_part_details = $this->input->post("part");
         /* if parts empty no need to run loop */
         if(!empty($shipped_part_details)) {
@@ -2290,7 +2288,7 @@ class Partner extends CI_Controller {
         }
         
         $shipped_part_details = $this->input->post("part");
-     
+        
         if (!empty($shipped_part_details)) {
             $spare_id_array = array();
             $invoide_data = array();
