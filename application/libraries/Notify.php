ab39b22fd35a4b70b7c058376c651817e248dfb3
Abhay Anand <abhaya@247around.com>
2020-09-11 08:46:45 +0530

    FT- Fwd: SMS to Red Zone Customers #CRMS-1003

--- application/libraries/Notify.php
+++ application/libraries/Notify.php
@@ -665,6 +665,25 @@ class Notify {
                         
                         $this->send_sms_msg91($dealerSms);
                     }
+                    
+                    if(covid_sms() == TRUE){
+                        $city_details = $this->indiapincode_model->getPinCoordinates($query1[0]['district']);
+                        if(!empty($city_details))
+                        {
+                            $zone_color = $city_details[0]['zone_color'];
+                            if($zone_color == "Red")
+                            {
+                                $sms1['tag'] = "sms_to_redzone_customers";
+                                $sms1['phone_no'] = $query1[0]['booking_primary_contact_no'];
+                                $sms1['smsData']['appliance'] = $query1[0]['services'];
+                                $sms1['smsData']['partner'] = $sms['smsData']['public_name'];
+                                $sms1['type'] = "user";
+                                $sms1['booking_id'] = $booking_id;
+                                $sms1['type_id'] = $query1[0]['user_id'];    
+                                $this->send_sms_msg91($sms1);
+                            }
+                        }
+                    }
                     // send sms to user for  Brand Collateral file link
                     $data = $this->My_CI->service_centers_model->get_collateral_for_service_center_bookings($query1[0]['booking_id']);
                     if(!empty($data))
