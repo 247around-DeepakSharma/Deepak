ac4be16e417143465b5f877eae10e5565b4d77b6
aakriti <aakritis@247around.com>
2020-09-11 09:53:25 +0530

    CRMS-1343 : Improve UI of Admin Booking Reschedule Form

--- application/views/employee/reschedulebooking.php
+++ application/views/employee/reschedulebooking.php
@@ -41,7 +41,9 @@
                       </div>
                     </div>
 
-                    <div class="form-group <?php if( form_error('booking_timeslot') ) { echo 'has-error';} ?>">
+                    
+              </div>
+              <div class="form-group <?php if( form_error('booking_timeslot') ) { echo 'has-error';} ?>">
                       <label for="reason" class="col-md-4">Current Booking Timeslot</label>
                       <div class="col-md-6">
                         <input type="text"  class="form-control" name="booking_timeslot" value="<?php if (isset($data[0]['booking_timeslot'])) {echo $data[0]['booking_timeslot']; }?>"  disabled>
@@ -103,9 +105,9 @@
                       </div>
                         <?php echo form_error('remark'); ?>
                       </div>
-              </div>
-
-                  <div class="col-md-6">
+                  
+                </div>
+                <div class="col-md-6">
 
                     <div class="form-group <?php if( form_error('name') ) { echo 'has-error';} ?>">
                       <label for="Mobile" class="col-md-4">Mobile</label>
@@ -158,8 +160,8 @@
 -->
                       
                   </div>
-
                 </div>
+
                
 
                 <div>
