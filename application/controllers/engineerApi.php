153b0f7843b99e3d6a6a8e87ba7cb9762b0b36ff
Abhishek Awasthi <abhisheka@247around.com>
2020-09-25 15:43:44 +0530

    CRMS-1413 Warranty Status Flag in Engg Home Screen
    Warranty Status Flag in Engg Home Screen

--- application/controllers/engineerApi.php
+++ application/controllers/engineerApi.php
@@ -2636,13 +2636,13 @@ class engineerApi extends CI_Controller {
                 }
 
                 if (isset($requestData['invoice_number_pic_exist'])) {
-                    if ($requestData['invoice_number_pic_exist']) {
+                    if (isset($requestData['invoice_number_pic_exist']) && $requestData['invoice_number_pic_exist']) {
                         $invoice_pic = "invoice_" . $requestData['booking_id'] . "_" . date("YmdHis") . ".png";
                         $this->miscelleneous->generate_image($requestData['invoice_number_pic_exist'], $invoice_pic, "misc-images");
                         $requestData['invoice_pic'] = $invoice_pic;
                     }
                 } else {
-                    if ($requestData['existing_purchase_invoice']) {
+                    if (isset($requestData['existing_purchase_invoice']) && $requestData['existing_purchase_invoice']) {
                         $invoice_pic = "invoice_" . $requestData['booking_id'] . "_" . date("YmdHis") . ".png";
                         $this->miscelleneous->generate_image($requestData['existing_purchase_invoice'], $invoice_pic, "misc-images");
                         $requestData['invoice_pic'] = $invoice_pic;
