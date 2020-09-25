112090d25b8a275c4a03dd0a7ea8d09fef167021
Abhishek Awasthi <abhisheka@247around.com>
2020-09-25 12:40:48 +0530

    CRMS-1124
    Add provision to change App Version from the database and view

--- application/controllers/employee/engineer.php
+++ application/controllers/employee/engineer.php
@@ -891,8 +891,6 @@ class Engineer extends CI_Controller {
     function get_all_engineers_for_notification() {
 
         $this->miscelleneous->load_nav_header();
-        $response = $this->engineer_model->get_engineer_config(FORCE_UPGRADE);
-        $data['app_version'] = $response[0]->app_version;
         $this->load->view('employee/engineers_list_for_notifications',$data);
     }
 
