b366d592308fdd43c51a7e8534df208c878d451d
aakriti <aakritis@247around.com>
2020-09-28 08:42:15 +0530

    CRMS-1366 : add panel to map rejection reasons & cancellation reasons with penalty

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -8101,8 +8101,45 @@ class Inventory extends CI_Controller {
 
         echo json_encode($output);
     }
+     function rejection_cancellation_reason() {
+        $where['reason_of']='vendor';
+        $data['reason'] = $this->booking_model->cancelreason($where);
+         $data['cancel_reject'] = $this->inventory_model->show_cancel_reject_list();
+        $data['penalty'] = $this->inventory_model->get_review_reject();
+         $this->miscelleneous->load_nav_header();
+        $this->load->view('employee/rejection_cancellation_reason',$data);
+    }
+    public function save_reject_cancel() {
+        //print_r($_POST); die;
+        $data['id'] = $this->input->post('review_id');
+        $data['rejection_reason'] = $this->input->post('review_reject');
+        $data['cancellation_reason'] = $this->input->post('cancellation');
+        $data['active'] = $this->input->post('active');
+        $where['rejection_reason'] = $this->input->post('review_reject');
+        $where['cancellation_reason'] = $this->input->post('cancellation');
+        $count_data_present = $this->inventory_model->show_cancel_reject_list($where);
+        if(empty($count_data_present) || !empty($data['id'])){
+            $this->inventory_model->save_review_data($data);  
+            $this->session->set_userdata(array("success"=> "Mapping added"));
+        }else{
+        $this->session->set_userdata(array("error"=> "Mapping already exist"));
+        }
+        // $this->inventory_model->save_review_data($data);                
+        redirect(base_url() . 'employee/inventory/rejection_cancellation_reason');
+    }
     
     
+    public function get_cancel_reject_data() {
+        $data = $this->input->post();
+        $id = !empty($data['id']) ? $data['id'] : "";
+        $cancel_reject_data = [];
+        if(!empty($id))
+        {
+            $cancel_reject_data = $this->db->get_where('cancellation_rejection_penalty_mapping', array('id' => $id))->row();
+        }
+        
+        echo(json_encode($cancel_reject_data));
+    }
      /** 
      * @desc: This function is used to create table body to table.
      * @param: Array
