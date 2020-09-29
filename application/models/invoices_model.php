a6f578510cfa3cd278e7ad33ec0954e7415bee81
Abhay Anand <abhaya@247around.com>
2020-09-29 14:20:09 +0530

    Sf closed date added in invoice annx file #CRMS-1468

--- application/models/invoices_model.php
+++ application/models/invoices_model.php
@@ -439,7 +439,8 @@ class invoices_model extends CI_Model {
                     (Select CASE WHEN (file_name = '' OR file_name IS NULL) THEN ('') ELSE (GROUP_CONCAT(CONCAT('".S3_WEBSITE_URL."misc-images/', file_name) SEPARATOR ' , ')) END as support_file FROM booking_files WHERE booking_files.booking_id = booking_unit_details.booking_id AND (file_name != '' AND file_name IS NOT NULL)) as support_file, 
               
                     CASE WHEN(serial_number IS NULL OR serial_number = '') THEN '' ELSE (CONCAT('''', booking_unit_details.serial_number))  END AS serial_number,
-                    CASE WHEN(sf_model_number IS NULL OR sf_model_number = '') THEN (model_number) ELSE (sf_model_number) END AS model_number
+                    CASE WHEN(sf_model_number IS NULL OR sf_model_number = '') THEN (model_number) ELSE (sf_model_number) END AS model_number,
+                    DATE_FORMAT(booking_details.service_center_closed_date, '%d-%b-%Y') as service_center_closed_date
 
               From booking_details, booking_unit_details, services, partners, users
                   WHERE `booking_details`.booking_id = `booking_unit_details`.booking_id 
@@ -3701,7 +3702,8 @@ class invoices_model extends CI_Model {
                     (Select CASE WHEN (file_name = '' OR file_name IS NULL) THEN ('') ELSE (GROUP_CONCAT(CONCAT('".S3_WEBSITE_URL."misc-images/', file_name) SEPARATOR ' , ')) END as support_file FROM booking_files WHERE booking_files.booking_id = booking_unit_details.booking_id AND (file_name != '' AND file_name IS NOT NULL)) as support_file, 
               
                     CASE WHEN(serial_number IS NULL OR serial_number = '') THEN '' ELSE (CONCAT('''', booking_unit_details.serial_number))  END AS serial_number,
-                    CASE WHEN(sf_model_number IS NULL OR sf_model_number = '') THEN (model_number) ELSE (sf_model_number) END AS model_number
+                    CASE WHEN(sf_model_number IS NULL OR sf_model_number = '') THEN (model_number) ELSE (sf_model_number) END AS model_number,
+                    DATE_FORMAT(booking_details.service_center_closed_date, '%d-%b-%Y') as service_center_closed_date
 
               From booking_details, booking_unit_details, services, users, spare_nrn_approval as sn
                   WHERE `booking_details`.booking_id = `booking_unit_details`.booking_id 
