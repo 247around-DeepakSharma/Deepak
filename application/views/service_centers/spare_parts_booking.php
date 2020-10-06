a340bdec4f94cdda95a8e7085c5ac535d76e4b15
Gorakh Mehta <gorakh@247around.com>
2020-10-06 12:11:09 +0530

    Challan not downloaded #CRMS-1509

--- application/views/service_centers/spare_parts_booking.php
+++ application/views/service_centers/spare_parts_booking.php
@@ -218,6 +218,7 @@ if ($this->uri->segment(3)) {
                             }
                             ?>
                         </div>
+                        <input type="hidden" name="challan_send_to_sf" value="1">
                         <center style="margin-bottom: 10px;"><input type= "submit" onclick="return checkValidationForBlank()"  class="btn btn-md" style="background-color:#2C9D9C; border-color: #2C9D9C; color:#fff;" name="download_shippment_address" id="download_shippment_address" value ="Print / Download" > </center>
                     </form>
                 </div>
