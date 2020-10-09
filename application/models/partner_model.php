bd5af49c8e2c5299915afa2cf7139d08f31c990e
pritys <pritys@247around.com>
2020-10-09 16:22:34 +0530

    CRMS-1231 : Show agent in case on website booking in detailed summary booking

--- application/models/partner_model.php
+++ application/models/partner_model.php
@@ -2936,7 +2936,7 @@ function get_data_for_partner_callback($booking_id) {
                     `Sale Invoice Id`	                    
             FROM (SELECT
                     booking_details.booking_id as '247around Booking ID',
-                    (CASE WHEN booking_details.created_by_agent_type IN ('"._247AROUND_PARTNER_STRING."', '".BOOKING_AGENT_Dealer."') then entity_login_table.agent_name ELSE employee.full_name END) as 'Agent Name',
+                    (CASE WHEN booking_details.created_by_agent_type IN ('"._247AROUND_PARTNER_STRING."', '".BOOKING_AGENT_Dealer."') then entity_login_table.agent_name WHEN booking_details.created_by_agent_type = '".BOOKING_AGENT_Website."' THEN '".BOOKING_AGENT_Website."' ELSE employee.full_name END) as 'Agent Name',
                     partner_channel.channel_name as 'Creation Source',
                     DATE_FORMAT(DATE(booking_details.create_date), '%d-%m-%Y') as 'Create Date',
                     ud.appliance_brand as 'Brand',
@@ -3113,7 +3113,7 @@ function get_data_for_partner_callback($booking_id) {
 
             SELECT
                     booking_details.booking_id as '247around Booking ID',
-                    '' as 'Agent Name',
+                    (CASE WHEN booking_details.created_by_agent_type IN ('"._247AROUND_PARTNER_STRING."', '".BOOKING_AGENT_Dealer."') then entity_login_table.agent_name WHEN booking_details.created_by_agent_type = '".BOOKING_AGENT_Website."' THEN '".BOOKING_AGENT_Website."' ELSE employee.full_name END) as 'Agent Name',
                     partner_channel.channel_name as 'Creation Source',
                     DATE_FORMAT(DATE(booking_details.create_date), '%d-%m-%Y') as 'Create Date',
                     ud.appliance_brand as 'Brand',
@@ -3292,6 +3292,8 @@ function get_data_for_partner_callback($booking_id) {
                 LEFT JOIN spare_consumption_status ON (spare_parts_details.consumed_part_status_id = spare_consumption_status.id)
                 LEFT JOIN booking_cancellation_reasons b_cr ON (booking_details.cancellation_reason = b_cr.id)
                 LEFT JOIN booking_cancellation_reasons ssba_cr ON (service_center_booking_action.cancellation_reason = ssba_cr.id)
+                LEFT JOIN entity_login_table ON (booking_details.created_by_agent_id = entity_login_table.agent_id)
+                LEFT JOIN employee ON (booking_details.created_by_agent_id = employee.id)
             WHERE {$where}
                 AND product_or_services != 'Product' AND spare_parts_details.booking_id is not null 
             GROUP BY
