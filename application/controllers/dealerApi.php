82168b10ed40cbd9e3b648b286107460ae684d45
Ghanshyam <ghanshyamg@247around.com>
2020-10-26 19:58:52 +0530

    CRMS-1601 Dealer app show all option in state

--- application/controllers/dealerApi.php
+++ application/controllers/dealerApi.php
@@ -739,9 +739,9 @@ function getHomeDashboard(){
                     $citi['district'] = 'All';
                     array_push($state_with_cities,array('state'=>'All','cities'=>array($citi)));
                     foreach($response_state as $state){ 
-                            //$cities[0]['district'] = 'All';
-                            $cities = $this->indiapincode_model->getStateCities($state['state_code']);
-                            //$cities = array_merge($cities,$cities_states);                      
+                            $cities_all[0]['district'] = 'All';
+                            $cities_states = $this->indiapincode_model->getStateCities($state['state_code']);
+                            $cities = array_merge($cities_all,$cities_states);                      
                     
                     $state_with_cities[] = array('state'=>$state['state'],'cities'=>$cities);   
                     } 
