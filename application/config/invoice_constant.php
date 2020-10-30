547f2827c51e0a59d50672d2fc3509e9166ed657
Abhay Anand <abhaya@247around.com>
2020-10-30 15:45:32 +0530

    Create Bulk Debit Note #CRMS-1643

--- application/config/invoice_constant.php
+++ application/config/invoice_constant.php
@@ -144,4 +144,6 @@ define('DEFECTIVE_PACKAGING_LARGE_TAG', 'defective-packaging-variable-large-box'
 define('MSL_Debit_Note', 'MSL Debit Note');
 // MSL Credit Note
 define('MSL_Credit_Note', 'MSL Credit Note');
+//BULK Debite Note TAG
+define('BULK_DEBIT_NOTE_TAG', 'BULK_DEBIT_NOTE_TAG');
 ?>
