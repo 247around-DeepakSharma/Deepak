a46fc42f0901a6871867b9cd52f8282b9eedb0d6
Ghanshyam <ghanshyamg@247around.com>
2020-11-04 13:19:12 +0530

    CRMS-1156 SF can not transfer others SF booking parts.

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -9418,12 +9418,19 @@ class Service_centers extends CI_Controller {
         $data = array();
         $from = trim($this->input->post('frombooking'));
         $to = trim($this->input->post('tobooking'));
+        $sf_id = '';
+        if(!empty($this->session->userdata('warehouse_id'))) {
+            $sf_id = $this->session->userdata('warehouse_id');
+        } else {
+            $sf_id = $this->session->userdata('service_center_id');
+        }
+        
         if (isset($from) && isset($to) && !empty($from) && !empty($to)) {
-            $from_details = $this->partner_model->get_spare_parts_by_any("spare_parts_details.*", array('booking_id' => $from, 'wh_ack_received_part' => 1, 'status' => SPARE_DELIVERED_TO_SF, 'part_warranty_status' => SPARE_PART_IN_WARRANTY_STATUS));
+            $from_details = $this->partner_model->get_spare_parts_by_any("spare_parts_details.*", array('booking_id' => $from, 'wh_ack_received_part' => 1, 'status' => SPARE_DELIVERED_TO_SF, 'part_warranty_status' => SPARE_PART_IN_WARRANTY_STATUS, 'service_center_id' => $sf_id));
             if (!empty($from_details)) {
                 $frominventory_req_id = $from_details[0]['requested_inventory_id'];
             }
-            $to_details = $this->partner_model->get_spare_parts_by_any("spare_parts_details.*", array('booking_id' => $to, 'wh_ack_received_part' => 1, 'status' => SPARE_PARTS_REQUESTED, 'part_warranty_status' => SPARE_PART_IN_WARRANTY_STATUS));
+            $to_details = $this->partner_model->get_spare_parts_by_any("spare_parts_details.*", array('booking_id' => $to, 'wh_ack_received_part' => 1, 'status' => SPARE_PARTS_REQUESTED, 'part_warranty_status' => SPARE_PART_IN_WARRANTY_STATUS, 'service_center_id' => $sf_id));
             if (!empty($to_details)) {
                 $toinventory_req_id = $to_details[0]['requested_inventory_id'];
             }
