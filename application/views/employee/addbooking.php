ac097fc97457ba3275432496a347b8305b2156f0
LAPTOP-NB62FNGF\L0319-04 <muskang@247around.com>
2020-11-09 17:15:23 +0530

    CRMS-1623 "Add the * sign after remarks "

--- application/views/employee/addbooking.php
+++ application/views/employee/addbooking.php
@@ -402,7 +402,7 @@
                                 </div>
                             </div>
                             <div class="form-group ">
-                                <label for="type" class="col-md-4">Remarks</label>
+                                <label for="type" class="col-md-4">Remarks *</label>
                                 <div class="col-md-6">
                                     <textarea class="form-control" rows="4" name="query_remarks" id="query_remarks" placeholder="Enter Query Remarks"  ></textarea>
                                 </div>
