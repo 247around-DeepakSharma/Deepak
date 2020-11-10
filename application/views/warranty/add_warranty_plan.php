3b0418a37d98c23103d3e8b3261622b1bb3ae5eb
pritys <pritys@247around.com>
2020-11-10 11:56:19 +0530

    CRMS-1141 : Button showing as 0 , in grace period

--- application/views/warranty/add_warranty_plan.php
+++ application/views/warranty/add_warranty_plan.php
@@ -498,7 +498,7 @@
             
             if(!$('#warranty_grace_period').val())
             {
-                 $(this).val(0);
+                 display_error("warranty_grace_period_error", "Please Enter Warranty Grace Period");
             }
             else
             {
