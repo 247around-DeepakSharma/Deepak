0acb8c0054fe2a169ec65653a5bba7396e5bc06e
Ghanshyam <ghanshyamg@247around.com>
2020-11-16 10:57:57 +0530

    CRMS-1323 Staging All changes in one commit - ok part to be shipped by SF in Spare transfer

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -9378,12 +9378,29 @@ class Service_centers extends CI_Controller {
         $data = array();
         $from = trim($this->input->post('frombooking'));
         $to = trim($this->input->post('tobooking'));
+        $sf_id = '';
+        if(!empty($this->session->userdata('service_center_id'))) {
+            $sf_id = $this->session->userdata('service_center_id');
+        }
+        
         if (isset($from) && isset($to) && !empty($from) && !empty($to)) {
-            $from_details = $this->partner_model->get_spare_parts_by_any("spare_parts_details.*", array('booking_id' => $from, 'wh_ack_received_part' => 1, 'status' => SPARE_DELIVERED_TO_SF, 'part_warranty_status' => SPARE_PART_IN_WARRANTY_STATUS));
+            $where_from = array('spare_parts_details.booking_id' => $from,
+                'wh_ack_received_part' => 1,
+                "status in ('" . SPARE_DELIVERED_TO_SF . "','" . OK_PART_TO_BE_SHIPPED . "')" => null,
+                'part_warranty_status' => SPARE_PART_IN_WARRANTY_STATUS,
+                "`booking_details.current_status` !='". _247AROUND_COMPLETED."'"  => null
+            );
+            if (!empty($sf_id)) {
+                $where_from['service_center_id'] = $sf_id;
+            }
+            $from_details = $this->partner_model->get_spare_parts_by_any("spare_parts_details.*", $where_from,true);
             if (!empty($from_details)) {
                 $frominventory_req_id = $from_details[0]['requested_inventory_id'];
+                if(empty($sf_id)){
+                    $sf_id = $from_details[0]['service_center_id'];
+                }
             }
-            $to_details = $this->partner_model->get_spare_parts_by_any("spare_parts_details.*", array('booking_id' => $to, 'wh_ack_received_part' => 1, 'status' => SPARE_PARTS_REQUESTED, 'part_warranty_status' => SPARE_PART_IN_WARRANTY_STATUS));
+            $to_details = $this->partner_model->get_spare_parts_by_any("spare_parts_details.*", array('booking_id' => $to, 'wh_ack_received_part' => 1, 'status' => SPARE_PARTS_REQUESTED, 'part_warranty_status' => SPARE_PART_IN_WARRANTY_STATUS, 'service_center_id' => $sf_id));
             if (!empty($to_details)) {
                 $toinventory_req_id = $to_details[0]['requested_inventory_id'];
             }
