4915ca0159ef1d01cf577f1c4867447d572db4ea
Ghanshyam <ghanshyamg@247around.com>
2020-11-17 11:47:16 +0530

    CRMS-1709 Show STate name alphabetically by default

--- application/controllers/dealerApi.php
+++ application/controllers/dealerApi.php
@@ -1555,16 +1555,8 @@ function  getPartnerCompareTAT(){
                     curl_close($ch);
                     
                     if(!empty($curl_response)){
-                    $tat = $curl_response->TAT;
-                    $db = (array) $tat;
-                    $col  = 'entity';
-                    $sort = array();
-                    foreach ($db as $i => $obj) {
-                        $sort[$i] = $obj->{$col};
-                    }
-                    array_multisort($sort, SORT_ASC,SORT_NATURAL|SORT_FLAG_CASE, $db);
-                    $object = (object) $db;
-                    foreach ($object as $key=>$value){
+                    
+                    foreach ($curl_response->TAT as $key=>$value){
                      $state =   $value->entity; 
                      if($state!='Total'){
                      $return_data['D0'][]  = array('state'=>ucwords($state),'percent'=>(int)$value->TAT_0_per,'count'=>$value->TAT_0)  ;
@@ -2038,7 +2030,16 @@ function  getPartnerCompareTAT(){
                     curl_close($ch);
                     
                     if(!empty($curl_response)){
-                    foreach ($curl_response->TAT as $key=>$value){
+                    $tat = $curl_response->TAT;
+                    $db = (array) $tat;
+                    $col  = 'entity';
+                    $sort = array();
+                    foreach ($db as $i => $obj) {
+                        $sort[$i] = $obj->{$col};
+                    }
+                    array_multisort($sort, SORT_ASC,SORT_NATURAL|SORT_FLAG_CASE, $db);
+                    $object = (object) $db;
+                    foreach ($object as $key=>$value){
                      $state =   $value->entity; 
                      if($state!='Total' && ($value->TAT_0_per!=0 || $value->TAT_1_per!=0 || $value->TAT_2_per!=0 || $value->TAT_3_per!=0)){
                      $return_data['D0'][]  = array('state'=>ucwords($state),'percent'=>$value->TAT_0_per)  ;
