962a5c8189acc3843b7a66d7a111d4ae7fbe31a7
Abhay Anand <abhaya@247around.com>
2020-11-26 20:24:37 +0530

    Partner Panel - Spare Part Bookings - Shipped Spare By Partner - PHP error showing on downloaded report #CRMS-1778

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -5542,6 +5542,7 @@ class Service_centers extends CI_Controller {
                     }
                     $a .= "</ul>
                           </div>";
+                    $row[] = $a;
                     break;
                 default:
                     $row[] = "";
