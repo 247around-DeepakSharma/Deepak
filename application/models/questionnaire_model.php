0ca926d38b5210704e07f7d30400b89d940367a8
pritys <pritys@247around.com>
2020-12-11 13:27:02 +0530

    Revert "CRMS-1803 " Modified the code as per the required changes i.e. Accepting empty required checked ""
    This reverts commit dfe7e2b458469438c13e094422bf79569f7b3d7c.

--- application/models/questionnaire_model.php
+++ application/models/questionnaire_model.php
@@ -43,14 +43,9 @@ class Questionnaire_model extends CI_Model {
         unset($data['request_type']);
         unset($data['service_id']);
         unset($data['options']);        
-        
+            
         // insert data in review_questionare 
         $data['created_by'] = $this->session->userdata('id');
-        if($this->session->userdata('is_required')=='Yes')
-        {
-            $data['is_required']=1;
-        }
-        else $data['is_required']=1;
         $data['updated_by'] = $this->session->userdata('id');
         $this->db->insert('review_questionare', $data);
         $q_id = $this->db->insert_id();
@@ -87,14 +82,6 @@ class Questionnaire_model extends CI_Model {
             unset($data['options']);
         }
         $data['updated_by'] = $this->session->userdata('id');
-        if($this->session->userdata('is_required')=='Yes')
-        {
-            $data['is_required']=1;
-        }
-        else
-        {
-            $data['is_required']=0;
-        }
         // update data in review_questionare
         $this->db->where($where, FALSE);
         $this->db->update('review_questionare', $data);
