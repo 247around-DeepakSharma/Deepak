1fcfe7d4d95357fd31694b0b4ee7313b3a924b66
Ghanshyam <ghanshyamg@247around.com>
2020-12-14 10:31:36 +0530

    CRMS-1323 check service center id for admin panel transfer, do not allow to

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -9485,10 +9485,9 @@ class Service_centers extends CI_Controller {
         $data = array();
         $from = trim($this->input->post('frombooking'));
         $to = trim($this->input->post('tobooking'));
-		 $sf_id = '';
-        if(!empty($this->session->userdata('warehouse_id'))) {
-            $sf_id = $this->session->userdata('warehouse_id');
-        } else {
+
+        $sf_id = '';
+        if(!empty($this->session->userdata('service_center_id'))) {
             $sf_id = $this->session->userdata('service_center_id');
         }
         if (isset($from) && isset($to) && !empty($from) && !empty($to)) {
@@ -9497,11 +9496,18 @@ class Service_centers extends CI_Controller {
                 'wh_ack_received_part' => 1,
                 "status in ('" . SPARE_DELIVERED_TO_SF . "','" . OK_PART_TO_BE_SHIPPED . "')" => null,
                 'part_warranty_status' => SPARE_PART_IN_WARRANTY_STATUS,
-                'service_center_id' => $sf_id);
+                "booking_details.current_status!='" . _247AROUND_COMPLETED . "'"
+            );
+            if (!empty($sf_id)) {
+                $where_from['service_center_id'] = $sf_id;
+            }
             $from_details = $this->partner_model->get_spare_parts_by_any("spare_parts_details.*", $where_from);
 
             if (!empty($from_details)) {
                 $frominventory_req_id = $from_details[0]['requested_inventory_id'];
+                if(empty($sf_id)){
+                    $sf_id = $from_details[0]['service_center_id'];
+                }
             }
             $to_details = $this->partner_model->get_spare_parts_by_any("spare_parts_details.*", array('booking_id' => $to, 'wh_ack_received_part' => 1, 'status' => SPARE_PARTS_REQUESTED, 'part_warranty_status' => SPARE_PART_IN_WARRANTY_STATUS));
             if (!empty($to_details)) {
