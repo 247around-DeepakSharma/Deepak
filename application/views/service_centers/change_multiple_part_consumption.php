c8126a27b645d0d5695f4172338f6b819555efa1
Ghanshyam <ghanshyamg@247around.com>
2020-12-18 15:00:58 +0530

    CRMS-1935 Precheck all spare if any one is already acknowledged give error

--- application/views/service_centers/change_multiple_part_consumption.php
+++ application/views/service_centers/change_multiple_part_consumption.php
@@ -54,7 +54,8 @@
 </div>    
 <div class="row form-group"> 
     <div class="col-md-12" style="text-align: center; padding: 5px;"> 
-        <input type="submit" name="change-consumption" class="btn btn-primary change-consumption-multiple" id="multiple_received" value="Submit">
+        <input type="submit" name="change-consumption" class="btn btn-primary change-consumption-multiple" id="multiple_received" value="Submit" style='display:none'>
+        <input type="submit"  class="btn btn-primary change-consumption-multiple_precheck"  value="Submit">
     </div>
 </div>
 
