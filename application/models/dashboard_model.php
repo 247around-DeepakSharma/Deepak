5ed713cc9d7b20b872daefa361e78f43523d9174
Ghanshyam <ghanshyamg@247around.com>
2020-12-24 12:22:12 +0530

    CRMS-1986 graph not showing in dashboard for completed with review

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -235,12 +235,29 @@ class dashboard_model extends CI_Model {
      * @return array
      */
     function get_completed_booking_graph_data($startDate, $endDate){
-        $sql = "SELECT employee.full_name, employee.id, SUM(IF (new_state = 'Completed_Rejected', 1, 0)) as completed_rejected,
-                    SUM(IF (new_state = 'Completed_Approved', 1, 0)) as completed_approved,
-                    SUM(IF (new_state = 'Completed', 1, 0)) as total_completed,
-                    (SUM(IF (new_state = 'Completed', 1, 0)) - SUM(IF (new_state = 'Completed_Approved', 1, 0))) as edit_completed,
-                    (SUM(IF (new_state = 'Completed_Rejected', 1, 0)) + SUM(IF (new_state = 'Completed', 1, 0))) as total_bookings
-                    FROM booking_state_change as bsc, employee WHERE employee.groups ='closure' AND bsc.create_date >= '$startDate' AND bsc.create_date <= '$endDate' AND agent_id = employee.id AND partner_id='"._247AROUND."' group by employee_id";
+
+        $sql = "SELECT 
+                    data.full_name,
+                    data.id,
+                    SUM(IF(new_state = 'Completed_Rejected',1,0)) AS completed_rejected,
+                    SUM(IF(new_state = 'Completed_Approved',1,0)) AS completed_approved,
+                    SUM(IF(new_state = 'Completed' OR new_state = 'Completed_With_Rating', 1, 0)) AS total_completed,
+                    (SUM(IF(new_state = 'Completed' OR new_state = 'Completed_With_Rating', 1, 0)) - SUM(IF(new_state = 'Completed_Approved',1,0))) AS edit_completed,
+                    (SUM(IF(new_state = 'Completed_Rejected',1,0)) + SUM(IF(new_state = 'Completed', 1, 0))) AS total_bookings
+                FROM
+                    (SELECT 
+                        employee.full_name, employee.id, new_state, employee_id
+                    FROM
+                        booking_state_change AS bsc, employee
+                    WHERE
+                        employee.groups = 'closure'
+                            AND bsc.create_date >= '$startDate'
+                            AND bsc.create_date <= '$endDate'
+                            AND agent_id = employee.id
+                            AND partner_id = '"._247AROUND."'
+                    GROUP BY employee_id , booking_id , bsc.new_state, DATE(bsc.create_date)) AS data
+                GROUP BY data.employee_id";            
+
         $query = $this->db->query($sql);
         return $query->result_array();
     }
