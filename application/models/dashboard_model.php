3aaa56b6c27cdb8c4c84a925e2a32f848ad9a353
Ghanshyam <ghanshyamg@247around.com>
2020-12-24 16:06:59 +0530

    CRMS-1986 Add condition for total booking

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -243,7 +243,7 @@ class dashboard_model extends CI_Model {
                     SUM(IF(new_state = 'Completed_Approved',1,0)) AS completed_approved,
                     SUM(IF(new_state = 'Completed' OR new_state = 'Completed_With_Rating', 1, 0)) AS total_completed,
                     (SUM(IF(new_state = 'Completed' OR new_state = 'Completed_With_Rating', 1, 0)) - SUM(IF(new_state = 'Completed_Approved',1,0))) AS edit_completed,
-                    (SUM(IF(new_state = 'Completed_Rejected',1,0)) + SUM(IF(new_state = 'Completed', 1, 0))) AS total_bookings
+                    (SUM(IF(new_state = 'Completed_Rejected',1,0)) + SUM(IF(new_state = 'Completed' OR new_state = 'Completed_With_Rating', 1, 0))) AS total_bookings
                 FROM
                     (SELECT 
                         employee.full_name, employee.id, new_state, employee_id
