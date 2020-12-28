02b2193c526ba85ac042a52ae8e5ab1f271e23fd
Gorakh Mehta <gorakh@247around.com>
2020-12-28 18:42:08 +0530

    Courier Name and Courier AWB Number not coming at SF panel#CRMS-1600

--- application/controllers/employee/spare_parts.php
+++ application/controllers/employee/spare_parts.php
@@ -4496,7 +4496,7 @@ $select = 'spare_parts_details.entity_type,spare_parts_details.quantity,spare_pa
         $post = $this->get_post_data();
         $post['is_courier_details_required'] = TRUE;
         $post['column_order'] = array();
-        $post['column_search'] = array('inventory_master_list.part_name', 'inventory_master_list.type', 'courier_details.AWB_no', 'courier_details.courier_name', 'i.booking_id','sc.name','i.invoice_id');
+        $post['column_search'] = array('inventory_master_list.part_name', 'inventory_master_list.type', 'courier_company_invoice_details.awb_number', 'courier_company_invoice_details.company_name', 'i.booking_id', 'sc.name', 'i.invoice_id');
         $post['where'] = array(
             'i.receiver_entity_type' => trim($this->input->post('receiver_entity_type')),
             'i.sender_entity_id' => trim($this->input->post('sender_entity_id')),
@@ -4509,7 +4509,7 @@ $select = 'spare_parts_details.entity_type,spare_parts_details.quantity,spare_pa
                     WHEN (e.full_name IS NOT NULL) THEN (e.full_name) END as receiver, 
                     CASE WHEN(sc1.name IS NOT NULL) THEN (sc1.name) 
                     WHEN(p1.public_name IS NOT NULL) THEN (p1.public_name) 
-                    WHEN (e1.full_name IS NOT NULL) THEN (e1.full_name) END as sender,i.*,courier_details.AWB_no,courier_details.courier_name,courier_details.status";
+                    WHEN (e1.full_name IS NOT NULL) THEN (e1.full_name) END as sender,i.*,courier_company_invoice_details.awb_number as AWB_no, courier_company_invoice_details.company_name as courier_name";
         $list = $this->inventory_model->get_spare_need_to_acknowledge($post, $select);
         // print_r($this->db->last_query());
         $data = array();
@@ -4558,7 +4558,6 @@ $select = 'spare_parts_details.entity_type,spare_parts_details.quantity,spare_pa
         $a = "<a href='javascript:void(0);' onclick='";
         $a .= "get_msl_awb_details(" . '"' . $inventory_list->courier_name . '"';
         $a .= ', "' . $inventory_list->AWB_no . '"';
-        $a .= ', "' . $inventory_list->status . '"';
         $a .= ', "msl_awb_loader_' . $no . '"';
         $a .= ")'>" . $inventory_list->AWB_no . "</a>";
         $a .="<span id='msl_awb_loader_$no' style='display:none;'><i class='fa fa-spinner fa-spin'></i></span>";
