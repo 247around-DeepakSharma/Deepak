b698731ee159057c1b4e126b5c2d410c51f387bb
Ghanshyam <ghanshyamg@247around.com>
2020-12-31 09:56:59 +0530

    CRMS-1986 Correct excel download data

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -271,16 +271,16 @@ class dashboard_model extends CI_Model {
         $this->db->_protect_identifiers = FALSE;
         
         // set select statement
-        $select = "employee.full_name as 'Agent Name',
-                    booking_details.booking_id as 'Booking Id',
-                    service_centres.name as 'Vendor Name',
-                    booking_details.request_type as 'Request Type',
-                    partners.public_name as Partner,
-                    services.services as Product,
-                    SUM(IF(new_state = 'Completed_Rejected', 1, 0)) AS 'Rejected Bookings',
-                    SUM(IF(new_state = 'Completed_Approved', 1, 0)) AS 'Directly Approved Bookings',
-                    (SUM(IF(new_state = 'Completed', 1, 0)) - SUM(IF(new_state = 'Completed_Approved', 1,0))) AS 'Approved With Edit Bookings',
-                    (SUM(IF(new_state = 'Completed_Rejected', 1, 0)) + SUM(IF(new_state = 'Completed', 1, 0))) AS 'Total Bookings'";
+        $select = "employee.full_name AS 'Agent Name',
+                    booking_details.booking_id AS 'Booking Id',
+                    service_centres.name AS 'Vendor Name',
+                    booking_details.request_type AS 'Request Type',
+                    partners.public_name AS Partner,
+                    services.services AS Product,
+                    IF(new_state = 'Completed_Rejected',1,0) AS 'Rejected Bookings',
+                    IF(new_state = 'Completed_Approved',1,0) AS 'Directly Approved Bookings',
+                    IF(((new_state = 'Completed' or new_state = 'Completed_With_Rating' or new_state = 'Customer_unreachable_for_rating')  && old_state != 'Completed_Approved'), 1, 0) AS 'Approved With Edit Bookings',
+                    IF((new_state = 'Completed_Rejected' || new_state = 'Completed_Approved' || ((new_state = 'Completed' or new_state = 'Completed_With_Rating' or new_state = 'Customer_unreachable_for_rating')  && old_state != 'Completed_Approved')),1,0) AS 'Total Bookings'";
         
         // set where condition
         $where = "employee.groups = 'closure'
@@ -288,6 +288,7 @@ class dashboard_model extends CI_Model {
                     AND bsc.create_date <= '$endDate'
                     AND bsc.partner_id = '"._247AROUND."'";
         
+        $condition = "`Rejected Bookings` = '1' OR `Directly Approved Bookings` = '1' OR `Approved With Edit Bookings` = '1' OR `Total Bookings` = '1'";
         // Query here
         $this->db->select($select);
         $this->db->from('booking_state_change AS bsc');
@@ -298,8 +299,8 @@ class dashboard_model extends CI_Model {
         $this->db->join('employee', 'bsc.agent_id = employee.id');
         $this->db->where($where);
         $this->db->order_by('employee.id');
-        $this->db->group_by('bsc.booking_id');
-        
+        $this->db->group_by("employee.id,bsc.booking_id, bsc.new_state, DATE(bsc.create_date)");
+        $this->db->having($condition);
         // return query object
         $query = $this->db->get();
         return $query;
@@ -347,10 +348,9 @@ class dashboard_model extends CI_Model {
         $where = "employee.groups = 'closure'
                     AND bsc.create_date >= '$startDate'
                     AND bsc.create_date <= '$endDate'
-                    AND bsc.partner_id = '"._247AROUND."'";
-					
+                    AND bsc.partner_id = '"._247AROUND."'"
+                . "And new_state in ('Cancelled_Rejected','Cancelled','Cancelled_Approved','Completed')";
         $condition = "`Rejected Bookings` != '0' OR `Directly Approved Bookings` != '0' OR `Cancelled to Completed Bookings` != '0' OR `Edit Cancelled Bookings` != '0' OR `Total Bookings` != '0'";
-
         // Query here
         $this->db->select($select);
         $this->db->from('booking_state_change AS bsc');
@@ -362,9 +362,11 @@ class dashboard_model extends CI_Model {
         $this->db->where($where);
         $this->db->order_by('employee.id');
         $this->db->group_by('bsc.booking_id');
-        
+        $this->db->having($condition);
         // return query object
-        $query = $this->db->get();        
+
+        $query = $this->db->get();
+
         return $query;
     }
         
