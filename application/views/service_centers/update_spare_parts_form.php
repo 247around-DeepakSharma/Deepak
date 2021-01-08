6ceca3800284fc5fcf7564a83adacb292cc4aa24
Gorakh Mehta <gorakh@247around.com>
2021-01-08 15:20:08 +0530

    Number of boxes to be subjected to mark 0 to avoid unnecessary charges in Brand Invoicing #CRMS-2016

--- application/views/service_centers/update_spare_parts_form.php
+++ application/views/service_centers/update_spare_parts_form.php
@@ -458,7 +458,7 @@
                                 <div class="col-md-6">
                                     <select class="form-control" id="shipped_spare_parts_boxes_count" name="shipped_spare_parts_boxes_count"  required="">
                                         <option selected="" disabled="" value="">Select Boxes</option>
-                                        <?php for ($i = 1; $i < 31; $i++) { ?>
+                                        <?php for ($i = 0; $i < 31; $i++) { ?>
                                             <option value="<?php echo $i; ?>" ><?php echo $i; ?></option>
                                         <?php } ?>
                                     </select>
