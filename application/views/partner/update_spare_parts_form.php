fe0bf6b98d966bbc6551d2eed193943c64c39193
Gorakh Mehta <gorakh@247around.com>
2021-02-01 17:31:02 +0530

    Invoice PDF Null #CRMS-2104

--- application/views/partner/update_spare_parts_form.php
+++ application/views/partner/update_spare_parts_form.php
@@ -1382,7 +1382,7 @@
             
             var numb = $(this)[0].files[0].size/1024/1024;
             numb = numb.toFixed(2);
-            if(numb > 5){
+            if(numb > 2){
                 $(this).val(''); 
                 alert('Not allow file size greater than 5MB');
             } 
