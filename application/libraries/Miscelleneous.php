044c751df2dbbc153b222b6e21866b14d9818829
pritys <pritys@247around.com>
2021-02-09 12:45:06 +0530

    CRMS-2002 : Show Blank in contacts in case of ASM not exist for Booking

--- application/libraries/Miscelleneous.php
+++ application/libraries/Miscelleneous.php
@@ -3599,7 +3599,7 @@ function generate_image($base64, $image_name,$directory){
 
         $select = "e.phone as am_caontact,e.official_email as am_email, e.full_name as am,partners.primary_contact_name as partner_poc,"
                 . "partners.primary_contact_phone_1 as poc_contact,service_centres.primary_contact_email as service_center_email,partners.public_name as partner,"
-                . "booking_details.assigned_vendor_id,employee.official_email as rm_email,employee.full_name as rm ,employee.phone as rm_contact, emp.official_email as asm_email,emp.full_name as asm, emp.phone as asm_contact, group_concat(distinct agent_filters.state) as am_state";
+                . "booking_details.assigned_vendor_id,ifnull(employee.official_email, '') as rm_email,ifnull(employee.full_name, '') as rm ,ifnull(employee.phone, '') as rm_contact, ifnull(emp.official_email, '') as asm_email,ifnull(emp.full_name, '') as asm, ifnull(emp.phone, '') as asm_contact, group_concat(distinct agent_filters.state) as am_state";
         $join['partners'] = "partners.id = booking_details.partner_id";
         $join['agent_filters'] = "partners.id = agent_filters.entity_id";
         $join['service_centres'] = "service_centres.id = booking_details.assigned_vendor_id";
