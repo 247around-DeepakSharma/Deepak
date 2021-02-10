e33444306ae537098780c1bf38161a3d7e54449e
pritys <pritys@247around.com>
2021-02-10 14:12:17 +0530

    CRMS-2042 : Added Department & DatePicker in Custom Report

--- application/controllers/employee/reports.php
+++ application/controllers/employee/reports.php
@@ -46,47 +46,74 @@ class reports extends CI_Controller {
 
     /**
      * This function is used to return the query for the chosen tag.
-     * @param-$tag
+     * @param - $tag
+     * @param - $arr_date_filter (Array of Date Filter)
      * @return - $query 
      */
-     function custom_reporting($tag = "") {
+    function custom_reporting($tag, $arr_date_filter) {
+        log_message('info', __FUNCTION__ . ' => Report => ' . $tag . 'Filter => ' . json_encode($arr_date_filter));
+
         $query = "";
-        $data = $this->reporting_utils->get_custom_query_data($tag);
-        if(!empty($data[0]['query'])){
+        $data = $this->reporting_utils->get_custom_query_data($tag, $arr_date_filter);       
+
+        if (!empty($data[0]['query'])) {
             $sql = $data[0]['query'];
-            $query = $this->db->query($sql); 
-            return $query;
-        }
+            if(!empty($data[0]['date_filter'])){
+                $start_date = $end_date = date("Y-m-d"); 
+                if(!empty($arr_date_filter[0])){
+                    $start_date = $arr_date_filter[0];
+                }
+                if(!empty($arr_date_filter[1])){
+                    $end_date = $arr_date_filter[1];
+                }
+                $sql = sprintf($sql, $start_date, $end_date);
+            }
+            $query = $this->db->query($sql);
+        }         
+        return $query;
     }
+    
     /** Desc- This function is used to download Custom Report dynamically.
     */
-    function download_custom_report($tag){
-        
-        $custom_report = $tag.time().".csv";
+    function download_custom_report($tag, $period = "") {
+        $arr_date_filter = array();
+        if(!empty($period)){
+            $period = urldecode($period);
+            $arr_date_filter = explode(" - ", $period);
+        }
+        log_message('info', __FUNCTION__ . ' => Tag: ' . $tag);
+		
+        $custom_report = $tag . "-" . time() . ".csv";
         $csv = TMP_FOLDER . $custom_report;
-        $tag = $this->uri->segment(4);
-        $custom_report= "Custom_report_" . $tag . ".csv";
-        $csv = TMP_FOLDER . $custom_report;
-        $report = $this->custom_reporting($tag);
-        if(!empty($report))
-        {
+		
+        log_message('info', __FUNCTION__ . ' => Tag-URI: %%%' . $tag . " => " .json_encode($arr_date_filter));
+
+        $report = $this->custom_reporting($tag, $arr_date_filter);
+
+        if (!empty($report)) {
             $delimiter = ",";
             $newline = "\r\n";
             $new_report = $this->dbutil->csv_from_result($report, $delimiter, $newline);
+
             log_message('info', __FUNCTION__ . ' => Rendered CSV');
+
             write_file($csv, $new_report);
-            if(!empty($csv)){
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename="' . basename($csv) . '"');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($csv));
-            readfile($csv);
-            exec("rm -rf " . escapeshellarg($csv));
+
+            if (!empty($csv)) {
+                header('Content-Description: File Transfer');
+                header('Content-Type: application/octet-stream');
+                header('Content-Disposition: attachment; filename="' . basename($csv) . '"');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($csv));
+
+                readfile($csv);
+
+                exec("rm -rf " . escapeshellarg($csv));
             }
         }
+
         log_message('info', __FUNCTION__ . ' Function End');
     }
 
