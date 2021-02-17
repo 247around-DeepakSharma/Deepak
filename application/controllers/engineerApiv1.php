0a532b5513c83049792e6ea93c85d08fe70676e4
Ghanshyam <ghanshyamg@247around.com>
2021-02-17 09:40:46 +0530

    CRMS-1515 Bom model to show and serial number check

--- application/controllers/engineerApiv1.php
+++ application/controllers/engineerApiv1.php
@@ -3577,9 +3577,16 @@ class engineerApiv1 extends CI_Controller {
             }
         }
         if ($check) {
-            $where = array('entity_id' => $requestData['partner_id'], 'entity_type' => _247AROUND_PARTNER_STRING, 'service_id' => $requestData['service_id'], 'inventory_model_mapping.active' => 1, 'appliance_model_details.active' => 1);
-            $model_numbers = $this->inventory_model->get_inventory_mapped_model_numbers('appliance_model_details.id,appliance_model_details.model_number', $where);
-            if (empty($model_numbers)) {
+            $type = '';
+            if(!empty($requestData['type'])){
+                $type = $requestData['type'];
+            }
+            //For spare part request show inventory mapped mpdel, for complete booking warranty checker show model non mapped model also
+            if($type!='complete_booking'){
+                $where = array('entity_id' => $requestData['partner_id'], 'entity_type' => _247AROUND_PARTNER_STRING, 'service_id' => $requestData['service_id'], 'inventory_model_mapping.active' => 1, 'appliance_model_details.active' => 1);
+                $model_numbers = $this->inventory_model->get_inventory_mapped_model_numbers('appliance_model_details.id,appliance_model_details.model_number', $where);
+            }
+            if (empty($model_numbers) || $type=='complete_booking') {
                 $where = array('entity_id' => $requestData['partner_id'], 'entity_type' => _247AROUND_PARTNER_STRING, 'service_id' => $requestData['service_id'], 'active' => 1);
                 $model_numbers = $this->inventory_model->get_appliance_model_details('id, model_number', $where);
             }
@@ -3744,22 +3751,31 @@ class engineerApiv1 extends CI_Controller {
                     }
                 }
             }
-            
+            $serial_number_img_submit = false;
             $serial_number_array = json_decode($requestData['submitWarrantyCheckerAndEditCallType'], true);
             if (isset($serial_number_array['serial_number_pic_exist'])) {
                 if ($serial_number_array['serial_number_pic_exist']) {
                     $serial_number_pic = "serial_number_pic_" . date("YmdHis") . ".png";
                     $this->miscelleneous->generate_image($serial_number_array['serial_number_pic_exist'], $serial_number_pic, SERIAL_NUMBER_PIC_DIR);
                     $unit_detail['serial_number_pic'] = $serial_number_pic;
+                    $serial_number_img_submit = true;
                 }
             } else {
                 if (isset($serial_number_array['existing_serial_number_pic'])) {
                     if ($serial_number_array['existing_serial_number_pic']) {
                         $serial_number_pic_exp = array_reverse(explode('/', $serial_number_array['existing_serial_number_pic']));
                         $unit_detail['serial_number_pic'] = $serial_number_pic_exp[0];
+                        $serial_number_img_submit = true;
                     }
                 }
             }
+            if (empty($serial_number_img_submit)) {
+                $response['warranty_flag'] = 1;
+                $this->jsonResponseString['response'] = $response;
+                $this->sendJsonResponse(array('0055', "Serial image is required"));
+                exit;
+            }
+
             $unit_detail['serial_number'] = $requestData['serial_number'];
             $this->booking_model->update_booking_unit_details($requestData['booking_id'], $unit_detail);
 
