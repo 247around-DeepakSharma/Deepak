340a9f3efcef8fbf84fa9cafb2c2c6eb2858fe28
Ghanshyam <ghanshyamg@247around.com>
2021-02-17 12:25:46 +0530

    CRMS-2163 Do not allow to mark RTO on same awb by partner when status is Defective Ok Part Shipped By SF

--- application/controllers/employee/spare_parts.php
+++ application/controllers/employee/spare_parts.php
@@ -5256,9 +5256,26 @@ $select = 'spare_parts_details.entity_type,spare_parts_details.quantity,spare_pa
         
         /* get spare part detail of $spare_id */ 
         $spare_part_detail = $this->reusable_model->get_search_result_data('spare_parts_details', '*', ['id' => $data['spare_id']], NULL, NULL, NULL, NULL, NULL)[0];
+<<<<<<< HEAD
         if(empty($spare_part_detail['awb_by_partner'])){
             echo "<div class='alert alert-warning'>AWB Number not found for this booking, you cannot mark RTO for this booking.</div>";
             exit;
+=======
+		if(empty($spare_part_detail['awb_by_partner'])){
+			echo "<div class='alert alert-warning'>AWB Number not found for this booking, you cannot mark RTO for this booking.</div>";
+			exit;
+		}
+                
+        $spare_part_details = $this->reusable_model->get_search_result_data('spare_parts_details', 'id,defective_part_shipped_date,status,booking_id', ['awb_by_partner' => $spare_part_detail['awb_by_partner'], 'status != "'._247AROUND_CANCELLED.'"' => NULL], NULL, NULL, NULL, NULL, NULL);
+        if(!empty($spare_part_details)){
+            foreach($spare_part_details as $keysc => $valuesc){
+            if(!in_array($valuesc['status'],array(DEFECTIVE_PARTS_REJECTED_BY_WAREHOUSE,OK_PARTS_REJECTED_BY_WAREHOUSE)) && !empty($valuesc['defective_part_shipped_date'])){
+                echo "<div class='alert alert-warning'>Defective part already shipped for booking ID ".$valuesc['booking_id'].". You can not mark RTO.</div>";
+                echo $valuesc['status'];
+                exit;
+            }
+            }
+>>>>>>> c945c899a... CRMS-2163 Do not allow to mark RTO on same awb by partner when status is Defective Ok Part Shipped By SF
         }
         if (!empty($post_data['rto'])) {
             // upload rto document.
@@ -5271,7 +5288,7 @@ $select = 'spare_parts_details.entity_type,spare_parts_details.quantity,spare_pa
             }
             
             /* fetch all spare associated with this awb number */
-            $spare_part_details = $this->reusable_model->get_search_result_data('spare_parts_details', 'id', ['awb_by_partner' => $spare_part_detail['awb_by_partner'], 'status != "'._247AROUND_CANCELLED.'"' => NULL], NULL, NULL, NULL, NULL, NULL);
+            
             if(!empty($spare_part_details)) {
                 foreach($spare_part_details as $spare_part) {
                     $this->inventory_model->handle_rto_case($spare_part['id'], $post_data);
