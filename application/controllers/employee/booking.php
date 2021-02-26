f2fece6ab987c774c3c211f625efb4f9de7100b3
pritys <pritys@247around.com>
2021-02-26 16:41:35 +0530

    CRMS-1923 : Pending Bookings Excel Report : Open call flow chart sequence need to reshuffle

--- application/controllers/employee/booking.php
+++ application/controllers/employee/booking.php
@@ -5696,10 +5696,6 @@ function check_and_update_partner_extra_spare($booking_id) {
         
     }
 
-
-
-
-    
     function download_pending_bookings($status) {
         $arr_post = $this->input->post();
         $bulk_booking_id = !empty($arr_post['bookingIDString']) ? $arr_post['bookingIDString'] : "";
@@ -5741,42 +5737,43 @@ function check_and_update_partner_extra_spare($booking_id) {
             $post['where']['booking_details.internal_status NOT IN ("'.SPARE_PARTS_SHIPPED.'","'.SPARE_OOW_SHIPPED.'","'.SF_BOOKING_CANCELLED_STATUS.'","'.SF_BOOKING_COMPLETE_STATUS.'","'.SPARE_PARTS_SHIPPED_BY_WAREHOUSE.'")'] = NULL; 
             // Join with employee Table to fetch AM name
 
+             
             $post['join']['spare_parts_details'] = "booking_details.booking_id  = spare_parts_details.booking_id and spare_parts_details.status!='"._247AROUND_CANCELLED."'";
-
             $post['join']['partners'] = "booking_details.partner_id  = partners.id";
-            $post['join']['agent_filters'] =  "booking_details.partner_id=agent_filters.entity_id AND agent_filters.state = booking_details.state AND agent_filters.entity_type = '"._247AROUND_EMPLOYEE_STRING."' ";
+            $post['join']['agent_filters'] =  "partners.id=agent_filters.entity_id AND agent_filters.state = booking_details.state AND agent_filters.entity_type = '"._247AROUND_EMPLOYEE_STRING."'";
             $post['join']['employee as employee_am'] = "agent_filters.agent_id = employee_am.id";
             $post['join']['inventory_master_list as in_req'] = "spare_parts_details.requested_inventory_id = in_req.inventory_id";
             $post['join']['inventory_master_list as in_sh'] = "spare_parts_details.shipped_inventory_id = in_sh.inventory_id";
             
             // $post['join']['employee as emp_asm'] = "service_centres.asm_id = emp_asm.id";    
             $post['joinTypeArray'] = ['spare_parts_details' => "left",'partners' => "left",'agent_filters' => 'left', 'employee as employee_am' => "left",  'inventory_master_list as in_req' => 'left', 'inventory_master_list as in_sh' => 'left'];
-
-			$post['unit_not_required'] = true;
+            $post['unit_not_required'] = true;
             // Select Statement
             
-            $select = " booking_details.booking_id as 'Booking ID', booking_details.create_date as 'Create Date', partners.public_name as Partner, "
-                    . "employee_am.full_name as AM, users.name as 'Customer Name',booking_details.booking_pincode as 'Pincode',booking_details.city as 'City'"
-                    . ",booking_details.state as 'State',booking_details.booking_address as 'Booking Address', users.phone_number as 'Phone',"
-                    . "CASE WHEN booking_details.is_upcountry = 1 THEN 'YES' ELSE 'No' END AS 'Is Upcountry'"
-                    . ",booking_details.upcountry_distance as 'Upcountry Distance',service_centres.name as 'Service Center',"
-                    . "service_centres.primary_contact_name as 'SF Primary Contact Name'"
-                    . ",service_centres.primary_contact_phone_1 as 'SF Primary Contact No',engineer_details.name as 'Engineer Name',employee.full_name as 'RM'"
-                    . ",emp_asm.full_name  as  'ASM', services.services as 'Product', spare_parts_details.model_number as 'Model',"
-                    . "booking_details.request_type as 'Service Type', booking_details.initial_booking_date as 'First Booking Date',"
-                    . "booking_details.booking_date as 'Current Booking Date', booking_details.booking_remarks as 'Booking Remarks', booking_details.reschedule_reason as 'Reschedule Remarks',"
-                    . "booking_details.partner_internal_status as 'Final Status Level 2',"
-                    . "booking_details.current_status as 'Final Status Level 1', booking_details.actor as 'Dependency On',"
+            $select = " booking_details.booking_id as 'Booking ID', "
                     . "DATEDIFF(CURDATE(),STR_TO_DATE(booking_details.initial_booking_date,'%Y-%m-%d')) as Ageing,"
-                    . "CASE WHEN spare_parts_details.parts_requested != '' THEN 'YES' ELSE 'No' END AS 'Is Part Involved', REPLACE(group_concat(in_req.part_number),trim(','),' | ') as 'Requested Part Code', REPLACE(group_concat(spare_parts_details.parts_requested),trim(','),' | ') as 'Requested Part Name',"
-                    . "REPLACE(group_concat(spare_parts_details.parts_requested_type),trim(','),' | ') as 'Requested Part Type',spare_parts_details.date_of_request as 'Part Requested Date'"
-                    . ",REPLACE(group_concat(in_sh.part_number),trim(','),' | ')  as 'Shipped Part Code', REPLACE(group_concat(spare_parts_details.parts_shipped),trim(','),' | ') as 'Shipped Part Name',REPLACE(group_concat(spare_parts_details.shipped_parts_type),trim(','),' | ') as 'Shipped Part Type'"
-                    . ",spare_parts_details.shipped_date as 'Part Shipped Date',max(spare_parts_details.acknowledge_date) as 'SF Acknowledged Date'"
-                    . ",CASE WHEN (spare_parts_details.auto_acknowledeged = 1 or spare_parts_details.auto_acknowledeged = 2) THEN 'YES' ELSE 'No' END AS 'Is auto Acknowledge',penalty_on_booking.active as 'Penalty Active'";
+                    . "partners.public_name as Partner, "
+                    . "employee_am.full_name as AM,emp_asm.full_name  as  'ASM',service_centres.name as 'Service Center',"
+                    . "services.services as 'Product',booking_details.request_type as 'Service Type',booking_details.partner_internal_status as 'Final Status Level 2',"
+                    . "users.name as 'Customer Name',users.phone_number as 'Phone',booking_details.booking_pincode as 'Pincode',booking_details.city as 'City',"
+                    . "CASE WHEN booking_details.is_upcountry = 1 THEN 'YES' ELSE 'No' END AS 'Is Upcountry',booking_details.upcountry_distance as 'Upcountry Distance',"
+                    . "CASE WHEN spare_parts_details.parts_requested != '' THEN 'YES' ELSE 'No' END AS 'Is Part Involved',CASE WHEN (spare_parts_details.auto_acknowledeged = 1 or spare_parts_details.auto_acknowledeged = 2) THEN 'YES' ELSE 'No' END AS 'Is auto Acknowledge',"
+                    . "spare_parts_details.shipped_date as 'Part Shipped Date',max(spare_parts_details.acknowledge_date) as 'SF Acknowledged Date',"
+                    . "REPLACE(group_concat(spare_parts_details.shipped_parts_type),trim(','),' | ') as 'Shipped Part Type',"
+                    . "spare_parts_details.model_number as 'Model',"                                        
+                    . "booking_details.state as 'State',booking_details.booking_address as 'Booking Address',"
+                    . "service_centres.primary_contact_name as 'SF Primary Contact Name',"
+                    . "service_centres.primary_contact_phone_1 as 'SF Primary Contact No',engineer_details.name as 'Engineer Name',booking_details.create_date as 'Create Date',booking_details.initial_booking_date as 'First Booking Date',booking_details.booking_date as 'Current Booking Date',"
+                    . "booking_details.booking_remarks as 'Booking Remarks', booking_details.reschedule_reason as 'Reschedule Remarks',"
+                    . "booking_details.current_status as 'Final Status Level 1', booking_details.actor as 'Dependency On',"
+                    . "REPLACE(group_concat(in_req.part_number),trim(','),' | ') as 'Requested Part Code', REPLACE(group_concat(spare_parts_details.parts_requested),trim(','),' | ') as 'Requested Part Name',"
+                    . "REPLACE(group_concat(spare_parts_details.parts_requested_type),trim(','),' | ') as 'Requested Part Type',spare_parts_details.date_of_request as 'Part Requested Date',"
+                    . "REPLACE(group_concat(in_sh.part_number),trim(','),' | ')  as 'Shipped Part Code', REPLACE(group_concat(spare_parts_details.parts_shipped),trim(','),' | ') as 'Shipped Part Name',"                                   
+                    . "penalty_on_booking.active as 'Penalty Active',employee.full_name as 'RM'";
             // Show Distinct Bookings
-            $post['group_by'] = 'booking_details.booking_id';            
-            $list =  $this->booking_model->get_bookings_by_status($post,$select,$sfIDArray,1,'',0,array(),array());
-
+            $group_by = 'booking_details.booking_id';
+            
+            $list =  $this->booking_model->get_bookings_by_status($post,$select,$sfIDArray,1,'',0,array(),array(),$group_by);
         }
         else if($booking_status == 'Completed' || $booking_status == 'Cancelled'){
             $post['where']  = array('booking_details.current_status' => $booking_status,'type' => 'Booking'); 
