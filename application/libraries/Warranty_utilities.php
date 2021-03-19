86128b6f86b939be4e97044417719069af649851
pritys <pritys@247around.com>
2021-03-19 11:29:13 +0530

    CRMS-2363 : Configure New Service Type

--- application/libraries/Warranty_utilities.php
+++ application/libraries/Warranty_utilities.php
@@ -253,7 +253,7 @@ class Warranty_utilities {
         $booking_request_type = $this->My_CI->booking_utilities->get_booking_request_type($selected_booking_request_types); 
         $booking_id = $arrBookings[0]['booking_id'];
         $arr_warranty_status = [
-            'IW' => ['In Warranty', 'Presale Repair', 'AMC', 'Repeat', 'Installation', 'PDI', 'Demo', 'Tech Visit', 'Replacement', 'Spare Cannibalization', 'Free Remote Assistance', 'Handling Charges', 'Dead after Purchase (DaP)', 'Dead On Arrival (DOA)'],
+            'IW' => ['In Warranty', 'Presale Repair', 'AMC', 'Repeat', 'Installation', 'PDI', 'Demo', 'Tech Visit', 'Replacement', 'Spare Cannibalization', 'Free Remote Assistance', 'Handling Charges', 'Dead after Purchase (DaP)', 'Dead On Arrival (DOA)', 'Video Trouble Shooting', 'Service With 2 Free Filter Change', 'AC PowerJet Wet Service'],
             'OW' => ['Out Of Warranty', 'Out Warranty', 'AMC', 'Repeat', 'PDI', 'Tech Visit', 'Spare Cannibalization', 'Free Remote Assistance', 'Handling Charges'],
             'EW' => ['Extended', 'AMC', 'Repeat', 'PDI', 'Tech Visit', 'Spare Cannibalization', 'Free Remote Assistance', 'Handling Charges']
         ];
