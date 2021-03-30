45a3952cc76ec6be63aea99a9dc9cdb54938a83a
Ghanshyam <ghanshyamg@247around.com>
2021-03-30 11:37:49 +0530

    Revert "CRMS-1515 Add price tage in case of spare part request"
    This reverts commit c953bb983e90c01b28d2fc182d18f0cb65cdfa0d.

--- application/controllers/engineerApiv1.php
+++ application/controllers/engineerApiv1.php
@@ -3526,7 +3526,7 @@ class engineerApiv1 extends CI_Controller {
                         } else {
                             $est_approved_msg = "Spare estimate cost given. Please update Approved by customer and then order spare part";
                         }
-                    } else if (stristr($value['price_tags'], "Repair") || stristr($value['price_tags'], "Repeat") || stristr($value['price_tags'], EXTENDED_WARRANTY_TAG) || stristr($value['price_tags'], PRESALE_REPAIR_TAG) || stristr($value['price_tags'], GAS_RECHARGE_IN_WARRANTY) || stristr($value['price_tags'], AMC_PRICE_TAGS) || stristr($value['price_tags'], GAS_RECHARGE_OUT_OF_WARRANTY) || stristr($value['price_tags'], "Replacement")  || stristr($value['price_tags'], "Dead On Arrival (DOA)") || stristr($value['price_tags'], "Dead after Purchase (DaP)") || stristr($value['price_tags'], "Video Trouble Shooting") || stristr($value['price_tags'], "Service With 2 Free Filter Change")  || stristr($value['price_tags'], "AC PowerJet Wet Service")) {
+                    } else if (stristr($value['price_tags'], "Repair") || stristr($value['price_tags'], "Repeat") || stristr($value['price_tags'], EXTENDED_WARRANTY_TAG) || stristr($value['price_tags'], PRESALE_REPAIR_TAG) || stristr($value['price_tags'], GAS_RECHARGE_IN_WARRANTY) || stristr($value['price_tags'], AMC_PRICE_TAGS) || stristr($value['price_tags'], GAS_RECHARGE_OUT_OF_WARRANTY) || stristr($value['price_tags'], "Replacement")) {
 
                         $check_spare_flag = true;
                     }
