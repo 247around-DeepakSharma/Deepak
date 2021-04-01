ffc6c63fa200c4e3ed23b7a5cca92f15648cb307
247around-DeepakSharma <deepaks@247around.com>
2021-04-01 10:18:00 +0530

    CRMS-907 add date validation and file hint

--- application/views/employee/addpartner.php
+++ application/views/employee/addpartner.php
@@ -1288,7 +1288,7 @@ if(!empty($this->session->userdata('user_group')) && $this->session->userdata('u
         </div>
         <div class="clear"></div>
         <div id="container_4" style="<?php if($current_tab!=4){ ?>display:none;<?php } else { ?>display:block<?php } ?>" class="form_container">
-            <form name="document_form" class="form-horizontal" id ="document_form" action="<?php echo base_url() ?>employee/partner/process_partner_contracts" method="POST" enctype="multipart/form-data">
+            <form   onsubmit="hide_submit_button()" name="document_form" class="form-horizontal" id ="document_form" action="<?php echo base_url() ?>employee/partner/process_partner_contracts" method="POST" enctype="multipart/form-data">
                 <?php
                     if(isset($query[0]['id'])){
                         if($query[0]['id']){
@@ -1334,11 +1334,12 @@ if(!empty($this->session->userdata('user_group')) && $this->session->userdata('u
                                 <label for="agreement_end_date" class="col-md-4">Partnership End Date<span class="text-danger">*</span></label>
                                 <div class="col-md-6">
                                     <div class="input-group input-append date" >
-                                        <input type="date" class="form-control agreement_end_date" required name="agreement_end_date[]" id="agreement_end_date">
+                                        <input type="date" class="form-control agreement_end_date" required name="agreement_end_date[]" id="agreement_end_date" required ="true">
                                         <span class="input-group-addon add-on"><span class="glyphicon glyphicon-calendar"></span></span>
                                     </div>
                                     <?php echo form_error('agreement_end_date'); ?>
                                 </div>
+                             <a class="hide-section form-control" title="Remove Contract" style="float:right;width:7%;display:none;"><span class="glyphicon glyphicon-minus"></span></a>
                             </div>
                             <div class="col-md-6 form-group <?php if (form_error('contract_type')) {
                                 echo 'has-error';
@@ -1363,7 +1364,8 @@ if(!empty($this->session->userdata('user_group')) && $this->session->userdata('u
                                 } ?>">
                                 <label for="contract_file" class="col-md-4">Contract File<span class="text-danger">*</span></label>
                                 <div class="col-md-5">
-                                    <input type="file" class="form-control"  name="contract_file[]" required="" accept="application/pdf">
+                                    <input type="file" class="form-control" name="contract_file[]" id="file" required="true" accept="application/pdf">
+                                    <span id="size" class="text-primary" style="color:blue;">Only pdf file allow</span>
                                     <?php echo form_error('contract_file'); ?>
                                 </div>
                                 <div class="col-md-1">
@@ -1388,9 +1390,7 @@ if(!empty($this->session->userdata('user_group')) && $this->session->userdata('u
                                 </div>
 <!--                                <hr style="border: 1px solid #a1e8a1;">-->
                             </div>
-                            <div class="col-md-6">
-                               <a class="hide-section form-control" title="Remove Contract" style="float:right;width:7%;display:none;"><span class="glyphicon glyphicon-minus"></span></a>
-                            </div>
+                            
                         </div>
                         <div id="cloned"></div>
                     </div>
@@ -1401,7 +1401,7 @@ if(!empty($this->session->userdata('user_group')) && $this->session->userdata('u
                         echo "Update Contracts";
                         } else {
                         echo "Save Contracts";
-                        } ?>" class="btn btn-primary" id="submit_contract_btn" onclick = " return date_validatetion();">
+                        } ?>" class="btn btn-primary" onclick = " return date_validatetion();" id="submit_contract_btn">
                         <?php echo "<a class='btn btn-small btn-primary' href=" . base_url() . "employee/partner/viewpartner>Cancel</a>"; ?>
                     </center>
                 </div>
@@ -6215,7 +6215,7 @@ if(!empty($this->session->userdata('user_group')) && $this->session->userdata('u
            // $("#"+lastUrl).trigger('click');
         }
     })
-   function date_validatetion(){
+    function date_validatetion(){
     var flag = 0;
     $('.contract_holder').each(function(){  
       var start_date = $(this).find('.agreement_start_date').val();
@@ -6226,13 +6226,16 @@ if(!empty($this->session->userdata('user_group')) && $this->session->userdata('u
       }
     });
     if( flag == 1){
+        alert("Start Date Can't Be Greater Than End Date");
         return false;
     }
     else{
         return true;
     }
    }
- function hide_submit_button(){
+   //Auther: Deepak Sharma
+   // this fnction use to hide submit button when you click at a time 
+   function hide_submit_button(){
     $("#submit_contract_btn").css("pointer-events",'none');
     $("#submit_contract_btn").css("opacity",'.5');
    }
