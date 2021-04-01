b075c5a9c86ce397ff919aa160eade6e6dc73703
pritys <pritys@247around.com>
2021-04-01 14:51:13 +0530

    CRMS-2409 : Set Part Warranty Status internally on Spare Request Page

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -2658,8 +2658,14 @@ class Service_centers extends CI_Controller {
                         $data['spare_request_symptom'] = null;
                     }
 
-                    $data['part_warranty_status'] = $value['part_warranty_status'];
-
+//                    $data['part_warranty_status'] = $value['part_warranty_status'];
+                    // ----------------- Set Part Warranty -------------------------------
+                    $arrPartWarrantyStatus = $this->get_part_warranty_data($this->input->post());
+                    $data['part_warranty_status'] = SPARE_PART_IN_OUT_OF_WARRANTY_STATUS;
+                    if(!empty($arrPartWarrantyStatus[$data['parts_requested_type']]) && ((strtoupper(trim($arrPartWarrantyStatus[$data['parts_requested_type']])) == 'IW') || (strtoupper(trim($arrPartWarrantyStatus[$data['parts_requested_type']])) == 'EW'))){
+                        $data['part_warranty_status'] = SPARE_PART_IN_WARRANTY_STATUS;
+                    }                    
+                    // -------------------------------------------------------------------
                     $data['part_requested_on_approval'] = 0;
 
                     if (isset($value['requested_inventory_id'])) {
@@ -10760,8 +10766,7 @@ class Service_centers extends CI_Controller {
         $part_types = array_column($data['part'], 'parts_type');
         if(empty($part_types)){
             return;
-        }
-        
+        }        
         // create booking wise Array
         $booking_id = $data['booking_id'];
         $booking_data[$booking_id] = [
@@ -10775,7 +10780,8 @@ class Service_centers extends CI_Controller {
         ];        
         $arrBookingsWarrantyStatus = $this->warranty_utilities->get_warranty_status_of_parts($booking_data, $booking_id);
         return $arrBookingsWarrantyStatus;
-    }    
+    }
+
     /**
      * This function is used to send mail to RM/ASM/Talevar when service center request T-shirt
      * @param : Service_center_ID
