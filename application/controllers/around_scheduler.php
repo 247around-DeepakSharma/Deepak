cc5c08955e7d73ac8b236ae6b0bebd2c4a9d621b
Ghanshyam <ghanshyamg@247around.com>
2021-04-09 10:49:15 +0530

    CRMS-2351 initialize variable

--- application/controllers/around_scheduler.php
+++ application/controllers/around_scheduler.php
@@ -3431,6 +3431,7 @@ class Around_scheduler extends CI_Controller {
 				$body = $template[0];
 				$from = $template[2];
 				$cc = '';
+				$to = '';
 				if (!empty($template[1])) {
 					$to = $template[1] . ",";
 				}
