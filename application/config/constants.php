d716d7106997875989914ec27ca386947191bf0c
pritys <pritys@247around.com>
2021-04-16 17:47:49 +0530

    CRMS-2143 : IF booking is cancelled by admin then actor / action is showing wrong

--- application/config/constants.php
+++ application/config/constants.php
@@ -798,6 +798,7 @@ define('CANCELLATION_THRESHOLD', 20);
 define('VENDOR_RESCHEDULED', "Reschedule");
 define('REQUEST_TYPE_ELS', "LO-ELS");
 
+define('NEXT_ACTION_CLOSURE_REVIEW', 'Closure Review');
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
 
