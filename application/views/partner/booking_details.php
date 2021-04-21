a36a170f9ecd0739a04fcfd89b94b5320f881afa
247around-DeepakSharma <deepaks@247around.com>
2021-04-21 14:17:52 +0530

    CRMS-2539 add column

--- application/views/partner/booking_details.php
+++ application/views/partner/booking_details.php
@@ -384,6 +384,8 @@
                                                             <th >Current Status</th>
                                                             <th >Spare Cancellation Reason</th>
                                                             <th>Consumption</th>
+                                                            <th>Consumption Reason</th>
+                                                            <th>Consumption Remarks</th>
                                                         </tr>
                                                     </thead>
                                                     <tbody> 
@@ -485,8 +487,10 @@
                                                                     ?>
                                                                     <?php echo $sp['part_cancel_reason']; ?>
                                                                 <?php } ?>
-                                                                </td>
-                                                                <td><?php if($sp['is_consumed'] == 1) { echo 'Yes';} else { echo 'No';} ?></td>
+                                                                </td>     
+                                                                <td><?php if(!empty($sp['is_consumed']) && $sp['is_consumed'] == 1) { echo 'Yes';} else if(!empty($sp['consumed_part_status_id'])){ echo 'No';} ?></td>
+                                                                  <td><?php if(!empty($sp['consumed_status'])) { echo $sp['consumed_status']; } ?></td>
+                                                                 <td><?php if(!empty($sp['consumption_remarks'])) { echo $sp['consumption_remarks']; } ?></td>
                                                             </tr>
                                                             <?php
                                                             if (!is_null($sp['parts_shipped'])) {
