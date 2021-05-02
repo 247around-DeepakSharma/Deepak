e2b27827f0711b116914ca095e513f2c78924639
Ghanshyam <ghanshyamg@247around.com>
2021-05-02 18:50:13 +0530

    CRMS-2565 correct add remark in auto approve booking

--- application/controllers/employee/service_centers.php
+++ application/controllers/employee/service_centers.php
@@ -10648,8 +10648,14 @@ class Service_centers extends CI_Controller {
             // Update SF Closed date
             $this->booking_model->update_booking($booking_id, ['service_center_closed_date' => $closed_date]);
 
+
             // Insert data into booking state change
-			$remarks_auto_close = "Booking Auto Approved - ".$engg_completed_booking->remarks;
+			if ($engg_completed_booking->internal_status == _247AROUND_COMPLETED) {
+				$remarks_auto_close = "Booking Auto Approved - ".$engg_completed_booking->closing_remark;
+			 }else{
+				 $remarks_auto_close = "Booking Auto Approved - ".$engg_completed_booking->cancellation_remark;
+			 }
+
             $this->insert_details_in_state_change($booking_id, $sf_booking_status, $remarks_auto_close, "247Around", "Review the Booking", NULL, true);
 
             //Update spare consumption as entered by engineer Booking Completed
