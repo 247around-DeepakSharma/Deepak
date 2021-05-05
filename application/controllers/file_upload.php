248ce90b81c6402211c2d028e8d026f80c32d5a3
pritys <pritys@247around.com>
2021-05-05 17:53:07 +0530

    CRMS-2353 : Added non-empty check for file upload

--- application/controllers/file_upload.php
+++ application/controllers/file_upload.php
@@ -122,7 +122,6 @@ class File_upload extends CI_Controller {
                         
                         //save file and upload on s3
                         $this->miscelleneous->update_file_uploads($data['file_name'], TMP_FOLDER . $data['file_name'], $file_type, FILE_UPLOAD_SUCCESS_STATUS, "default", $data['post_data']['entity_type'], $data['post_data']['entity_id']);
-                        
                         $this->session->set_userdata('file_success', $response['message']);
                     } else {
                         //save file and upload on s3
