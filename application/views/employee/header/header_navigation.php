457fb892a8511bad95ab98b0b29da1fbc75e4758
247around-nadeem <nadeemriyasat2@gmail.com>
2021-05-05 15:01:39 +0530

    CRMS-2551 Mobile Entry Form Commit UI

--- application/views/employee/header/header_navigation.php
+++ application/views/employee/header/header_navigation.php
@@ -194,12 +194,10 @@
             <?php 
             if($this->session->userdata('user_group') == _247AROUND_RM || $this->session->userdata('user_group') == 'admin'){  ?>
             <!-- <li><a target="_blank" href="https://forms.gle/Bbvh95cKXDHQcWB8A" >Feature Requirement</a></li> -->
-            <li><a target="_blank" href="<?php echo base_url();?>employee/user/mobile_entry_form" >Mobile Entry</a></li>
             <?php } ?>
+            <li><a target="_blank" href="<?php echo base_url();?>mobile_entry" >Mobile Entry</a></li>
             <li><a style="color:#00ff7e;font-weight:900;cursor:pointer;" id="myBtn" data-toggle="modal" data-target="#myModalCovid" >COVID-19</a></li>
             </ul>
-           
-                
         
             <ul class="nav navbar-top-links navbar-right">
                 <li>
