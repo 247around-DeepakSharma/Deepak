ead6b5c912d4d282afaa0c29358fa0d0e69610f3
aditya <adityat@247around.com>
2021-05-26 09:34:00 +0530

    When Sending MSL to CWH or MWH only Single QTY is Updating at Receiver end #CRMS-2631

--- application/controllers/employee/inventory.php
+++ application/controllers/employee/inventory.php
@@ -4349,7 +4349,7 @@ class Inventory extends CI_Controller {
      *  @return : $res JSON // consist response message and response status
      */
 
-   function process_spare_invoice_tagging() {
+function process_spare_invoice_tagging() {
         log_message("info", __METHOD__ . json_encode($this->input->post(), true));
 //        $str = '{"is_wh_micro":"2","247around_gst_number":"09AAFCB1281J1ZM","partner_id":"247130","wh_id":"870","awb_number":"12587455","courier_name":"gati-kwe","courier_shipment_date":"04/07/2019","from_gst_number":"7","part":[{"shippingStatus":"1","service_id":"37","part_name":"TRAY,BOTTOM,ER180I,INSTA","part_number":"1100023151","booking_id":"","quantity":"1","part_total_price":"158.25","hsn_code":"39239090","gst_rate":"18","inventory_id":"6011"},{"shippingStatus":"1","service_id":"37","part_name":"LEG,ADJUSTABLE,27MM L,ER180I,INSTA","part_number":"1100028374","booking_id":"","quantity":"2","part_total_price":"15","hsn_code":"84189900","gst_rate":"18","inventory_id":"7463"}],"partner_name":" Videocon","wh_name":" Amritsar Baldev Electronics - (Micro Warehouse) ","dated":"04/07/2019","sender_entity_type":"vendor","sender_entity_id":"15","invoice_tag":"MSL","transfered_by":"2"}';
         //        $_POST = json_decode($str, true);  
@@ -4556,7 +4556,7 @@ class Inventory extends CI_Controller {
                                 
                                 foreach ($parts_details as $key => $value) {
                                     $request_type = '';
-                                    if(!empty($value['quantity'])){
+                                    if(empty($value['quantity'])){
                                         $value['quantity'] = 1;
                                     }
                                     if (isset($value['request_type']) && !empty($value['request_type'])) {
@@ -4750,8 +4750,7 @@ class Inventory extends CI_Controller {
         }
 
         echo json_encode($res);
-    }   
-    
+    }    
     function check_msl_invoice_id($transfered_by, $invoice_id) {
         if ($transfered_by == MSL_TRANSFERED_BY_PARTNER){
             if (strpos($invoice_id, '/') === false) {
